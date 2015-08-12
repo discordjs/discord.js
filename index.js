dda5d4272ec4a7d51cfff4ea0cfda7e7105f4ff8
hydrabolt <amishshah.2k@gmail.com>
2015-08-12 16:05:21 +0100

    Most arrays are now using lists.

--- index.js
+++ index.js
@@ -1,300 +1,320 @@
-var request = require("superagent");
-var Endpoints = require("./lib/endpoints.js");
-var Server = require("./lib/server.js").Server;
-var Message = require("./lib/message.js").Message;
-var User = require("./lib/user.js").User;
-var Channel = require("./lib/channel.js").Channel;
-var List = require("./lib/list.js").List;
-var WebSocket = require('ws');
+var request = require( "superagent" );
+var Endpoints = require( "./lib/endpoints.js" );
+var Server = require( "./lib/server.js" ).Server;
+var Message = require( "./lib/message.js" ).Message;
+var User = require( "./lib/user.js" ).User;
+var Channel = require( "./lib/channel.js" ).Channel;
+var List = require( "./lib/list.js" ).List;
+var WebSocket = require( 'ws' );
 
-exports.Client = function(options) {
+exports.Client = function( options ) {
 
-    this.options = options || {};
-    this.token = "";
-    this.loggedIn = false;
-    this.websocket = null;
-    this.events = {};
-    this.user = null;
+	this.options = options || {};
+	this.token = "";
+	this.loggedIn = false;
+	this.websocket = null;
+	this.events = {};
+	this.user = null;
 
-    this.serverList = new List("id");
-
-}
-
-exports.Client.prototype.triggerEvent = function(event, args) {
-
-    if (this.events[event]) {
-        this.events[event].apply(this, args);
-    } else {
-        return false;
-    }
+	this.serverList = new List( "id" );
 
 }
 
-exports.Client.prototype.on = function(name, fn) {
-    this.events[name] = fn;
-}
+exports.Client.prototype.triggerEvent = function( event, args ) {
 
-exports.Client.prototype.off = function(name) {
-    this.events[name] = function() {};
-}
-
-exports.Client.prototype.cacheServer = function(id, cb, members) {
-
-    if ( this.serverList.filter("id", id).length > 0 ) {
-        return;
-    }
-
-    var self = this;
-
-    request
-        .get(Endpoints.SERVERS + "/" + id)
-        .set("authorization", this.token)
-        .end(function(err, res) {
-            var dat = res.body;
-            var server = new Server(dat.region, dat.owner_id, dat.name, dat.roles[0].id, members || dat.members);
-
-            request
-            .get(Endpoints.SERVERS + "/" + id + "/channels")
-            .set("authorization", self.token)
-            .end(function(err, res){
-
-                var channelList = res.body;
-                for(channel of channelList){
-                    server.channels.add( new Channel(channel, server) );
-                }
-
-                self.serverList.add(server);
-
-                cb(server);
-            });
-        });
+	if ( this.events[ event ] ) {
+		this.events[ event ].apply( this, args );
+	} else {
+		return false;
+	}
 
 }
 
-exports.Client.prototype.login = function(email, password) {
+exports.Client.prototype.on = function( name, fn ) {
+	this.events[ name ] = fn;
+}
 
-    var client = this;
+exports.Client.prototype.off = function( name ) {
+	this.events[ name ] = function() {};
+}
 
-    var details = {
-        email: email,
-        password: password
-    };
+exports.Client.prototype.cacheServer = function( id, cb, members ) {
 
-    request
-        .post(Endpoints.LOGIN)
-        .send(details)
-        .end(function(err, res) {
-            if (!res.ok) {
-                client.triggerEvent("disconnected", {
-                    reason : "failed to log in",
-                    error : err
-                });
-            } else {
-                client.token = res.body.token;
-                client.loggedIn = true;
-                client.connectWebsocket();
-            }
-        });
+	if ( this.serverList.filter( "id", id ).length > 0 ) {
+		return;
+	}
+
+	var self = this;
+
+	request
+		.get( Endpoints.SERVERS + "/" + id )
+		.set( "authorization", this.token )
+		.end( function( err, res ) {
+			var dat = res.body;
+			var server = new Server( dat.region, dat.owner_id, dat.name, dat.roles[ 0 ].id, members || dat.members );
+
+			request
+				.get( Endpoints.SERVERS + "/" + id + "/channels" )
+				.set( "authorization", self.token )
+				.end( function( err, res ) {
+
+					var channelList = res.body;
+					for ( channel of channelList ) {
+						server.channels.add( new Channel( channel, server ) );
+					}
+
+					self.serverList.add( server );
+
+					cb( server );
+				} );
+		} );
 
 }
 
-exports.Client.prototype.connectWebsocket = function(cb) {
+exports.Client.prototype.login = function( email, password ) {
 
-    var client = this;
+	var client = this;
 
-    this.websocket = new WebSocket(Endpoints.WEBSOCKET_HUB);
-    this.websocket.onclose = function(e) {
-        client.triggerEvent("disconnected", {
-            reason : "websocket disconnected",
-            error : e
-        });
-    };
-    this.websocket.onmessage = function(e) {
+	var details = {
+		email: email,
+		password: password
+	};
 
-        var dat = JSON.parse(e.data);
-        switch (dat.op) {
+	request
+		.post( Endpoints.LOGIN )
+		.send( details )
+		.end( function( err, res ) {
+			if ( !res.ok ) {
+				client.triggerEvent( "disconnected", {
+					reason: "failed to log in",
+					error: err
+				} );
+			} else {
+				client.token = res.body.token;
+				client.loggedIn = true;
+				client.connectWebsocket();
+			}
+		} );
 
-            case 0:
-                if (dat.t === "READY") {
+}
 
-                    var data = dat.d;
+exports.Client.prototype.connectWebsocket = function( cb ) {
 
-                    self = this;
-                    setInterval(function() {
-                        self.keepAlive.apply(self);
-                    }, data.heartbeat_interval);
+	var client = this;
 
-                    var _servers = data.guilds,
-                        servers = [];
+	this.websocket = new WebSocket( Endpoints.WEBSOCKET_HUB );
+	this.websocket.onclose = function( e ) {
+		client.triggerEvent( "disconnected", {
+			reason: "websocket disconnected",
+			error: e
+		} );
+	};
+	this.websocket.onmessage = function( e ) {
 
-                    var cached = 0, toCache = _servers.length;
+		var dat = JSON.parse( e.data );
+		switch ( dat.op ) {
 
-                    for (x in _servers) {
-                        _server = _servers[x];
-                        client.cacheServer(_server.roles[0].id, function(server) {
-                            cached++;
-                            if(cached >= toCache){
-                                client.triggerEvent("ready");
-                            }
-                        }, _server.members);
-                    }
+			case 0:
+				if ( dat.t === "READY" ) {
 
-                    client.user = new User(data.user.username, data.user.id, data.user.discriminator, data.user.avatar);
-                } else if (dat.t === "MESSAGE_CREATE") {
-                    var data = dat.d;
+					var data = dat.d;
 
-                    var channel = client.channelFromId(data.channel_id);
+					self = this;
+					setInterval( function() {
+						self.keepAlive.apply( self );
+					}, data.heartbeat_interval );
 
-                    var message = new Message(data, channel);
+					var _servers = data.guilds,
+						servers = [];
 
-                    client.triggerEvent("message", [message]);
+					var cached = 0,
+						toCache = _servers.length;
 
-                } else if (dat.t === "PRESENCE_UPDATE"){
+					for ( x in _servers ) {
+						_server = _servers[ x ];
+						client.cacheServer( _server.roles[ 0 ].id, function( server ) {
+							cached++;
+							if ( cached >= toCache ) {
+								client.triggerEvent( "ready" );
+							}
+						}, _server.members );
+					}
 
-                    var data = dat.d;
+					client.user = new User( data.user.username, data.user.id, data.user.discriminator, data.user.avatar );
+				} else if ( dat.t === "MESSAGE_CREATE" ) {
+					var data = dat.d;
 
-                    client.triggerEvent("presence", [new User(data.user), data.status, client.serverList.filter("id", data.guild_id, true)]);
+					var channel = client.channelFromId( data.channel_id );
 
-                }
-                break;
+					var message = new Message( data, channel );
 
-        }
+					client.triggerEvent( "message", [ message ] );
 
-    };
-    this.websocket.sendPacket = function(p) {
-        this.send(JSON.stringify(p));
-    }
-    this.websocket.keepAlive = function() {
+				} else if ( dat.t === "PRESENCE_UPDATE" ) {
 
-        if(this.readyState !== 1)
-            return false;
+					var data = dat.d;
 
-        this.sendPacket({
-            op: 1,
-            d: Date.now()
-        });
+					client.triggerEvent( "presence", [ new User( data.user ), data.status, client.serverList.filter( "id", data.guild_id, true ) ] );
 
-    }
-    this.websocket.onopen = function() {
+				}
+				break;
 
-        var connDat = {
-            op: 2,
-            d: {
-                token: client.token,
-                v: 2
-            }
-        };
+		}
 
-        connDat.d.properties = {
-            "$os": "DiscordJS",
-            "$browser": "Dischromecord", // ;)
-            "$device": "discord.js",
-            "$referrer": "",
-            "$referring_domain": ""
-        };
+	};
+	this.websocket.sendPacket = function( p ) {
+		this.send( JSON.stringify( p ) );
+	}
+	this.websocket.keepAlive = function() {
 
-        this.sendPacket(connDat);
-    }
+		if ( this.readyState !== 1 )
+			return false;
+
+		this.sendPacket( {
+			op: 1,
+			d: Date.now()
+		} );
+
+	}
+	this.websocket.onopen = function() {
+
+		var connDat = {
+			op: 2,
+			d: {
+				token: client.token,
+				v: 2
+			}
+		};
+
+		connDat.d.properties = {
+			"$os": "discord.js",
+			"$browser": "discord.js",
+			"$device": "discord.js",
+			"$referrer": "",
+			"$referring_domain": ""
+		};
+
+		this.sendPacket( connDat );
+	}
 }
 
 exports.Client.prototype.logout = function() {
 
-    var client = this;
+	var client = this;
 
-    request
-        .post(Endpoints.LOGOUT)
-        .end(function() {
-            client.loggedIn = false;
-        });
-
-}
-
-exports.Client.prototype.createServer = function(details, cb) {
-
-    var client = this;
-
-    request
-        .post(Endpoints.SERVERS)
-        .set("authorization", client.token)
-        .send(details)
-        .end(function(err, res) {
-            if (!res.ok) {
-                cb(err);
-            } else {
-                cb(new Server(res.body));
-            }
-        });
+	request
+		.post( Endpoints.LOGOUT )
+		.end( function() {
+			client.loggedIn = false;
+		} );
 
 }
 
-exports.Client.prototype.sendMessage = function(channel, message, cb, _mentions) {
+exports.Client.prototype.createServer = function( details, cb ) {
 
-    var cb = cb || function(){};
+	var client = this;
 
-    for (mention in _mentions) {
-        _mentions[mention] = _mentions[mention].id;
-    }
+	request
+		.post( Endpoints.SERVERS )
+		.set( "authorization", client.token )
+		.send( details )
+		.end( function( err, res ) {
+			if ( !res.ok ) {
+				cb( err );
+			} else {
+				cb( new Server( res.body ) );
+			}
+		} );
 
-    var client = this;
-    var details = {
-        content: message.substring(0,2000),
-        mentions: _mentions || []
-    };
-
-    request
-        .post(Endpoints.CHANNELS + "/" + channel.id + "/messages")
-        .set("authorization", client.token)
-        .send(details)
-        .end(function(err, res) {
-            cb(new Message(res.body, client.channelFromId(res.body.channel_id)));
-        });
 }
 
-exports.Client.prototype.deleteMessage = function(message) {
+exports.Client.prototype.sendMessage = function( channel, message, cb, _mentions, options ) {
 
-    if(!message)
-        return false;
+	options = options || {};
 
-    var client = this;
+	var cb = cb || function() {};
 
-    request
-        .del(Endpoints.CHANNELS + "/" + message.channel.id + "/messages/" + message.id)
-        .set("authorization", client.token)
-        .end(function(err, res) {
+	if ( _mentions === false ) {
+		//mentions is false, explicitly don't want to mention someone
+		_mentions = [];
+	} else if ( _mentions === true || _mentions === "auto" || _mentions == null || _mentions == undefined ) {
+		//want to auto sort mentions
+		_mentions = [];
+		var mentionsArray = message.match( /<[^>]*>/g ) || [];
+		for ( mention of mentionsArray ) {
+			_mentions.push( mention.substring( 2, mention.length - 1 ) );
+		}
 
-        });
+	} else if ( _mentions instanceof Array ) {
+		//specific mentions
+		for ( mention in _mentions ) {
+			_mentions[ mention ] = _mentions[ mention ].id;
+		}
+	} else {
+
+	}
+
+	var client = this;
+	var details = {
+		content: message.substring( 0, 2000 ),
+		mentions: _mentions || []
+	};
+
+	request
+		.post( Endpoints.CHANNELS + "/" + channel.id + "/messages" )
+		.set( "authorization", client.token )
+		.send( details )
+		.end( function( err, res ) {
+			var msg = new Message( res.body, client.channelFromId( res.body.channel_id ) );
+			if ( options.selfDestruct ) {
+				setTimeout( function() {
+					client.deleteMessage( msg );
+				}, options.selfDestruct );
+			}
+			cb( msg );
+		} );
 }
 
-exports.Client.prototype.channelFromId = function(id){
-    var channelList = this.serverList.concatSublists("channels", "id");
-    var channel = channelList.filter("id", id, true);
+exports.Client.prototype.deleteMessage = function( message ) {
 
-    return channel;
+	if ( !message )
+		return false;
+
+	var client = this;
+
+	request
+		.del( Endpoints.CHANNELS + "/" + message.channel.id + "/messages/" + message.id )
+		.set( "authorization", client.token )
+		.end();
 }
 
-exports.Client.prototype.getChannelLogs = function(channel, amount, cb){
+exports.Client.prototype.channelFromId = function( id ) {
+	var channelList = this.serverList.concatSublists( "channels", "id" );
+	var channel = channelList.filter( "id", id, true );
 
-    amount = amount || 0;
-    var client = this;
+	return channel;
+}
 
-    request
-    .get(Endpoints.CHANNELS + "/" + channel.id + "/messages?limit="+amount)
-    .set("authorization", client.token)
-    .end(function(err, res){
+exports.Client.prototype.getChannelLogs = function( channel, amount, cb ) {
+	amount = amount || 0;
+	var client = this;
 
-        if(err){
-            cb(new List("id"));
-            return;
-        }
+	request
+		.get( Endpoints.CHANNELS + "/" + channel.id + "/messages?limit=" + amount )
+		.set( "authorization", client.token )
+		.end( function( err, res ) {
 
-        var datList = new List("id");
+			if ( err ) {
+				cb( new List( "id" ) );
+				return;
+			}
 
-        for(item of res.body){
-            datList.add( new Message(item, channel) );
-        }
-        
-        cb(datList);
+			var datList = new List( "id" );
 
-    });
+			for ( item of res.body ) {
+				datList.add( new Message( item, channel ) );
+			}
 
+			cb( datList );
+		} );
 }
