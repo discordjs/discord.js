cd2b79393ad4486297d1dc1cdebcbf1ff61554bb
meew0 <blactbt@live.de>
2015-08-12 13:54:39 +0200

    Add an example to show message formatting

---
+++ examples/formatting.js
@@ -0,0 +1,17 @@
+/*
+ * Discord uses a subset of Markdown for formatting, so adding formatting to
+ * messages is as simple as inserting the formatting codes into the message.
+ */
+
+var Discord = require( "discord.js" );
+var myBot = new Discord.Client();
+
+myBot.login( "hello@example.com", "password1" );
+
+myBot.on( "message", function( message ) {
+	// React to all messages with the content "$formatting".
+	if ( message.content === "$formatting" ) {
+    myBot.sendMessage( message.channel, "**bold** ****semibold**** *italic* " +
+        "_**bold and italic**_ __underline__ ~~strikethrough~~")
+	}
+} );
