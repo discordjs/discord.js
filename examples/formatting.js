365746f2888752a47eb3ebaba47ca291ff3437d9
meew0 <blactbt@live.de>
2015-08-12 13:56:25 +0200

    Fix formatting.js code formatting and add a comment

--- examples/formatting.js
+++ examples/formatting.js
@@ -11,7 +11,8 @@ myBot.login( "hello@example.com", "password1" );
 myBot.on( "message", function( message ) {
 	// React to all messages with the content "$formatting".
 	if ( message.content === "$formatting" ) {
+    // Show off formatting by sending a simple message with formatting codes.
     myBot.sendMessage( message.channel, "**bold** ****semibold**** *italic* " +
-        "_**bold and italic**_ __underline__ ~~strikethrough~~")
+        "_**bold and italic**_ __underline__ ~~strikethrough~~" );
 	}
 } );
