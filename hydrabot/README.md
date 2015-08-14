9549fea940595e680e69c35cf00a1bd6c8c90078
hydrabolt <amishshah.2k@gmail.com>
2015-08-14 13:24:42 +0100

    basic setup

--- hydrabot/README.md
+++ hydrabot/README.md
@@ -1,2 +1,11 @@
 # hydrabot
 Hydrabot is an open-source bot made with the intents of demonstrating the capabilities of [discord.js](https://github.com/hydrabolt/discord.js/).
+
+### Set up
+The easiest setup would be to clone the discord.js repo, and then open a terminal/cmd in this directory and run `node hydrabot.js`.
+
+If you don't want to clone the repo but instead just use this folder, you need to edit `hydrabot.js` to use `require("discord.js")` as opposed to `require("../")`. Cloned directories will always be using the latest **discord.js**.
+
+### Setting up credentials
+
+Edit `config.json` to use your Discord email and password, and then run `node hydrabot.js`.
---
+++ hydrabot/hydrabot.js
@@ -0,0 +1,26 @@
+// If you did not clone discord.js, change the require parameter to `discord.js`
+// and then run `npm install --save discord.js` in the same directory as this
+// file. The bot should then run.
+var Discord = require( "../" );
+
+// Load the config file. If you have not already, make one that follows the
+// structure : { "email" : "discordEmail", "password" : "discordPassword" }
+var BotConfig = require( "./config.json" );
+
+// Create a new Discord Client
+var hydrabot = new Discord.Client();
+
+// Log the client in using the auth details in config.json
+hydrabot.login( BotConfig.email, BotConfig.password+"a" );
+
+console.log("Starting up...");
+
+// When the bot is ready to go, output to the console
+hydrabot.on( "ready", function() {
+	console.log( "Ready!" );
+} );
+
+// When the bot gets disconnected, exit.
+hydrabot.on( "disconnected", function( obj ) {
+	console.log( "Disconnected", obj.reason );
+} );
