88b4332583cc1ddcd6784167184e629754bca58d
hydrabolt <amishshah.2k@gmail.com>
2015-09-25 17:29:32 +0100

    updated theme on docs

--- docs/conf.py
+++ docs/conf.py
@@ -108,7 +108,7 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
