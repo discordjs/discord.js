3a04a1506692e9a985402e89fd1af921b00b2471
hydrabolt <amishshah.2k@gmail.com>
2015-10-31 13:56:08 +0000

    Started again

--- src/Embeds/ImageEmbed.js
+++
@@ -1,9 +0,0 @@
-var Embed = require("./Embed.js");
-
-class ImageEmbed extends Embed{
-	constructor(data){
-		super(data);
-	}
-}
-
-module.exports = ImageEmbed;
--- src/Embeds/LinkEmbed.js
+++
@@ -1,9 +0,0 @@
-var Embed = require("./Embed.js");
-
-class LinkEmbed extends Embed{
-	constructor(data){
-		super(data);
-	}
-}
-
-module.exports = LinkEmbed;
