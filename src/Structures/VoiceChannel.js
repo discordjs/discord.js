7b2f4e3357255d36696960b8b1faecc148509956
Amish Shah <amishshah.2k@gmail.com>
2015-12-26 15:12:58 +0000

    Added  Cache

--- src/Structures/VoiceChannel.js
+++ src/Structures/VoiceChannel.js
@@ -1,11 +1,13 @@
 "use strict";
 
 import ServerChannel from "./ServerChannel";
+import Cache from "../Util/Cache";
 import {reg} from "../Util/ArgumentRegulariser";
 
 export default class VoiceChannel extends ServerChannel{
 	constructor(data, client, server){
 		super(data, client, server);
+		this.speaking = new Cache();
 	}
 
 	join(callback = function () { }) {
