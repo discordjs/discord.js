ccffe09bca04ff88a1d7ff07d88d64b87d8aa0a6
hydrabolt <amishshah.2k@gmail.com>
2016-04-16 23:01:58 +0100

    remove unused shim object.entries

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 'use strict';
 
-const entries = require('object.entries');
 const values = require('object.values');
 const Client = require('./client/Client');
-
-if (!Object.entries) {
-	entries.shim();
-}
 
 if (!Object.values) {
 	values.shim();
