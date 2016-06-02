f3eeef318c514ce90601b16040e2af0ac86e7250
meishu <theme97@gmail.com>
2016-06-02 10:59:50 -0700

    Fix misnamed variable in setChannelTopic

--- src/Client/InternalClient.js
+++ src/Client/InternalClient.js
@@ -1299,7 +1299,7 @@ export default class InternalClient {
 	}
 
 	//def setChannelTopic
-	setChannelTopic(chann, topic = "") {
+	setChannelTopic(channel, topic = "") {
 		topic = topic || "";
 
 		return this.updateChannel(channel, {topic: topic});
