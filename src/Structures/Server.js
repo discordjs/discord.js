c55fbe43f088ca5f79d51602f064e1b4ccc6be7d
Hugo Holmqvist <hugo.holm@live.com>
2016-07-27 22:02:16 +0300

    Check channelIDs instead of names

--- src/Structures/Server.js
+++ src/Structures/Server.js
@@ -192,7 +192,7 @@ export default class Server extends Equality {
 		channel.members.add(user);
 		user.voiceChannel = channel;
 
-		if (oldChannel.id && channel.name !== oldChannel.name) {
+		if (oldChannel.id && channel.id !== oldChannel.id) {
 			this.client.emit("voiceLeave", oldChannel, user);
 			this.client.emit("voiceSwitch", oldChannel, channel, user);
 		}
