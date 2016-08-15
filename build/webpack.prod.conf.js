48f53b3f4da7feb509f623cee7d31a677aa658f7
Amish Shah <amishshah.2k@gmail.com>
2016-08-15 21:05:30 +0100

    Build the site

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -18,7 +18,8 @@ var webpackConfig = merge(baseWebpackConfig, {
   output: {
     path: config.build.assetsRoot,
     filename: utils.assetsPath('js/[name].[chunkhash].js'),
-    chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
+    chunkFilename: utils.assetsPath('js/[id].[chunkhash].js'),
+    publicPath: '/discord.js/'
   },
   vue: {
     loaders: utils.cssLoaders({
