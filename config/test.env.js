24422f683d1817c11893702445f5998c7a55c80b
Amish Shah <amishshah.2k@gmail.com>
2016-08-15 21:05:30 +0100

    Build the site

--- config/test.env.js
+++
@@ -1,6 +0,0 @@
-var merge = require('webpack-merge')
-var devEnv = require('./dev.env')
-
-module.exports = merge(devEnv, {
-  NODE_ENV: '"testing"'
-})
--- src/views/loader.js
+++
@@ -1,7 +0,0 @@
-import Index from './IndexView.vue';
-import Docs from './DocsView.vue';
-
-module.exports = {
-  Index,
-  Docs,
-};
