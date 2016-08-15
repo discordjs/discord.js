ea7b430b9dacde763ebeb2c07a9c5c48756d8344
Amish Shah <amishshah.2k@gmail.com>
2016-08-15 14:00:42 +0100

    crappy site commiting as a backup

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import IndexView from './views/IndexView';
 import DocsView from './views/DocsView';
+import FileViewer from './components/FileViewer';
+import ClassViewer from './components/ClassViewer';
 import Navbar from './components/Navbar';
 import $ from 'jquery';
 
@@ -13,6 +15,9 @@ const store = {
   downloads: '50,000+ ',
   contributors: '30+ ',
   stars: '250+ ',
+  currentDocs: null,
+  branches: [],
+  loading: true,
 };
 
 const App = Vue.extend({
@@ -31,6 +36,13 @@ $.getJSON('https://api.npmjs.org/downloads/range/2013-08-21:2100-08-21/discord.j
       count += day.downloads;
     }
     store.downloads = count;
+  }
+});
+
+$.getJSON('https://api.github.com/repos/hydrabolt/discord.js/branches', data => {
+  if (data) {
+    store.branches = data;
+    store.loading = false;
   }
 });
 
@@ -54,6 +66,14 @@ router.map({
   },
   '/docs': {
     component: DocsView,
+    subRoutes: {
+      '/file/:category/:file': {
+        component: FileViewer,
+      },
+      '/class/:id': {
+        component: ClassViewer,
+      },
+    },
   },
 });
 
--- src/views/DocsView.vue
+++ src/views/DocsView.vue
@@ -1,22 +1,37 @@
 <template>
   <div class="docs-view">
+    <div class="loading" v-if="store.loading">Loading...</div>
     <div class="docs-topbar">
       <div class="container">
-        Documentation
+        <span class="heading">Documentation</span>
+        <div class="options">
+          <div v-on:click="chooseOption('branch')" class="option {{ options.branch ? 'selected' : '' }}">branch</div>
+          <div v-on:click="chooseOption('release')" class="option {{ options.release ? 'selected' : '' }}">release</div>
+          <div v-on:click="chooseOption('commit')" class="option {{ options.commit ? 'selected' : '' }}">commit</div>
+        </div>
+        <select v-if="options.branch" v-model="selectedBranch">
+          <option v-for="branch in store.branches" v-bind:value="branch.name">{{ branch.name }}</option>
+        </select>
       </div>
     </div>
     <div class="docs-page">
       <div class="container">
         <ul class="docs-sidebar">
-          <li class="category">General</li>
-          <li class="link">Getting Started</li>
-          <li class="link">Installation</li>
-          <li class="category">Classes</li>
-          <li class="link">User</li>
-          <li class="link">Guild</li>
+          <ul class="category" v-for="(category, items) in store.currentDocs.custom">
+            <li>{{ category }}</li>
+            <li class="link" v-for="(name, data) in items" v-link='{ path:"/docs/file/"+category+"/"+name }'>
+              {{ name }}
+            </li>
+          </ul>
+          <ul class="category">
+            <li>Classes</li>
+            <li class="link" v-for="piece in store.currentDocs.json" v-if="piece.kind=='class'"  v-link='{ path:"/docs/class/"+piece.name }'>
+              {{ piece.name }}
+            </li>
+          </ul>
         </ul>
         <div class="docs-content">
-          <h1>Getting Started...</h1>
+          <router-view></router-view>
         </div>
       </div>
     </div>
@@ -24,11 +39,69 @@
 </template>
 
 <script>
+import $ from 'jquery';
+
+const options = {
+  branch: true,
+  commit: false,
+  release: false,
+};
+
+let store = {};
+
+function loadDocs(name) {
+  return new Promise((resolve, reject) => {
+    if (!name) {
+      return reject(new Error('no name'));
+    }
+    if (store.branches[name]) {
+      store.currentDocs = store.branches[name];
+      return resolve(store.branches[name]);
+    }
+    store.loading = true;
+    $.getJSON(`https://raw.githubusercontent.com/hydrabolt/discord.js/${name}/docs/docs.json`, data => {
+      if (data) {
+        store.branches[name] = data;
+        store.currentDocs = data;
+        store.loading = false;
+        return resolve(data);
+      }
+      store.loading = false;
+      return alert(`Couldn't find documentation for ${name}`);
+    })
+    .fail(error => {
+      store.loading = false;
+      return alert(`Couldn't find documentation for ${name} - ${error.responseText}`);
+    });
+    return null;
+  });
+}
+
 export default {
   data() {
+    store = this.$root.store;
     return {
       store: this.$root.store,
+      options,
+      selectedBranch: null,
     };
+  },
+  methods: {
+    chooseOption: option => {
+      for (const opt in options) {
+        options[opt] = option === opt;
+      }
+    },
+  },
+  watch: {
+    selectedBranch: (val) => {
+      loadDocs(val);
+    },
+  },
+  computed: {
+    categories() {
+      return Object.keys(store.currentDocs.custom);
+    },
   },
 };
 </script>
