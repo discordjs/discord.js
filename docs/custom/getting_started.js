bca3cc79158818f7690168419a8177fcfeb38bf4
Amish Shah <amishshah.2k@gmail.com>
2016-08-18 00:16:51 +0100

    work on getting started

--- docs/custom/getting_started.js
+++ docs/custom/getting_started.js
@@ -4,5 +4,26 @@ module.exports = {
   data:
 `# Welcome!
 discord.js is an easy-to-use and intuitive JavaScript library that wraps around the Discord API.
+
+## Installation
+To install discord.js from npm:
+\`\`\`bash
+npm i --save discord.js
+\`\`\`
+
+To install discord.js from the master branch:
+\`\`\`bash
+npm i --save hydrabolt/discord.js
+\`\`\`
+
+To install discord.js from the development branch:
+\`\`\`bash
+npm i --save hydrabolt/discord.js#indev
+\`\`\`
+
+To install discord.js from the rewrite branch:
+\`\`\`bash
+npm i --save hydrabolt/discord.js#indev-rewrite
+\`\`\`
 `,
 };
