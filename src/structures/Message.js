614c488f966d29d1a1bb1e86845c1bff656e7ddb
Amish Shah <amishshah.2k@gmail.com>
2016-08-17 19:15:33 +0100

    work on docs

--- src/structures/Message.js
+++ src/structures/Message.js
@@ -3,12 +3,24 @@
  */
 class Message {
   constructor(channel, data, client) {
+    /**
+     * The channel that the message was sent in
+     * @type {Channel}
+     */
     this.channel = channel;
 
     if (channel.guild) {
+      /**
+       * If the message was sent in a guild, this will be the guild the message was sent in
+       * @type {?Guild}
+       */
       this.guild = channel.guild;
     }
 
+    /**
+     * The client that instantiated the Object
+     * @type {Client}
+     */
     this.client = client;
     if (data) {
       this.setup(data);
@@ -16,16 +28,59 @@ class Message {
   }
 
   setup(data) {
+    /**
+     * The author of the message
+     * @type {User}
+     */
     this.author = this.client.store.newUser(data.author);
+    /**
+     * The content of the message
+     * @type {String}
+     */
     this.content = data.content;
+    /**
+     * When the message was sent
+     * @type {Date}
+     */
     this.timestamp = new Date(data.timestamp);
+    /**
+     * If the message was edited, the timestamp at which it was last edited
+     * @type {?Date}
+     */
     this.editedTimestamp = data.edited_timestamp ? new Date(data.edited_timestamp) : null;
+    /**
+     * Whether or not the message was Text-To-Speech
+     * @type {Boolean}
+     */
     this.tts = data.tts;
+    /**
+     * Whether the message mentioned @everyone or not
+     * @type {Boolean}
+     */
     this.mentionEveryone = data.mention_everyone;
+    /**
+     * A random number used for checking message delivery
+     * @type {String}
+     */
     this.nonce = data.nonce;
+    /**
+     * A list of embeds in the message - e.g. YouTube Player
+     * @type {Array<Object>}
+     */
     this.embeds = data.embeds;
+    /**
+     * A list of attachments in the message - e.g. Pictures
+     * @type {Array<Object>}
+     */
     this.attachments = data.attachments;
+    /**
+     * A list of users mentioned in the message
+     * @type {Array<User>}
+     */
     this.mentions = [];
+    /**
+     * The ID of the message (unique in the channel it was sent)
+     */
     this.id = data.id;
     for (const mention of data.mentions) {
       let user = this.client.store.get('users', mention.id);
@@ -83,6 +138,14 @@ class Message {
     }
   }
 
+  /**
+   * Used mainly internally. Whether two messages are identical in properties. If you want to compare messages
+   * without checking all the properties, use `message.id === message2.id`, which is much more efficient. This
+   * method allows you to see if there are differences in content, embeds, attachments, nonce and tts properties.
+   * @param {Message} message The message to compare it to
+   * @param {Object} rawData Raw data passed through the WebSocket about this message
+   * @returns {Boolean}
+   */
   equals(message, rawData) {
     const embedUpdate = !message.author && !message.attachments;
 
@@ -108,10 +171,29 @@ class Message {
     return base;
   }
 
+  /**
+   * Deletes the message
+   * @returns {Promise<Message, Error>}
+   * @example
+   * // delete a message
+   * message.delete()
+   *  .then(msg => console.log(`Deleted message from ${msg.author}`))
+   *  .catch(console.log);
+   */
   delete() {
     return this.client.rest.methods.deleteMessage(this);
   }
 
+  /**
+   * Edit the content of a message
+   * @param {String} content the new content of a message
+   * @returns {Promise<Message, Error>}
+   * @example
+   * // update the content of a message
+   * message.edit('This is my new content!')
+   *  .then(msg => console.log(`Updated the content of a message from ${msg.author}`))
+   *  .catch(console.log);
+   */
   edit(content) {
     return this.client.rest.methods.updateMessage(this, content);
   }
