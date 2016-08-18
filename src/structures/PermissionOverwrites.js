839ff5ce4381cfb194b4af4d9b4e2b82bfeef92f
Amish Shah <amishshah.2k@gmail.com>
2016-08-18 13:33:02 +0100

    Docs for permissionoverwrites

--- src/structures/PermissionOverwrites.js
+++ src/structures/PermissionOverwrites.js
@@ -1,5 +1,12 @@
+/**
+ * Represents a permission overwrite for a Role or Member in a Guild Channel.
+ */
 class PermissionOverwrites {
   constructor(guildChannel, data) {
+    /**
+     * The GuildChannel this overwrite is for
+     * @type {GuildChannel}
+     */
     this.channel = guildChannel;
     if (data) {
       this.setup(data);
@@ -7,7 +14,15 @@ class PermissionOverwrites {
   }
 
   setup(data) {
+    /**
+     * The type of this overwrite
+     * @type {String}
+     */
     this.type = data.type;
+    /**
+     * The ID of this overwrite, either a User ID or a Role ID
+     * @type {String}
+     */
     this.id = data.id;
     this.denyData = data.deny;
     this.allowData = data.allow;
