2337e1de9611b98d42d4f4d2cc292353580a59ce
Amish Shah <amishshah.2k@gmail.com>
2016-08-22 23:14:22 +0100

    add prop table rendering for members

--- src/components/docs/classviewer/PropRenderer.vue
+++ src/components/docs/classviewer/PropRenderer.vue
@@ -2,14 +2,17 @@
   <div class="classProp" id="doc_for_{{* prop.name }}">
     <div class="propName" v-link='{ name:"classview", query:{scrollto:prop.name}}'>.{{* prop.name }}</div>
     <div class="propDescription">{{{* prop.description | normalise | marked }}}</div>
+    <param-table :params="prop.props" v-if="prop.props.length > 0"></param-table>
     <div class="propType"><b>Type:</b> <type-renderer v-for="type in prop.type.types" :names="type"></type-renderer></div>
   </div>
 </template>
 <script>
 import TypeRenderer from './TypeRenderer.vue';
+import ParamTable from './ParamTable.vue';
 export default {
   components: {
     TypeRenderer,
+    ParamTable,
   },
   props: ['prop'],
 };
