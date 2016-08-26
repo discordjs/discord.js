642d768cdc49413bd6270ce8a1f9e34c6dea82e8
Amish Shah <amishshah.2k@gmail.com>
2016-08-26 22:13:23 +0100

    Add textBasedChannel.bulkDelete()

---
+++ src/client/websocket/packets/handlers/MessageDeleteBulk.js
@@ -0,0 +1,21 @@
+const AbstractHandler = require('./AbstractHandler');
+
+class MessageDeleteBulkHandler extends AbstractHandler {
+
+  handle(packet) {
+    const data = packet.d;
+    const client = this.packetManager.client;
+
+    client.actions.MessageDeleteBulk.handle(data);
+  }
+
+}
+
+/**
+* Emitted whenever a messages are deleted in bulk
+*
+* @event Client#messageDeleteBulk
+* @param {Collection<String, Message>} messages The deleted messages, mapped by their ID
+*/
+
+module.exports = MessageDeleteBulkHandler;
