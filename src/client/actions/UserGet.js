4acc08fccc3b4e532470a44f3cab06c398f1083b
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2016-08-26 20:46:18 -0400

    Fixed ESLint issues

--- src/client/actions/UserGet.js
+++ src/client/actions/UserGet.js
@@ -2,14 +2,14 @@ const Action = require('./Action');
 
 class UserGetAction extends Action {
 
-	handle(data) {
-		const client = this.client;
-		const user = this.client.dataManager.newUser(data);
+  handle(data) {
+    const client = this.client;
+    const user = client.dataManager.newUser(data);
 
-		return {
-			user,
-		};
-	}
+    return {
+      user,
+    };
+  }
 }
 
 module.exports = UserGetAction;
