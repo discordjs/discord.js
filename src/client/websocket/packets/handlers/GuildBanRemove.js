1c3e37c4c4aad59373b212d7a6d3a7dd2e284964
Amish Shah <amishshah.2k@gmail.com>
2016-08-27 22:31:14 +0100

    Added guild.unban

--- src/client/websocket/packets/handlers/GuildBanRemove.js
+++ src/client/websocket/packets/handlers/GuildBanRemove.js
@@ -2,20 +2,13 @@
 
 const AbstractHandler = require('./AbstractHandler');
 
-const Constants = require('../../../../util/Constants');
-
 class GuildBanRemoveHandler extends AbstractHandler {
 
   handle(packet) {
     const data = packet.d;
     const client = this.packetManager.client;
 
-    const guild = client.guilds.get(data.guild_id);
-    const user = client.users.get(data.user.id);
-
-    if (guild && user) {
-      client.emit(Constants.Events.GUILD_BAN_REMOVE, guild, user);
-    }
+    client.actions.GuildBanRemove.handle(data);
   }
 
 }
