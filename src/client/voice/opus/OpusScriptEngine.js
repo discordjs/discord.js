e70b4a7649e4f2f436618d6eedc2e8c01ee4218f
abalabahaha <hi@abal.moe>
2016-08-28 16:06:34 +0900

    Add opusscript as a possible opus encoder

--- src/client/voice/opus/OpusScriptEngine.js
+++ src/client/voice/opus/OpusScriptEngine.js
@@ -1,27 +1,27 @@
 const OpusEngine = require('./BaseOpusEngine');
 
-let opus;
+let opusscript;
 
 class NodeOpusEngine extends OpusEngine {
   constructor(player) {
     super(player);
     try {
       // eslint-disable-next-line import/no-unresolved
-      opus = require('node-opus');
+      opusscript = require('opusscript');
     } catch (err) {
       throw err;
     }
-    this.encoder = new opus.OpusEncoder(48000, 2);
+    this.encoder = new opusscript(48000, 2);
   }
 
   encode(buffer) {
     super.encode(buffer);
-    return this.encoder.encode(buffer, 1920);
+    return this.encoder.encode(buffer, 960);
   }
 
   decode(buffer) {
     super.decode(buffer);
-    return this.encoder.decode(buffer, 1920);
+    return this.encoder.decode(buffer);
   }
 }
 
