50515101069de73309451f6f724a294f3f6c48c1
Amish Shah <amishshah.2k@gmail.com>
2016-09-04 12:42:07 +0100

    reset indev

--- src/Client/ConnectionState.js
+++
@@ -1,7 +0,0 @@
-export default {
-	IDLE: 0,
-	LOGGING_IN: 1,
-	LOGGED_IN: 2,
-	READY: 3,
-	DISCONNECTED: 4
-};
--- src/Util/ArgumentRegulariser.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-export function reg (c, a) {
-	return [c].concat(Array.prototype.slice.call(a));
-}
