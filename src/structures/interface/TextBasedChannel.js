998a0aae285d9e99c86d60f04f332922801ddd56
Hackzzila <admin@hackzzila.com>
2016-09-04 12:59:42 -0500

    Added disable_everyone

--- src/structures/interface/TextBasedChannel.js
+++ src/structures/interface/TextBasedChannel.js
@@ -51,7 +51,7 @@ class TextBasedChannel {
    *  .catch(console.log);
    */
   sendMessage(content, options = {}) {
-    return this.client.rest.methods.sendMessage(this, content, options.tts, options.nonce);
+    return this.client.rest.methods.sendMessage(this, content, options.tts, options.nonce, options.disable_everyone);
   }
 
   /**
@@ -66,7 +66,7 @@ class TextBasedChannel {
    *  .catch(console.log);
    */
   sendTTSMessage(content, options = {}) {
-    return this.client.rest.methods.sendMessage(this, content, true, options.nonce);
+    return this.client.rest.methods.sendMessage(this, content, true, options.nonce, options.disable_everyone);
   }
 
   /**
@@ -87,7 +87,7 @@ class TextBasedChannel {
     }
     return new Promise((resolve, reject) => {
       this.client.resolver.resolveFile(attachment).then(file => {
-        this.client.rest.methods.sendMessage(this, undefined, false, undefined, {
+        this.client.rest.methods.sendMessage(this, undefined, false, undefined, false, {
           file,
           name: fileName,
         }).then(resolve).catch(reject);
