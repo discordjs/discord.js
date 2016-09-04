f23c07a08e98ea09edaa173ee92792c113231a1d
Amish Shah <amishshah.2k@gmail.com>
2016-09-04 12:27:01 +0100

    document sharding stuff

--- src/sharding/ShardingManager.js
+++ src/sharding/ShardingManager.js
@@ -1,17 +1,35 @@
-const childProcess = require('child_process');
 const path = require('path');
 const EventEmitter = require('events').EventEmitter;
 const Collection = require('../util/Collection');
 const Shard = require('./Shard');
 
+/**
+ * This is a utility class that can be used to help you spawn shards of your Client. Each shard is completely separate
+ * from the other. The Shard Manager takes a path to a file and spawns it under the specified amount of shards safely.
+ * <warn>The Sharding Manager is still experimental</warn>
+ * @extends {EventEmitter}
+ */
 class ShardingManager extends EventEmitter {
+  /**
+   * Creates an instance of ShardingManager.
+   * @param {string} file the path to your file
+   * @param {number} totalShards the number of shards you would like to spawn
+   */
   constructor(file, totalShards) {
     super();
     this.file = file;
     if (!path.isAbsolute(file)) {
       this.file = path.resolve(`${process.cwd()}${file}`);
     }
+    /**
+     * The amount of shards that this manager is going to spawn
+     * @type {number}
+     */
     this.totalShards = totalShards;
+    /**
+     * A collection of shards that this manager has spawned.
+     * @type {Collection<number, Shard>}
+     */
     this.shards = new Collection();
     this.waiting = new Collection();
   }
@@ -30,7 +48,7 @@ class ShardingManager extends EventEmitter {
       if (this.shards.size === this.totalShards) {
         return clearInterval(interval);
       }
-      this.createShard();
+      return this.createShard();
     }, 5500);
   }
 }
