2c6b804fc9824c636c8913e482a7212b8b7d3e7f
Schuyler Cebulskie <Gawdl3y@gmail.com>
2016-09-04 17:02:52 -0400

    Improve sharding stuff (#608)
    * Improve sharding stuff
    
    * Build docs

--- src/sharding/Shard.js
+++ src/sharding/Shard.js
@@ -5,6 +5,10 @@ const path = require('path');
  * Represents a Shard spawned by the ShardingManager.
  */
 class Shard {
+  /**
+   * @param {ShardingManager} manager The sharding manager
+   * @param {number} id The ID of this shard
+   */
   constructor(manager, id) {
     /**
      * The manager of the spawned shard
@@ -12,13 +16,13 @@ class Shard {
      */
     this.manager = manager;
     /**
-     * The shard id
+     * The shard ID
      * @type {number}
      */
     this.id = id;
     /**
      * The process of the shard
-     * @type {process}
+     * @type {ChildProcess}
      */
     this.process = childProcess.fork(path.resolve(this.manager.file), [id, this.manager.shards.size]);
   }
