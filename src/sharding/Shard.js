65b70504cf64f98af691fda0b0b79066e6d973b0
Amish Shah <amishshah.2k@gmail.com>
2016-09-04 12:37:36 +0100

    document shard

--- src/sharding/Shard.js
+++ src/sharding/Shard.js
@@ -6,8 +6,20 @@ const path = require('path');
  */
 class Shard {
   constructor(manager, id) {
+    /**
+     * The manager of the spawned shard
+     * @type {ShardingManager}
+     */
     this.manager = manager;
+    /**
+     * The shard id
+     * @type {number}
+     */
     this.id = id;
+    /**
+     * The process of the shard
+     * @type {process}
+     */
     this.process = childProcess.fork(path.resolve(this.manager.file), [id, this.manager.shards.size]);
   }
 }
