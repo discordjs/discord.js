838f4d8719665297db146ffdfd264b8418a31d44
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2016-09-06 20:36:58 -0400

    Add Guild.fetchMember

---
+++ src/client/actions/GuildMemberGet.js
@@ -0,0 +1,13 @@
+const Action = require('./Action');
+
+class GuildMemberGetAction extends Action {
+  handle(guild, data) {
+    const client = this.client;
+    const member = client.dataManager.newGuildMember(guild, data);
+    return {
+      member,
+    };
+  }
+}
+
+module.exports = GuildMemberGetAction;
