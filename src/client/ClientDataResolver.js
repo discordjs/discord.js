ac2ad0dd237e0ba211eb9b080c2afb718e849917
Amish Shah <amishshah.2k@gmail.com>
2016-09-12 17:58:52 +0100

    Document PermissionResolvables

--- src/client/ClientDataResolver.js
+++ src/client/ClientDataResolver.js
@@ -136,6 +136,34 @@ class ClientDataResolver {
    * Data that can be resolved to give a permission number. This can be:
    * * A string
    * * A permission number
+   * ```js
+   * [
+   *   "CREATE_INSTANT_INVITE",
+   *   "KICK_MEMBERS",
+   *   "BAN_MEMBERS",
+   *   "ADMINISTRATOR",
+   *   "MANAGE_CHANNELS",
+   *   "MANAGE_GUILD",
+   *   "READ_MESSAGES",
+   *   "SEND_MESSAGES",
+   *   "SEND_TTS_MESSAGES",
+   *   "MANAGE_MESSAGES",
+   *   "EMBED_LINKS",
+   *   "ATTACH_FILES",
+   *   "READ_MESSAGE_HISTORY",
+   *   "MENTION_EVERYONE",
+   *   "EXTERNAL_EMOJIS", // use external emojis
+   *   "CONNECT", // connect to voice
+   *   "SPEAK", // speak on voice
+   *   "MUTE_MEMBERS", // globally mute members on voice
+   *   "DEAFEN_MEMBERS", // globally deafen members on voice
+   *   "MOVE_MEMBERS", // move member's voice channels
+   *   "USE_VAD", // use voice activity detection
+   *   "CHANGE_NICKNAME",
+   *   "MANAGE_NICKNAMES", // change nicknames of others
+   *   "MANAGE_ROLES_OR_PERMISSIONS"
+   * ]
+   * ```
    * @typedef {string|number} PermissionResolvable
    */
 
