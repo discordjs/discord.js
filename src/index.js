cdf3136cde02d459d1c7e670afcb7217cb357f97
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2016-09-13 01:34:45 -0400

    Make version a regular property instead of getter

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 const path = require('path');
 
-let version;
-
 module.exports = {
   Client: require('./client/Client'),
   Shard: require('./sharding/Shard'),
   ShardingManager: require('./sharding/ShardingManager'),
   Collection: require('./util/Collection'),
-
-  get version() {
-    if (!version) version = require(path.join(__dirname, '..', 'package.json')).version;
-    return version;
-  },
+  version: require(path.join(__dirname, '..', 'package.json')).version,
 };
