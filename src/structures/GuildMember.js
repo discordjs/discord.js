9d9429108e9cdebbde643e766db06aa0f3472c5a
bdistin <bdistin@gmail.com>
2016-09-30 16:08:19 -0500

    Added .missingPermissions() (#756)
    * Added .missingPermissions()
    
    * Added .missingPermissions()
    
    * Fixed docs for .missingPermissions()
    
    * Fixed Travis CI trailing spaces?
    
    * Fixed Travis CI trailing spaces?

--- src/structures/GuildMember.js
+++ src/structures/GuildMember.js
@@ -254,6 +254,16 @@ class GuildMember {
   }
 
   /**
+   * Checks whether the roles of the member allows them to perform specific actions, and lists any missing permissions.
+   * @param {PermissionResolvable[]} permissions The permissions to check for
+   * @param {boolean} [explicit=false] Whether to require the member to explicitly have the exact permissions
+   * @returns {array}
+   */
+  missingPermissions(permissions, explicit = false) {
+    return permissions.filter(p => !this.hasPermission(p, explicit));
+  }
+
+  /**
    * Edit a Guild Member
    * @param {GuildmemberEditData} data The data to edit the member with
    * @returns {Promise<GuildMember>}
