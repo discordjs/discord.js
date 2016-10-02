2bb6ecdc28df2797445c054a99e59aefb1146227
Amish Shah <amishshah.2k@gmail.com>
2016-10-02 15:16:54 +0100

    documentation

--- src/client/voice/util/SecretKey.js
+++ src/client/voice/util/SecretKey.js
@@ -1,5 +1,12 @@
+/**
+ * Represents a Secret Key used in encryption over voice
+ */
 class SecretKey {
   constructor(key) {
+    /**
+     * The key used for encryption
+     * @type {Uint8Array}
+     */
     this.key = new Uint8Array(new ArrayBuffer(key.length));
     for (const index in key) {
       this.key[index] = key[index];
