12e267b95e10aef0005967898ebf5e11c1180901
isonmad <isonmad@github>
2016-10-23 00:37:40 +0000

    fix indentation for eslint

--- src/client/Client.js
+++ src/client/Client.js
@@ -235,13 +235,13 @@ class Client extends EventEmitter {
    */
   destroy() {
     return this.manager.destroy().then(() => {
-        for (const t of this._timeouts) clearTimeout(t);
-        for (const i of this._intervals) clearInterval(i);
-        this._timeouts.clear();
-        this._intervals.clear();
-        this.token = null;
-        this.email = null;
-        this.password = null;
+      for (const t of this._timeouts) clearTimeout(t);
+      for (const i of this._intervals) clearInterval(i);
+      this._timeouts.clear();
+      this._intervals.clear();
+      this.token = null;
+      this.email = null;
+      this.password = null;
     });
   }
 
