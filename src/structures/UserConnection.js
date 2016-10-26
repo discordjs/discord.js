c6bcf69dc35849f0c85b11e7136895c6318c39a2
Gus Caplan <gustavcaplan@gmail.com>
2016-10-26 10:23:39 -0500

    added User#fetchProfile (#835)
    * add User#fetchProfile
    
    * fix merge conflicts?

---
+++ src/structures/UserConnection.js
@@ -0,0 +1,48 @@
+/**
+ * Represents a User Connection object (or "platform identity")
+ */
+class UserConnection {
+  constructor(user, data) {
+    /**
+     * The user that owns the Connection
+     * @type {User}
+     */
+    this.user = user;
+
+    this.setup(data);
+  }
+
+  setup(data) {
+    /**
+     * The type of the Connection
+     * @type {string}
+     */
+    this.type = data.type;
+
+    /**
+     * The username of the connection account
+     * @type {string}
+     */
+    this.name = data.name;
+
+    /**
+     * The id of the connection account
+     * @type {string}
+     */
+    this.id = data.id;
+
+    /**
+     * Whether the connection is revoked
+     * @type {Boolean}
+     */
+    this.revoked = data.revoked;
+
+    /**
+     * an array of partial server integrations (not yet implemented in this lib)
+     * @type {Object[]}
+     */
+    this.integrations = data.integrations;
+  }
+}
+
+module.exports = UserConnection;
