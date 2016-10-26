58233c775c120e87de4d52276b501f5f1ac2ef6f
Amish Shah <amishshah.2k@gmail.com>
2016-10-26 17:30:07 +0100

    Revert "fix Client.destroy bugs"

--- src/client/Client.js
+++ src/client/Client.js
@@ -234,14 +234,17 @@ class Client extends EventEmitter {
    * @returns {Promise}
    */
   destroy() {
-    return this.manager.destroy().then(() => {
-      for (const t of this._timeouts) clearTimeout(t);
-      for (const i of this._intervals) clearInterval(i);
-      this._timeouts.clear();
-      this._intervals.clear();
-      this.token = null;
-      this.email = null;
-      this.password = null;
+    return new Promise((resolve, reject) => {
+      this.manager.destroy().then(() => {
+        for (const t of this._timeouts) clearTimeout(t);
+        for (const i of this._intervals) clearInterval(i);
+        this._timeouts = [];
+        this._intervals = [];
+        this.token = null;
+        this.email = null;
+        this.password = null;
+        resolve();
+      }).catch(reject);
     });
   }
 
