a2f125e41650d79525b65e21583df0a26cd564e4
Amish Shah <amishshah.2k@gmail.com>
2016-10-26 19:27:52 +0100

    case sensitive

--- src/sharding/ShardingManager.js
+++ src/sharding/ShardingManager.js
@@ -4,7 +4,7 @@ const EventEmitter = require('events').EventEmitter;
 const mergeDefault = require('../util/MergeDefault');
 const Shard = require('./Shard');
 const Collection = require('../util/Collection');
-const fetchRecommendedShards = require('../util/fetchRecommendedShards');
+const fetchRecommendedShards = require('../util/FetchRecommendedShards');
 
 /**
  * This is a utility class that can be used to help you spawn shards of your Client. Each shard is completely separate
