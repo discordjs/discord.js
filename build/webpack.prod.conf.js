060588defd5a425f70696321fbf029492e811e4f
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2016-10-27 04:23:23 -0400

    Fix publicPath for new domain

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -19,7 +19,7 @@ var webpackConfig = merge(baseWebpackConfig, {
     path: config.build.assetsRoot,
     filename: utils.assetsPath('js/[name].[chunkhash].js'),
     chunkFilename: utils.assetsPath('js/[id].[chunkhash].js'),
-    publicPath: '/discord.js/'
+    publicPath: '/'
   },
   vue: {
     loaders: utils.cssLoaders({
