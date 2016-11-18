b8532b514d236c746997abb5abcd7ff0939a9c55
Gus Caplan <me@gus.host>
2016-11-18 14:44:42 -0600

    add minify builds and a nice package.json script to run it all

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,45 @@
 const webpack = require('webpack');
+const createVariants = require('parallel-webpack').createVariants;
 const version = require('./package.json').version;
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: __dirname,
-    filename: `./webpack/discord.${version}.js`,
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.json$/,
-        loader: 'json-loader',
-      },
-      {
-        test: /\.md$/,
-        loader: 'ignore-loader',
-      },
-    ],
-  },
-  node: {
-    fs: 'empty',
-    dns: 'empty',
-    tls: 'empty',
-    child_process: 'empty',
-    dgram: 'empty',
-    __dirname: true,
-  },
-  plugins: [
+const createConfig = (options) => {
+  const plugins = [
     new webpack.DefinePlugin({ 'global.GENTLY': false }),
-  ],
+  ];
+  const loaders = [
+    { test: /\.json$/, loader: 'json-loader' },
+    { test: /\.md$/, loader: 'ignore-loader' },
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015'],
+      },
+    },
+  ];
+
+  if (options.minify) plugins.push(new webpack.optimize.UglifyJsPlugin({ minimize: true }));
+
+  return {
+    entry: './src/index.js',
+    output: {
+      path: __dirname,
+      filename: `./webpack/discord.${version}${options.minify ? '.min' : ''}.js`,
+    },
+    module: { loaders },
+    node: {
+      fs: 'empty',
+      dns: 'empty',
+      tls: 'empty',
+      child_process: 'empty',
+      dgram: 'empty',
+      __dirname: true,
+    },
+    plugins,
+  };
 };
+
+module.exports = createVariants({}, {
+  minify: [false, true],
+}, createConfig);
