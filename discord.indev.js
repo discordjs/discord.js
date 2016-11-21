57859d50cde3c9f811ac0a8e4e88188212588501
Travis CI <amishshah.2k@gmail.com>
2016-11-21 03:47:54 +0000

    Webpack build: ee4a8bb3b6819f8638e7715049d4953784aa3e45

--- discord.indev.js
+++ discord.indev.js
@@ -10358,12 +10358,6 @@ class Client extends EventEmitter {
   constructor(options = {}) {
     super();
 
-    /**
-     * Whether the client is in a browser environment
-     * @type {boolean}
-     */
-    this.browser = typeof window !== 'undefined';
-
     // Obtain shard details from environment
     if (!options.shardId && 'SHARD_ID' in process.env) options.shardId = Number(process.env.SHARD_ID);
     if (!options.shardCount && 'SHARD_COUNT' in process.env) options.shardCount = Number(process.env.SHARD_COUNT);
@@ -10545,6 +10539,15 @@ class Client extends EventEmitter {
    */
   get readyTimestamp() {
     return this.readyAt ? this.readyAt.getTime() : null;
+  }
+
+  /**
+   * Whether the client is in a browser environment
+   * @type {boolean}
+   * @readonly
+   */
+  get browser() {
+    return typeof window !== 'undefined';
   }
 
   /**
