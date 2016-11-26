72a5fb68ec182cfa30366220246bb33623dc6c8f
Travis CI <amishshah.2k@gmail.com>
2016-11-26 22:00:36 +0000

    Webpack build: 4ae1f63a97829706fb50aa15703d9021f60c294d

--- discord.indev.js
+++ discord.indev.js
@@ -3155,7 +3155,9 @@ class Message {
    * @readonly
    */
   get edits() {
-    return this._edits.slice().unshift(this);
+    const copy = this._edits.slice();
+    copy.unshift(this);
+    return copy;
   }
 
   /**
