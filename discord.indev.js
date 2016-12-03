f2156cfffd1c66ed556696e0c70b3be09e5e0a56
Travis CI <amishshah.2k@gmail.com>
2016-12-03 03:48:08 +0000

    Webpack build: 6adc8a9d0da153414413a46b01d5c273d0241e5b

--- discord.indev.js
+++ discord.indev.js
@@ -1388,6 +1388,18 @@ class Role {
   }
 
   /**
+   * Whether the role is editable by the client user.
+   * @type {boolean}
+   * @readonly
+   */
+  get editable() {
+    if (this.managed) return false;
+    const clientMember = this.guild.member(this.client.user);
+    if (!clientMember.hasPermission(Constants.PermissionFlags.MANAGE_ROLES_OR_PERMISSIONS)) return false;
+    return clientMember.highestRole.comparePositionTo(this) > 0;
+  }
+
+  /**
    * Get an object mapping permission names to whether or not the role enables that permission
    * @returns {Object<string, boolean>}
    * @example
@@ -1561,18 +1573,6 @@ class Role {
    */
   delete() {
     return this.client.rest.methods.deleteGuildRole(this);
-  }
-
-  /**
-   * Whether the role is managable by the client user.
-   * @type {boolean}
-   * @readonly
-   */
-  get editable() {
-    if (this.managed) return false;
-    const clientMember = this.guild.member(this.client.user);
-    if (!clientMember.hasPermission(Constants.PermissionFlags.MANAGE_ROLES_OR_PERMISSIONS)) return false;
-    return clientMember.highestRole.comparePositionTo(this) > 0;
   }
 
   /**
