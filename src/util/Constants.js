94701a8e3d5075295ef7911346fde711362f4052
duke605 <cmdenslow14@hotmail.com>
2016-12-09 17:49:58 -0600

    * Fixed missing commas and semi-colon

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -246,12 +246,12 @@ exports.MessageTypes = {
 };
 
 exports.DefaultAvatars = {
-	BLURPLE: '6debd47ed13483642cf09e832ed0bc1b'
-    GREY: '322c936a8c8be1b803cd94861bdfa868'
-    GREEN: 'dd4dbc0016779df1378e7812eabaa04d'
-    ORANGE: '0e291f67c9274a1abdddeb3fd919cbaa'
-    RED: '1cbd08c76f8af6dddce02c5138971129'
-}
+	BLURPLE: '6debd47ed13483642cf09e832ed0bc1b',
+    GREY: '322c936a8c8be1b803cd94861bdfa868',
+    GREEN: 'dd4dbc0016779df1378e7812eabaa04d',
+    ORANGE: '0e291f67c9274a1abdddeb3fd919cbaa',
+    RED: '1cbd08c76f8af6dddce02c5138971129',
+};
 
 const PermissionFlags = exports.PermissionFlags = {
   CREATE_INSTANT_INVITE: 1 << 0,
