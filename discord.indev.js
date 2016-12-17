d81437615e163da3506e1ad767ded4d5aadeb2b4
Travis CI <amishshah.2k@gmail.com>
2016-12-17 18:23:33 +0000

    Webpack build: 736fa7c6113e549762572def4a5746194545a058

--- discord.indev.js
+++ discord.indev.js
@@ -138,6 +138,7 @@ exports.Errors = {
   INVALID_RATE_LIMIT_METHOD: 'Unknown rate limiting method.',
   BAD_LOGIN: 'Incorrect login details were provided.',
   INVALID_SHARD: 'Invalid shard settings were provided.',
+  INVALID_TOKEN: 'An invalid token was provided.',
 };
 
 const PROTOCOL_VERSION = exports.PROTOCOL_VERSION = 6;
@@ -20242,6 +20243,7 @@ class RESTMethods {
 
   login(token = this.rest.client.token) {
     return new Promise((resolve, reject) => {
+      if (typeof token !== 'string') throw new Error(Constants.Errors.INVALID_TOKEN);
       token = token.replace(/^Bot\s*/i, '');
       this.rest.client.manager.connectToWebSocket(token, resolve, reject);
     });
