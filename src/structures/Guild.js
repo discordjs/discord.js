a0a3989e59cce22aa50c39f39c4d18ec743c8d40
Gus Caplan <gustavcaplan@gmail.com>
2016-12-19 01:11:55 -0600

    fix issue with uncached channels and adding members (#1001)
    * fix this thing
    
    * fix a silly
    
    * Update Guild.js

--- src/structures/Guild.js
+++ src/structures/Guild.js
@@ -752,7 +752,11 @@ class Guild {
       member.selfDeaf = voiceState.self_deaf;
       member.voiceSessionID = voiceState.session_id;
       member.voiceChannelID = voiceState.channel_id;
-      this.channels.get(voiceState.channel_id).members.set(member.user.id, member);
+      if (this.client.channels.has(voiceState.channel_id)) {
+        this.client.channels.get(voiceState.channel_id).members.set(member.user.id, member);
+      } else {
+        this.client.emit('warn', `Member ${member.id} added in guild ${this.id} with an uncached voice channel`);
+      }
     }
 
     /**
