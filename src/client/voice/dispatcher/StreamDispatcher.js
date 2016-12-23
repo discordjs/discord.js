f726db21523f7cabc43d7bd380d8eefab91c934a
bdistin <bdistin@gmail.com>
2016-12-22 21:24:31 -0600

    Revert error emit on Slow Stream Gen (#1011)

--- src/client/voice/dispatcher/StreamDispatcher.js
+++ src/client/voice/dispatcher/StreamDispatcher.js
@@ -180,7 +180,7 @@ class StreamDispatcher extends EventEmitter {
       const data = this.streamingData;
 
       if (data.missed >= 5) {
-        this._triggerTerminalState('error', 'Stream is not generating quickly enough.');
+        this._triggerTerminalState('end', 'Stream is not generating quickly enough.');
         return;
       }
 
