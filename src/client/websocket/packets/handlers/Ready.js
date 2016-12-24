3eca3ba95e5fbfbc4db201ada9ca2a9292739bb1
Gus Caplan <gustavcaplan@gmail.com>
2016-12-24 05:04:27 -0600

    add handler in case heartbeat is not acked (#1013)
    * add handler in case heartbeat is not acked
    
    * ffs

--- src/client/websocket/packets/handlers/Ready.js
+++ src/client/websocket/packets/handlers/Ready.js
@@ -7,6 +7,8 @@ class ReadyHandler extends AbstractHandler {
     const client = this.packetManager.client;
     const data = packet.d;
 
+    client.manager.ping();
+
     const clientUser = new ClientUser(client, data.user);
     client.user = clientUser;
     client.readyAt = new Date();
