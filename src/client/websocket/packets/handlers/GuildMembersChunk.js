f2dde1769b1f1e6abe8f01be6ad58dc62e657c67
Amish Shah <amishshah.2k@gmail.com>
2016-12-26 20:20:58 +0000

    Fix potential ReferenceError with GuildMembersChunk

--- src/client/websocket/packets/handlers/GuildMembersChunk.js
+++ src/client/websocket/packets/handlers/GuildMembersChunk.js
@@ -8,11 +8,9 @@ class GuildMembersChunkHandler extends AbstractHandler {
     const client = this.packetManager.client;
     const data = packet.d;
     const guild = client.guilds.get(data.guild_id);
-    const members = [];
+    if (!guild) return;
 
-    if (guild) {
-      for (const member of data.members) members.push(guild._addMember(member, false));
-    }
+    const members = data.members.map(member => guild._addMember(member, false));
 
     guild._checkChunks();
     client.emit(Constants.Events.GUILD_MEMBERS_CHUNK, members);
