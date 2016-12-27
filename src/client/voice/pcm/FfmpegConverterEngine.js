eb8acbf968b075c4841ad86ef05090179ebac161
appellation <nelson.will@live.com>
2016-12-27 12:55:21 -0800

    nope

--- src/client/voice/pcm/FfmpegConverterEngine.js
+++ src/client/voice/pcm/FfmpegConverterEngine.js
@@ -32,13 +32,7 @@ class PCMConversionProcess extends EventEmitter {
       this.emit('destroyed the process stdin');
     }
     if (this.process.kill) {
-      this.process.once('exit', () => {
-        this.emit('killed the process');
-      });
-      this.process.once('error', () => {
-        this.process.kill('SIGKILL');
-      });
-      this.process.kill();
+      this.process.kill('SIGKILL');
     }
   }
 
