265021bfa54258c5212b73fe5f8a66ccf3f85352
Zack Campbell <zajrik@users.noreply.github.com>
2016-12-27 21:35:11 -0600

    Add sendEmbed to GuildMember for docs (#1028)
    * Add sendEmbed to GuildMember for docs
    
    * Update GuildMember.js

--- src/structures/GuildMember.js
+++ src/structures/GuildMember.js
@@ -432,6 +432,7 @@ class GuildMember {
   // These are here only for documentation purposes - they are implemented by TextBasedChannel
   sendMessage() { return; }
   sendTTSMessage() { return; }
+  sendEmbed() { return; }
   sendFile() { return; }
   sendCode() { return; }
 }
