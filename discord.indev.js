e085f6e2cff28d1f14224ac4226186672d79e412
Travis CI <amishshah.2k@gmail.com>
2016-12-28 23:29:06 +0000

    Webpack build: ea798eaaf3242728f5029befe3a276ca4be6da91

--- discord.indev.js
+++ discord.indev.js
@@ -10959,7 +10959,9 @@ class ClientDataResolver {
    *   "USE_VAD", // use voice activity detection
    *   "CHANGE_NICKNAME",
    *   "MANAGE_NICKNAMES", // change nicknames of others
-   *   "MANAGE_ROLES_OR_PERMISSIONS"
+   *   "MANAGE_ROLES_OR_PERMISSIONS",
+   *   "MANAGE_WEBHOOKS",
+   *   "MANAGE_EMOJIS"
    * ]
    * ```
    * @typedef {string|number} PermissionResolvable
