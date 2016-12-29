5064b932f864f72891b8ce4ba4acb90671ae5992
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2016-12-29 03:21:22 -0500

    Update User#equals doc

--- src/structures/User.js
+++ src/structures/User.js
@@ -237,7 +237,7 @@ class User {
   }
 
   /**
-   * Checks if the user is equal to another. It compares username, ID, discriminator, status and the game being played.
+   * Checks if the user is equal to another. It compares ID, username, discriminator, avatar, and bot flags.
    * It is recommended to compare equality by using `user.id === user2.id` unless you want to compare all properties.
    * @param {User} user User to compare with
    * @returns {boolean}
