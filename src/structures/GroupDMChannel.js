ad4173d07b4e7d6dd7d9d8fd4ff94d4657df74ee
Zack Campbell <zajrik@users.noreply.github.com>
2016-12-29 10:04:41 -0600

    Update DMChannel, GroupDMChannel for docs (#1038)

--- src/structures/GroupDMChannel.js
+++ src/structures/GroupDMChannel.js
@@ -121,8 +121,8 @@ class GroupDMChannel extends Channel {
   }
 
   // These are here only for documentation purposes - they are implemented by TextBasedChannel
+  send() { return; }
   sendMessage() { return; }
-  sendTTSMessage() { return; }
   sendEmbed() { return; }
   sendFile() { return; }
   sendCode() { return; }
