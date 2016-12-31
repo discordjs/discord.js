5381c3214e05626648d108d976befe6d054669c2
Travis CI <amishshah.2k@gmail.com>
2016-12-31 02:05:40 +0000

    Webpack build: 2b256412709a807365c95dec9ea50f08e1009175

--- discord.master.js
+++ discord.master.js
@@ -11946,17 +11946,18 @@ const botGateway = __webpack_require__(0).Endpoints.botGateway;
 
 /**
  * Gets the recommended shard count from Discord
- * @param {number} token Discord auth token
+ * @param {string} token Discord auth token
+ * @param {number} [guildsPerShard=1000] Number of guilds per shard
  * @returns {Promise<number>} the recommended number of shards
  */
-module.exports = function fetchRecommendedShards(token) {
+module.exports = function fetchRecommendedShards(token, guildsPerShard = 1000) {
   return new Promise((resolve, reject) => {
     if (!token) throw new Error('A token must be provided.');
     superagent.get(botGateway)
       .set('Authorization', `Bot ${token.replace(/^Bot\s*/i, '')}`)
       .end((err, res) => {
         if (err) reject(err);
-        resolve(res.body.shards);
+        resolve(res.body.shards * (1000 / guildsPerShard));
       });
   });
 };
