6688942253b0528efc363fcef8df8618b66a6e73
Travis CI <amishshah.2k@gmail.com>
2017-01-02 06:53:56 +0000

    Webpack build: 2c26d79f505a5ab0f734b212e06bce566c138da2

--- discord.master.js
+++ discord.master.js
@@ -11096,14 +11096,7 @@ module.exports = RequestHandler;
 /* 53 */
 /***/ function(module, exports, __webpack_require__) {
 
-/* WEBPACK VAR INJECTION */(function(Buffer) {function arrayBufferToBuffer(ab) {
-  const buffer = new Buffer(ab.byteLength);
-  const view = new Uint8Array(ab);
-  for (var i = 0; i < buffer.length; ++i) buffer[i] = view[i];
-  return buffer;
-}
-
-function str2ab(str) {
+/* WEBPACK VAR INJECTION */(function(Buffer) {function str2ab(str) {
   const buffer = new ArrayBuffer(str.length * 2);
   const view = new Uint16Array(buffer);
   for (var i = 0, strLen = str.length; i < strLen; i++) view[i] = str.charCodeAt(i);
@@ -11112,7 +11105,7 @@ function str2ab(str) {
 
 module.exports = function convertArrayBuffer(x) {
   if (typeof x === 'string') x = str2ab(x);
-  return arrayBufferToBuffer(x);
+  return Buffer.from(x);
 };
 
 /* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(20).Buffer))
