1b781874f224d619ee87322ad3071de4633c4b4b
HyperCoder <HyperCoder2975@users.noreply.github.com>
2017-01-03 13:24:43 -0500

    To make hydrabolt happy

--- src/structures/Guild.js
+++ src/structures/Guild.js
@@ -106,7 +106,7 @@ class Guild {
      * Whether the guild is "large" (has more than 250 members)
      * @type {boolean}
      */
-    this.large = !!data.large;
+    this.large = (large in data ? !!data.large : !!this.large);
 
     /**
      * An array of guild features.
