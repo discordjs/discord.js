dcb53baa1a434f4f17124113e69733396cce3d10
Gus Caplan <gustavcaplan@gmail.com>
2017-01-12 12:36:03 -0600

    Update RESTMethods.js

--- src/client/rest/RESTMethods.js
+++ src/client/rest/RESTMethods.js
@@ -433,8 +433,7 @@ class RESTMethods {
     const data = {};
     data.name = _data.name || role.name;
     data.position = typeof _data.position !== 'undefined' ? _data.position : role.position;
-    data.color = _data.color || role.color;
-    data.color = this.client.resolver.resolveColor(data.color);
+    data.color = this.client.resolver.resolveColor(_data.color || role.color);
     data.hoist = typeof _data.hoist !== 'undefined' ? _data.hoist : role.hoist;
     data.mentionable = typeof _data.mentionable !== 'undefined' ? _data.mentionable : role.mentionable;
 
