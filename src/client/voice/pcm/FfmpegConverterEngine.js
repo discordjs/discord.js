35eb66a9326e107bfee31bc77cb63dae2e0b8e64
Zachary Vacura <zvacura@outlook.com>
2017-01-12 12:35:25 -0600

    Move things around

--- src/client/voice/pcm/FfmpegConverterEngine.js
+++ src/client/voice/pcm/FfmpegConverterEngine.js
@@ -42,11 +42,7 @@ class PCMConversionProcess extends EventEmitter {
 class FfmpegConverterEngine extends ConverterEngine {
   constructor(player) {
     super(player);
-    try {
-      this.command = require('ffmpeg-binaries').ffmpegPath();
-    } catch (err) {
-      this.command = chooseCommand();
-    }
+    this.command = chooseCommand();
   }
 
   handleError(encoder, err) {
@@ -71,13 +67,17 @@ class FfmpegConverterEngine extends ConverterEngine {
 }
 
 function chooseCommand() {
-  for (const cmd of ['ffmpeg', 'avconv', './ffmpeg', './avconv']) {
-    if (!ChildProcess.spawnSync(cmd, ['-h']).error) return cmd;
+  try {
+    return require('ffmpeg-binaries').ffmpegPath();
+  } catch (err) {
+    for (const cmd of ['ffmpeg', 'avconv', './ffmpeg', './avconv']) {
+      if (!ChildProcess.spawnSync(cmd, ['-h']).error) return cmd;
+    }
+    throw new Error(
+      'FFMPEG was not found on your system, so audio cannot be played. ' +
+      'Please make sure FFMPEG is installed and in your PATH.'
+    );
   }
-  throw new Error(
-    'FFMPEG was not found on your system, so audio cannot be played. ' +
-    'Please make sure FFMPEG is installed and in your PATH.'
-  );
 }
 
 module.exports = FfmpegConverterEngine;
