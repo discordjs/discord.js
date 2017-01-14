df02eee065ba0de674e3f12f019000f24b78154e
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2017-01-13 21:47:47 -0500

    Fix Guild#large not being accurate for user accounts

--- src/client/actions/GuildSync.js
+++ src/client/actions/GuildSync.js
@@ -6,20 +6,22 @@ class GuildSync extends Action {
 
     const guild = client.guilds.get(data.id);
     if (guild) {
-      data.presences = data.presences || [];
-      for (const presence of data.presences) {
-        guild._setPresence(presence.user.id, presence);
+      if (data.presences) {
+        for (const presence of data.presences) guild._setPresence(presence.user.id, presence);
       }
 
-      data.members = data.members || [];
-      for (const syncMember of data.members) {
-        const member = guild.members.get(syncMember.user.id);
-        if (member) {
-          guild._updateMember(member, syncMember);
-        } else {
-          guild._addMember(syncMember, false);
+      if (data.members) {
+        for (const syncMember of data.members) {
+          const member = guild.members.get(syncMember.user.id);
+          if (member) {
+            guild._updateMember(member, syncMember);
+          } else {
+            guild._addMember(syncMember, false);
+          }
         }
       }
+
+      if ('large' in data) guild.large = data.large;
     }
   }
 }
