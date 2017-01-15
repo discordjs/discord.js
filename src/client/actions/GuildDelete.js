87f503f2e37db7a2f21ccab7a18ea5c763234b9d
Schuyler Cebulskie <Gawdl3y@gmail.com>
2017-01-14 23:30:47 -0500

    Update GuildDelete.js

--- src/client/actions/GuildDelete.js
+++ src/client/actions/GuildDelete.js
@@ -13,7 +13,7 @@ class GuildDeleteAction extends Action {
     let guild = client.guilds.get(data.id);
     if (guild) {
       for (const channel of guild.channels.values()) {
-        if (channel.type !== 'voice') channel.stopTyping(true);
+        if (channel.type === 'text') channel.stopTyping(true);
       }
 
       if (guild.available && data.unavailable) {
