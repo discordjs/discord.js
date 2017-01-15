58f089c218a057e3b94b63df5a3aadc6aea2387f
Programmix <programmixdev@gmail.com>
2017-01-14 20:32:17 -0800

    Add VoiceChannel#full and improve joinable/join permission checks (#1100)
    * Improve voice channel join permission checks
    
    * Update ClientVoiceManager.js

--- src/client/voice/ClientVoiceManager.js
+++ src/client/voice/ClientVoiceManager.js
@@ -79,7 +79,13 @@ class ClientVoiceManager {
   joinChannel(channel) {
     return new Promise((resolve, reject) => {
       if (this.pending.get(channel.guild.id)) throw new Error('Already connecting to this guild\'s voice server.');
-      if (!channel.joinable) throw new Error('You do not have permission to join this voice channel.');
+      if (!channel.joinable) {
+        if (channel.full) {
+          throw new Error('You do not have permission to join this voice channel; it is full.');
+        } else {
+          throw new Error('You do not have permission to join this voice channel.');
+        }
+      }
 
       const existingConnection = this.connections.get(channel.guild.id);
       if (existingConnection) {
