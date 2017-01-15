d10ca8e7baf62da1d8472901c83d02992d101d2e
Programmix <programmixdev@gmail.com>
2017-01-14 20:32:17 -0800

    Add VoiceChannel#full and improve joinable/join permission checks (#1100)
    * Improve voice channel join permission checks
    
    * Update ClientVoiceManager.js

--- src/structures/VoiceChannel.js
+++ src/structures/VoiceChannel.js
@@ -46,12 +46,22 @@ class VoiceChannel extends GuildChannel {
   }
 
   /**
+   * Checks if the voice channel is full
+   * @type {boolean}
+   */
+  get full() {
+    return this.members.size >= this.userLimit;
+  }
+
+  /**
    * Checks if the client has permission join the voice channel
    * @type {boolean}
    */
   get joinable() {
     if (this.client.browser) return false;
-    return this.permissionsFor(this.client.user).hasPermission('CONNECT');
+    if (!this.permissionsFor(this.client.user).hasPermission('CONNECT')) return false;
+    if (this.full && !this.permissionsFor(this.client.user).hasPermission('MOVE_MEMBERS')) return false;
+    return true;
   }
 
   /**
