7bd46d23c2b5c7dbc97698cc140a5135fb296283
Peter Vu <me@petervu.ca>
2017-01-16 22:59:16 -0500

    VoiceChannel#full fix on no limit channel for joinable

--- src/structures/VoiceChannel.js
+++ src/structures/VoiceChannel.js
@@ -50,7 +50,7 @@ class VoiceChannel extends GuildChannel {
    * @type {boolean}
    */
   get full() {
-    return this.members.size >= this.userLimit;
+    return this.members.size >= this.userLimit && this.userLimit !== 0;
   }
 
   /**
