a4d2e18d623f5c167559e6d0b3afede052dd8821
Schuyler Cebulskie <Gawdl3y@gmail.com>
2017-01-18 18:39:42 -0500

    Update VoiceRegion.js

--- src/structures/VoiceRegion.js
+++ src/structures/VoiceRegion.js
@@ -1,46 +1,49 @@
+/**
+ * Represents a Discord voice region for guilds
+ */
 class VoiceRegion {
   constructor(data) {
     /**
-     * A sample hostname for what a connection might look like
-     * @type {string}
-     */
-    this.sampleHostname = data.sample_hostname;
-
-    /**
-     * If this voice region is VIP
-     * @type {boolean}
-     */
-    this.vip = data.vip;
-
-    /**
-     * The name of this voice region
-     * @type {string}
-     */
-    this.name = data.name;
-
-    /**
-     * If this voice region is deprecated
-     * @type {boolean}
-     */
-    this.deprecated = data.deprecated;
-
-    /**
-     * If this voice region is optimal
-     * @type {boolean}
-     */
-    this.optimal = data.optimal;
-
-    /**
-     * The ID of this voice region
+     * ID of the region
      * @type {string}
      */
     this.id = data.id;
 
     /**
-     * If this is a custom voice region
+     * Name of the region
+     * @type {string}
+     */
+    this.name = data.name;
+
+    /**
+     * Whether the region is VIP-only
+     * @type {boolean}
+     */
+    this.vip = data.vip;
+
+    /**
+     * Whether the region is deprecated
+     * @type {boolean}
+     */
+    this.deprecated = data.deprecated;
+
+    /**
+     * Whether the region is optimal
+     * @type {boolean}
+     */
+    this.optimal = data.optimal;
+
+    /**
+     * Whether the region is custom
      * @type {boolean}
      */
     this.custom = data.custom;
+
+    /**
+     * A sample hostname for what a connection might look like
+     * @type {string}
+     */
+    this.sampleHostname = data.sample_hostname;
   }
 }
 
