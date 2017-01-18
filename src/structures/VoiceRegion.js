e9c5eb7a14de5a0059a5119190f9f84724d92a35
Gus Caplan <me@gus.host>
2017-01-18 15:33:03 -0600

    add docstrings

--- src/structures/VoiceRegion.js
+++ src/structures/VoiceRegion.js
@@ -1,11 +1,45 @@
 class VoiceRegion {
   constructor(data) {
+    /**
+     * A sample hostname for what a connection might look like
+     * @type {string}
+     */
     this.sampleHostname = data.sample_hostname;
+
+    /**
+     * If this voice region is VIP
+     * @type {boolean}
+     */
     this.vip = data.vip;
+
+    /**
+     * The name of this voice region
+     * @type {string}
+     */
     this.name = data.name;
+
+    /**
+     * If this voice region is deprecated
+     * @type {boolean}
+     */
     this.deprecated = data.deprecated;
+
+    /**
+     * If this voice region is optimal
+     * @type {boolean}
+     */
     this.optimal = data.optimal;
+
+    /**
+     * The ID of this voice region
+     * @type {string}
+     */
     this.id = data.id;
+
+    /**
+     * If this is a custom voice region
+     * @type {boolean}
+     */
     this.custom = data.custom;
   }
 }
