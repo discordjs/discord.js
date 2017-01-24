3814d78d7d9994fcc729f142d9f906cf0f66b2f7
Alexander <slamakanss@gmail.com>
2017-01-24 22:55:59 +0100

    Fix empty search query parameters (#1119)
    The search function was sending request to stuff like:
    search?author_id=&content=&channel_id=135823828352838352

--- src/client/rest/RESTMethods.js
+++ src/client/rest/RESTMethods.js
@@ -165,8 +165,9 @@ class RESTMethods {
 
   search(target, options) {
     options = transformSearchOptions(options, this.client);
+    for (const key in options) if (options[key] === undefined) delete options[key];
 
-    const queryString = querystring.stringify(options);
+    const queryString = (querystring.stringify(options).match(/[^=&?]+=[^=&?]+/g) || []).join('&');
 
     let type;
     if (target instanceof Channel) {
