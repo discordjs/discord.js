8b8154f8322f962b57af504ebe18284f997645e0
Schuyler Cebulskie <Gawdl3y@gmail.com>
2017-02-02 15:37:24 -0500

    Update GuildChannel.js

--- src/structures/GuildChannel.js
+++ src/structures/GuildChannel.js
@@ -250,10 +250,10 @@ class GuildChannel extends Channel {
    * Clone this channel
    * @param {string} [name=this.name] Optional name for the new channel, otherwise it has the name of this channel
    * @param {boolean} [withPermissions=true] Whether to clone the channel with this channel's permission overwrites
-   * @param {boolean} [withTopic=false] Whether to clone the channel with this channel's topic
+   * @param {boolean} [withTopic=true] Whether to clone the channel with this channel's topic
    * @returns {Promise<GuildChannel>}
    */
-  clone(name = this.name, withPermissions = true, withTopic = false) {
+  clone(name = this.name, withPermissions = true, withTopic = true) {
     return this.guild.createChannel(name, this.type, withPermissions ? this.permissionOverwrites : [])
       .then(channel => withTopic ? channel.setTopic(this.topic) : channel);
   }
