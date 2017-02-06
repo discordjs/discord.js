37a22e8333d4a41f83925308cd48e82c30373576
Gus Caplan <gustavcaplan@gmail.com>
2017-02-05 21:56:56 -0600

    Update StreamDispatcher.js

--- src/client/voice/dispatcher/StreamDispatcher.js
+++ src/client/voice/dispatcher/StreamDispatcher.js
@@ -149,7 +149,7 @@ class StreamDispatcher extends VolumeInterface {
     packetBuffer.writeUIntBE(this.player.voiceConnection.authentication.ssrc, 8, 4);
 
     packetBuffer.copy(nonce, 0, 0, 12);
-    buffer = secretbox.close(buffer, nonce, this.player.voiceConnection.authentication.secretKey.key)
+    buffer = secretbox.close(buffer, nonce, this.player.voiceConnection.authentication.secretKey.key);
     for (let i = 0; i < buffer.length; i++) packetBuffer[i + 12] = buffer[i];
 
     return packetBuffer;
