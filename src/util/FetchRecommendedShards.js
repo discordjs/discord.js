4bdef5c2e300a92f44f78dcc4faabfad858c168e
Gus Caplan <gustavcaplan@gmail.com>
2017-02-05 21:50:14 -0600

    Update FetchRecommendedShards.js

--- src/util/FetchRecommendedShards.js
+++ src/util/FetchRecommendedShards.js
@@ -7,14 +7,14 @@ const botGateway = require('./Constants').Endpoints.botGateway;
  * @param {number} [guildsPerShard=1000] Number of guilds per shard
  * @returns {Promise<number>} the recommended number of shards
  */
-module.exports = function fetchRecommendedShards(token, guildsPerShard = 1000) {
+module.exports = function fetchRecommendedShards(token) {
   return new Promise((resolve, reject) => {
     if (!token) throw new Error('A token must be provided.');
     superagent.get(botGateway)
       .set('Authorization', `Bot ${token.replace(/^Bot\s*/i, '')}`)
       .end((err, res) => {
         if (err) reject(err);
-        resolve(res.body.shards * (1000 / guildsPerShard));
+        resolve(res.body.shards);
       });
   });
 };
