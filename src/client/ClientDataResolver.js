c55d63bcec1b780ff540dc2e4ee3d635010cf40c
Schuyler Cebulskie <Gawdl3y@gmail.com>
2017-02-07 01:12:32 -0500

    Update ClientDataResolver.js

--- src/client/ClientDataResolver.js
+++ src/client/ClientDataResolver.js
@@ -361,7 +361,7 @@ class ClientDataResolver {
    */
   static resolveColor(color) {
     if (typeof color === 'string') {
-      if (color === 'RANDOM') return Math.floor(Math.random() * 16777214);
+      if (color === 'RANDOM') return Math.floor(Math.random() * (0xFFFFFF + 1));
       color = Constants.Colors[color] || parseInt(color.replace('#', ''), 16);
     } else if (color instanceof Array) {
       color = (color[0] << 16) + (color[1] << 8) + color[2];
