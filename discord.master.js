43cbff37b3967de437f962cf60b666fabb429bf8
Travis CI <amishshah.2k@gmail.com>
2017-02-22 20:25:51 +0000

    Webpack build for branch master: 5c2086b351afe85d81d154dd5805e8562296e751

--- discord.master.js
+++ discord.master.js
@@ -9590,6 +9590,8 @@ class ClientUser extends User {
         if (game.url) game.type = 1;
       }
 
+      if (data.game === null) game = null;
+
       if (typeof data.afk !== 'undefined') afk = data.afk;
       afk = Boolean(afk);
 
@@ -9628,11 +9630,12 @@ class ClientUser extends User {
 
   /**
    * Sets the game the client user is playing.
-   * @param {string} game Game being played
+   * @param {?string} game Game being played
    * @param {string} [streamingURL] Twitch stream URL
    * @returns {Promise<ClientUser>}
    */
   setGame(game, streamingURL) {
+    if (game === null) return this.setPresence({ game });
     return this.setPresence({ game: {
       name: game,
       url: streamingURL,
