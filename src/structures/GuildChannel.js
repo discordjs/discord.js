f22e8c19f76a0e34bba85fc5b0a9e12124f2c250
SpaceEEC <wermail2002-kap@yahoo.de>
2017-02-25 15:46:11 +0100

    updated descripton

--- src/structures/GuildChannel.js
+++ src/structures/GuildChannel.js
@@ -235,7 +235,7 @@ class GuildChannel extends Channel {
    * Options given when creating a guild channel invite
    * @typedef {Object} InviteOptions
    * @property {boolean} [temporary=false] Whether the invite should kick users after 24hrs if they are not given a role
-   * @property {number} [maxAge=86400] Time in seconds the invite expires in, 0 for never, default is one day
+   * @property {number} [maxAge=86400] Time in seconds the invite expires in, the default value is one day. 0 for never
    * @property {number} [maxUses=0] Maximum amount of uses for this invite
    */
 
