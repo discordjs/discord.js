74d2a881e6a27d483047b69349e40ceaecef2b9a
Programmix <programmixdev@gmail.com>
2017-02-25 20:28:21 -0800

    Wrong open method name

--- src/client/voice/util/Secretbox.js
+++ src/client/voice/util/Secretbox.js
@@ -1,7 +1,7 @@
 try {
   const sodium = require('sodium');
   module.exports = {
-    open: sodium.api.crypto_secretbox_easy_open,
+    open: sodium.api.crypto_secretbox_open_easy,
     close: sodium.api.crypto_secretbox_easy,
   };
 } catch (err) {
