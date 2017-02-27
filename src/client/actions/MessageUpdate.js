cd6a69c700a05f099010ff4c11f6f6d3eadb5e3c
bdistin <bdistin@gmail.com>
2017-02-27 15:09:16 -0600

    [requires more testing] Fix #1089

--- src/client/actions/MessageUpdate.js
+++ src/client/actions/MessageUpdate.js
@@ -1,6 +1,6 @@
 const Action = require('./Action');
 const Constants = require('../../util/Constants');
-const Util = require('../../util/Util');
+const Message = require('../../structures/Message');
 
 class MessageUpdateAction extends Action {
   handle(data) {
@@ -10,13 +10,14 @@ class MessageUpdateAction extends Action {
     if (channel) {
       const message = channel.messages.get(data.id);
       if (message) {
-        const oldMessage = Util.cloneObject(message);
-        message.patch(data);
-        message._edits.unshift(oldMessage);
-        client.emit(Constants.Events.MESSAGE_UPDATE, oldMessage, message);
+        const newMessage = new Message(message.channel, data, client);
+        newMessage._edits.push(...message._edits);
+        newMessage._edits.unshift(message);
+        channel.messages.set(data.id, newMessage);
+        client.emit(Constants.Events.MESSAGE_UPDATE, message, newMessage);
         return {
-          old: oldMessage,
-          updated: message,
+          old: message,
+          updated: newMessage,
         };
       }
 
