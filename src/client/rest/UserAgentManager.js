7128d1fd7d45445a7cf3ef16abe540e9e50ddf01
Gus Caplan <gustavcaplan@gmail.com>
2017-03-01 23:17:11 -0600

    Update UserAgentManager.js

--- src/client/rest/UserAgentManager.js
+++ src/client/rest/UserAgentManager.js
@@ -1,21 +1,28 @@
 const Constants = require('../../util/Constants');
 
 class UserAgentManager {
-  constructor(restManager) {
-    this.restManager = restManager;
-    this._userAgent = {
-      url: 'https://github.com/hydrabolt/discord.js',
+  constructor() {
+    this._default = {
+      url: Constants.Package.homepage.split('#')[0],
       version: Constants.Package.version,
+    };
+    this._override = {};
+  }
+
+  set({ url, version } = {}) {
+    this._override.url = url;
+    this._override.version = version;
+  }
+
+  get _current() {
+    return {
+      url: this._override.url || this._default.url,
+      version: this._override.version || this._default.version,
     };
   }
 
-  set(info) {
-    this._userAgent.url = info.url || 'https://github.com/hydrabolt/discord.js';
-    this._userAgent.version = info.version || Constants.Package.version;
-  }
-
   get userAgent() {
-    return `DiscordBot (${this._userAgent.url}, ${this._userAgent.version})`;
+    return `DiscordBot (${this._current.url}, ${this._current.version}) Node.js/${process.version}`;
   }
 }
 
