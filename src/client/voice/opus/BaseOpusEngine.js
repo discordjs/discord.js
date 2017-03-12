880c5bc2b9eae3b78637d746a3a1f9ba83e6e844
Programmix <programmixdev@gmail.com>
2017-03-11 19:18:11 -0700

    Add FEC and PLP support for Opus encoding

--- src/client/voice/opus/BaseOpusEngine.js
+++ src/client/voice/opus/BaseOpusEngine.js
@@ -1,6 +1,19 @@
 class BaseOpus {
   constructor(player) {
     this.player = player;
+
+    this.ctl = {
+      FEC: 4012,
+      PLP: 4014,
+    };
+  }
+
+  init() {
+    // enable FEC (forward error correction)
+    this.setFEC(true);
+
+    // set PLP (expected packet loss percentage) to 15%
+    this.setPLP(0.15);
   }
 
   encode(buffer) {
