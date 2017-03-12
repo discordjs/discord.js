59616cbb5c123a98350e32b8b2185f6e6ae52730
Programmix <programmixdev@gmail.com>
2017-03-12 10:04:49 -0700

    Silently fail if the Opus engine doesn't support CTL

--- src/client/voice/opus/BaseOpusEngine.js
+++ src/client/voice/opus/BaseOpusEngine.js
@@ -9,11 +9,15 @@ class BaseOpus {
   }
 
   init() {
-    // enable FEC (forward error correction)
-    this.setFEC(true);
+    try {
+      // enable FEC (forward error correction)
+      this.setFEC(true);
 
-    // set PLP (expected packet loss percentage) to 15%
-    this.setPLP(0.15);
+      // set PLP (expected packet loss percentage) to 15%
+      this.setPLP(0.15);
+    } catch (err) {
+      // Opus engine likely has no support for libopus CTL
+    }
   }
 
   encode(buffer) {
