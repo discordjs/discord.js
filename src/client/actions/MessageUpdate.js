07224e35e356b21d63733377e3631b2321d337cc
bdistin <bdistin@gmail.com>
2017-03-14 21:03:39 -0500

    Some Data Packets come Incomplete
    And several properties can be falsy, so instead of || opted for hasOwnProperty().

--- src/client/actions/MessageUpdate.js
+++ src/client/actions/MessageUpdate.js
@@ -10,7 +10,7 @@ class MessageUpdateAction extends Action {
     if (channel) {
       const message = channel.messages.get(data.id);
       if (message) {
-        const newMessage = new Message(message.channel, data, client);
+        const newMessage = new Message(message.channel, this.patchDataPacket(data, message), client);
         newMessage._edits.push(message, ...message._edits);
         newMessage.reactions = message.reactions;
         channel.messages.set(data.id, newMessage);
@@ -32,6 +32,29 @@ class MessageUpdateAction extends Action {
       updated: null,
     };
   }
+
+  patchDataPacket(data, message) {
+    data.type = data.hasOwnProperty('type') ? data.type : Constants.MessageTypes.indexOf(message.type);
+    data.tts = data.hasOwnProperty('tts') ? data.tts : message.tts;
+    data.timestamp = data.hasOwnProperty('timestamp') ? data.timestamp : message.createdAt.toString();
+    data.pinned = data.hasOwnProperty('pinned') ? data.pinned : message.pinned;
+    data.nonce = data.hasOwnProperty('nonce') ? data.nonce : message.nonce;
+    data.mentions = data.hasOwnProperty('mentions') ? data.mentions : message.mentions.users.keyArray();
+    data.mentions_roles = data.hasOwnProperty('mentions_roles') ?
+			data.mentions_roles : message.mentions.roles.keyArray();
+    data.mention_everyone = data.hasOwnProperty('mention_everyone') ? data.mention_everyone : message.mentions.everyone;
+    data.embeds = data.hasOwnProperty('embeds') ? data.embeds : message.embeds;
+    data.content = data.hasOwnProperty('content') ? data.content : message.content;
+    data.author = data.hasOwnProperty('author') ? data.author : {
+      username: message.author.username,
+      id: message.author.id,
+      discriminator: message.author.discriminator,
+      avatar: message.author.avatar,
+    };
+    data.attachments = data.hasOwnProperty('attachments') ? data.attachments : message.attachments.array();
+    return data;
+  }
+
 }
 
 /**
