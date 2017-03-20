c2f7185972f47108c3e4753fb8eb8c03dc006525
Pg Biel <pgbiel@hotmail.com.br>
2017-03-19 21:43:05 -0300

    oops

--- src/structures/interface/TextBasedChannel.js
+++ src/structures/interface/TextBasedChannel.js
@@ -191,7 +191,9 @@ class TextBasedChannel {
    *   .catch(console.error);
    */
   fetchMessage(messageID) {
-    if (!this.client.user.bot) return this.fetchMessages({ limit: 1, around: messageID });
+    if (!this.client.user.bot) {
+      return this.fetchMessages({ limit: 1, around: messageID }).then(messageCol => messageCol.first());
+    }
     return this.client.rest.methods.getChannelMessage(this, messageID).then(data => {
       const msg = data instanceof Message ? data : new Message(this, data, this.client);
       this._cacheMessage(msg);
