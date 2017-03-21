7570a89bf0e8a0e9ffa32ef4c6b8ca7995ee9d4f
Schuyler Cebulskie <Gawdl3y@gmail.com>
2017-03-21 03:08:42 -0400

    Update UserAgentManager.js

--- src/client/rest/UserAgentManager.js
+++ src/client/rest/UserAgentManager.js
@@ -2,17 +2,17 @@ const Constants = require('../../util/Constants');
 
 class UserAgentManager {
   constructor() {
-    this._build(Object.assign({}, this.constructor.DEFAULT));
+    this.build(Object.assign({}, this.constructor.DEFAULT));
   }
 
   set({ url, version } = {}) {
-    this._build({
+    this.build({
       url: url || this.constructor.DFEAULT.url,
       version: version || this.constructor.DEFAULT.version,
     });
   }
   
-  _build(ua) {
+  build(ua) {
     this.userAgent = `DiscordBot (${ua.url}, ${ua.version}) Node.js/${process.version}`;
   }
 }
