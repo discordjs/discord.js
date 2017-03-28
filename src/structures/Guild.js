14125f03bd35489c1c1f83a6aa0745f5bb4dc28a
Gus Caplan <me@gus.host>
2017-03-28 00:12:50 -0500

    (╯°□°）╯︵ ┻━┻

--- src/structures/Guild.js
+++ src/structures/Guild.js
@@ -278,12 +278,21 @@ class Guild {
   }
 
   /**
-   * The `#general` TextChannel of the server.
+   * The `#general` TextChannel of the guild.
    * @type {TextChannel}
    * @readonly
    */
   get defaultChannel() {
     return this.channels.get(this.id);
+  }
+  
+  /**
+   * The `@everyone` Role of the guild.
+   * @type {Role}
+   * @readonly
+   */
+  get defaultRole() {
+    return this.roles.get(this.id);
   }
 
   /**
