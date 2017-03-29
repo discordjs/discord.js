e164a96e0da52c72d35f7d8b8c3ef42187c477b0
Vap0r1ze <superdash993@gmail.com>
2017-03-29 07:50:48 -0400

    Update APIRequest.js

--- src/client/rest/APIRequest.js
+++ src/client/rest/APIRequest.js
@@ -1,5 +1,7 @@
 const request = require('superagent');
 const Constants = require('../../util/Constants');
+const os = require('os');
+const version = require('../../../package').version;
 
 class APIRequest {
   constructor(rest, method, url, auth, data, files) {
@@ -31,9 +33,30 @@ class APIRequest {
     throw new Error(Constants.Errors.NO_TOKEN);
   }
 
+  getCtx(ctx) {
+    return new Buffer(JSON.stringify({
+      location: ctx,
+    })).toString('base64'));
+  }
+
+  getSuper() {
+    const super = {
+      os: os.type(),
+      browser: 'DiscordJS',
+      client_version: version,
+      os_version: os.release(),
+    };
+    if (super.os === 'Windows_NT') super.os = 'Windows';
+    return new Buffer(JSON.stringify(super)).toString('base64'));
+  }
+
   gen() {
     const apiRequest = request[this.method](this.url);
     if (this.auth) apiRequest.set('authorization', this.getAuth());
+    if (!this.getAuth().startsWith('Bot')) {
+      apiRequest.set('x-super-properties', this.getSuper());
+      if (this.ctx) apiRequest.set('x-context-properties', this.getCtx(this.ctx));
+    }
     if (this.files) {
       for (const file of this.files) if (file && file.file) apiRequest.attach(file.name, file.file, file.name);
       this.data = this.data || {};
