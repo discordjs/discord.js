dc902b279a6b715be825782c61c3437858ede21e
snek <me@gus.host>
2017-03-29 16:41:49 -0500

    modularize

--- src/structures/ClientUserSettings.js
+++ src/structures/ClientUserSettings.js
@@ -8,117 +8,14 @@ class ClientUserSettings {
   }
 
   patch(data) {
-    const has = data.hasOwnProperty.bind(data);
-    /**
-     * Automatically convert emoticons in your messages to emoji.
-     * For example, when you type `:-)` Discord will convert it to 😃
-     * @type {boolean}
-     */
-    if (has('convert_emoticons')) this.convertEmoticons = data.convertEmoticons;
-
-    /**
-     * If new guilds should automatically disable DMs between you and its members
-     * @type {boolean}
-     */
-    if (has('default_guilds_restricted')) this.defaultGuildsRestricted = data.default_guilds_restricted;
-
-    /**
-     * Automatically detect accounts from services like Steam and Blizzard when you open the Discord client
-     * @type {boolean}
-     */
-    if (has('detect_platform_accounts')) this.detectPlatformAccounts = data.detect_platform_accounts;
-
-    /**
-     * Developer Mode exposes context menu items helpful for people writing bots using the Discord API
-     * @type {boolean}
-     */
-    if (has('developer_mode')) this.developerMode = data.developer_mode;
-
-    /**
-     * Allow playback and usage of the `/tts` command
-     * @type {boolean}
-     */
-    if (has('enable_tts_command')) this.enableTTSCommand = data.enable_tts_command;
-
-    /**
-     * The theme of the client. Either `light` or `dark`
-     * @type {String}
-     */
-    if (has('theme')) this.theme = data.theme;
-
-    /**
-     * Last status set in the client
-     * @type {PresenceStatus}
-     */
-    if (has('status')) this.status = data.status;
-
-    /**
-     * Display currently running game as status message
-     */
-    if (has('show_current_game')) this.showCurrentGame = data.show_current_game;
-
-    /**
-     * Display images, videos, and lolcats when uploaded directly to Discord
-     * @type {boolean}
-     */
-    if (has('inline_attachment_media')) this.inlineAttachmentMedia = data.inline_attachment_media;
-
-    /**
-     * Display images, videos, and lolcats when uploaded posted as links in chat
-     * @type {boolean}
-     */
-    if (has('inline_embed_media')) this.inlineEmbedMedia = data.inline_embed_media;
-
-    /**
-     * Language the Discord client will use, as an RFC 3066 language identifier
-     * @type {string}
-     */
-    if (has('locale')) this.locale = data.locale;
-
-    /**
-     * Display messages in compact mode
-     * @type {boolean}
-     */
-    if (has('message_display_compact')) this.messageDisplayCompact = data.message_display_compact;
-
-    /**
-     * Show emoji reactions on messages
-     * @type {boolean}
-     */
-    if (has('render_reactions')) this.renderReactions = data.render_reactions;
-
-    /**
-     * Array of snowflake IDs for guilds, in the order they appear in the Discord client
-     * @type {Snowflake[]}
-     */
-    if (has('guild_positions')) this.guildPositions = data.guild_positions;
-
-    /**
-     * Array of snowflake IDs for guilds which you will not recieve DMs from
-     * @type {Snowflake[]}
-     */
-    if (has('restricted_guilds')) this.restrictedGuilds = data.restricted_guilds;
-
-    if (has('explicit_content_filter')) {
-      /**
-       * Safe direct messaging; force people's messages with images to be scanned before they are sent to you
-       * one of `DISABLED`, `NON_FRIENDS`, `FRIENDS_AND_NON_FRIENDS`
-       * @type {string}
-       */
-      this.explicitContentFilter = Constants.ExplicitContentFilterTypes[data.explicit_content_filter];
-    }
-
-    if (has('friend_source_flags')) {
-      const flags = data.friend_source_flags;
-      /**
-       * Who can add you as a friend
-       * @type {Object}
-       */
-      this.friendSources = {
-        all: flags.all || false,
-        mutualGuilds: flags.all ? true : flags.mutual_guilds || false,
-        mutualFriends: flags.all ? true : flags.mutualFriends || false,
-      };
+    for (const key of Object.keys(Constants.UserSettingsMap)) {
+      const value = Constants.UserSettingsMap[key];
+      if (!data.hasOwnProperty(key)) continue;
+      if (typeof value === 'function') {
+        this[value.name] = value(data[key]);
+      } else {
+        this[value] = data[key];
+      }
     }
   }
 
--- src/util/Constants.js
+++ src/util/Constants.js
@@ -382,6 +382,136 @@ exports.ExplicitContentFilterTypes = [
   'FRIENDS_AND_NON_FRIENDS',
 ];
 
+exports.UserSettingsMap = {
+  /**
+   * Automatically convert emoticons in your messages to emoji.
+   * For example, when you type `:-)` Discord will convert it to 😃
+   * @name ClientUserSettings#convertEmoticons
+   * @type {boolean}
+   */
+  convert_emoticons: 'convertEmoticons',
+
+  /**
+   * If new guilds should automatically disable DMs between you and its members
+   * @name ClientUserSettings#defaultGuildsRestricted
+   * @type {boolean}
+   */
+  default_guilds_restricted: 'defaultGuildsRestricted',
+
+  /**
+   * Automatically detect accounts from services like Steam and Blizzard when you open the Discord client
+   * @name ClientUserSettings#detectPlatformAccounts
+   * @type {boolean}
+   */
+  detect_platform_accounts: 'detectPlatformAccounts',
+
+  /**
+   * Developer Mode exposes context menu items helpful for people writing bots using the Discord API
+   * @name ClientUserSettings#developerMode
+   * @type {boolean}
+   */
+  developer_mode: 'developerMode',
+
+  /**
+   * Allow playback and usage of the `/tts` command
+   * @name ClientUserSettings#enableTTSCommand
+   * @type {boolean}
+   */
+  enable_tts_command: 'enableTTSCommand',
+
+  /**
+   * The theme of the client. Either `light` or `dark`
+   * @name ClientUserSettings#theme
+   * @type {String}
+   */
+  theme: 'theme',
+
+  /**
+   * Last status set in the client
+   * @name ClientUserSettings#status
+   * @type {PresenceStatus}
+   */
+  status: 'status',
+
+  /**
+   * Display currently running game as status message
+   * @name ClientUserSettings#showCurrentGame
+   * @type {boolean}
+   */
+  show_current_game: 'showCurrentGame',
+
+  /**
+   * Display images, videos, and lolcats when uploaded directly to Discord
+   * @name ClientUserSettings#inlineAttachmentMedia
+   * @type {boolean}
+   */
+  inline_attachment_media: 'inlineAttachmentMedia',
+
+  /**
+   * Display images, videos, and lolcats when uploaded posted as links in chat
+   * @name ClientUserSettings#inlineEmbedMedia
+   * @type {boolean}
+   */
+  inline_embed_media: 'inlineEmbedMedia',
+
+  /**
+   * Language the Discord client will use, as an RFC 3066 language identifier
+   * @name ClientUserSettings#locale
+   * @type {string}
+   */
+  locale: 'locale',
+
+  /**
+   * Display messages in compact mode
+   * @name ClientUserSettings#messageDisplayCompact
+   * @type {boolean}
+   */
+  message_display_compact: 'messageDisplayCompact',
+
+  /**
+   * Show emoji reactions on messages
+   * @name ClientUserSettings#renderReactions
+   * @type {boolean}
+   */
+  render_reactions: 'renderReactions',
+
+  /**
+   * Array of snowflake IDs for guilds, in the order they appear in the Discord client
+   * @name ClientUserSettings#guildPositions
+   * @type {Snowflake[]}
+   */
+  guild_positions: 'guildPositions',
+
+  /**
+   * Array of snowflake IDs for guilds which you will not recieve DMs from
+   * @name ClientUserSettings#restrictedGuilds
+   * @type {Snowflake[]}
+   */
+  restricted_guilds: 'restrictedGuilds',
+
+  explicit_content_filter: function explicitContentFilter(type) { // eslint-disable-line func-name-matching
+    /**
+     * Safe direct messaging; force people's messages with images to be scanned before they are sent to you
+     * one of `DISABLED`, `NON_FRIENDS`, `FRIENDS_AND_NON_FRIENDS`
+     * @name ClientUserSettings#explicitContentFilter
+     * @type {string}
+     */
+    return exports.ExplicitContentFilterTypes[type];
+  },
+  friend_source_flags: function friendSources(flags) { // eslint-disable-line func-name-matching
+    /**
+     * Who can add you as a friend
+     * @name ClientUserSettings#friendSources
+     * @type {Object}
+     */
+    return {
+      all: flags.all || false,
+      mutualGuilds: flags.all ? true : flags.mutual_guilds || false,
+      mutualFriends: flags.all ? true : flags.mutualFriends || false,
+    };
+  },
+};
+
 exports.Colors = {
   DEFAULT: 0x000000,
   AQUA: 0x1ABC9C,
