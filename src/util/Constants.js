973b9725f1f9d406a4d5347cfd930bd48a650def
Gus Caplan <me@gus.host>
2017-03-30 15:23:25 -0500

    Update Constants.js

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -500,9 +500,17 @@ exports.UserSettingsMap = {
   },
   friend_source_flags: function friendSources(flags) { // eslint-disable-line func-name-matching
     /**
+     * Properties for friend sources
+     * @typedef {Object} ClientUserSettingsFriendSources
+     * @property {boolean} all Mutual friends and mutual guilds
+     * @property {boolean} mutualGuilds Only mutual guilds
+     * @property {boolean} mutualFriends Only mutual friends
+     */
+
+    /**
      * Who can add you as a friend
      * @name ClientUserSettings#friendSources
-     * @type {Object}
+     * @type {ClientUserSettingsFriendSources}
      */
     return {
       all: flags.all || false,
