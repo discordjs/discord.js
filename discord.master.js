94ec01f9dbe8767e5d47db711c67b9e8eae2d1f6
Travis CI <amishshah.2k@gmail.com>
2017-04-01 14:17:12 +0000

    Webpack build for branch master: d2e3d6dc637969729daf49d8d5fec9d7aa447130

--- discord.master.js
+++ discord.master.js
@@ -16799,7 +16799,7 @@ class RESTMethods {
   }
 
   deleteMessage(message) {
-    return this.rest.makeRequest('del', Endpoints.Message(message), true)
+    return this.rest.makeRequest('delete', Endpoints.Message(message), true)
       .then(() =>
         this.client.actions.MessageDelete.handle({
           id: message.id,
@@ -16946,7 +16946,7 @@ class RESTMethods {
   deleteChannel(channel) {
     if (channel instanceof User || channel instanceof GuildMember) channel = this.getExistingDM(channel);
     if (!channel) return Promise.reject(new Error('No channel to delete.'));
-    return this.rest.makeRequest('del', Endpoints.Channel(channel), true).then(data => {
+    return this.rest.makeRequest('delete', Endpoints.Channel(channel), true).then(data => {
       data.id = channel.id;
       return this.client.actions.ChannelDelete.handle(data).channel;
     });
@@ -16966,7 +16966,7 @@ class RESTMethods {
 
   leaveGuild(guild) {
     if (guild.ownerID === this.client.user.id) return Promise.reject(new Error('Guild is owned by the client.'));
-    return this.rest.makeRequest('del', Endpoints.User('@me').Guild(guild.id), true).then(() =>
+    return this.rest.makeRequest('delete', Endpoints.User('@me').Guild(guild.id), true).then(() =>
       this.client.actions.GuildDelete.handle({ id: guild.id }).guild
     );
   }
@@ -16998,7 +16998,7 @@ class RESTMethods {
 
   // Untested but probably will work
   deleteGuild(guild) {
-    return this.rest.makeRequest('del', Endpoints.Guild(guild), true).then(() =>
+    return this.rest.makeRequest('delete', Endpoints.Guild(guild), true).then(() =>
       this.client.actions.GuildDelete.handle({ id: guild.id }).guild
     );
   }
@@ -17041,7 +17041,7 @@ class RESTMethods {
   }
 
   kickGuildMember(guild, member) {
-    return this.rest.makeRequest('del', Endpoints.Guild(guild).Member(member), true).then(() =>
+    return this.rest.makeRequest('delete', Endpoints.Guild(guild).Member(member), true).then(() =>
       this.client.actions.GuildMemberRemove.handle({
         guild_id: guild.id,
         user: member.user,
@@ -17061,7 +17061,7 @@ class RESTMethods {
   }
 
   deleteGuildRole(role) {
-    return this.rest.makeRequest('del', Endpoints.Guild(role.guild).Role(role.id), true).then(() =>
+    return this.rest.makeRequest('delete', Endpoints.Guild(role.guild).Role(role.id), true).then(() =>
       this.client.actions.GuildRoleDelete.handle({
         guild_id: role.guild.id,
         role_id: role.id,
@@ -17075,7 +17075,7 @@ class RESTMethods {
 
   deletePermissionOverwrites(overwrite) {
     return this.rest.makeRequest(
-      'del', `${Endpoints.Channel(overwrite.channel).permissions}/${overwrite.id}`, true
+      'delete', `${Endpoints.Channel(overwrite.channel).permissions}/${overwrite.id}`, true
     ).then(() => overwrite);
   }
 
@@ -17221,7 +17221,7 @@ class RESTMethods {
         reject(new Error('Took too long to receive the ban remove event.'));
       }, 10000);
 
-      this.rest.makeRequest('del', `${Endpoints.Guild(guild).bans}/${id}`, true).catch(err => {
+      this.rest.makeRequest('delete', `${Endpoints.Guild(guild).bans}/${id}`, true).catch(err => {
         this.client.removeListener(Constants.Events.GUILD_BAN_REMOVE, listener);
         this.client.clearTimeout(timeout);
         reject(err);
@@ -17265,7 +17265,7 @@ class RESTMethods {
   }
 
   unpinMessage(message) {
-    return this.rest.makeRequest('del', Endpoints.Channel(message.channel).Pin(message.id), true)
+    return this.rest.makeRequest('delete', Endpoints.Channel(message.channel).Pin(message.id), true)
       .then(() => message);
   }
 
@@ -17283,7 +17283,7 @@ class RESTMethods {
   }
 
   deleteInvite(invite) {
-    return this.rest.makeRequest('del', Endpoints.Invite(invite.code), true).then(() => invite);
+    return this.rest.makeRequest('delete', Endpoints.Invite(invite.code), true).then(() => invite);
   }
 
   getInvite(code) {
