8421a4cccc51b28ce8310d69fbd53c05ae117496
SpaceEEC <SpaceEEC@users.noreply.github.com>
2017-04-01 21:16:15 +0200

    to Endpoints.CND with you (#1330)

--- src/structures/Emoji.js
+++ src/structures/Emoji.js
@@ -89,7 +89,7 @@ class Emoji {
    * @readonly
    */
   get url() {
-    return Constants.Endpoints.emoji(this.id);
+    return Constants.Endpoints.CDN(this.client.options.http.host).Emoji(this.id);
   }
 
   /**
