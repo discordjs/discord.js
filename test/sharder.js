5c5aba21babc6da2a7f63b280dd55875e562ab33
Crawl <icrawltogo@gmail.com>
2017-04-02 09:45:59 +0200

    Update sharder.js

--- test/sharder.js
+++ test/sharder.js
@@ -2,6 +2,6 @@ const Discord = require('../');
 
 const sharder = new Discord.ShardingManager(`${process.cwd()}/test/shard.js`, 5, false);
 
-sharder.on('launch', id => console.log(`launched ${id}`));
+sharder.on('launch', shard => console.log(`launched ${shard.id}`));
 
 sharder.spawn(5);
