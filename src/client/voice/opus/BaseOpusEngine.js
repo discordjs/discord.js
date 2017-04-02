ef666002589005b003b18a9e05e93906b2695d44
aemino <programmixdev@gmail.com>
2017-04-02 10:43:46 -0700

    Add FEC and PLP support for Opus encoding (#1264)
    * Add FEC and PLP support for Opus encoding
    
    * Silently fail if the Opus engine doesn't support CTL
    
    * Fixed inversed max/min functions

--- src/client/voice/opus/BaseOpusEngine.js
+++ src/client/voice/opus/BaseOpusEngine.js
@@ -1,6 +1,23 @@
 class BaseOpus {
   constructor(player) {
     this.player = player;
+
+    this.ctl = {
+      FEC: 4012,
+      PLP: 4014,
+    };
+  }
+
+  init() {
+    try {
+      // enable FEC (forward error correction)
+      this.setFEC(true);
+
+      // set PLP (expected packet loss percentage) to 15%
+      this.setPLP(0.15);
+    } catch (err) {
+      // Opus engine likely has no support for libopus CTL
+    }
   }
 
   encode(buffer) {
