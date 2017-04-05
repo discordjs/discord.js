5d85de088372c7143e74fee786feb3f81e49386f
Gus Caplan <me@gus.host>
2017-04-05 14:52:55 -0500

    add auth session change handling (#1339)
    * add auth session change handling
    
    * eek

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -356,6 +356,7 @@ exports.Events = {
  * - VOICE_SERVER_UPDATE
  * - RELATIONSHIP_ADD
  * - RELATIONSHIP_REMOVE
+ * - AUTH_SESSION_CHANGE
  * @typedef {string} WSEventType
  */
 exports.WSEvents = {
@@ -393,6 +394,7 @@ exports.WSEvents = {
   VOICE_SERVER_UPDATE: 'VOICE_SERVER_UPDATE',
   RELATIONSHIP_ADD: 'RELATIONSHIP_ADD',
   RELATIONSHIP_REMOVE: 'RELATIONSHIP_REMOVE',
+  AUTH_SESSION_CHANGE: 'AUTH_SESSION_CHANGE',
 };
 
 exports.MessageTypes = [
@@ -441,3 +443,4 @@ exports.Colors = {
   DARK_BUT_NOT_BLACK: 0x2C2F33,
   NOT_QUITE_BLACK: 0x23272A,
 };
+
