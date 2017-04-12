204cebc855f5526bc1dbc7d971b542ab75135787
SpaceEEC <wermail2002-kap@yahoo.de>
2017-04-12 18:07:43 +0200

    commit

--- src/util/Util.js
+++ src/util/Util.js
@@ -98,10 +98,9 @@ class Util {
     if (a === b) return true;
     if (a.length !== b.length) return false;
 
-    for (const itemInd in a) {
-      const item = a[itemInd];
+    for (const item of a) {
       const ind = b.indexOf(item);
-      if (ind) b.splice(ind, 1);
+      if (ind !== -1) b.splice(ind, 1);
     }
 
     return b.length === 0;
