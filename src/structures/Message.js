bd217a58366e9141f26e6d8ec30127aa834d9b86
Amish Shah <amishshah.2k@gmail.com>
2017-04-12 21:22:08 +0100

    Fix reactions not being uncached once they hit a count of 0

--- src/structures/Message.js
+++ src/structures/Message.js
@@ -513,6 +513,7 @@ class Message {
         reaction.users.delete(user.id);
         reaction.count--;
         if (user.id === this.client.user.id) reaction.me = false;
+        if (reaction.count <= 0) this.reactions.delete(emojiID);
         return reaction;
       }
     }
