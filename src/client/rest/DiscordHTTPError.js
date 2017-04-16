ff8e336e4ba076cba9ca3bd4ecdc06396f74bcfe
Schuyler Cebulskie <Gawdl3y@gmail.com>
2017-04-16 19:46:48 -0400

    Update DiscordHTTPError.js

--- src/client/rest/DiscordHTTPError.js
+++ src/client/rest/DiscordHTTPError.js
@@ -1,45 +1,32 @@
-const kCode = Symbol('code');
-
 /**
  * Represents an error from Discord's HTTP Gateway
  */
 class DiscordHTTPError extends Error {
   constructor(error) {
-    const flatErrors = error.errors ? makeFlatErrors(error.errors).join('\n') : '';
+    const flatErrors = error.errors ? this.constructor.flattenErrors(error.errors).join('\n') : '';
     super(`${error.message}\n${flatErrors}`.trim());
-    this[kCode] = error.code;
+    this.name = `DiscordHTTPError ${this.code}`;
+    
+    /**
+     * The error code from Discord
+     * @type {number}
+     */
+    this.code = error.code;
   }
-
-  /**
-   * The name of this error
-   * @type {string}
-   */
-  get name() {
-    return `DiscordHTTPError ${this[kCode]}`;
-  }
-
-  /**
-   * The error code from Discord
-   * @type {number}
-   */
-  get code() {
-    return this[kCode];
-  }
-}
-
-function makeFlatErrors(obj, key = '') {
-  let messages = [];
-  for (const k of Object.keys(obj)) {
-    const newKey = key ? isNaN(parseInt(k)) ? `${key}.${k}` : `${key}[${k}]` : k;
-    if (obj[k]._errors) {
-      messages.push(`${newKey}: ${obj[k]._errors.map(e => e.message).join(' ')}`);
-    } else {
-      messages = messages.concat(makeFlatErrors(obj[k], newKey));
+  
+  static flattenErrors(obj, key = '') {
+    let messages = [];
+    for (const k of Object.keys(obj)) {
+      const newKey = key ? isNaN(parseInt(k)) ? `${key}.${k}` : `${key}[${k}]` : k;
+      if (obj[k]._errors) {
+        messages.push(`${newKey}: ${obj[k]._errors.map(e => e.message).join(' ')}`);
+      } else {
+        messages = messages.concat(this.flattenErrors(obj[k], newKey));
+      }
     }
+
+    return messages;
   }
-
-  return messages;
 }
-
 
 module.exports = DiscordHTTPError;
