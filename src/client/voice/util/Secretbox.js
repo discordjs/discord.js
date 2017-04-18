a04eed682e592792ea08bc274b1ab11aed178e1e
Skiptir Engu <skiptir.engu@yandex.com>
2017-04-18 15:37:13 -0300

    Use libsodium.js if available

--- src/client/voice/util/Secretbox.js
+++ src/client/voice/util/Secretbox.js
@@ -1,13 +1,24 @@
-try {
-  const sodium = require('sodium');
-  module.exports = {
+const libs = {
+  sodium: sodium => ({
     open: sodium.api.crypto_secretbox_open_easy,
     close: sodium.api.crypto_secretbox_easy,
-  };
-} catch (err) {
-  const tweetnacl = require('tweetnacl');
-  module.exports = {
+  }),
+  'libsodium-wrappers': sodium => ({
+    open: sodium.crypto_secretbox_open_easy,
+    close: sodium.crypto_secretbox_easy,
+  }),
+  tweetnacl: tweetnacl => ({
     open: tweetnacl.secretbox.open,
     close: tweetnacl.secretbox,
-  };
+  }),
+};
+
+for (const libName of Object.keys(libs)) {
+  try {
+    const lib = require(libName);
+    module.exports = libs[libName](lib);
+    break;
+  } catch (err) {
+    //
+  }
 }
