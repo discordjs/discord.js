e7ee81c43721663aa870b7114f01741aac26e2d0
Isabella <isabellafj97@gmail.com>
2017-04-19 11:57:54 -0500

    Throw Error for non role-resolvables (#1322)
    * throw error for role names/integers being provided for role resolvables
    
    * Update RESTMethods.js
    
    * Update RESTMethods.js
    
    im really bad
    
    * Update RESTMethods.js
    
    * removed new promise reject
    
    ty gus
    
    * Update RESTMethods.js
    
    * move error to guildmember.addRole method
    
    * Update GuildMember.js

--- src/structures/GuildMember.js
+++ src/structures/GuildMember.js
@@ -393,6 +393,7 @@ class GuildMember {
    */
   addRole(role) {
     if (!(role instanceof Role)) role = this.guild.roles.get(role);
+    if (!role) throw new TypeError('Supplied parameter was neither a Role nor a Snowflake.');
     return this.client.rest.methods.addMemberRole(this, role);
   }
 
@@ -419,6 +420,7 @@ class GuildMember {
    */
   removeRole(role) {
     if (!(role instanceof Role)) role = this.guild.roles.get(role);
+    if (!role) throw new TypeError('Supplied parameter was neither a Role nor a Snowflake.');
     return this.client.rest.methods.removeMemberRole(this, role);
   }
 
