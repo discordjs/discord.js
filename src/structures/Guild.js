401822a0943a169d1e1e7511041045687bebf4e1
SpaceEEC <SpaceEEC@users.noreply.github.com>
2017-04-24 14:31:39 +0200

    Made search argument optional, added useronly warning and fixed endpoint (#1407)

--- src/structures/Guild.js
+++ src/structures/Guild.js
@@ -444,6 +444,7 @@ class Guild {
 
   /**
    * Performs a search within the entire guild.
+   * <warn>This is only available when using a user account.</warn>
    * @param {MessageSearchOptions} [options={}] Options to pass to the search
    * @returns {Promise<Array<Message[]>>}
    * An array containing arrays of messages. Each inner array is a search context cluster.
@@ -457,7 +458,7 @@ class Guild {
    *   console.log(`I found: **${hit}**, total results: ${res.totalResults}`);
    * }).catch(console.error);
    */
-  search(options) {
+  search(options = {}) {
     return this.client.rest.methods.search(this, options);
   }
 
