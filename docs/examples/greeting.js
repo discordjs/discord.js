a167de18391fbccc95c9cade71b9d82c291fb2a1
Crawl <icrawltogo@gmail.com>
2017-04-24 08:45:29 +0200

    Make sentence better (English is hard)

--- docs/examples/greeting.js
+++ docs/examples/greeting.js
@@ -19,7 +19,7 @@ client.on('ready', () => {
 
 // Create an event listener for new guild members
 client.on('guildMemberAdd', member => {
-  // Send the message, mentioning the member to the guilds default channel (usually #general)
+  // Send the message to the guilds default channel (usually #general), mentioning the member
   member.guild.defaultChannel.send(`Welcome to the server, ${member}!`);
 
   // If you want to send the message to a designated channel on a server instead
