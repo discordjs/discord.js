0bd109d9764133f85706ed670a0c0a0480dfe7b8
Travis CI <amishshah.2k@gmail.com>
2017-04-25 19:08:44 +0000

    Webpack build for branch wsrewrite: 1e4abe0fd83e0239bd80a7b832afad07d2165a00

--- discord.wsrewrite.js
+++ discord.wsrewrite.js
@@ -16265,6 +16265,7 @@ class WebSocketConnection extends EventEmitter {
    * Acknowledges a heartbeat
    */
   ackHeartbeat() {
+    this.debug(`Heartbeat acknowledged, latency of ${Date.now() - this.pingSendTime}ms`);
     this.client._pong(this.pingSendTime);
   }
 
