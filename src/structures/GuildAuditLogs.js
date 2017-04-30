c3be798663436648d6a831ff115ca2c65e61a1d0
Crawl <icrawltogo@gmail.com>
2017-05-01 01:03:12 +0200

    Update GuildAuditLogs.js

--- src/structures/GuildAuditLogs.js
+++ src/structures/GuildAuditLogs.js
@@ -40,7 +40,7 @@ const Actions = {
 
 
 /**
- * Audit logs entries are held in this class
+ * Audit logs entries are held in this class.
  */
 class GuildAuditLogs {
   constructor(guild, data) {
@@ -129,7 +129,7 @@ class GuildAuditLogs {
 }
 
 /**
- * Audit logs entry
+ * Audit logs entry.
  */
 class GuildAuditLogsEntry {
   constructor(guild, data) {
