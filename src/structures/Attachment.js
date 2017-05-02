1303a0b2359e93741b202cd01f422611656046d9
Amish Shah <amishshah.2k@gmail.com>
2017-05-02 17:04:26 +0100

    Create Attachment class

---
+++ src/structures/Attachment.js
@@ -0,0 +1,11 @@
+/**
+ * Represents a single attachment to a message
+ */
+class Attachment {
+  constructor(attachment, name) {
+    this.attachment = attachment;
+    this.name = name;
+  }
+}
+
+module.exports = Attachment;
