309d4e3d8e6a1e8bd3e35a31b5df723beec9592a
Travis CI <amishshah.2k@gmail.com>
2017-05-05 15:11:01 +0000

    Webpack build for branch 11.1-dev: 328d75be7d2d70d80e59f09b9529b58d95a23a3a

--- discord.11.1-dev.js
+++ discord.11.1-dev.js
@@ -12376,7 +12376,7 @@ module.exports = ClientDataResolver;
 
 module.exports = {
 	"name": "discord.js",
-	"version": "11.1.0",
+	"version": "11.2.0",
 	"description": "A powerful library for interacting with the Discord API",
 	"main": "./src/index",
 	"types": "./typings/index.d.ts",
