41e0be1db3957bd8067de1b7598580ecb1fc7c23
Crawl <icrawltogo@gmail.com>
2017-05-05 02:23:25 +0200

    Endpoints.Guild(...).Emoji(...) should not use CDN (#1462)

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -133,7 +133,7 @@ const Endpoints = exports.Endpoints = {
       ack: `${base}/ack`,
       settings: `${base}/settings`,
       auditLogs: `${base}/audit-logs`,
-      Emoji: emojiID => Endpoints.CDN(root).Emoji(emojiID),
+      Emoji: emojiID => `${base}/emojis/${emojiID}`,
       Icon: (root, hash) => Endpoints.CDN(root).Icon(guildID, hash),
       Splash: (root, hash) => Endpoints.CDN(root).Splash(guildID, hash),
       Role: roleID => `${base}/roles/${roleID}`,
