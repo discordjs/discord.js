9ca395c3a392a7044f8840c99cf3f759964e0815
Travis CI <amishshah.2k@gmail.com>
2017-05-05 18:15:22 +0000

    Webpack build for branch master: a4e57137904cc79a198aa2f3973db206d320adf1

--- discord.master.js
+++ discord.master.js
@@ -17509,6 +17509,10 @@ class WebSocketConnection extends EventEmitter {
       this.debug('Tried to mark self as ready, but already ready');
       return;
     }
+    /**
+     * Emitted when the client becomes ready to start working.
+     * @event Client#ready
+     */
     this.status = Constants.Status.READY;
     this.client.emit(Constants.Events.READY);
     this.packetManager.handleQueue();
@@ -17748,6 +17752,11 @@ class WebSocketConnection extends EventEmitter {
    * @param {Error} error Error that occurred
    */
   onError(error) {
+    /**
+     * Emitted whenever the client's WebSocket encounters a connection error.
+     * @event Client#error
+     * @param {Error} error The encountered error
+     */
     this.client.emit(Constants.Events.ERROR, error);
     if (error.message === 'uWs client connection error') this.reconnect();
   }
