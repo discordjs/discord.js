d5d3d4441c89df85eefbe2413c978d48fad94adc
Gus Caplan <me@gus.host>
2017-05-12 12:44:09 -0500

    Update Constants.js

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -451,14 +451,6 @@ exports.MessageTypes = [
   'GUILD_MEMBER_JOIN',
 ];
 
-exports.DefaultAvatars = {
-  BLURPLE: '6debd47ed13483642cf09e832ed0bc1b',
-  GREY: '322c936a8c8be1b803cd94861bdfa868',
-  GREEN: 'dd4dbc0016779df1378e7812eabaa04d',
-  ORANGE: '0e291f67c9274a1abdddeb3fd919cbaa',
-  RED: '1cbd08c76f8af6dddce02c5138971129',
-};
-
 exports.ExplicitContentFilterTypes = [
   'DISABLED',
   'NON_FRIENDS',
