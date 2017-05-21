694f8278f1373ee562d60eacbbf705cdf3f3f9a7
Gus Caplan <me@gus.host>
2017-05-21 15:09:37 -0500

    make fetching members way better (#1124)
    * make fetching members way better
    
    * fix up fetching edge cases yet again
    
    * stop making useless collections
    
    * Update Guild.js
    
    * Update Guild.js
    
    * consistency?
    
    * Update GuildMembersChunk.js
    
    * Update Guild.js
    
    * Update Guild.js
    
    * Open editor to fix issues instead of GH: check

--- src/structures/Guild.js
+++ src/structures/Guild.js
@@ -500,14 +500,12 @@ class Guild {
    * this should not be necessary.
    * @param {string} [query=''] Limit fetch to members with similar usernames
    * @param {number} [limit=0] Maximum number of members to request
-   * @returns {Promise<Guild>}
+   * @returns {Promise<Collection<Snowflake, GuildMember>>}
    */
   fetchMembers(query = '', limit = 0) {
     return new Promise((resolve, reject) => {
       if (this.memberCount === this.members.size) {
-        // Uncomment in v12
-        // resolve(this.members)
-        resolve(this);
+        resolve(new Collection());
         return;
       }
       this.client.ws.send({
@@ -518,17 +516,20 @@ class Guild {
           limit,
         },
       });
+      const fetchedMembers = new Collection();
       const handler = (members, guild) => {
         if (guild.id !== this.id) return;
-        if (this.memberCount === this.members.size || members.length < 1000) {
+        for (const member of members.values()) fetchedMembers.set(member.user.id, member);
+        if (this.memberCount === this.members.size || ((query || limit) && members.size < 1000)) {
           this.client.removeListener(Constants.Events.GUILD_MEMBERS_CHUNK, handler);
-          // Uncomment in v12
-          // resolve(this.members)
-          resolve(this);
+          resolve(fetchedMembers);
         }
       };
       this.client.on(Constants.Events.GUILD_MEMBERS_CHUNK, handler);
-      this.client.setTimeout(() => reject(new Error('Members didn\'t arrive in time.')), 120 * 1000);
+      this.client.setTimeout(() => {
+        this.client.removeListener(Constants.Events.GUILD_MEMBERS_CHUNK, handler);
+        reject(new Error('Members didn\'t arrive in time.'));
+      }, 120 * 1000);
     });
   }
 
