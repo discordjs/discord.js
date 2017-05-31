802c201944b6d145faf7f7b255ca345081632c25
Évelyne Lachance <eslachance@gmail.com>
2017-05-31 05:11:29 -0400

    Update Collection.js
    Fixed spacing/line length errors according to suggestions by codacy/pr

--- src/util/Collection.js
+++ src/util/Collection.js
@@ -60,15 +60,16 @@ class Collection extends Map {
 
   /**
    * Obtains the first item in this collection.
-   * @param {Integer} [count] Number of values to return. If omited, returns a single values. If present, returns an array of values.
+   * @param {Integer} [count] Number of values to return. If omited, returns a single values.
+   * If present, returns an array of values.
    * @returns {*}
    */
   first(count) {
-    if(!count) return this.values().next().value;
+    if (!count) return this.values().next().value;
     if (!Number.isInteger(count)) throw new TypeError('Count must be an integer');
     const values = this.values();
     const retArr = [];
-    for(let i=0;i<count;i++) {
+    for (let i = 0; i < count; i++) {
       retArr[i] = values.next().value;
     }
     return retArr;
@@ -76,15 +77,16 @@ class Collection extends Map {
 
   /**
    * Obtains the first key in this collection.
-   * @param {Integer} [count] Number of keys to return. If omited, returns a single key. If present, returns an array of keys.
+   * @param {Integer} [count] Number of keys to return. If omited, returns a single key.
+   * If present, returns an array of keys.
    * @returns {*}
    */
   firstKey(count) {
-    if(!count) return this.keys().next().value;
+    if (!count) return this.keys().next().value;
     if (!Number.isInteger(count)) throw new TypeError('Count must be an integer');
     const keys = this.keys();
     const retArr = [];
-    for(let i=0;i<count;i++) {
+    for (let i = 0; i < count; i++) {
       retArr[i] = keys.next().value;
     }
     return retArr;
@@ -93,16 +95,17 @@ class Collection extends Map {
   /**
    * Obtains the last item in this collection. This relies on the `array()` method, and thus the caching mechanism
    * applies here as well.
-   * @param {Integer} [count] Number of values to return. If omited, returns a single values. If present, returns an array of values.
+   * @param {Integer} [count] Number of values to return. If omited, returns a single values.
+   * If present, returns an array of values.
    * @returns {*}
    */
   last(count) {
     const arr = this.array();
-    if(!count) return arr[arr.length - 1];
+    if (!count) return arr[arr.length - 1];
     if (!Number.isInteger(count)) throw new TypeError('Count must be an integer');
     const retArr = [];
     const arrLen = arr.length;
-    for(let i=0;i<count;i++) {
+    for (let i = 0; i < count; i++) {
       retArr[i] = arr[arrLen - (i+1)];
     }
     return retArr;
@@ -111,16 +114,17 @@ class Collection extends Map {
   /**
    * Obtains the last key in this collection. This relies on the `keyArray()` method, and thus the caching mechanism
    * applies here as well.
-   * @param {Integer} [count] Number of keys to return. If omited, returns a single key. If present, returns an array of keys.
+   * @param {Integer} [count] Number of keys to return. If omited, returns a single key.
+   * If present, returns an array of keys.
    * @returns {*}
    */
   lastKey(count) {
     const arr = this.keyArray();
-    if(!count) return arr[arr.length - 1];
+    if (!count) return arr[arr.length - 1];
     if (!Number.isInteger(count)) throw new TypeError('Count must be an integer');
     const retArr = [];
     const arrLen = arr.length;
-    for(let i=0;i<count;i++) {
+    for (let i = 0; i < count; i++) {
       retArr[i] = arr[arrLen - (i+1)];
     }
     return retArr;
@@ -129,15 +133,14 @@ class Collection extends Map {
   /**
    * Obtains a random item from this collection. This relies on the `array()` method, and thus the caching mechanism
    * applies here as well.
-   * @param {Integer} [count] Number of values to return. If omited, returns a single values. If present, returns an array of values.
    * @returns {*}
    */
   random(count) {
     const arr = this.array();
-    if(!count) return arr[Math.floor(Math.random() * arr.length)];
+    if (!count) return arr[Math.floor(Math.random() * arr.length)];
     if (!Number.isInteger(count)) throw new TypeError('Count must be an integer');
     const retArr = [];
-    for(let i=0;i<count;i++) {
+    for (let i = 0; i < count; i++) {
       retArr[i] = arr[Math.floor(Math.random() * arr.length)];
     }
     return retArr;
@@ -146,15 +149,14 @@ class Collection extends Map {
   /**
    * Obtains a random key from this collection. This relies on the `keyArray()` method, and thus the caching mechanism
    * applies here as well.
-   * @param {Integer} [count] Number of keys to return. If omited, returns a single key. If present, returns an array of keys.
    * @returns {*}
    */
   randomKey(count) {
     const arr = this.keyArray();
-    if(!count) return arr[Math.floor(Math.random() * arr.length)];
+    if (!count) return arr[Math.floor(Math.random() * arr.length)];
     if (!Number.isInteger(count)) throw new TypeError('Count must be an integer');
     const retArr = [];
-    for(let i=0;i<count;i++) {
+    for (let i = 0; i < count; i++) {
       retArr[i] = arr[Math.floor(Math.random() * arr.length)];
     }
     return retArr;
