28dc3e6a2e95871b5dc674785ef69b2faf18fe3f
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2017-06-02 00:36:34 -0400

    Utilise Object.values for Permissions.ALL

--- src/util/Permissions.js
+++ src/util/Permissions.js
@@ -171,7 +171,7 @@ Permissions.FLAGS = {
  * Bitfield representing every permission combined
  * @type {number}
  */
-Permissions.ALL = Object.keys(Permissions.FLAGS).reduce((all, p) => all | Permissions.FLAGS[p], 0);
+Permissions.ALL = Object.values(Permissions.FLAGS).reduce((all, p) => all | p, 0);
 
 /**
  * Bitfield representing the default permissions for users
