46d93e8061f2cfa6c943dfcb923187727ba643a0
Travis CI <amishshah.2k@gmail.com>
2017-06-25 17:39:14 +0000

    Webpack build for branch data-store-refactor: 262e1699b58ed82ffc3f56f62b7b108cb3e31a52

--- discord.data-store-refactor.js
+++ discord.data-store-refactor.js
@@ -22832,20 +22832,6 @@ class ClientDataManager {
     this.client = client;
   }
 
-  get pastReady() {
-    return this.client.ws.connection.status === Constants.Status.READY;
-  }
-
-  updateGuild(currentGuild, newData) {
-    const oldGuild = Util.cloneObject(currentGuild);
-    currentGuild.setup(newData);
-    if (this.pastReady) this.client.emit(Constants.Events.GUILD_UPDATE, oldGuild, currentGuild);
-  }
-
-  updateChannel(currentChannel, newData) {
-    currentChannel.setup(newData);
-  }
-
   updateEmoji(currentEmoji, newData) {
     const oldEmoji = Util.cloneObject(currentEmoji);
     currentEmoji.setup(newData);
