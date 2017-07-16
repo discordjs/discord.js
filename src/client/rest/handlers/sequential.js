90815e495b35063f2f03555cb2b2d14a347097be
Gus Caplan <me@gus.host>
2017-07-16 11:58:52 -0500

    Update sequential.js

--- src/client/rest/handlers/sequential.js
+++ src/client/rest/handlers/sequential.js
@@ -1,16 +1,16 @@
 module.exports = function sequential() {
   if (this.busy || this.limited) return;
   this.busy = true;
-  this.execute(this.queue.shift()).then(time => {
-    if (time || this.remaining <= 0) {
+  this.execute(this.queue.shift())
+    .then(() => {
+      this.busy = false;
+      this.handle();
+    })
+    .catch(({ timeout }) => {
       this.client.setTimeout(() => {
         this.reset();
         this.busy = false;
         this.handle();
-      }, time || (this.resetTime - Date.now() + this.timeDifference + this.client.options.restTimeOffset));
-    } else {
-      this.busy = false;
-      this.handle();
-    }
-  });
+      }, timeout || (this.resetTime - Date.now() + this.timeDifference + this.client.options.restTimeOffset));
+    });
 };
