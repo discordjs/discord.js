67234ef5cd390c6299c86a455b616396be98f953
Funnbot <dillonbayless1@gmail.com>
2017-07-17 10:33:27 -0700

    Update ReactionCollector.js

--- src/structures/ReactionCollector.js
+++ src/structures/ReactionCollector.js
@@ -39,7 +39,17 @@ class ReactionCollector extends Collector {
      */
     this.total = 0;
 
-    this.client.on('messageReactionAdd', this.listener);
+    this.empty = this.empty.bind(this);
+
+    this.client.on('messageReactionAdd', this.collect);
+    this.client.on('messageReactionRemove', this.uncollect);
+    this.client.on('messageReactionRemoveAll', this.empty);
+
+    this.once('end', () => {
+      this.client.removeListener('messageReactionAdd', this.collect);
+      this.client.removeListener('messageReactionRemove', this.uncollect);
+      this.client.removeListener('messageReactionRemoveAll', this.empty);
+    });
   }
 
   /**
@@ -48,35 +58,67 @@ class ReactionCollector extends Collector {
    * @returns {?{key: Snowflake, value: MessageReaction}} Reaction data to collect
    * @private
    */
-  handle(reaction) {
+  shouldCollect(reaction) {
     if (reaction.message.id !== this.message.id) return null;
     return {
-      key: reaction.emoji.id || reaction.emoji.name,
+      key: ReactionCollector.key(reaction),
       value: reaction,
     };
   }
 
   /**
-   * Check after collection to see if the collector is done.
+   * Handle a reaction deletion for possible uncollection.
+   * @param {MessageReaction} reaction The reaction to possibly uncollect
+   * @returns {?Snowflake|string} The reaction key
+   */
+  shouldUncollect(reaction) {
+    return reaction.message.id === this.message.id ? ReactionCollector.key(reaction) : null;
+  }
+
+  /**
+   * Empty this reaction collector.
+   */
+  empty() {
+    this.total = 0;
+    this.collected.clear();
+    this.users.clear();
+    this.checkShouldEnd();
+  }
+
+  /**
+   * Maintain users collection for reaction additions.
    * @param {MessageReaction} reaction The reaction that was collected
    * @param {User} user The user that reacted
-   * @returns {?string} Reason to end the collector, if any
-   * @private
    */
-  postCheck(reaction, user) {
+  postCollect(reaction, user) {
+    this.total++;
     this.users.set(user.id, user);
-    if (this.options.max && ++this.total >= this.options.max) return 'limit';
+  }
+
+  /**
+   * Maintain users collection for reaction removals.
+   * @param {MessageReaction} reaction The reaction that was removed
+   * @param {User} user The user that removed their reaction
+   */
+  postUncollect(reaction, user) {
+    this.total--;
+    if (!this.collected.some(r => r.users.has(user.id))) this.users.delete(user.id);
+  }
+
+  shouldEnd() {
+    if (this.options.max && this.total >= this.options.max) return 'limit';
     if (this.options.maxEmojis && this.collected.size >= this.options.maxEmojis) return 'emojiLimit';
     if (this.options.maxUsers && this.users.size >= this.options.maxUsers) return 'userLimit';
     return null;
   }
 
   /**
-   * Remove event listeners.
-   * @private
+   * Get the collector key for a reaction.
+   * @param {MessageReaction} reaction The message reaction to get the key for
+   * @returns {Snowflake|string} The emoji ID (if custom) or the emoji name (if native; will be unicode)
    */
-  cleanup() {
-    this.client.removeListener('messageReactionAdd', this.listener);
+  static key(reaction) {
+    return reaction.emoji.id || reaction.emoji.name;
   }
 }
 
