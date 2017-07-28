8d42cb6f3ba1a7a88a6b1aa158e49270a60c5b8d
Travis CI <amishshah.2k@gmail.com>
2017-07-28 02:57:07 +0000

    Webpack build for branch 11.1-dev: b27198ebe5bf6eb2614b77295dc2593bdcf14c72

--- discord.11.1-dev.js
+++ discord.11.1-dev.js
@@ -17014,7 +17014,7 @@ class TextChannel extends GuildChannel {
      * @type {boolean}
      * @readonly
      */
-    this.nsfw = data.nsfw;
+    this.nsfw = !!data.nsfw;
 
     this.lastMessageID = data.last_message_id;
   }
