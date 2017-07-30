e7ebb23f14f1b6d68ac6a6036b1a0625a6b35029
bdistin <bdistin@gmail.com>
2017-07-30 08:00:01 -0500

    Fix guildMembersChunk documents for v11.1-dev (#1734)

--- src/client/websocket/packets/handlers/GuildMembersChunk.js
+++ src/client/websocket/packets/handlers/GuildMembersChunk.js
@@ -26,7 +26,7 @@ class GuildMembersChunkHandler extends AbstractHandler {
 /**
  * Emitted whenever a chunk of guild members is received (all members come from the same guild).
  * @event Client#guildMembersChunk
- * @param {Collection<Snowflake, GuildMember>} members The members in the chunk
+ * @param {GuildMember[]} members The members in the chunk
  * @param {Guild} guild The guild related to the member chunk
  */
 
