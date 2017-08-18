036b996510dea5fddd8370c150e2c44fcd21d2f0
Gus Caplan <me@gus.host>
2017-08-17 23:05:31 -0700

    update some examples

--- docs/examples/rpc.js
+++ docs/examples/rpc.js
@@ -1,12 +1,28 @@
-const Discord = require('discord.js');
+/* global document URLSearchParams Discord */
+/* eslint-disable no-console */
 
-const client = new Discord.RPCClient({
-  transport: 'ipc',
-});
+// This example assumes you are running in a browser (hence document and URLSearchParams
+
+const clientID = '187406016902594560';
+const scopes = ['rpc', 'rpc.api', 'messages.read'];
+
+// This demonstrates discord's implicit oauth2 flow
+// http://discordapi.com/topics/oauth2#implicit-grant
+
+const params = new URLSearchParams(document.location.hash.slice(1));
+
+if (!params.has('access_token')) {
+  // Redirect to discord to get an access token
+  document.location.href =
+    `https://discordapp.com/oauth2/authorize?response_type=token&client_id=${clientID}&scope=${scopes.join('+')}`;
+}
+
+const client = new Discord.RPCClient();
 
 client.on('ready', () => {
   console.log('Logged in as', client.application.name);
   console.log('Authed for user', client.user.tag);
 });
 
-client.login('my client id', { clientSecret: 'my client secret' });
+// Log in to rpc with client id and access token
+client.login(clientID, { accessToken: params.get('access_token') });
