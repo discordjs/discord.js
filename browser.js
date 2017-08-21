c31933ad02a271a27cf960d8bb3d4ca291bfb961
Crawl <icrawltogo@gmail.com>
2017-08-22 00:28:49 +0200

    Update browser.js

--- browser.js
+++ browser.js
@@ -6,4 +6,4 @@ const Discord = require('./');
 module.exports = Discord;
 if (browser && webpack) window.Discord = Discord; // eslint-disable-line no-undef
 // eslint-disable-next-line no-console
-else if (!browser) console.warn('Warning: using browser version of Discord.js in a non-browser environment');
+else if (!browser) console.warn('Warning: Attempting to use browser version of Discord.js in a non-browser environment!');
