b29bfe9448b30ff7a1fef1acd3dd7e6fb51f1d49
KyeNormanGill <kye.normangill@tafensw.net.au>
2017-08-22 13:38:20 +1000

    Oh and this too :facepalm:

--- src/client/ClientDataResolver.js
+++ src/client/ClientDataResolver.js
@@ -141,7 +141,6 @@ class ClientDataResolver {
     if (channel instanceof Channel) return channel;
     if (typeof channel === 'string') return this.client.channels.get(channel) || null;
     if (channel instanceof Message) return channel.channel;
-    if (channel instanceof Guild) return channel.channels.get(channel.id) || null;
     return null;
   }
 
