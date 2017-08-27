04cb1df9d7019099e7023acf43fada49bc0ee1c8
SpaceEEC <wermail2002-kap@yahoo.de>
2017-08-27 20:49:12 +0200

    Added resume event constant and used event constants wherever possible

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -196,6 +196,7 @@ exports.VoiceOPCodes = {
 
 exports.Events = {
   READY: 'ready',
+  RESUME: 'resume',
   GUILD_CREATE: 'guildCreate',
   GUILD_DELETE: 'guildDelete',
   GUILD_UPDATE: 'guildUpdate',
