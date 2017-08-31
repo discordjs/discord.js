0abbe465886003b8ca090bf348366fbedb1c4090
Will Nelson <nelson.will@live.com>
2017-08-30 17:29:43 -0700

    remove unused import

--- src/util/Collection.js
+++ src/util/Collection.js
@@ -1,5 +1,3 @@
-const Util = require('./Util');
-
 /**
  * A Map with additional utility methods. This is used throughout discord.js rather than Arrays for anything that has
  * an ID, for significantly improved performance and ease-of-use.
