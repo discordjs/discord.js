f0cfb2f25845f9995c66a28390442a8484a23f8b
Richard <hardy263@gmail.com>
2017-09-01 03:05:51 -0400

    fix/feat(MessageEmbed): deep copy fields and fix files when passed into constructor

--- src/structures/MessageEmbed.js
+++ src/structures/MessageEmbed.js
@@ -54,7 +54,7 @@ class MessageEmbed {
      * @property {string} value The value of this field
      * @property {boolean} inline If this field will be displayed inline
      */
-    this.fields = data.fields || [];
+    this.fields = data.fields ? data.fields.map(Util.cloneObject) : [];
 
     /**
      * The thumbnail of this embed (if there is one)
@@ -138,8 +138,13 @@ class MessageEmbed {
      * @property {Array<FileOptions|string|Attachment>} files Files to attach
      */
     if (data.files) {
-      for (let file of data.files) if (file instanceof Attachment) file = file.file;
-    } else { data.files = null; }
+      this.files = data.files.map(file => {
+        if (file instanceof Attachment) {
+          return typeof file.file === 'string' ? file.file : Util.cloneObject(file.file);
+        }
+        return file;
+      });
+    } else { this.files = null; }
   }
 
   /**
