4a8ada9c43cc3726c77f0b7db16b76d399e957bc
Amish Shah <amishshah.2k@gmail.com>
2017-09-05 12:02:21 +0100

    Fix ESLint error

--- src/structures/User.js
+++ src/structures/User.js
@@ -244,7 +244,7 @@ class User extends Base {
       this.id === user.id &&
       this.username === user.username &&
       this.discriminator === user.discriminator &&
-      this.avatar === user.avatar
+      this.avatar === user.avatar;
 
     return equal;
   }
