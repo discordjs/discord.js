750370807b7aa0a3d6d94864ba3f92a5f3cea218
Frangu Vlad <contact@kingdgrizzle.ml>
2017-09-09 17:20:52 +0300

    Document CategoryChannel

--- src/structures/CategoryChannel.js
+++ src/structures/CategoryChannel.js
@@ -1,6 +1,15 @@
 const GuildChannel = require('./GuildChannel');
 
+/**
+ * Represents a guild category channel on Discord
+ * @extends {GuildChannel}
+ */
 class CategoryChannel extends GuildChannel {
+  /**
+   * The channels that are part of this category
+   * @type {?Collection}
+   * @readonly
+   */
   get children() {
     return this.guild.channels.filter(c => c.parentID === this.id);
   }
