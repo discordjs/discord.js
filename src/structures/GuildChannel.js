dd1e956a3c5a04fc52c19b3743dd36d87af9e382
Will Nelson <nelson.will@live.com>
2017-09-09 09:38:13 -0700

    .

--- src/structures/GuildChannel.js
+++ src/structures/GuildChannel.js
@@ -302,7 +302,7 @@ class GuildChannel extends Channel {
   }
 
   /**
-   * Set the category parent of this channel
+   * Set the category parent of this channel.
    * @param {GuildChannel|Snowflake} channel Parent channel
    * @param {boolean} [options.lockPermissions=true] Lock the permission to what the parent's permissions are
    * @param {string} [options.reason] Reason for modifying the parent of this channel
