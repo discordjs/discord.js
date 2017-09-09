d888e7fb20f64bd1a0ae0b19f15241185be3c01e
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-09-09 01:37:20 +0000

    Proper fix

--- src/client/Client.js
+++ src/client/Client.js
@@ -217,11 +217,11 @@ class Client extends BaseClient {
    * @readonly
    */
   get emojis() {
-    const emojis = new Collection();
+    const emojis = new EmojiStore({ client: this });
     for (const guild of this.guilds.values()) {
       if (guild.available) for (const emoji of guild.emojis.values()) emojis.set(emoji.id, emoji);
     }
-    return new EmojiStore({ client: this }, emojis);
+    return emojis;
   }
 
   /**
