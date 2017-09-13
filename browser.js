8e760f48c2c22efa7b60fa8f4d9cbf4d66d114f1
Gus Caplan <me@gus.host>
2017-09-13 10:29:19 -0500

    Update browser.js

--- browser.js
+++ browser.js
@@ -4,6 +4,9 @@ const webpack = !!process.env.__DISCORD_WEBPACK__;
 const Discord = require('./');
 
 module.exports = Discord;
-if (browser && webpack) window.Discord = Discord; // eslint-disable-line no-undef
-// eslint-disable-next-line no-console
-else if (!browser) console.warn('Warning: Attempting to use browser version of Discord.js in a non-browser environment!');
+if (browser && webpack) {
+  window.Discord = Discord; // eslint-disable-line no-undef
+} else if (!browser) {
+  // eslint-disable-next-line no-console
+  console.warn('Warning: Attempting to use browser version of Discord.js in a non-browser environment!');
+}
