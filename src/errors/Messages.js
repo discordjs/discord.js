d70e5e6fa1c4590de32ee742b49401cf39f6f283
SpaceEEC <wermail2002-kap@yahoo.de>
2017-09-16 19:17:39 +0200

    fix(ClientManager): reject login with proper error on timeout or on connection failure

--- src/errors/Messages.js
+++ src/errors/Messages.js
@@ -8,6 +8,7 @@ const Messages = {
 
   FEATURE_USER_ONLY: 'Only user accounts are able to make use of this feature.',
 
+  WS_CONNECTION_TIMEOUT: 'The connection to the gateway timed out.',
   WS_CONNECTION_EXISTS: 'There is already an existing WebSocket connection.',
   WS_NOT_OPEN: (data = 'data') => `Websocket not open to send ${data}`,
 
