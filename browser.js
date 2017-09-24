6ce9a8743f3b59de59475764022dc71360f405e8
Gus Caplan <me@gus.host>
2017-09-24 15:01:47 -0500

    update browser stuff and browser eslint (#1938)
    * Update browser.js
    
    * Update .eslintrc.json
    
    * Update package.json
    
    * Update package.json
    
    * stop doing manually what webpack can do for us
    
    * Update .eslintrc.json
    
    * Update package.json

--- browser.js
+++
@@ -1,9 +0,0 @@
-const browser = typeof window !== 'undefined';
-const webpack = !!process.env.__DISCORD_WEBPACK__;
-
-const Discord = require('./');
-
-module.exports = Discord;
-if (browser && webpack) window.Discord = Discord; // eslint-disable-line no-undef
-// eslint-disable-next-line no-console
-else if (!browser) console.warn('Warning: Attempting to use browser version of Discord.js in a non-browser environment!');
