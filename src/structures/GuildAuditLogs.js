45e9b76d532ea15fea2ad48473b1cedfcb981b3a
Lewdcario <isabellafj97@gmail.com>
2017-10-03 16:53:07 -0600

    no null

--- src/structures/GuildAuditLogs.js
+++ src/structures/GuildAuditLogs.js
@@ -281,13 +281,7 @@ class GuildAuditLogsEntry {
     } else if ([Targets.USER, Targets.GUILD].includes(targetType)) {
       this.target = guild.client[`${targetType.toLowerCase()}s`].get(data.target_id);
     } else if (targetType === Targets.WEBHOOK) {
-      this.target = logs.webhooks.get(data.target_id) || null;
-      if (!this.target) {
-        guild.fetchWebhooks()
-          .then(hooks => {
-            this.target = hooks.find(h => h.id === data.target_id);
-          });
-      }
+      this.target = logs.webhooks.get(data.target_id) || new Webhook(guild.client, { id: data.target_id, guild_id: guild.id });
     } else if (targetType === Targets.INVITE) {
       if (guild.me.permissions.has('MANAGE_GUILD')) {
         const change = this.changes.find(c => c.key === 'code');
