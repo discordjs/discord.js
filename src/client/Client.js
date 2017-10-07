6f1b0e347785a193bcf969776763a33c857dd573
Will Nelson <nelson.will@live.com>
2017-10-07 07:28:14 -0700

    reject promise when no token is provided
    fixes #2011

--- src/client/Client.js
+++ src/client/Client.js
@@ -259,7 +259,7 @@ class Client extends BaseClient {
    */
   login(token) {
     return new Promise((resolve, reject) => {
-      if (typeof token !== 'string') throw new Error('TOKEN_INVALID');
+      if (typeof token !== 'string') return reject(new Error('TOKEN_INVALID'));
       token = token.replace(/^Bot\s*/i, '');
       this.manager.connectToWebSocket(token, resolve, reject);
     }).catch(e => {
