296046dbc6faa0b6aeabd0c7efdc470c0c834b16
Gus Caplan <me@gus.host>
2017-10-19 00:11:07 -0500

    remove ua manager (#2015)

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -1,6 +1,6 @@
-exports.Package = require('../../package.json');
+const Package = exports.Package = require('../../package.json');
 const { Error, RangeError } = require('../errors');
-exports.browser = typeof window !== 'undefined';
+const browser = exports.browser = typeof window !== 'undefined';
 
 /**
  * Options for a client.
@@ -57,9 +57,9 @@ exports.DefaultOptions = {
    */
   ws: {
     large_threshold: 250,
-    compress: !exports.browser,
+    compress: !browser,
     properties: {
-      $os: exports.browser ? 'browser' : process.platform,
+      $os: browser ? 'browser' : process.platform,
       $browser: 'discord.js',
       $device: 'discord.js',
     },
@@ -81,6 +81,9 @@ exports.DefaultOptions = {
     invite: 'https://discord.gg',
   },
 };
+
+exports.UserAgent = browser ? null :
+  `DiscordBot (${Package.homepage.split('#')[0]}, ${Package.version}) Node.js/${process.version}`;
 
 exports.WSCodes = {
   1000: 'Connection gracefully closed',
