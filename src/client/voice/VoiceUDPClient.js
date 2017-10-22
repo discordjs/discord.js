0004e19ecade46d705768ef5c5b0932e6dc3cabf
Amish Shah <amishshah.2k@gmail.com>
2017-10-22 16:03:35 +0100

    Fix ESLint issue

--- src/client/voice/VoiceUDPClient.js
+++ src/client/voice/VoiceUDPClient.js
@@ -103,7 +103,6 @@ class VoiceConnectionUDPClient extends EventEmitter {
     const socket = this.socket = udp.createSocket('udp4');
 
     socket.once('message', message => {
-
       // Stop if the sockets have been deleted because the connection has been closed already
       if (!this.voiceConnection.sockets.ws) return;
 
