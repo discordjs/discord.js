954a1c8d1afdf50f068ca9ca9320d265f6cb0633
Darqam <anhim2@gmail.com>
2018-01-03 18:00:39 -0600

    updating channelCreate example to reflect recent changes (#2194)
    * updating channelCreate example to reflect recent changes
    
    Type is now part of the options object, so the example should reflect that.
    
    * changing type to reason per comment

--- src/structures/Guild.js
+++ src/structures/Guild.js
@@ -921,7 +921,7 @@ class Guild extends Base {
    * @returns {Promise<GuildChannel>}
    * @example
    * // Create a new text channel
-   * guild.createChannel('new-general', 'text')
+   * guild.createChannel('new-general', { reason: 'My reason here.' })
    *   .then(channel => console.log(`Created new channel ${channel}`))
    *   .catch(console.error);
    */
