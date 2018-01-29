52c402faea7f3256e1bb466d2b5727d68beff362
Lewdcario <isabellafj97@gmail.com>
2018-01-28 19:04:55 -0600

    fix(resolveColor): not interpreting DEFAULT correctly

--- src/client/ClientDataResolver.js
+++ src/client/ClientDataResolver.js
@@ -351,6 +351,7 @@ class ClientDataResolver {
   static resolveColor(color) {
     if (typeof color === 'string') {
       if (color === 'RANDOM') return Math.floor(Math.random() * (0xFFFFFF + 1));
+      if (color === 'DEFAULT') return 0;
       color = Constants.Colors[color] || parseInt(color.replace('#', ''), 16);
     } else if (color instanceof Array) {
       color = (color[0] << 16) + (color[1] << 8) + color[2];
