7332d6221e6c080b2e48466b07785854268ae904
Striker <34070194+Strikerrr@users.noreply.github.com>
2018-02-12 13:34:35 +0000

    Update GuildChannel.js

--- src/structures/GuildChannel.js
+++ src/structures/GuildChannel.js
@@ -444,7 +444,19 @@ class GuildChannel extends Channel {
    * @param {ChannelResolvable} [options.parent=this.parent] The parent of the new channel
    * @param {string} [options.reason] Reason for cloning this channel
    * @returns {Promise<GuildChannel>}
-   */
+   * @example 
+   * // Clone a channel
+   * channel.clone({
+   *  name: 'Some name',
+   *  withPermissions: true,
+   *  withTopic: false,
+   *  nsfw: false,
+   *  parent: this.parent,
+   *  reason: 'Some reason'
+   * })
+   *  .then(() => console.log('Channel cloned!))
+   *  .catch(console.error)
+  */
   clone(options = {}) {
     if (typeof options.withPermissions === 'undefined') options.withPermissions = true;
     Util.mergeDefault({
