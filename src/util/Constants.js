02b51cabbc50d7a05dd8e6793b8ea3bed4d0b4ec
Gus Caplan <me@gus.host>
2018-03-01 15:29:53 -0600

    the new deal

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -6,7 +6,7 @@ const browser = exports.browser = typeof window !== 'undefined';
  * Options for a client.
  * @typedef {Object} ClientOptions
  * @property {number} [shardId=0] ID of the shard to run
- * @property {number} [shardCount=0] Total number of shards
+ * @property {number} [shardCount=1|"auto"] Total number of shards
  * @property {number} [messageCacheMaxSize=200] Maximum number of messages to cache per channel
  * (-1 or Infinity for unlimited - don't do this without message sweeping, otherwise memory usage will climb
  * indefinitely)
