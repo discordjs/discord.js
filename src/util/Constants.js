04dc8d6004727dccb33efc1433ef2ea15c2dacbc
Gus Caplan <me@gus.host>
2018-03-01 15:29:53 -0600

    the new deal

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -10,7 +10,7 @@ const browser = exports.browser = typeof window !== 'undefined';
  * provide the guarantee of any particular order. Burst mode is more likely to hit a 429 ratelimit error by its nature,
  * and is therefore slightly riskier to use.
  * @property {number} [shardId=0] ID of the shard to run
- * @property {number} [shardCount=0] Total number of shards
+ * @property {number} [shardCount=1|"auto"] Total number of shards
  * @property {number} [messageCacheMaxSize=200] Maximum number of messages to cache per channel
  * (-1 or Infinity for unlimited - don't do this without message sweeping, otherwise memory usage will climb
  * indefinitely)
