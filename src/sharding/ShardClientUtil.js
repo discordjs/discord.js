acd1740f0b44064a0df0e4eecb5340af35869f72
bdistin <bdistin@gmail.com>
2018-03-15 00:52:23 -0500

    docs: fix missing parameter type/description from #2193 (#2396)
    * docs: Fix missing parameter type/description from #2193
    
    * requested changes

--- src/sharding/ShardClientUtil.js
+++ src/sharding/ShardClientUtil.js
@@ -79,8 +79,8 @@ class ShardClientUtil {
   }
 
   /**
-   * Evaluates a script on all shards, in the context of the {@link Clients}.
-   * @param {string} script JavaScript to run on each shard
+   * Evaluates a script or function on all shards, in the context of the {@link Clients}.
+   * @param {string|Function} script JavaScript to run on each shard
    * @returns {Promise<Array<*>>} Results of the script execution
    * @see {@link ShardingManager#broadcastEval}
    */
