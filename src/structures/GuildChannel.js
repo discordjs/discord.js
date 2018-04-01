266fff9183c3937fffb07fc75c9c0b02330f86bd
Favna <favna#2846@discordmail.com>
2018-04-01 17:41:23 +0200

    Resolve requested changes

--- src/structures/GuildChannel.js
+++ src/structures/GuildChannel.js
@@ -540,14 +540,13 @@ class GuildChannel extends Channel {
   }
 
   /**
- * Whether the channel is manageable in terms of role hierarchy by the client user
+ * Whether the channel is manageable by the client user
  * @type {boolean}
  * @readonly
  */
   get manageable() {
     if (this.client.user.id === this.guild.ownerID) return true;
-    if (this.permissionsFor(this.client.user).has(Permissions.FLAGS.MANAGE_CHANNELS) &&
-    this.permissionsFor(this.client.user).has(Permissions.FLAGS.VIEW_CHANNEL)) return true;
+    if (this.permissionsFor(this.client.user).has([Permissions.FLAGS.MANAGE_CHANNELS, Permissions.FLAGS.VIEW_CHANNEL])) return true;
     return false;
   }
 
