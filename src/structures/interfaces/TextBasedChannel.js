discordjs/discord.js

commit 556e679bdb4346ae286bcb66aee51d8267e20778

Improved TextBasedChannel#send (#2537)

src/structures/interfaces/TextBasedChannel.js
@@ -107,8 +107,8 @@ class TextBasedChannel {
    *   .then(console.log)
    *   .catch(console.error);
    */
-  send(content, options) { // eslint-disable-line complexity
-    if (!options && typeof content === 'object' && !(content instanceof Array)) {
+  send(content, options) {
+    if (content && content.constructor === Object && !options) {
       options = content;
       content = null;
     } else if (!options) {

commit 90312952d91c6cf8fdea891f5cdc2478c81cec0d

revert: "Improved TextBasedChannel#send (#2537)"
This reverts commit 556e679bdb4346ae286bcb66aee51d8267e20778,
which broke resolving of non object literal message options.

src/structures/interfaces/TextBasedChannel.js
@@ -107,8 +107,8 @@ class TextBasedChannel {
    *   .then(console.log)
    *   .catch(console.error);
    */
-  send(content, options) {
-    if (content && content.constructor === Object && !options) {
+  send(content, options) { // eslint-disable-line complexity
+    if (!options && typeof content === 'object' && !(content instanceof Array)) {
       options = content;
       content = null;
     } else if (!options) {