512af54e5df43bb85a617708501892ec6b5b0a42
Lewdcario <isabellafj97@gmail.com>
2018-06-04 18:01:10 -0500

    fix(Shard): erroneously returning false The node documentation fails to correctly document when the backlog of unsent messages exceeds a certain threshhold the function will return false. This does not mean it will refuse to send- simply that it will take time. Issue in point: https://github.com/nodejs/node/issues/7657#issuecomment-240581726

--- src/sharding/Shard.js
+++ src/sharding/Shard.js
@@ -140,10 +140,9 @@ class Shard extends EventEmitter {
    */
   send(message) {
     return new Promise((resolve, reject) => {
-      const sent = this.process.send(message, err => {
+      this.process.send(message, err => {
         if (err) reject(err); else resolve(this);
       });
-      if (!sent) throw new Error('SHARDING_CHILD_CONNECTION');
     });
   }
 
