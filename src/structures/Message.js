8818ef4e2d4d3edcbf0a3159986bad39a98644be
Lewdcario <isabellafj97@gmail.com>
2018-06-10 14:36:44 -0500

    feat(Message): add jumpToURL getter

--- src/structures/Message.js
+++ src/structures/Message.js
@@ -238,6 +238,15 @@ class Message extends Base {
   }
 
   /**
+   * The url to jump to this message
+   * @type {string}
+   * @readonly
+   */
+  get jumpToURL() {
+    return `https://discordapp.com/channels/@me/${this.channel.id}?jump=${this.id}`;
+  }
+
+  /**
    * The message contents with all mentions replaced by the equivalent text.
    * If mentions cannot be resolved to a name, the relevant mention in the message content will not be converted.
    * @type {string}
