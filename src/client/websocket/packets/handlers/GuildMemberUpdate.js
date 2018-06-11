5eff360a4daadff13345553fcd91044f85a8cff9
Lewdcario <isabellafj97@gmail.com>
2018-06-11 16:37:34 -0500

    docs: wording improvements

--- src/client/websocket/packets/handlers/GuildMemberUpdate.js
+++ src/client/websocket/packets/handlers/GuildMemberUpdate.js
@@ -14,7 +14,7 @@ class GuildMemberUpdateHandler extends AbstractHandler {
         const old = member._update(data);
         if (client.ws.connection.status === Status.READY) {
           /**
-           * Emitted whenever a guild member changes - i.e. new role, removed role, nickname.
+           * Emitted whenever a guild member's details (e.g. role, nickname) is changed
            * @event Client#guildMemberUpdate
            * @param {GuildMember} oldMember The member before the update
            * @param {GuildMember} newMember The member after the update
--- src/client/websocket/packets/handlers/PresenceUpdate.js
+++ src/client/websocket/packets/handlers/PresenceUpdate.js
@@ -52,14 +52,14 @@ class PresenceUpdateHandler extends AbstractHandler {
 }
 
 /**
- * Emitted whenever a guild member's presence changes, or they change one of their details.
+ * Emitted whenever a guild member's presence (e.g. status, activity) is changed.
  * @event Client#presenceUpdate
  * @param {GuildMember} oldMember The member before the presence update
  * @param {GuildMember} newMember The member after the presence update
  */
 
 /**
- * Emitted whenever a user's details (e.g. username) are changed.
+ * Emitted whenever a user's details (e.g. username, avatar) is changed.
  * <info>Disabling {@link Client#presenceUpdate} will cause this event to only fire
  * on {@link ClientUser} update.</info>
  * @event Client#userUpdate
