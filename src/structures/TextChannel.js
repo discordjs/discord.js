fbb239d089cb68aa4a43fb135582385b9d955647
reeeeeeeeeeeeeeeeeeeeeeeeeeeeeee <37480488+reeeeeeeeeeeeeeeeeeeeeeeeeeeeeee@users.noreply.github.com>
2018-06-16 12:26:02 +0100

    Adds JSDocs for TextChannel.lastMessageID

--- src/structures/TextChannel.js
+++ src/structures/TextChannel.js
@@ -30,7 +30,11 @@ class TextChannel extends GuildChannel {
      * @readonly
      */
     this.nsfw = Boolean(data.nsfw);
-
+    
+    /**
+     * The ID of the last message sent in this channel, if one was sent
+     * @type {?Snowflake}
+     */
     this.lastMessageID = data.last_message_id;
   }
 
