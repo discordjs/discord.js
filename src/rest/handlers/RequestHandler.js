5e011dbc1104d1d5daf1042704b3d08e3d0b1880
Will Nelson <nelson.will@live.com>
2018-06-19 11:10:55 -0700

    switch to node-fetch (#2587)
    * switch to node-fetch
    
    * remove useless var declaration
    
    * remove method uppercasing
    
    * rework concurrency
    
    * Revert "rework concurrency"
    
    This reverts commit ef6aa2697e07277ae9f561f72558f38f358d2e08.
    
    * fix headers

--- src/rest/handlers/RequestHandler.js
+++ src/rest/handlers/RequestHandler.js
@@ -68,34 +68,37 @@ class RequestHandler {
           resolve();
         }
       };
-      item.request.gen().end((err, res) => {
+      item.request.make().then(res => {
         if (res && res.headers) {
-          if (res.headers['x-ratelimit-global']) this.manager.globallyRateLimited = true;
-          this.limit = Number(res.headers['x-ratelimit-limit']);
-          this.resetTime = Date.now() + Number(res.headers['retry-after']);
-          this.remaining = Number(res.headers['x-ratelimit-remaining']);
+          if (res.headers.get('x-ratelimit-global')) this.manager.globallyRateLimited = true;
+          this.limit = Number(res.headers.get('x-ratelimit-limit') || Infinity);
+          this.resetTime = Number(res.headers.get('x-ratelimit-reset') || 0);
+          this.remaining = Number(res.headers.get('x-ratelimit-remaining') || 1);
         }
-        if (err) {
-          if (err.status === 429) {
-            this.queue.unshift(item);
-            finish(Number(res.headers['retry-after']) + this.client.options.restTimeOffset);
-          } else if (err.status >= 500 && err.status < 600) {
-            if (item.retried) {
-              item.reject(err);
-              finish();
-            } else {
-              item.retried = true;
-              this.queue.unshift(item);
-              finish(1e3 + this.client.options.restTimeOffset);
-            }
-          } else {
-            item.reject(err.status >= 400 && err.status < 500 ?
-              new DiscordAPIError(res.request.path, res.body, res.request.method) : err);
+
+        if (res.ok) {
+          res.json().then(item.resolve, item.reject);
+          finish();
+          return;
+        }
+
+        if (res.status === 429) {
+          this.queue.unshift(item);
+          finish(Number(res.headers.get('retry-after')) + this.client.options.restTimeOffset);
+        } else if (res.status >= 500 && res.status < 600) {
+          if (item.retried) {
+            item.reject(res);
             finish();
+          } else {
+            item.retried = true;
+            this.queue.unshift(item);
+            finish(1e3 + this.client.options.restTimeOffset);
           }
         } else {
-          const data = res && res.body ? res.body : {};
-          item.resolve(data);
+          res.json().then(data => {
+            item.reject(res.status >= 400 && res.status < 500 ?
+              new DiscordAPIError(item.path, data, item.method) : res);
+          }, item.reject);
           finish();
         }
       });
