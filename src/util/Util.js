ada9e1e2bebf9f810f8530143878174d8d7920bf
Unknown <bdistin@gmail.com>
2018-06-22 18:50:26 -0500

    update discordSort

--- src/util/Util.js
+++ src/util/Util.js
@@ -302,11 +302,7 @@ class Util {
    * @returns {Collection}
    */
   static discordSort(collection) {
-    return collection.sort((a, b) =>
-      a.rawPosition - b.rawPosition ||
-      parseInt(b.id.slice(0, -10)) - parseInt(a.id.slice(0, -10)) ||
-      parseInt(b.id.slice(10)) - parseInt(a.id.slice(10))
-    );
+    return collection.sort((a, b) => a.rawPosition - b.rawPosition || b.id - a.id);
   }
 
   /**
