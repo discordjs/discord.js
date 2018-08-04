93652ca3620198ac0119c1946dfa559631810846
Lewdcario <isabellafj97@gmail.com>
2018-08-03 19:54:01 -0600

    suggested changes

--- src/rest/RequestHandler.js
+++ src/rest/RequestHandler.js
@@ -5,7 +5,6 @@ const { Events: { RATE_LIMIT }, browser } = require('../util/Constants');
 
 function parseResponse(res) {
   if (res.headers.get('content-type').startsWith('application/json')) return res.json();
-  // eslint-disable-next-line no-undef
   if (browser) return res.blob();
   return res.buffer();
 }
@@ -47,7 +46,8 @@ class RequestHandler {
     return new Date(Number(reset) * 1000).getTime() - this._getAPIOffset(serverDate);
   }
 
-  execute(item) {
+  /* eslint-disable-next-line complexity */
+  async execute(item) {
     // Insert item back to the beginning if currently busy
     if (this.busy) {
       this.queue.unshift(item);
@@ -57,117 +57,120 @@ class RequestHandler {
     this.busy = true;
 
     // Perform the request
-    return item.request.make()
-      .then(async res => {
-        if (res && res.headers) {
-          if (res.headers.get('x-ratelimit-global')) this.manager.globallyRateLimited = true;
+    let res;
+    try {
+      res = await item.request.make();
+    } catch (error) {
+      // NodeFetch error expected for all "operational" errors, such as 500 status code
+      this.busy = false;
+      return item.reject(
+        new HTTPError(error.message, error.constructor.name, error.status, item.request.method, item.request.route)
+      );
+    }
 
-          const serverDate = res.headers.get('date');
-          const limit = res.headers.get('x-ratelimit-limit');
-          const remaining = res.headers.get('x-ratelimit-remaining');
-          const reset = res.headers.get('x-ratelimit-reset');
-          const retryAfter = res.headers.get('retry-after');
+    if (res && res.headers) {
+      if (res.headers.get('x-ratelimit-global')) this.manager.globallyRateLimited = true;
 
-          this.limit = limit ? Number(limit) : Infinity;
-          this.remaining = remaining ? Number(remaining) : 1;
-          this.reset = reset ? this._calculateReset(reset, serverDate) + 100 : Date.now();
-          this.retryAfter = retryAfter ? Number(retryAfter) : -1;
+      const serverDate = res.headers.get('date');
+      const limit = res.headers.get('x-ratelimit-limit');
+      const remaining = res.headers.get('x-ratelimit-remaining');
+      const reset = res.headers.get('x-ratelimit-reset');
+      const retryAfter = res.headers.get('retry-after');
 
-          // https://github.com/discordapp/discord-api-docs/issues/182
-          if (item.request.route.includes('reactions')) {
-            this.reset = Date.now() + this._getAPIOffset() + 250;
-          }
-        }
+      this.limit = limit ? Number(limit) : Infinity;
+      this.remaining = remaining ? Number(remaining) : 1;
+      this.reset = reset ? this._calculateReset(reset, serverDate) + 100 : Date.now();
+      this.retryAfter = retryAfter ? Number(retryAfter) : -1;
 
-        // After calculations, pre-emptively stop farther requests
-        if (this.limited) {
-          const timeout = this.reset - Date.now();
+      // https://github.com/discordapp/discord-api-docs/issues/182
+      if (item.request.route.includes('reactions')) {
+        this.reset = Date.now() + this._getAPIOffset() + 250;
+      }
+    }
 
-          if (this.manager.client.listenerCount(RATE_LIMIT)) {
-            /**
-             * Emitted when the client hits a rate limit while making a request
-             * @event Client#rateLimit
-             * @param {Object} rateLimitInfo Object containing the rate limit info
-             * @param {number} rateLimitInfo.timeout Timeout in ms
-             * @param {number} rateLimitInfo.limit Number of requests that can be made to this endpoint
-             * @param {string} rateLimitInfo.method HTTP method used for request that triggered this event
-             * @param {string} rateLimitInfo.path Path used for request that triggered this event
-             * @param {string} rateLimitInfo.route Route used for request that triggered this event
-             */
-            this.client.emit(RATE_LIMIT, {
-              timeout,
-              limit: this.limit,
-              method: item.request.method,
-              path: item.request.path,
-              route: item.request.route,
-            });
-          }
+    // After calculations, pre-emptively stop farther requests
+    if (this.limited) {
+      const timeout = this.reset - Date.now();
 
-          if (this.manager.globallyRateLimited && !this.manager.globalTimeout) {
-            // Set a global rate limit for all of the handlers instead of each one individually
-            this.manager.globalTimeout = this.manager.client.setTimeout(() => {
-              this.manager.globalTimeout = null;
-              this.manager.globallyRateLimited = false;
-              this.busy = false;
-              this.run();
-            }, timeout);
-          } else if (this.manager.globalTimeout) {
-            // Already waiting for a global rate limit to clear
-            this.queue.unshift(item);
-            return null;
-          } else {
-            // Wait for the timeout to expire in order to avoid an actual 429
-            await Util.delayFor(timeout);
-            this.busy = false;
-            return this.run();
-          }
-        }
+      if (this.manager.client.listenerCount(RATE_LIMIT)) {
+        /**
+         * Emitted when the client hits a rate limit while making a request
+         * @event Client#rateLimit
+         * @param {Object} rateLimitInfo Object containing the rate limit info
+         * @param {number} rateLimitInfo.timeout Timeout in ms
+         * @param {number} rateLimitInfo.limit Number of requests that can be made to this endpoint
+         * @param {string} rateLimitInfo.method HTTP method used for request that triggered this event
+         * @param {string} rateLimitInfo.path Path used for request that triggered this event
+         * @param {string} rateLimitInfo.route Route used for request that triggered this event
+         */
+        this.manager.client.emit(RATE_LIMIT, {
+          timeout,
+          limit: this.limit,
+          method: item.request.method,
+          path: item.request.path,
+          route: item.request.route,
+        });
+      }
 
-        // Finished handling headers, safe to unlock manager
+      if (this.manager.globallyRateLimited && !this.manager.globalTimeout) {
+        // Set a global rate limit for all of the handlers instead of each one individually
+        this.manager.globalTimeout = this.manager.client.setTimeout(() => {
+          this.manager.globalTimeout = null;
+          this.manager.globallyRateLimited = false;
+          this.busy = false;
+          this.run();
+        }, timeout);
+      } else if (this.manager.globalTimeout) {
+        // Already waiting for a global rate limit to clear
+        this.queue.unshift(item);
+        return null;
+      } else {
+        // Wait for the timeout to expire in order to avoid an actual 429
+        await Util.delayFor(timeout);
         this.busy = false;
+        return this.run();
+      }
+    }
 
-        if (res.ok) {
-          return parseResponse(res)
-            .then(success => {
-              // Nothing wrong with the request, proceed with the next
-              item.resolve(success);
-              this.run();
-            });
-        } else if (res.status === 429) {
-          // A ratelimit was hit - this should never happen
-          this.queue.unshift(item);
-          await Util.delayFor(this.retryAfter);
-          return this.run();
-        } else if (res.status >= 500 && res.status < 600) {
-          // Retry once for possible serverside issues
-          if (item.retried) {
-            return item.reject(
-              new HTTPError(res.statusText, res.constructor.name, res.status, item.request.method, item.request.route)
-            );
-          } else {
-            item.retried = true;
-            this.queue.unshift(item);
-            return this.run();
-          }
-        } else {
-          // Handle possible malformed requessts
-          return parseResponse(res)
-            .then(data => {
-              if (res.status >= 400 && res.status < 500) {
-                return item.reject(new DiscordAPIError(item.request.route, data, item.request.method));
-              }
-              return null;
-            }, err => item.reject(
-              new HTTPError(err.message, err.constructor.name, err.status, item.request.method, item.request.route)
-            ));
-        }
-      }, e => {
-        // NodeFetch error expected for all "operational" errors, such as 500 status code
-        this.busy = false;
+    // Finished handling headers, safe to unlock manager
+    this.busy = false;
+
+    if (res.ok) {
+      const success = await parseResponse(res);
+      // Nothing wrong with the request, proceed with the next
+      item.resolve(success);
+      return this.run();
+    } else if (res.status === 429) {
+      // A ratelimit was hit - this should never happen
+      this.queue.unshift(item);
+      this.manager.client.emit('debug', `429 hit on route ${item.request.route}`);
+      await Util.delayFor(this.retryAfter);
+      return this.run();
+    } else if (res.status >= 500 && res.status < 600) {
+      // Retry once for possible serverside issues
+      if (item.retried) {
         return item.reject(
-          new HTTPError(e.message, e.constructor.name, e.status, item.request.method, item.request.route)
+          new HTTPError(res.statusText, res.constructor.name, res.status, item.request.method, item.request.route)
         );
-      });
+      } else {
+        item.retried = true;
+        this.queue.unshift(item);
+        return this.run();
+      }
+    } else {
+      // Handle possible malformed requessts
+      try {
+        const data = await parseResponse(res);
+        if (res.status >= 400 && res.status < 500) {
+          return item.reject(new DiscordAPIError(item.request.route, data, item.request.method));
+        }
+        return null;
+      } catch (err) {
+        return item.reject(
+          new HTTPError(err.message, err.constructor.name, err.status, item.request.method, item.request.route)
+        );
+      }
+    }
   }
 }
 
