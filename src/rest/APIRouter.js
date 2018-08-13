c202a4b1c9c0a859d262f682d6d29aace3511012
kyraNET <kyradiscord@gmail.com>
2018-08-13 18:26:04 +0200

    fix: Capture stack traces in APIRouter to preserve async stack

--- src/rest/APIRouter.js
+++ src/rest/APIRouter.js
@@ -11,13 +11,21 @@ function buildRoute(manager) {
     get(target, name) {
       if (reflectors.includes(name)) return () => route.join('/');
       if (methods.includes(name)) {
+        // Preserve async stack
+        const stackTrace = {};
+        Error.captureStackTrace(stackTrace, this.get);
+
         return options => manager.request(name, route.join('/'), Object.assign({
           versioned: manager.versioned,
           route: route.map((r, i) => {
             if (/\d{16,19}/g.test(r)) return /channels|guilds/.test(route[i - 1]) ? r : ':id';
             return r;
           }).join('/'),
-        }, options));
+        }, options)).catch(error => {
+          stackTrace.message = error.message;
+          error.stack = `${error.constructor.name}:${stackTrace.stack.substring(6)}`;
+          throw error;
+        });
       }
       route.push(name);
       return new Proxy(noop, handler);
