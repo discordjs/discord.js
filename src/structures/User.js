0ad7c24aa394249a5213a8e11a826821d4b56266
bdistin <bdistin@gmail.com>
2018-08-14 12:09:10 -0500

    docs(User): fix hydrabolt's tag (#2747)

--- src/structures/User.js
+++ src/structures/User.js
@@ -141,7 +141,7 @@ class User extends Base {
   }
 
   /**
-   * The Discord "tag" (e.g. `hydrabolt#0086`) for this user
+   * The Discord "tag" (e.g. `hydrabolt#0001`) for this user
    * @type {string}
    * @readonly
    */
