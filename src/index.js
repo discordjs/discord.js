b559b2b9afe51033ff89d85e375a28e17fc9f63f
1Computer1 <mineox100@gmail.com>
2018-08-19 22:28:13 -0400

    Expose APIMessage

--- src/index.js
+++ src/index.js
@@ -48,6 +48,7 @@ module.exports = {
   // Structures
   Base: require('./structures/Base'),
   Activity: require('./structures/Presence').Activity,
+  APIMessage: require('./structures/APIMessage'),
   CategoryChannel: require('./structures/CategoryChannel'),
   Channel: require('./structures/Channel'),
   ClientApplication: require('./structures/ClientApplication'),
