b05f5ac9b47076ef97875ef6f8d3eab4f0c57f7c
Yukine <blazylp@gmail.com>
2018-09-05 15:42:56 +0200

    fix indent

--- src/structures/GuildBan.js
+++ src/structures/GuildBan.js
@@ -7,27 +7,27 @@ class GuildBan extends Base {
   constructor(guild, data, fetched) {
     super(guild.client);
     /**
-	 * Reason the user was banned
-	 * @type {string}
-	 */
+     * Reason the user was banned
+     * @type {string}
+     */
     this.reason = data.reason;
 
     /**
-	 * Guild where the Ban occurred
-	 * @type {Guild}
-	 */
+     * Guild where the Ban occurred
+     * @type {Guild}
+     */
     this.guild = guild;
 
     /**
-	 * If this GuildBan is fetched and will be accurate about the reason
-	 * @type {boolean}
-	 */
+     * If this GuildBan is fetched and will be accurate about the reason
+     * @type {boolean}
+     */
     this.fetched = fetched;
 
     /**
-	 * User that was banned.
-	 * @type {User}
-	 */
+     * User that was banned.
+     * @type {User}
+     */
     this.user = this.client.users.add(data.user);
   }
 }
