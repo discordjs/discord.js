6ffacb13978985b7a0e46233d9813fbfe61e2084
ndr3w221 <kalaz.bb@gmail.com>
2018-11-20 19:09:00 +0200

    fix1.b

--- src/util/Util.js
+++ src/util/Util.js
@@ -279,11 +279,11 @@ class Util {
    */
   static resolveColor(color) {
     if (typeof color === 'string') {
-      if (color === 'RANDOM') return Math.floor(Math.random() * (0xFFFFFF + 1));
-      if (color === 'DEFAULT') return 0;
-      color = Colors[color] || parseInt(color.replace('#', ''), 16);
+      if (color.toUpperCase() === 'RANDOM') return Math.floor(Math.random() * (0xFFFFFF + 1));
+      if (color.toUpperCase() === 'DEFAULT') return 0;
+      color = Colors[color.toUpperCase()] || parseInt(color.toUpperCase().replace('#', ''), 16);
     } else if (color instanceof Array) {
-      color = (color[0] << 16) + (color[1] << 8) + color[2];
+      color = (color[0].toUpperCase() << 16) + (color[1].toUpperCase() << 8) + color[2].toUpperCase();
     }
 
     if (color < 0 || color > 0xFFFFFF) throw new RangeError('COLOR_RANGE');
