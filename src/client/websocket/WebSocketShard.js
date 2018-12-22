5cbdf380289f0fd1d16330325dab71f8815396fc
Will Nelson <will@pleb.xyz>
2018-12-21 23:49:56 -0800

    fix(WebSocketShard): add websocket send error handling (#2981)
    * websocket send error handling
    
    * fix: emit only when error is present
    
    * refactor: use an if instead

--- src/client/websocket/WebSocketShard.js
+++ src/client/websocket/WebSocketShard.js
@@ -437,7 +437,9 @@ class WebSocketShard extends EventEmitter {
       this.debug(`Tried to send packet ${data} but no WebSocket is available!`);
       return;
     }
-    this.ws.send(WebSocket.pack(data));
+    this.ws.send(WebSocket.pack(data), err => {
+      if (err) this.manager.client.emit(Events.ERROR, err);
+    });
   }
 
   /**
