998b128c9af5f0c3d4fe1ccbfc422d70dba90510
Darqam <anhim2@gmail.com>
2019-02-01 20:50:58 +0100

    Add spoiler syntax to escapeMarkdown

--- src/util/Util.js
+++ src/util/Util.js
@@ -79,7 +79,7 @@ class Util {
   static escapeMarkdown(text, onlyCodeBlock = false, onlyInlineCode = false) {
     if (onlyCodeBlock) return text.replace(/```/g, '`\u200b``');
     if (onlyInlineCode) return text.replace(/\\(`|\\)/g, '$1').replace(/(`|\\)/g, '\\$1');
-    return text.replace(/\\(\*|_|`|~|\\)/g, '$1').replace(/(\*|_|`|~|\\)/g, '\\$1');
+    return text.replace(/\\(\*|_|`|~|\||\\)/g, '$1').replace(/(\*|_|`|~|\||\\)/g, '\\$1');
   }
 
   /**
