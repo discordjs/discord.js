db3ae0159bf2d8a44e7ecda4b9835a08b9210e8b
SpaceEEC <spaceeec@yahoo.com>
2019-02-02 20:28:24 +0100

    docs(Structures): note about extending prior to instantiating client (#2884)

--- src/util/Structures.js
+++ src/util/Structures.js
@@ -20,6 +20,8 @@ class Structures {
 
   /**
    * Extends a structure.
+   * <warn> Make sure to extend all structures before instantiating your client.
+   * Extending after doing so may not work as expected. </warn>
    * @param {string} structure Name of the structure class to extend
    * @param {Function} extender Function that takes the base class to extend as its only parameter and returns the
    * extended class/prototype
