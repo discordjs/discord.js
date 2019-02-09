49e0839e54c8d705cd2b4795921a94dc9f089b00
Amish Shah <amishshah.2k@gmail.com>
2019-02-09 18:13:15 +0000

    Reaction remove partials

--- src/client/actions/MessageReactionRemove.js
+++ src/client/actions/MessageReactionRemove.js
@@ -14,13 +14,16 @@ class MessageReactionRemove extends Action {
   handle(data) {
     const user = this.client.users.get(data.user_id);
     if (!user) return false;
+
     // Verify channel
-    const channel = this.client.channels.get(data.channel_id);
+    const channel = this.getChannel(data);
     if (!channel || channel.type === 'voice') return false;
+
     // Verify message
-    const message = channel.messages.get(data.message_id);
+    const message = this.getMessage(data, channel);
     if (!message) return false;
     if (!data.emoji) return false;
+
     // Verify reaction
     const emojiID = data.emoji.id || decodeURIComponent(data.emoji.name);
     const reaction = message.reactions.get(emojiID);
