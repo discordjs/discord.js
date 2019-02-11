8d574dfcbbcf42bf106bf94b71b14005e0f9e5fb
Amish Shah <amishshah.2k@gmail.com>
2019-02-11 21:32:50 +0000

    partials: fix messageUpdate behaviour (now "old" message can be partial)

--- src/client/actions/MessageUpdate.js
+++ src/client/actions/MessageUpdate.js
@@ -6,7 +6,8 @@ class MessageUpdateAction extends Action {
   handle(data) {
     const channel = this.getChannel(data);
     if (channel) {
-      const message = this.getMessage(data, channel);
+      const { id, channel_id, guild_id, author, timestamp, type } = data;
+      const message = this.getMessage({ id, channel_id, guild_id, author, timestamp, type }, channel);
       if (message) {
         message.patch(data);
         return {
