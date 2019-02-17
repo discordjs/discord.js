74f639d3122253d03c86cdf9b5203779993440f0
1computer1 <mineox100@gmail.com>
2019-02-16 21:38:48 -0500

    Add clone since sort is now in-place

--- src/util/Util.js
+++ src/util/Util.js
@@ -300,7 +300,7 @@ class Util {
    * @returns {Collection}
    */
   static discordSort(collection) {
-    return collection.sort((a, b) =>
+    return collection.clone().sort((a, b) =>
       a.rawPosition - b.rawPosition ||
       parseInt(b.id.slice(0, -10)) - parseInt(a.id.slice(0, -10)) ||
       parseInt(b.id.slice(10)) - parseInt(a.id.slice(10))
