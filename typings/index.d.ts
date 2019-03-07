75a8b109faf84d860b8acef0f156b9cd1e37236b
almostSouji <timoqueezle@gmail.com>
2019-03-07 14:32:29 +0100

    adapt typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -12,7 +12,7 @@ declare module 'discord.js' {
 
 	export const version: string;
 
-//#region Classes
+	//#region Classes
 
 	class Attachment {
 		constructor(file: BufferResolvable | Stream, name?: string);
@@ -1531,9 +1531,9 @@ declare module 'discord.js' {
 		public setTimeout(fn: Function, delay: number, ...args: any[]): NodeJS.Timer;
 	}
 
-//#endregion
+	//#endregion
 
-//#region Mixins
+	//#region Mixins
 
 	// Model the TextBasedChannel mixin system, allowing application of these fields
 	// to the classes that use these methods without having to manually add them
@@ -1573,9 +1573,9 @@ declare module 'discord.js' {
 		stopTyping(force?: boolean): void;
 	} & PartialTextBasedChannelFields;
 
-//#endregion
+	//#endregion
 
-//#region Typedefs
+	//#region Typedefs
 
 	type ActivityType = 'PLAYING'
 		| 'STREAMING'
@@ -2037,7 +2037,7 @@ declare module 'discord.js' {
 
 	type ClientPresenceStatus = 'online' | 'idle' | 'dnd';
 
-	type PresenceStatus = ClientPresenceStatus | 'invisible' ;
+	type PresenceStatus = ClientPresenceStatus | 'invisible';
 	type PresenceStatusData = ClientPresenceStatus | 'offline';
 
 	type ClientPresenceStatusData = {
@@ -2047,7 +2047,7 @@ declare module 'discord.js' {
 	};
 
 	type RateLimitInfo = {
-		requestLimit: number;
+		limit: number;
 		timeDifference: number;
 		method: string;
 		path: string;
@@ -2166,5 +2166,5 @@ declare module 'discord.js' {
 		| 'RELATIONSHIP_ADD'
 		| 'RELATIONSHIP_REMOVE';
 
-//#endregion
+	//#endregion
 }
