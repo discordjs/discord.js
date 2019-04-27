4e0cf87d0fd717d781d6e633402fb2b1b6ac6fd3
Amish Shah <amishshah.2k@gmail.com>
2019-04-27 18:52:26 +0100

    fix: typing map being reset for ClientUser (#3216)

--- src/structures/ClientUser.js
+++ src/structures/ClientUser.js
@@ -8,6 +8,12 @@ const DataResolver = require('../util/DataResolver');
  * @extends {User}
  */
 class ClientUser extends Structures.get('User') {
+
+  constructor(client, data) {
+    super(client, data);
+    this._typing = new Map();
+  }
+
   _patch(data) {
     super._patch(data);
 
@@ -22,8 +28,6 @@ class ClientUser extends Structures.get('User') {
      * @type {?boolean}
      */
     this.mfaEnabled = typeof data.mfa_enabled === 'boolean' ? data.mfa_enabled : null;
-
-    this._typing = new Map();
 
     if (data.token) this.client.token = data.token;
   }
