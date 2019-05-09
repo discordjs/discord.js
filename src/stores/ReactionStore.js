ef564f6c19842dd159111aa1524ddc844e1c5297
izexi <43889168+izexi@users.noreply.github.com>
2019-05-10 00:23:37 +0100

    accommodate for fully removed reactions

--- src/stores/ReactionStore.js
+++ src/stores/ReactionStore.js
@@ -51,19 +51,24 @@ class ReactionStore extends DataStore {
       .then(() => this.message);
   }
 
-  _partial(reaction) {
-    const id = reaction.id || decodeURIComponent(reaction.name);
+  _partial(emoji) {
+    const id = emoji.id || emoji.name;
     const existing = this.get(id);
     return !existing || existing.partial;
   }
 
   async _fetchReaction(reactionEmoji, cache) {
-    const id = reactionEmoji.id || decodeURIComponent(reactionEmoji.name);
+    const id = reactionEmoji.id || reactionEmoji.name;
     const existing = this.get(id);
     if (!this._partial(reactionEmoji)) return existing;
     const data = await this.client.api.channels(this.message.channel.id).messages(this.message.id).get();
+    if (!data.reactions || !data.reactions.some(r => (r.emoji.id || r.emoji.name) === id)) {
+      reactionEmoji.reaction._patch({ count: 0 });
+      this.message.reactions.remove(id);
+      return existing;
+    }
     for (const reaction of data.reactions) {
-      if (this._partial(reaction)) this.add(reaction, cache);
+      if (this._partial(reaction.emoji)) this.add(reaction, cache);
     }
     return existing;
   }
