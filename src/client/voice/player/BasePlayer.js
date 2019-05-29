d1c4d423e9c7d9c8af9f9b562d1aeac1a29df317
Amish Shah <amishshah.2k@gmail.com>
2019-05-29 15:30:32 +0100

    voice: use ffmpeg libopus where available

--- src/client/voice/player/BasePlayer.js
+++ src/client/voice/player/BasePlayer.js
@@ -5,13 +5,24 @@ const { Readable: ReadableStream } = require('stream');
 const prism = require('prism-media');
 const StreamDispatcher = require('../dispatcher/StreamDispatcher');
 
-const FFMPEG_ARGUMENTS = [
-  '-analyzeduration', '0',
-  '-loglevel', '0',
-  '-f', 's16le',
-  '-ar', '48000',
-  '-ac', '2',
-];
+const FFMPEG_ARGUMENTS = {
+  DEFAULT: [
+    '-analyzeduration', '0',
+    '-loglevel', '0',
+    '-f', 's16le',
+    '-ar', '48000',
+    '-ac', '2',
+  ],
+  OGGOPUS: bitrate => [
+    '-analyzeduration', '0',
+    '-loglevel', '0',
+    '-acodec', 'libopus',
+    '-b:a', `${bitrate}K`,
+    '-f', 'opus',
+    '-ar', '48000',
+    '-ac', '2',
+  ],
+};
 
 /**
  * An Audio Player for a Voice Connection.
@@ -46,8 +57,19 @@ class BasePlayer extends EventEmitter {
     this.destroyDispatcher();
 
     const isStream = input instanceof ReadableStream;
+    const isFFmpegOpus = options.volume === false && prism.FFmpeg.load().info.includes('--enable-libopus');
 
-    const args = isStream ? FFMPEG_ARGUMENTS.slice() : ['-i', input, ...FFMPEG_ARGUMENTS];
+    let bitrate;
+    if (options.bitrate) {
+      bitrate = options.bitrate;
+    } else if (this.voiceConnection && this.voiceConnection.channel) {
+      bitrate = this.voiceConnection.channel.bitrate / 1000;
+    }
+
+    let args = isFFmpegOpus ? FFMPEG_ARGUMENTS.OGGOPUS(bitrate || 48).slice() : FFMPEG_ARGUMENTS.DEFAULT.slice();
+
+    if (!isStream) args.unshift('-i', input);
+
     if (options.seek) args.unshift('-ss', String(options.seek));
 
     const ffmpeg = new prism.FFmpeg({ args });
@@ -56,7 +78,12 @@ class BasePlayer extends EventEmitter {
       streams.input = input;
       input.pipe(ffmpeg);
     }
-    return this.playPCMStream(ffmpeg, options, streams);
+
+    if (isFFmpegOpus) {
+      return this.playOpusStream(ffmpeg.pipe(new prism.opus.OggDemuxer()), options, streams);
+    } else {
+      return this.playPCMStream(ffmpeg, options, streams);
+    }
   }
 
   playPCMStream(stream, options, streams = {}) {
