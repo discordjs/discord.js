989f0953545c82544f72505597813e62b26b41c1
izexi <43889168+izexi@users.noreply.github.com>
2019-06-02 09:30:15 +0100

    add premiumTier and premiumSubscriptionCount

--- src/structures/Guild.js
+++ src/structures/Guild.js
@@ -189,6 +189,30 @@ class Guild extends Base {
     this.embedEnabled = data.embed_enabled;
 
     /**
+     * The type of premium tier:
+     * * 0: NONE
+     * * 1: TIER_1
+     * * 2: TIER_2
+     * * 3: TIER_3
+     * @typedef {number} PremiumTier
+     */
+
+    /**
+     * The premium tier on this guild
+     * @type {PremiumTier}
+     */
+    this.premiumTier = data.premium_tier;
+
+    /**
+     * The total number of users currently boosting this server
+     * @type {?number}
+     * @name Guild#premiumSubscriptionCount
+     */
+    if (typeof data.premium_subscription_count !== 'undefined') {
+      this.premiumSubscriptionCount = data.premium_subscription_count;
+    }
+
+    /**
      * Whether widget images are enabled on this guild
      * @type {?boolean}
      * @name Guild#widgetEnabled
