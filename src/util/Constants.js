9cbbf35665844f9bbcc86ee3737371de32e5ff59
SpaceEEC <spaceeec@yahoo.com>
2019-06-08 12:12:32 +0200

    feat(Guild): default iconURL to gif if animated

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -131,8 +131,10 @@ exports.Endpoints = {
         if (format === 'default') format = hash.startsWith('a_') ? 'gif' : 'webp';
         return makeImageUrl(`${root}/avatars/${userID}/${hash}`, { format, size });
       },
-      Banner: (guildID, hash, format = 'webp', size) =>
-        makeImageUrl(`${root}/banners/${guildID}/${hash}`, { format, size }),
+      Banner: (guildID, hash, format = 'default', size) => {
+        if (format === 'default') format = hash.startsWith('a_') ? 'gif' : 'webp';
+        return makeImageUrl(`${root}/banners/${guildID}/${hash}`, { format, size });
+      },
       Icon: (guildID, hash, format = 'webp', size) =>
         makeImageUrl(`${root}/icons/${guildID}/${hash}`, { format, size }),
       AppIcon: (clientID, hash, { format = 'webp', size } = {}) =>
