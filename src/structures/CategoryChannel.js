cd5554bab2b422d5eaaadd16e83c22e431b996eb
RighteousPotato <36886629+RighteousPotato@users.noreply.github.com>
2019-07-17 11:52:05 +0100

    CategoryChannel#createChannel()
    Adds a convenience function to CategoryChannel to create a new channel with itself as the parent

--- src/structures/CategoryChannel.js
+++ src/structures/CategoryChannel.js
@@ -17,6 +17,36 @@ class CategoryChannel extends GuildChannel {
   get children() {
     return this.guild.channels.filter(c => c.parentID === this.id);
   }
+  /**
+   * Creates a new channel with this category as its parent.
+   * @param {string} name The name of the new channel
+   * @param {ChannelData} [options] Options for the new channel
+   * @returns {Promise<CategoryChannel|TextChannel|VoiceChannel>}
+   * @example
+   * // Create a new text channel
+   * category.createChannel('new-general')
+   *   .then(console.log)
+   *   .catch(console.error);
+   * @example
+   * // Create a new channel with permission overwrites
+   * category.createChannel('new-restricted', {
+   *   permissionOverwrites: [{
+   *     id: guild.id,
+   *     deny: ['MANAGE_MESSAGES'],
+   *     allow: ['SEND_MESSAGES']
+   *   }]
+   * })
+   *   .then(console.log)
+   *   .catch(console.error);
+   */
+  createChannel(name, options) {
+	if (!options) {
+	  options = { parent: this.id };
+	} else {
+	  options.parent = this.id;
+	}
+    return this.guild.createChannel(name, options);
+  }
 }
 
 module.exports = CategoryChannel;
