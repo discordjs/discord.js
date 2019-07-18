e63e1011f6d572a4ae81014b5305873c4e43a161
RighteousPotato <36886629+RighteousPotato@users.noreply.github.com>
2019-07-18 10:31:50 +0100

    Add parent property
    For use with CategoryChannel.children.create()

--- src/stores/GuildChannelStore.js
+++ src/stores/GuildChannelStore.js
@@ -23,6 +23,15 @@ class GuildChannelStore extends DataStore {
   }
 
   /**
+     * The parent {@link Channel} of the {@link GuildChannel}s in this store *(Only if the store
+     * was retrieved from {@link CategoryChannel}.children)*.
+     * @type {?GuildChannel>}
+     * @readonly
+     */
+  get parent() {
+    return this._parent;
+  }
+  /**
    * Data that can be resolved to give a Guild Channel object. This can be:
    * * A GuildChannel object
    * * A Snowflake
