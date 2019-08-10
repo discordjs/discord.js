32d5bc8864b93df90ff7360c7afbefca440aaac5
Vlad Frangu <kingdgrizzle@gmail.com>
2019-08-10 12:38:07 +0300

    src: Turn uncatchable error in MemberStore#unban into a catch-able one

--- src/stores/GuildMemberStore.js
+++ src/stores/GuildMemberStore.js
@@ -177,7 +177,7 @@ class GuildMemberStore extends DataStore {
    */
   unban(user, reason) {
     const id = this.client.users.resolveID(user);
-    if (!id) throw new Error('BAN_RESOLVE_ID');
+    if (!id) return Promise.reject(new Error('BAN_RESOLVE_ID'));
     return this.client.api.guilds(this.guild.id).bans[id].delete({ reason })
       .then(() => this.client.users.resolve(user));
   }
