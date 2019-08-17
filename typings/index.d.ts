6d3c55b68cbdb258421e017577f52da0d8deaf85
bdistin <bdistin@gmail.com>
2019-08-17 10:57:45 -0500

    feat(Collector): allow collectors to be consumed by for-await-of loops (#3269)

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -363,6 +363,7 @@ declare module 'discord.js' {
 		public handleCollect(...args: any[]): void;
 		public handleDispose(...args: any[]): void;
 		public stop(reason?: string): void;
+		public [Symbol.asyncIterator](): AsyncIterableIterator<V>;
 		public toJSON(): object;
 
 		protected listener: Function;
