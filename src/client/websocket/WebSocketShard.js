4d7c0d3b9bb8f630b72d90f3697970ae0db681eb
Vlad Frangu <kingdgrizzle@gmail.com>
2019-08-18 22:12:52 +0300

    src: Fix a possible bug for when the ws dies and the session becomes -1

--- src/client/websocket/WebSocketShard.js
+++ src/client/websocket/WebSocketShard.js
@@ -209,7 +209,7 @@ class WebSocketShard extends EventEmitter {
       this.once(ShardEvents.INVALID_SESSION, onInvalid);
 
       if (this.connection && this.connection.readyState === WebSocket.OPEN) {
-        this.identifyNew();
+        this.identify();
         return;
       }
 
@@ -661,6 +661,7 @@ class WebSocketShard extends EventEmitter {
     this.connection = null;
     // Set the shard status
     this.status = Status.DISCONNECTED;
+    this.closeSequence = this.sequence;
     // Reset the sequence
     this.sequence = -1;
     // Reset the ratelimit data
