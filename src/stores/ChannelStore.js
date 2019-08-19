de30a57d7baa91e2a674fb44db4dd3197fd8fc5c
izexi <43889168+izexi@users.noreply.github.com>
2019-08-19 10:56:39 +0100

    ref: group nested conditions

--- src/stores/ChannelStore.js
+++ src/stores/ChannelStore.js
@@ -54,8 +54,8 @@ class ChannelStore extends DataStore {
 
   add(data, guild, cache = true) {
     const existing = this.get(data.id);
-    if (existing && existing._patch && cache) existing._patch(data);
     if (existing) {
+      if (existing._patch && cache) existing._patch(data);
       if (guild) guild.channels.add(existing);
       return existing;
     }
