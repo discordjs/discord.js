5d95a4b264eae333af67ecfceaad1aa63dfdea0e
BannerBomb <BannerBomb55@gmail.com>
2019-09-03 10:24:20 -0400

    fix: Util#splitMessage when destructured (#3456)

--- src/util/Util.js
+++ src/util/Util.js
@@ -56,7 +56,7 @@ class Util {
    * @returns {string[]}
    */
   static splitMessage(text, { maxLength = 2000, char = '\n', prepend = '', append = '' } = {}) {
-    text = this.resolveString(text);
+    text = Util.resolveString(text);
     if (text.length <= maxLength) return [text];
     const splitText = text.split(char);
     if (splitText.some(chunk => chunk.length > maxLength)) throw new RangeError('SPLIT_MAX_LEN');
