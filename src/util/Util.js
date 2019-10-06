a17719a89b8304e25ded5c6a082336b1c7e681d6
matthewfripp <50251454+matthewfripp@users.noreply.github.com>
2019-10-06 18:41:26 +0100

    forgot return if null

--- src/util/Util.js
+++ src/util/Util.js
@@ -247,7 +247,9 @@ class Util {
     const uEmoji = /(?<unicode>[\u{1f300}-\u{1f5ff}\u{1f900}-\u{1f9ff}\u{1f600}-\u{1f64f}\u{1f680}-\u{1f6ff}\u{2600}-\u{26ff}\u{2700}-\u{27bf}\u{1f1e6}-\u{1f1ff}\u{1f191}-\u{1f251}\u{1f004}\u{1f0cf}\u{1f170}-\u{1f171}\u{1f17e}-\u{1f17f}\u{1f18e}\u{3030}\u{2b50}\u{2b55}\u{2934}-\u{2935}\u{2b05}-\u{2b07}\u{2b1b}-\u{2b1c}\u{3297}\u{3299}\u{303d}\u{00a9}\u{00ae}\u{2122}\u{23f3}\u{24c2}\u{23e9}-\u{23ef}\u{25b6}\u{23f8}-\u{23fa}])/u;
     const dEmoji = /<?(?:(?<animated>a):)?(?<name>\w{2,32}):(?<id>\d{17,19})?>?/;
     const regex = new RegExp(`${dEmoji.source}|${uEmoji.source}`, 'u');
-    const { groups: { animated, name, id, unicode } } = text.match(regex);
+    const match = text.match(regex);
+    if (!match) return null;
+    const { groups: { animated, name, id, unicode } } = match;
     return { animated: Boolean(animated), name: name || unicode, id: id || null };
   }
 
