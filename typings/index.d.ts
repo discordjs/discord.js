a6b247ef40f198e68b6cdedaca629e87bd56a06e
SpaceEEC <spaceeec@yahoo.com>
2019-11-14 09:53:50 +0100

    fix: make params optional

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1124,9 +1124,9 @@ declare module 'discord.js' {
 		public static FLAGS: PermissionFlags;
 		public static resolve(permission?: PermissionResolvable): number;
 
-		public missing(bit: BitFieldResolvable<PermissionString>, checkAdmin: boolean): PermissionString[];
-		public serialize(checkAdmin: boolean): Record<PermissionString, boolean>;
-		public toArray(checkAdmin: boolean): PermissionString[];
+		public missing(bit: BitFieldResolvable<PermissionString>, checkAdmin?: boolean): PermissionString[];
+		public serialize(checkAdmin?: boolean): Record<PermissionString, boolean>;
+		public toArray(checkAdmin?: boolean): PermissionString[];
 	}
 
 	export class Presence {
