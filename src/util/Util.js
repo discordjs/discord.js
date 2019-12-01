fb5c652731bd9c4ed69e8c17526a3a246ced1606
Timo <30553356+y21@users.noreply.github.com>
2019-12-01 11:57:44 +0100

    merge with first .replace call (cleanContent)

--- src/util/Util.js
+++ src/util/Util.js
@@ -524,8 +524,7 @@ class Util {
    */
   static cleanContent(str, message) {
     return str
-      .replace(/@(everyone|here)/g, '@\u200b$1')
-      .replace(/\u202e/g, '')
+      .replace(/@\u202e?(everyone|here)/g, '@\u200b$1')
       .replace(/<@!?[0-9]+>/g, input => {
         const id = input.replace(/<|!|>|@/g, '');
         if (message.channel.type === 'dm') {
