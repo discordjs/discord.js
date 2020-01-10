eaf56a3f723fa6b8b1e18d949ec18b8bca8b3284
Gryffon Bellish <owenbellish@gmail.com>
2020-01-10 12:03:23 -0500

    add docs

--- src/structures/GuildMemberCollector.js
+++ src/structures/GuildMemberCollector.js
@@ -3,12 +3,35 @@
 const Collector = require('./interfaces/Collector');
 const { Events } = require('../util/Constants');
 
+/**
+ * @typedef {CollectorOptions} GuildMemberCollectorOptions
+ * @property {number} max The maximum amount of guild members to collect
+ * @property {number} maxJoined The maximum amount of guild members that should join
+ */
+
+/**
+  * Collects guild members in a guild.
+  * @extends {Collector}
+  */
 class GuildMemberCollector extends Collector {
+  /**
+   * @param {Guild} guild The guild
+   * @param {CollectorFilter} filter The filter to be appleid to this collector
+   * @param {GuildMemberCollectorOptions} options The options to be applied to this collector
+   */
   constructor(guild, filter, options = {}) {
     super(guild.client, filter, options);
 
+    /**
+     * The guild upon which to collect joins
+     * @type {Guild}
+     */
     this.guild = guild;
 
+    /**
+     * The total number of members that joined the guild
+     * @type {number}
+     */
     this.joined = 0;
 
     const chunkListener = (members => {
@@ -28,16 +51,42 @@ class GuildMemberCollector extends Collector {
     });
   }
 
+  /**
+   * Handles a member for possible collection.
+   * @param {GuildMember} member The member that joined
+   * @returns {?Snowflake}
+   * @private
+   */
   collect(member) {
+    /**
+     * Emitted whenever a member joins the guild.
+     * @event GuildMemberCollector#collect
+     * @param {GuildMember} member The member that joined
+     */
     if (member.guild.id !== this.guild.id) return null;
     this.joined++;
     return member.id;
   }
 
+  /**
+   * Handles a member for possible disposal.
+   * @param {GuildMember} member The member that could be disposed of
+   * @returns {?Snowflake}
+   */
   dispose(member) {
+    /**
+     * Emitted whenever a member is disposed of.
+     * @event GuildMemberCollector#dispose
+     * @param {GuildMember} member The member that was disposed of
+     */
     return member.guild.id === this.guild.id ? member.id : null;
   }
 
+  /**
+   * Checks after un/collecting to see if the collector is done.
+   * @returns {?string}
+   * @private
+   */
   endReason() {
     if (this.options.max && this.collected.size > this.options.max) return 'limit';
     if (this.options.maxJoined && this.joined > this.options.maxJoined) return 'joinedLimit';
