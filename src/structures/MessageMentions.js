1f61313602ba87c4b9112cb52aea7121556b1d9e
Monbrey <rsm999@uowmail.edu.au>
2020-01-13 07:13:14 +1100

    Cache mentioned members

--- src/structures/MessageMentions.js
+++ src/structures/MessageMentions.js
@@ -21,6 +21,9 @@ class MessageMentions {
       } else {
         this.users = new Collection();
         for (const mention of users) {
+          if (mention.member && message.guild) {
+            message.guild.members.set(mention.id, Object.assign(mention.member, { user: mention }));
+          }
           let user = message.client.users.get(mention.id);
           if (!user) user = message.client.dataManager.newUser(mention);
           this.users.set(user.id, user);
