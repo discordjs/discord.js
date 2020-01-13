5ef37bfb6fa391c9cecd4e766ec997b71b9a5de0
Antonio Román <kyradiscord@gmail.com>
2020-01-13 16:44:52 +0100

    typings: Fixed build error

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1635,7 +1635,7 @@ declare module 'discord.js' {
 	export class Webhook extends WebhookMixin() {
 		constructor(client: Client, data?: object);
 		public avatar: string;
-		public avatarURL(options?: AvatarOptions): string | null;
+		public avatarURL(options?: ImageURLOptions): string | null;
 		public channelID: Snowflake;
 		public guildID: Snowflake;
 		public name: string;
