e80559c52e720f64a9ace001bbd69fe3d69e8a43
RDambrosio016 <cavalier.archer@gmail.com>
2020-01-19 17:55:11 -0500

    fix: more missed cache

--- src/structures/GuildChannel.js
+++ src/structures/GuildChannel.js
@@ -118,7 +118,7 @@ class GuildChannel extends Channel {
     if (!verified) member = this.guild.members.resolve(member);
     if (!member) return [];
 
-    roles = roles || member.roles;
+    roles = roles || member.roles.cache;
     const roleOverwrites = [];
     let memberOverwrites;
     let everyoneOverwrites;
@@ -274,7 +274,7 @@ class GuildChannel extends Channel {
    */
   get members() {
     const members = new Collection();
-    for (const member of this.guild.members.values()) {
+    for (const member of this.guild.members.cache.values()) {
       if (this.permissionsFor(member).has('VIEW_CHANNEL', false)) {
         members.set(member.id, member);
       }
