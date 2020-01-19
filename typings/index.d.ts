8aadfc7d05983f23a21119bfd6cabdadeba20c87
Vlad Frangu <kingdgrizzle@gmail.com>
2020-01-19 19:28:48 +0200

    typings: Forgot to update

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1723,8 +1723,9 @@ declare module 'discord.js' {
 		private identifyResume(): void;
 		private _send(data: object): void;
 		private processQueue(): void;
-		private destroy(closeCode: number): void;
+		private destroy(destroyOptions?: { closeCode?: number; reset?: boolean; emit?: boolean; log?: boolean }): void;
 		private _cleanupConnection(): void;
+		private _emitDestroyed(): void;
 
 		public send(data: object): void;
 		public on(event: 'ready', listener: () => void): this;
