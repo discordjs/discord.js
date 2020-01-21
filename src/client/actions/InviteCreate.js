93be6060fa3c44494364ca38aef404a6dbd2a830
Monbrey <rsm999@uowmail.edu.au>
2020-01-21 21:30:26 +1100

    Dont create an Invite if the guild is null

--- src/client/actions/InviteCreate.js
+++ src/client/actions/InviteCreate.js
@@ -7,20 +7,22 @@ const { Events } = require('../../util/Constants');
 class InviteCreateAction extends Action {
   handle(data) {
     const client = this.client;
-    const inviteData = Object.assign(data, {
-      guild: client.guilds.get(data.guild_id),
-      channel: client.channels.get(data.channel_id),
-    });
-    const invite = new Invite(client, inviteData);
-    /**
-     * Emitted when an invite is created.
-     * <info> This event only triggers if the client has `MANAGE_GUILD` permissions for the guild,
-     * or `MANAGE_CHANNEL` permissions for the channel.</info>
-     * @event Client#inviteCreate
-     * @param {Invite} invite The invite that was created
-     */
-    client.emit(Events.INVITE_CREATE, invite);
-    return { invite };
+    const guild = client.guilds.get(data.guild_id);
+    if (guild) {
+      const channel = client.channels.get(data.channel_id);
+      const inviteData = Object.assign(data, { guild, channel });
+      const invite = new Invite(client, inviteData);
+      /**
+       * Emitted when an invite is created.
+       * <info> This event only triggers if the client has `MANAGE_GUILD` permissions for the guild,
+       * or `MANAGE_CHANNEL` permissions for the channel.</info>
+       * @event Client#inviteCreate
+       * @param {Invite} invite The invite that was created
+       */
+      client.emit(Events.INVITE_CREATE, invite);
+      return { invite };
+    }
+    return { invite: null };
   }
 }
 
--- src/client/actions/InviteDelete.js
+++ src/client/actions/InviteDelete.js
@@ -5,19 +5,20 @@ const { Events } = require('../../util/Constants');
 class InviteDeleteAction extends Action {
   handle(data) {
     const client = this.client;
-    const inviteData = Object.assign(data, {
-      guild: client.guilds.get(data.guild_id),
-      channel: client.channels.get(data.channel_id),
-    });
-    const invite = new Invite(client, inviteData);
-    /**
-     * Emitted when an invite is deleted.
-     * <info> This event only triggers if the client has `MANAGE_GUILD` permissions for the guild,
-     * or `MANAGE_CHANNEL` permissions for the channel.</info>
-     * @event Client#inviteDelete
-     * @param {Invite} invite The invite that was deleted
-     */
-    client.emit(Events.INVITE_DELETE, invite);
+    const guild = client.guilds.get(data.guild_id);
+    if (guild) {
+      const channel = client.channels.get(data.channel_id);
+      const inviteData = Object.assign(data, { guild, channel });
+      const invite = new Invite(client, inviteData);
+      /**
+       * Emitted when an invite is deleted.
+       * <info> This event only triggers if the client has `MANAGE_GUILD` permissions for the guild,
+       *  or `MANAGE_CHANNEL` permissions for the channel.</info>
+       * @event Client#inviteDelete
+       * @param {Invite} invite The invite that was deleted
+       */
+      client.emit(Events.INVITE_DELETE, invite);
+    }
   }
 }
 
