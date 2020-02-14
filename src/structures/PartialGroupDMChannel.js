16cf30c3c2f218d2bd881931259187cb73513de3
Sugden <28943913+NotSugden@users.noreply.github.com>
2020-02-14 01:11:03 +0000

    add PartialGroupDMChannel class

---
+++ src/structures/PartialGroupDMChannel.js
@@ -0,0 +1,38 @@
+const Channel = require("./Channel");
+const { Error } = require("../errors");
+
+/**
+ * Represents a Partial Group DM Channel on Discord.
+ * @extends {Channel}
+ */
+class PartialGroupDMChannel extends Channel {
+	constructor(client, data) {
+	  super(client, data);
+	
+		/**
+		 * The hash of the channel icon
+		 * type {?string}
+		 */
+		this.icon = data.icon;
+	}
+
+	/**
+   * The URL to this channel's icon.
+   * @param {ImageURLOptions} [options={}] Options for the Image URL
+   * @returns {?string}
+   */
+	iconURL({ format, size } = {}) {
+    if (!this.icon) return null;
+    return this.client.rest.cdn.GDMIcon(this.id, this.icon, format, size);
+  }
+	
+	delete() {
+		throw new Error("DELETE_GROUP_DM_CHANNEL");
+	}
+	
+	fetch() {
+		throw new Error("FETCH_GROUP_DM_CHANNEL");
+	}
+}
+
+module.exports = PartialGroupDMChannel;
