bb0b94eb1d9c19879236d58d9f20f54af500f858
Sugden <28943913+NotSugden@users.noreply.github.com>
2020-02-14 01:58:10 +0000

    lint

--- src/structures/PartialGroupDMChannel.js
+++ src/structures/PartialGroupDMChannel.js
@@ -6,25 +6,25 @@ const { Error } = require('../errors');
  * @extends {Channel}
  */
 class PartialGroupDMChannel extends Channel {
-	constructor(client, data) {
-		super(client, data);
+  constructor(client, data) {
+    super(client, data);
 
-		/**
-		 * The hash of the channel icon
-		 * type {?string}
-		 */
-		this.icon = data.icon;
-	}
+    /**
+     * The hash of the channel icon
+     * type {?string}
+     */
+    this.icon = data.icon;
+  }
 
-	/**
-	 * The URL to this channel's icon.
-	 * @param {ImageURLOptions} [options={}] Options for the Image URL
-	 * @returns {?string}
-	 */
-	iconURL({ format, size } = {}) {
-		if (!this.icon) return null;
-		return this.client.rest.cdn.GDMIcon(this.id, this.icon, format, size);
-  	}
+  /**
+   * The URL to this channel's icon.
+   * @param {ImageURLOptions} [options={}] Options for the Image URL
+   * @returns {?string}
+   */
+  iconURL({ format, size } = {}) {
+    if (!this.icon) return null;
+    return this.client.rest.cdn.GDMIcon(this.id, this.icon, format, size);
+  }
 }
 
 module.exports = PartialGroupDMChannel;
