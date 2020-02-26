930d7751ab2ee902c8a80559ae9976f67ef6efb0
NotSugden <28943913+NotSugden@users.noreply.github.com>
2020-02-26 19:24:33 +0000

    refactor: destructure EventEmitter

--- src/client/voice/player/BasePlayer.js
+++ src/client/voice/player/BasePlayer.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const EventEmitter = require('events').EventEmitter;
+const { EventEmitter } = require('events');
 const { Readable: ReadableStream } = require('stream');
 const prism = require('prism-media');
 const StreamDispatcher = require('../dispatcher/StreamDispatcher');
