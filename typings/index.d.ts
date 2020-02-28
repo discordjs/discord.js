3f347fe529dd1c5bf0f6591f0c25684208d210aa
uhKevinMC <plainkevin123@gmail.com>
2020-02-27 20:07:56 -0600

    update(typings): ALMOST_WHITE property
    Updated typings to reflect color neutral white.

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -467,7 +467,7 @@ declare module 'discord.js' {
 		Colors: {
 			DEFAULT: 0x000000;
 			WHITE: 0xFFFFFF;
-			ALMOST_WHITE: 0xFFFFFE;
+			ALMOST_WHITE: 0xFEFEFE;
 			AQUA: 0x1ABC9C;
 			GREEN: 0x2ECC71;
 			BLUE: 0x3498DB;
