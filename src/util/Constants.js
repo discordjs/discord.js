f72a226666ef443ae17c2c59c61862a8840e8a43
Deivu <davexdme@gmail.com>
2020-03-01 18:46:08 +0800

    Add 4013, Invalid Intents close code

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -95,6 +95,7 @@ exports.WSCodes = {
   4004: 'TOKEN_INVALID',
   4010: 'SHARDING_INVALID',
   4011: 'SHARDING_REQUIRED',
+  4013: 'INVALID_INTENTS'
 };
 
 const AllowedImageFormats = ['webp', 'png', 'jpg', 'gif'];
