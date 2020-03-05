73f52f7eabd471ca2871f5ed6e07d2a937ff933d
Potch <thepotch@gmail.com>
2020-03-05 14:41:48 -0800

    fix try catch block in Client.js
    catch requires the error object to be caught- this is causing errors in the wild.

--- src/client/Client.js
+++ src/client/Client.js
@@ -37,7 +37,7 @@ class Client extends BaseClient {
     try {
       // Test if worker threads module is present and used
       data = require('worker_threads').workerData || data;
-    } catch {
+    } catch (e) {
       // Do nothing
     }
 
