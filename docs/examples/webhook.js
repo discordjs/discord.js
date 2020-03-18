5bb35874239fd01b6345b7995619f96a07f468bf
Sardonyx <canboraciner@gmail.com>
2020-03-18 20:30:29 +0300

    Documented how to get ID and Token of a webhook

--- docs/examples/webhook.js
+++ docs/examples/webhook.js
@@ -6,8 +6,13 @@
 
 // Import the discord.js module
 const Discord = require('discord.js');
-
-// Create a new webhook
+/*
+ * Create a new webhook
+ * Your webhook's id and token can be found at the URL or in the response body
+ * https://discordapp.com/api/webhooks/12345678910/T0kEn0fw3Bh00K
+ *                                     ^^^^^^^^^^  ^^^^^^^^^^^^ 
+ *                                   Webhook's ID   Webhook's Token
+ */
 const hook = new Discord.WebhookClient('webhook id', 'webhook token');
 
 // Send a message using the webhook
