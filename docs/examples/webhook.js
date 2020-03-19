6a91ba0d7defdfd52fc4b1a183d942b77480852e
Sardonyx <canboraciner@gmail.com>
2020-03-19 03:09:35 +0300

    Explained whats the response body

--- docs/examples/webhook.js
+++ docs/examples/webhook.js
@@ -8,7 +8,7 @@
 const Discord = require('discord.js');
 /*
  * Create a new webhook
- * The webhook's id and token can be found in the URL or in the response body
+ * The webhook's id and token can be found in the URL or when you request that URL, it can be found in the response body too.
  * https://discordapp.com/api/webhooks/12345678910/T0kEn0fw3Bh00K
  *                                     ^^^^^^^^^^  ^^^^^^^^^^^^ 
  *                                   Webhook's ID   Webhook's Token
