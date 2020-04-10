7533067d61cd1049866038d5618fc2a3c3c4afe9
Vlad Frangu <kingdgrizzle@gmail.com>
2020-04-10 22:25:13 +0300

    chore(WebSocketShard): Log Discord requested reconnects

--- src/client/websocket/WebSocketShard.js
+++ src/client/websocket/WebSocketShard.js
@@ -407,6 +407,7 @@ class WebSocketShard extends EventEmitter {
         this.identify();
         break;
       case OPCodes.RECONNECT:
+        this.debug('[RECONNECT] Discord asked us to reconnect');
         this.destroy({ closeCode: 4000 });
         break;
       case OPCodes.INVALID_SESSION:
