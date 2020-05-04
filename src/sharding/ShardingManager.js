discordjs/discord.js

commit 99612ba14dc70462c878fe8e34a003c4b2325ad3

docs(ShardingManager): remove experimental status of Worker threads (#4157)

src/sharding/ShardingManager.js
@@ -20,9 +20,7 @@ const Util = require('../util/Util');
 class ShardingManager extends EventEmitter {
   /**
    * The mode to spawn shards with for a {@link ShardingManager}: either "process" to use child processes, or
-   * "worker" to use workers. The "worker" mode relies on the experimental
-   * [Worker threads](https://nodejs.org/api/worker_threads.html) functionality that is present in Node v10.5.0 or
-   * newer. Node must be started with the `--experimental-worker` flag to expose it.
+   * "worker" to use [Worker threads][Worker threads](https://nodejs.org/api/worker_threads.html).
    * @typedef {Object} ShardingManagerMode
    */
 

commit e4e353ae4c85a4be62bc8ae69af1591b620113e0

docs(ShardingManager): fix typo in JSDoc
Fix typo introduced in PR #4157 in the link to Node docs

src/sharding/ShardingManager.js
@@ -20,7 +20,7 @@ const Util = require('../util/Util');
 class ShardingManager extends EventEmitter {
   /**
    * The mode to spawn shards with for a {@link ShardingManager}: either "process" to use child processes, or
-   * "worker" to use [Worker threads][Worker threads](https://nodejs.org/api/worker_threads.html).
+   * "worker" to use [Worker threads](https://nodejs.org/api/worker_threads.html).
    * @typedef {Object} ShardingManagerMode
    */
 