bedc971473e090a1d5df0dbe76babd2a1e010b99
SunGame7 <59129279+SunGame7@users.noreply.github.com>
2020-05-08 08:19:43 -0700

    Minor Update
    We can just make it make more sense for the new members using the example page.

--- docs/examples/greeting.js
+++ docs/examples/greeting.js
@@ -21,11 +21,11 @@ client.on('ready', () => {
 // Create an event listener for new guild members
 client.on('guildMemberAdd', member => {
   // Send the message to a designated channel on a server:
-  const channel = member.guild.channels.cache.find(ch => ch.name === 'member-log');
+  const channel = member.guild.channels.cache.find(channel => channel.name === 'member-log');
   // Do nothing if the channel wasn't found on this server
   if (!channel) return;
   // Send the message, mentioning the member
-  channel.send(`Welcome to the server, ${member}`);
+  channel.send(`Welcome to the server, ${member.displayName}`);
 });
 
 // Log our bot in using the token from https://discordapp.com/developers/applications/me
