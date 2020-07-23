cbf85f149ae5a98a73319f58d0b0524a103f7128
S0udiZ <60822881+S0udiZ@users.noreply.github.com>
2020-07-23 11:46:38 +0200

    Update embed.js

--- docs/examples/embed.js
+++ docs/examples/embed.js
@@ -24,7 +24,7 @@ client.on('message', message => {
     // We can create embeds using the MessageEmbed constructor
     // Read more about all that you can do with the constructor
     // over at https://discord.js.org/#/docs/main/master/class/MessageEmbed
-    const embed = new MessageEmbed()
+    const embed = new Discord.MessageEmbed()
       // Set the title of the field
       .setTitle('A slick little embed')
       // Set the color of the embed
