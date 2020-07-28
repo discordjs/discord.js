34db1ba3ffa523f1ac9c70cb61669f0ea7a644b0
Matthew Stead <matievisthekat@gmail.com>
2020-07-28 20:51:27 +0200

    chore: revert formatting change
    revert unnecessary formatting change

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -193,25 +193,16 @@ declare module 'discord.js' {
     public toJSON(): object;
 
     public on<K extends keyof ClientEvents>(event: K, listener: (...args: ClientEvents[K]) => void): this;
-    public on<S extends string | symbol>(
-      event: Exclude<S, keyof ClientEvents>,
-      listener: (...args: any[]) => void,
-    ): this;
+    public on<S extends string | symbol>(event: Exclude<S, keyof ClientEvents>, listener: (...args: any[]) => void): this;
 
     public once<K extends keyof ClientEvents>(event: K, listener: (...args: ClientEvents[K]) => void): this;
-    public once<S extends string | symbol>(
-      event: Exclude<S, keyof ClientEvents>,
-      listener: (...args: any[]) => void,
-    ): this;
+    public once<S extends string | symbol>(event: Exclude<S, keyof ClientEvents>, listener: (...args: any[]) => void): this;
 
     public emit<K extends keyof ClientEvents>(event: K, ...args: ClientEvents[K]): boolean;
     public emit<S extends string | symbol>(event: Exclude<S, keyof ClientEvents>, ...args: any[]): boolean;
 
     public off<K extends keyof ClientEvents>(event: K, listener: (...args: ClientEvents[K]) => void): this;
-    public off<S extends string | symbol>(
-      event: Exclude<S, keyof ClientEvents>,
-      listener: (...args: any[]) => void,
-    ): this;
+    public off<S extends string | symbol>(event: Exclude<S, keyof ClientEvents>, listener: (...args: any[]) => void): this;
 
     public removeAllListeners<K extends keyof ClientEvents>(event?: K): this;
     public removeAllListeners<S extends string | symbol>(event?: Exclude<S, keyof ClientEvents>): this;
