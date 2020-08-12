b03741dfe7b6dd6b4bdf3a197003dbf0f9e576a1
arthur-lchl <arthur.lechelle-dev@protonmail.com>
2020-08-12 19:28:30 +0200

    fix(MessageReactionAdd): add missing semicolon

--- src/client/actions/MessageReactionAdd.js
+++ src/client/actions/MessageReactionAdd.js
@@ -28,7 +28,7 @@ class MessageReactionAdd extends Action {
 
     // Verify reaction
     if (message.partial && !this.client.options.partials.includes(PartialTypes.REACTION)) return false;
-    const existing = message.reactions.cache.get(data.emoji.id || data.emoji.name)
+    const existing = message.reactions.cache.get(data.emoji.id || data.emoji.name);
     if (existing && existing.users.cache.has(user.id)) return { message, reaction: existing, user };
     const reaction = message.reactions.add({
       emoji: data.emoji,
