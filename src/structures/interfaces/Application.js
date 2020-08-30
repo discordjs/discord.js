9b5ba4ab79395c0ee13cfac36ccc38a34547385c
NotSugden <28943913+NotSugden@users.noreply.github.com>
2020-08-30 01:02:59 +0100

    fix(Application): use correct paths

--- src/structures/interfaces/Application.js
+++ src/structures/interfaces/Application.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const Base = require('./Base');
-const { ClientApplicationAssetTypes, Endpoints } = require('../util/Constants');
-const Snowflake = require('../util/Snowflake');
+const { ClientApplicationAssetTypes, Endpoints } = require('../../util/Constants');
+const Snowflake = require('../../util/Snowflake');
+const Base = require('../Base');
 
 const AssetTypes = Object.keys(ClientApplicationAssetTypes);
 
