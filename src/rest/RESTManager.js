5b39737d49706ee55f5d20d14ba4340d506fb6ab
Papaia <43409674+Papaia@users.noreply.github.com>
2020-09-05 21:16:09 +0300

    fix(lint): RESTManager warning (#4796)
    Co-authored-by: Papaia <43409674+ItsPapaia@users.noreply.github.com>

--- src/rest/RESTManager.js
+++ src/rest/RESTManager.js
@@ -60,6 +60,10 @@ class RESTManager {
     return this.push(handler, apiRequest);
   }
 
+  get endpoint() {
+    return this.client.options.http.api;
+  }
+
   set endpoint(endpoint) {
     this.client.options.http.api = endpoint;
   }
