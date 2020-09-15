8fa3a89482bb6aa878aa3ccfca51934b9ebe2356
MrWasdennnoch <leiter04@gmail.com>
2020-09-15 18:35:20 +0200

    fix(Action): Don't crash when partials are disabled (#4822)

--- src/client/actions/TypingStart.js
+++ src/client/actions/TypingStart.js
@@ -7,6 +7,9 @@ const textBasedChannelTypes = ['dm', 'text', 'news'];
 class TypingStart extends Action {
   handle(data) {
     const channel = this.getChannel(data);
+    if (!channel) {
+      return;
+    }
     if (!textBasedChannelTypes.includes(channel.type)) {
       this.client.emit(Events.WARN, `Discord sent a typing packet to a ${channel.type} channel ${channel.id}`);
       return;
