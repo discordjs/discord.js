a0f0e0a7005fc66cf124ea6857336f72e9b62819
IceHax <vecchiotom1@gmail.com>
2020-09-24 10:34:52 +0200

    fix conflict

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -2,7 +2,7 @@
 
 const Package = (exports.Package = require('../../package.json'));
 const { Error, RangeError } = require('../errors');
-const browser = (exports.browser = typeof window !== 'undefined');
+const browser = (exports.browser = typeof process === 'undefined');
 
 /**
  * Options for a client.
