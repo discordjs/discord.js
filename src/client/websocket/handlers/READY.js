53d42daa4e0b71f675395071835a13ad7a99f68a
Peppy8651 <73673342+Peppy8651@users.noreply.github.com>
2020-11-18 09:40:29 -0600

    Try to make linter happy.

--- src/client/websocket/handlers/READY.js
+++ src/client/websocket/handlers/READY.js
@@ -11,19 +11,23 @@ module.exports = async (client, { d: data }, shard) => {
     client.user = clientUser;
     client.users.cache.set(clientUser.id, clientUser);
   }
-  /* Checks if the version in package.json is not the same as 
-  the version of the latest Github release of discord.js. 
-  If it is not the same as the latest release and outdatedwarn 
+  /* Checks if the version in package.json is not the same as
+  the version of the latest Github release of discord.js.
+  If it is not the same as the latest release and outdatedwarn
   is true in the package.json, it will warn the user in the console
   and tell them to update.*/
   const { version } = require('../../../../package.json');
   const { outdatedwarn } = require('../../../../package.json');
-  const body = await miniget('https://api.github.com/repos/discordjs/discord.js/releases/latest', { headers: { 'User-Agent': 'a/b' } }).text();
+  const body = await miniget('https://api.github.com/repos/discordjs/discord.js/releases/latest',
+			{ headers: { 'User-Agent': 'a/b' } },
+		).text();
   const res = JSON.parse(body);
-  if (version !== res.tag_name && outdatedwarn === true) console.warn(`Your version is outdated. Consider updating to the newest version using 'npm install discord.js@latest'
+  if (version !== res.tag_name && outdatedwarn === true) {
+    console.warn(`Your version is outdated. Consider updating to the current version using 'npm install discord.js@latest'
 or if you're using voice support, use one of these;
 npm install discord.js @discordjs/opus
 npm install discord.js opusscript`);
+  }
   for (const guild of data.guilds) {
     guild.shardID = shard.id;
     client.guilds.add(guild);
