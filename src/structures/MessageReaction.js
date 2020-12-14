b2a672047745b0a47729ef775482e06a20b38db3
monbrey <rsm999@uowmail.edu.au>
2020-12-14 23:58:21 +1100

    revert: 5047 (#5050)

--- src/structures/MessageReaction.js
+++ src/structures/MessageReaction.js
@@ -29,6 +29,12 @@ class MessageReaction {
     this.message = message;
 
     /**
+     * Whether the client has given this reaction
+     * @type {boolean}
+     */
+    this.me = data.me;
+
+    /**
      * A manager of the users that have given this reaction
      * @type {ReactionUserManager}
      */
@@ -48,12 +54,6 @@ class MessageReaction {
        */
       this.count = data.count;
     }
-
-    /**
-     * Whether the client has given this reaction
-     * @type {boolean}
-     */
-    this.me = data.me;
   }
 
   /**
