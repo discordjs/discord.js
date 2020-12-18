7b1efaa2712000482c32590a0858fab38d7945d0
Ishmaam Khan <ishmaamk@gmail.com>
2020-12-18 19:29:04 +0600

    refactor +2
    Co-authored-by: Sugden <28943913+NotSugden@users.noreply.github.com>

--- src/managers/RoleManager.js
+++ src/managers/RoleManager.js
@@ -153,7 +153,7 @@ class RoleManager extends BaseManager {
    * @readonly
    */
   get premiumSubscriberRole() {
-    return this.cache.find(role => role.tags && role.tags.premiumSubscriberRole) ?? null;
+    return this.cache.find(role => role.tags?.premiumSubscriberRole) ?? null;
   }
 
   /**
