1c97f05685c7df7bcec5f780efb28e8a1c2651a0
Jay Rizuri <65318393+JayRizuri@users.noreply.github.com>
2020-12-31 14:46:07 -0500

    Update GuildMember.js

--- src/structures/GuildMember.js
+++ src/structures/GuildMember.js
@@ -87,6 +87,7 @@ class GuildMember extends Base {
     if ('joined_at' in data) this.joinedTimestamp = new Date(data.joined_at).getTime();
     if ('premium_since' in data) this.premiumSinceTimestamp = new Date(data.premium_since).getTime();
     if ('roles' in data) this._roles = data.roles;
+    if ('pending' in data) this.pending = data.pending;
   }
 
   _clone() {
