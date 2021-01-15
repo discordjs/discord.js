dc110d08e617c9ef8ad16ea7ced048a743a590b9
ckohen <chaikohen@gmail.com>
2021-01-15 12:34:08 -0800

    fix(Client): better handling of Array
    Co-authored-by: izexi <43889168+izexi@users.noreply.github.com>

--- src/client/Client.js
+++ src/client/Client.js
@@ -417,11 +417,10 @@ class Client extends BaseClient {
 
     if (options.additionalScopes) {
       const scopes = options.additionalScopes;
-      if (scopes.some(scope => !InviteScopes.includes(scope))) {
+      if (!Array.isArray(scopes) || scopes.some(scope => !InviteScopes.includes(scope))) {
         throw new TypeError('INVALID_TYPE', 'additionalScopes', 'Array of Invite Scopes', true);
       }
-      scopes.unshift('bot');
-      query.set('scope', scopes.join(' '));
+      query.set('scope', ['bot', ...scopes].join(' '));
     }
 
     return `${this.options.http.api}${this.api.oauth2.authorize}?${query}`;
