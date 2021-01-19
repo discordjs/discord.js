03a5b19a8fb88b7015ddb9ed63aa67631d356549
almostSouji <timoqueezle@gmail.com>
2021-01-19 12:40:50 +0100

    fix: remove intermediate constant

--- src/util/BitField.js
+++ src/util/BitField.js
@@ -147,8 +147,7 @@ class BitField {
     if (bit instanceof BitField) return bit.bitfield;
     if (Array.isArray(bit)) return bit.map(p => this.resolve(p)).reduce((prev, p) => prev | p, 0);
     if (typeof bit === 'string' && typeof this.FLAGS[bit] !== 'undefined') return this.FLAGS[bit];
-    const error = new RangeError('BITFIELD_INVALID', bit);
-    throw error;
+    throw new RangeError('BITFIELD_INVALID', bit);
   }
 }
 
