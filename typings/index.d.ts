e5ce43d328229f40dce8d92af40b21596ae50d98
Carter Himmel <carter@elhnet.net>
2021-01-24 11:00:42 -0700

    fix: prefer non-null obj over Record

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -50,8 +50,8 @@ declare module 'discord.js' {
     versioned?: boolean;
     auth?: boolean;
     reason?: string;
-    headers?: Record<string, string>;
-    data?: Record<string, unknown>;
+    headers?: {};
+    data?: {};
   }
 
   interface RouteBuilderMethods {
