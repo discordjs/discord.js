09d1f2f18f5ec536bb25156553986fee51c80d1e
Alon Livne <37940683+Alon-L@users.noreply.github.com>
2021-01-27 13:13:15 +0200

    feat(ReactionCollector): event create (#4108)
    * fix(Typing): setSpeaking public
    
    * feat(ReactionCollector): create event, close #2844
    
    * Revert "fix(Typing): setSpeaking public"
    
    This reverts commit ccc0e0cc768ccd9af5f5d6962ff259d250f21b64.

---
+++ test/reactionCollectorCreated.test.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const { token, guildId, channelId, messageId } = require('./auth.js');
+const { Client, ReactionCollector } = require('../src');
+
+const client = new Client();
+
+client.on('ready', async () => {
+  const guild = client.guilds.cache.get(guildId);
+
+  const channel = guild.channels.cache.get(channelId);
+
+  const message = await channel.messages.fetch(messageId);
+
+  await message.react('🔔');
+  // Await message.reactions.removeAll();
+
+  const collector = new ReactionCollector(message, () => true, { dispose: true });
+
+  collector.on('collect', () => {
+    console.log('collected');
+  });
+
+  collector.on('create', () => {
+    console.log('created');
+  });
+
+  collector.on('remove', () => {
+    console.log('removed');
+  });
+
+  collector.on('dispose', () => {
+    console.log('disposed');
+  });
+});
+
+client.login(token);
