ff49f2ce133d83e461e36f77f293fef6edbc25de
MBR-0001 <55142207+MBR-0001@users.noreply.github.com>
2021-02-01 23:10:47 +0100

    fix(GuildBanAdd): pass user instead of creating it

--- src/client/actions/GuildBanAdd.js
+++ src/client/actions/GuildBanAdd.js
@@ -7,15 +7,14 @@ class GuildBanAdd extends Action {
   handle(data) {
     const client = this.client;
     const guild = client.guilds.cache.get(data.guild_id);
-    const user = client.users.add(data.user);
 
     /**
      * Emitted whenever a member is banned from a guild.
      * @event Client#guildBanAdd
-     * @param {GuildBan} guild The guild that the ban occurred in
+     * @param {GuildBan} ban The ban that occurred
      */
-    if (guild && user) {
-      client.emit(Events.GUILD_BAN_ADD, guild.bans.add({ user: user, reason: data.reason }));
+    if (guild) {
+      client.emit(Events.GUILD_BAN_ADD, guild.bans.add({ user: data.user, reason: data.reason }));
     }
   }
 }
