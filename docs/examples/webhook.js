acc1447af5cf80f6bb8cf531f0c8f9708e1f910f
Carter <me@fyko.net>
2021-02-14 16:00:40 -0700

    chore: update carrot length
    Co-authored-by: Antonio Román <kyradiscord@gmail.com>

--- docs/examples/webhook.js
+++ docs/examples/webhook.js
@@ -10,7 +10,7 @@ const { WebhookClient } = require('discord.js');
  * Create a new webhook
  * The Webhooks ID and token can be found in the URL, when you request that URL, or in the response body.
  * https://discord.com/api/webhooks/12345678910/T0kEn0fw3Bh00K
- *                                  ^^^^^^^^^^  ^^^^^^^^^^^^
+ *                                  ^^^^^^^^^^^ ^^^^^^^^^^^^^^
  *                                  Webhook ID  Webhook Token
  */
 const hook = new WebhookClient('webhook id', 'webhook token');
