dcfaf00352d4e0014bdc3dbf4d07b9b80d1d185e
Jan <66554238+vaporox@users.noreply.github.com>
2021-03-31 12:02:41 +0200

    chore: this is obsolete now

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -266,7 +266,7 @@ declare module 'discord.js' {
     public owner: User | Team | null;
     public readonly partial: boolean;
     public rpcOrigins: string[];
-    public fetch(): Promise<Required<ClientApplication>>;
+    public fetch(): Promise<ClientApplication>;
   }
 
   export class ClientUser extends User {
