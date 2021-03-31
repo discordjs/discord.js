b5d567a6eabf67737be3fac1d8ecf00a74e3df40
Amish Shah <amishshah.2k@gmail.com>
2021-03-31 21:39:18 +0100

    feat: add computed properties to stage channel

--- src/structures/StageChannel.js
+++ src/structures/StageChannel.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const GuildChannel = require('./GuildChannel');
+const Collection = require('../util/Collection');
+const Permissions = require('../util/Permissions');
 
 /**
  * Represents a guild stage channel on Discord.
@@ -50,6 +52,61 @@ class StageChannel extends GuildChannel {
     this.userLimit = data.user_limit;
 
     if (typeof data.nsfw !== 'undefined') this.nsfw = Boolean(data.nsfw);
+  }
+
+  /**
+   * The members in this stage channel
+   * @type {Collection<Snowflake, GuildMember>}
+   * @readonly
+   */
+  get members() {
+    const coll = new Collection();
+    for (const state of this.guild.voiceStates.cache.values()) {
+      if (state.channelID === this.id && state.member) {
+        coll.set(state.id, state.member);
+      }
+    }
+    return coll;
+  }
+
+  /**
+   * Checks if the stage channel is full
+   * @type {boolean}
+   * @readonly
+   */
+  get full() {
+    return this.userLimit > 0 && this.members.size >= this.userLimit;
+  }
+
+  /**
+   * Whether the channel is deletable by the client user
+   * @type {boolean}
+   * @readonly
+   */
+  get deletable() {
+    return this.manageable && this.permissionsFor(this.client.user).has(Permissions.FLAGS.CONNECT, false);
+  }
+
+  /**
+   * Whether the channel is editable by the client user
+   * @type {boolean}
+   * @readonly
+   */
+  get editable() {
+    return this.manageable && this.permissionsFor(this.client.user).has(Permissions.FLAGS.CONNECT, false);
+  }
+
+  /**
+   * Whether the channel is joinable by the client user
+   * @type {boolean}
+   * @readonly
+   */
+  get joinable() {
+    if (!this.viewable) return false;
+    if (!this.permissionsFor(this.client.user).has(Permissions.FLAGS.CONNECT, false)) return false;
+    // Todo: unsure if the following statement works for stage channels
+    // if (this.full && !this.permissionsFor(this.client.user).has(Permissions.FLAGS.MOVE_MEMBERS, false)) return false;
+    return true;
   }
 
   /**
