dc12a56b0f1a11d0522a86c396f54d446f199689
Amish Shah <amishshah.2k@gmail.com>
2021-04-01 10:12:22 +0100

    style(VoiceState): fix lint errors

--- src/structures/VoiceState.js
+++ src/structures/VoiceState.js
@@ -267,15 +267,13 @@ class VoiceState extends Base {
         });
     } else {
       if (!hasMuteMembersPermission) throw new Error('VOICE_NEED_MUTE_MEMBERS');
-      await this.client.api
-        .guilds(this.guild.id, 'voice-states', this.id)
-        .patch({
-          data: {
-            channel_id: this.channelID,
-            suppress: suppressed,
-            request_to_speak_timestamp: suppressed || isBot ? null : new Date().toISOString(),
-          },
-        });
+      await this.client.api.guilds(this.guild.id, 'voice-states', this.id).patch({
+        data: {
+          channel_id: this.channelID,
+          suppress: suppressed,
+          request_to_speak_timestamp: suppressed || isBot ? null : new Date().toISOString(),
+        },
+      });
     }
   }
 
