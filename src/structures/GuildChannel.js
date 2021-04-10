8c497ddc1905f38560249afc028d6417d828a695
almostSouji <timoqueezle@gmail.com>
2021-04-10 13:53:50 +0200

    fix: handle targetApplication as resolvable

--- src/structures/GuildChannel.js
+++ src/structures/GuildChannel.js
@@ -517,7 +517,7 @@ class GuildChannel extends Channel {
           max_uses: maxUses,
           unique,
           target_user_id: this.client.users.resolveID(targetUser),
-          target_application_id: targetApplication,
+          target_application_id: targetApplication.id ?? targetApplication,
           target_type: targetType,
         },
         reason,
