6d3160d976ebfcaf80eb458bcae234ef6cf7862a
Souji <timoqueezle@gmail.com>
2021-04-12 11:21:11 +0200

    fix: correct fallback data access
    Co-authored-by: Antonio Román <kyradiscord@gmail.com>

--- src/structures/IntegrationApplication.js
+++ src/structures/IntegrationApplication.js
@@ -26,13 +26,13 @@ class IntegrationApplication extends Application {
      * The url of the app's privacy policy
      * @type {?string}
      */
-    this.privacyPolicyURL = data.privacyPolicyURL ?? this.privacyPolicyURL ?? null;
+    this.privacyPolicyURL = data.privacy_policy_url ?? this.privacyPolicyURL ?? null;
 
     /**
      * The Array of RPC origin urls
      * @type {string[]}
      */
-    this.rpcOrigins = data.rpc_origins ?? data.rpc_origins ?? [];
+    this.rpcOrigins = data.rpc_origins ?? this.rpcOrigins ?? [];
 
     /**
      * The application summary
