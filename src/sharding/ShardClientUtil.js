5e78df0a456f9867eae1ab1f2605c44c9a31ed4e
meister03 <69507874+meister03@users.noreply.github.com>
2021-04-25 10:14:43 +0200

    Update src/sharding/ShardClientUtil.js
    Upgrade from `let` to `const`
    
    Co-authored-by: Jan <66554238+vaporox@users.noreply.github.com>

--- src/sharding/ShardClientUtil.js
+++ src/sharding/ShardClientUtil.js
@@ -199,7 +199,7 @@ class ShardClientUtil {
    */
   _respond(type, message) {
     this.send(message).catch(err => {
-      let error = new Error(`Error when sending ${type} response to master process: ${err.message}`);
+      const error = new Error(`Error when sending ${type} response to master process: ${err.message}`);
       /**
        * Emitted when the client encounters an error.
        * @event Client#error
