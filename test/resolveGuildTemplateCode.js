7e10633fbdd56a9fbba4beec242256ed07d2af72
cherryblossom <31467609+cherryblossom000@users.noreply.github.com>
2021-05-13 19:23:37 +1000

    test(resolveGuildTemplateCode): use jest for assertions

--- test/resolveGuildTemplateCode.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-const assert = require('assert');
-const { DataResolver } = require('../src');
-
-assert.strictEqual(DataResolver.resolveGuildTemplateCode('https://discord.new/abc'), 'abc');
