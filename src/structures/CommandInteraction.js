a90e4d05b380ac267bc16fcd027e903923f4d426
Monbrey <rsm999@uowmail.edu.au>
2021-05-13 11:45:51 +1000

    feat: return the Message

--- src/structures/CommandInteraction.js
+++ src/structures/CommandInteraction.js
@@ -191,16 +191,18 @@ class CommandInteraction extends Interaction {
    * Send a followup message to this interaction.
    * @param {string|APIMessage|MessageAdditions} content The content for the reply
    * @param {InteractionReplyOptions} [options] Additional options for the reply
-   * @returns {Promise<void>}
+   * @returns {Promise<Message|Object>}
    */
   async followup(content, options) {
     const apiMessage = content instanceof APIMessage ? content : APIMessage.create(this, content, options);
     const { data, files } = await apiMessage.resolveData().resolveFiles();
 
-    await this.client.api.webhooks(this.applicationID, this.token).post({
+    const raw = await this.client.api.webhooks(this.applicationID, this.token).post({
       data,
       files,
     });
+
+    return this.channel?.messages.add(raw) ?? raw;
   }
 
   /**
