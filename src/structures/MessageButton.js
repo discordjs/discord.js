8ecbd367c13161db946125aebc9f292c2b8c94ca
Monbrey <rsm999@uowmail.edu.au>
2021-05-20 17:22:20 +1000

    feat(MessageButton): remaining builder functions

--- src/structures/MessageButton.js
+++ src/structures/MessageButton.js
@@ -34,16 +34,17 @@ class MessageButton extends MessageComponent {
      * A unique string to be sent in the interaction when clicked
      * @type {?string}
      */
-    this.customID = data.custom_id ?? null;
+    this.customID = data.custom_id ?? data.customID ?? null;
 
     /**
      * The style of this button
      * @type {MessageButtonStyle}
      */
-    this.style = MessageButtonStyles[data.style ?? 1];
+    this.style = data.style ? MessageButton.resolveStyle(data.style) : 'PRIMARY';
 
     /**
      * Emoji for this button
+     * @type {Emoji|string}
      */
     this.emoji = data.emoji ?? null;
 
@@ -60,16 +61,6 @@ class MessageButton extends MessageComponent {
   }
 
   /**
-   * Sets the label of this button
-   * @param {string} label The text to be displayed on this button
-   * @returns {MessageButton}
-   */
-  setLabel(label) {
-    this.label = Util.resolveString(label);
-    return this;
-  }
-
-  /**
    * Sets the custom ID of this button
    * @param {string} customID A unique string to be sent in the interaction when clicked
    * @returns {MessageButton}
@@ -80,12 +71,52 @@ class MessageButton extends MessageComponent {
   }
 
   /**
+   * Sets the interactive status of the button
+   * @param {boolean} disabled Whether this emoji should be disabled
+   * @returns {MessageEmbed}
+   */
+  setDisabled(disabled) {
+    this.disabled = disabled;
+    return this;
+  }
+
+  /**
+   * Set the emoji of this button
+   * @param {EmojiIdentifierResolvable} emoji The emoji to be displayed on this button
+   * @returns {MessageButton}
+   */
+  setEmoji(emoji) {
+    this.emoji = typeof emoji === 'string' ? { name: emoji } : emoji;
+    return this;
+  }
+
+  /**
+   * Sets the label of this button
+   * @param {string} label The text to be displayed on this button
+   * @returns {MessageButton}
+   */
+  setLabel(label) {
+    this.label = Util.resolveString(label);
+    return this;
+  }
+
+  /**
    * Sets the style of this button
    * @param {MessageButtonStyle} style The style of this button
    * @returns {MessageButton}
    */
   setStyle(style) {
     this.style = MessageButton.resolveStyle(style);
+    return this;
+  }
+
+  /**
+   * Sets the URL of this button. MessageButton#style should be LINK
+   * @param {string} url The URL of this button
+   * @returns {MessageButton}
+   */
+  setURL(url) {
+    this.url = Util.resolveString(url);
     return this;
   }
 
