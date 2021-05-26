adb2cbf7fcc286bb7b04ca3cf9a3f9369535cc28
Monbrey <monbrey@gmail.com>
2021-05-27 09:42:42 +1000

    fix: rename component type

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -830,7 +830,7 @@ exports.InteractionResponseTypes = createEnum([
  * BUTTON
  * @typedef {string} MessageComponentType
  */
-exports.MessageComponentTypes = createEnum([null, 'ACTION_ROW', 'BUTTON']);
+exports.MessageComponentTypes = createEnum([null, 'ACTION_ROW', 'MESSAGE_COMPONENT']);
 
 /**
  * The style of a message button
