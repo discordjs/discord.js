88e5dd4fda6e609bd142476ee4e945a005005a5e
Monbrey <monbrey@gmail.com>
2021-05-28 15:41:57 +1000

    feat: support creating select menus

--- src/structures/BaseMessageComponent.js
+++ src/structures/BaseMessageComponent.js
@@ -18,7 +18,7 @@ class BaseMessageComponent {
    * Data that can be resolved into options for a MessageComponent. This can be:
    * * MessageActionRowOptions
    * * MessageButtonOptions
-   * @typedef {MessageActionRowOptions|MessageButtonOptions} MessageComponentOptions
+   * @typedef {MessageActionRowOptions|MessageButtonOptions|MessageSelectMenuOptions} MessageComponentOptions
    */
 
   /**
@@ -70,6 +70,11 @@ class BaseMessageComponent {
       case MessageComponentTypes.BUTTON: {
         const MessageButton = require('./MessageButton');
         component = new MessageButton(data);
+        break;
+      }
+      case MessageComponentTypes.SELECT_MENU: {
+        const MessageSelectMenu = require('./MessageSelectMenu');
+        component = new MessageSelectMenu(data);
         break;
       }
       default:
