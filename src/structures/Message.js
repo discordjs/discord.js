f5ff348fd790587aeebc05126b4dc94c5f66ec06
Shubham Parihar <shubhamparihar391@gmail.com>
2021-06-03 21:19:32 +0530

    docs(MessageEditOptions): use ? instead of null for content
    Co-authored-by: Jan <66554238+vaporox@users.noreply.github.com>

--- src/structures/Message.js
+++ src/structures/Message.js
@@ -474,7 +474,7 @@ class Message extends Base {
   /**
    * Options that can be passed into {@link Message#edit}.
    * @typedef {Object} MessageEditOptions
-   * @property {string|null} [content] Content to be edited
+   * @property {?string} [content] Content to be edited
    * @property {MessageEmbed|Object} [embed] An embed to be added/edited
    * @property {string|boolean} [code] Language for optional codeblock formatting to apply
    * @property {MessageMentionOptions} [allowedMentions] Which mentions should be parsed from the message content
