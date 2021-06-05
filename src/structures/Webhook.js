2901fd595be847a0e6c86155b3229d3341cfad32
Shubham Parihar <shubhamparihar391@gmail.com>
2021-06-06 00:56:12 +0530

    fix(Webhook): #editMessage throws error when content is null (#5757)

--- src/structures/Webhook.js
+++ src/structures/Webhook.js
@@ -242,14 +242,14 @@ class Webhook {
   /**
    * Edits a message that was sent by this webhook.
    * @param {MessageResolvable|'@original'} message The message to edit
-   * @param {string|APIMessage} [content] The new content for the message
+   * @param {?(string|APIMessage)} [content] The new content for the message
    * @param {WebhookEditMessageOptions|MessageAdditions} [options] The options to provide
    * @returns {Promise<Message|Object>} Returns the raw message data if the webhook was instantiated as a
    * {@link WebhookClient} or if the channel is uncached, otherwise a {@link Message} will be returned
    */
   async editMessage(message, content, options) {
     const { data, files } = await (
-      content.resolveData?.() ?? APIMessage.create(this, content, options).resolveData()
+      content?.resolveData?.() ?? APIMessage.create(this, content, options).resolveData()
     ).resolveFiles();
     const d = await this.client.api
       .webhooks(this.id, this.token)
