cf224560bc59c05f7801088b0db2ec76c5369302
TheMaestroo <61483023+TheMaestro0@users.noreply.github.com>
2021-06-05 08:14:44 +0200

    fix(RoleManager): bug in #create (#5730)
    Co-authored-by: ckohen <chaikohen@gmail.com>

--- src/managers/RoleManager.js
+++ src/managers/RoleManager.js
@@ -115,7 +115,7 @@ class RoleManager extends BaseManager {
   create(options = {}) {
     let { name, color, hoist, permissions, position, mentionable, reason } = options;
     if (color) color = resolveColor(color);
-    if (permissions) permissions = Permissions.resolve(permissions).toString();
+    if (typeof permissions !== 'undefined') permissions = new Permissions(permissions);
 
     return this.client.api
       .guilds(this.guild.id)
