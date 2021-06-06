c0ea48d2365fddd78e7299789b019eac31dee06f
Monbrey <monbrey@gmail.com>
2021-06-06 23:05:34 +1000

    docs: less bland

--- src/structures/Message.js
+++ src/structures/Message.js
@@ -537,7 +537,8 @@ class Message extends Base {
    * @property {MessageAttachment[]} [attachments] An array of attachments to keep,
    * all attachments will be kept if omitted
    * @property {FileOptions[]|BufferResolvable[]|MessageAttachment[]} [files] Files to add to the message
-   * @property {MessageActionRow[]} [components] The components for the message
+   * @property {MessageActionRow[]} [components] Action rows containing interactive components for the message
+   * (buttons, select menus)
    */
 
   /**
