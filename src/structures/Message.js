932080ca78e6cf1f441bd1bc2e339246117748ce
adrifcastr <adrifcastr@gmail.com>
2021-06-08 00:05:48 +0200

    fix(Message): fix JSDoc

--- src/structures/Message.js
+++ src/structures/Message.js
@@ -541,8 +541,7 @@ class Message extends Base {
 
   /**
    * Edits the content of the message.
-   * @param {string|APIMessage|MessageEditOptions|MessageEmbed
-   * |MessageAttachment|MessageAttachment[]} options The options to provide
+   * @param {string|APIMessage|MessageEditOptions} options The options to provide
    * @returns {Promise<Message>}
    * @example
    * // Update the content of a message
