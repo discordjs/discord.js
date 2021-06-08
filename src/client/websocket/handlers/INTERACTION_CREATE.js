84e5b075b4214c5432e380840c90091643af07fa
Nordiago <nordiago@users.noreply.github.com>
2021-06-08 09:16:05 -0400

    refactor(Interactions): move Structures import out of switch block (#5763)

--- src/client/websocket/handlers/INTERACTION_CREATE.js
+++ src/client/websocket/handlers/INTERACTION_CREATE.js
@@ -1,22 +1,18 @@
 'use strict';
 
 const { Events, InteractionTypes } = require('../../../util/Constants');
-let Structures;
+const Structures = require('../../../util/Structures');
 
 module.exports = (client, { d: data }) => {
   let interaction;
   switch (data.type) {
     case InteractionTypes.APPLICATION_COMMAND: {
-      if (!Structures) Structures = require('../../../util/Structures');
       const CommandInteraction = Structures.get('CommandInteraction');
-
       interaction = new CommandInteraction(client, data);
       break;
     }
     case InteractionTypes.MESSAGE_COMPONENT: {
-      if (!Structures) Structures = require('../../../util/Structures');
       const MessageComponentInteraction = Structures.get('MessageComponentInteraction');
-
       interaction = new MessageComponentInteraction(client, data);
       break;
     }
