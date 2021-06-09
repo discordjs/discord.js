217ed980f30d77a97e87009d96a33672038a5c69
Austrian <60128595+AustrianDev@users.noreply.github.com>
2021-06-09 19:01:48 +0200

    fix indentation

--- src/structures/interfaces/TextBasedChannel.js
+++ src/structures/interfaces/TextBasedChannel.js
@@ -147,13 +147,13 @@ class TextBasedChannel {
    *   content: 'This is an embed',
    *   embed: {
    *     thumbnail: {
-   *          url: 'attachment://file.jpg'
-   *       }
-   *    },
-   *    files: [{
-   *       attachment: 'entire/path/to/file.jpg',
-   *       name: 'file.jpg'
-   *    }]
+   *       url: 'attachment://file.jpg'
+   *     }
+   *   },
+   *   files: [{
+   *     attachment: 'entire/path/to/file.jpg',
+   *     name: 'file.jpg'
+   *   }]
    * })
    *   .then(console.log)
    *   .catch(console.error);
