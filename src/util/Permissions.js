d31c7797a962e02934d977fec40892ab0eaceb5f
Kevin <biojoutk1106@gmail.com>
2021-06-09 14:29:57 +0200

    revert(Permissions): revert to default behavior

--- src/util/Permissions.js
+++ src/util/Permissions.js
@@ -41,7 +41,7 @@ class Permissions extends BitField {
    * @returns {boolean}
    */
   has(permission, checkAdmin = true) {
-    return (checkAdmin && super.has(this.constructor.FLAGS.ADMINISTRATOR)) || super.has(permission, checkAdmin);
+    return (checkAdmin && super.has(this.constructor.FLAGS.ADMINISTRATOR)) || super.has(permission);
   }
 }
 
