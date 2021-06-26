5b623f55c626f7bf86106edffd1b3c3452eb3bac
Monbrey <monbrey@gmail.com>
2021-06-26 12:40:23 +1000

    types: remove redundant renaming

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -139,17 +139,17 @@ declare module 'discord.js' {
   import BaseCollection from '@discordjs/collection';
   import { ChildProcess } from 'child_process';
   import {
-    APIActionRowComponent as RawActionRowComponent,
-    APIInteractionDataResolvedChannel as RawInteractionDataResolvedChannel,
-    APIInteractionDataResolvedGuildMember as RawInteractionDataResolvedGuildMember,
-    APIInteractionGuildMember as RawInteractionGuildMember,
-    APIMessage as RawMessage,
-    APIMessageComponent as RawMessageComponent,
-    APIOverwrite as RawOverwrite,
-    APIPartialEmoji as RawEmoji,
-    APIRole as RawRole,
-    Snowflake as APISnowflake,
+    APIActionRowComponent,
+    APIInteractionDataResolvedChannel,
+    APIInteractionDataResolvedGuildMember,
+    APIInteractionGuildMember,
+    APIMessage,
+    APIMessageComponent,
+    APIOverwrite,
+    APIPartialEmoji,
+    APIRole,
     APIUser,
+    Snowflake as APISnowflake,
     ApplicationCommandOptionType as ApplicationCommandOptionTypes,
     ApplicationCommandPermissionType as ApplicationCommandPermissionTypes,
   } from 'discord-api-types/v8';
@@ -524,9 +524,9 @@ declare module 'discord.js' {
     public webhook: InteractionWebhook;
     public defer(options?: InteractionDeferOptions): Promise<void>;
     public deleteReply(): Promise<void>;
-    public editReply(options: string | MessagePayload | WebhookEditMessageOptions): Promise<Message | RawMessage>;
-    public fetchReply(): Promise<Message | RawMessage>;
-    public followUp(options: string | MessagePayload | InteractionReplyOptions): Promise<Message | RawMessage>;
+    public editReply(options: string | MessagePayload | WebhookEditMessageOptions): Promise<Message | APIMessage>;
+    public fetchReply(): Promise<Message | APIMessage>;
+    public followUp(options: string | MessagePayload | InteractionReplyOptions): Promise<Message | APIMessage>;
     public reply(options: string | MessagePayload | InteractionReplyOptions): Promise<void>;
     private transformOption(option: unknown, resolved: unknown): CommandInteractionOption;
     private _createOptionsCollection(options: unknown, resolved: unknown): Collection<string, CommandInteractionOption>;
@@ -1171,7 +1171,7 @@ declare module 'discord.js' {
     public readonly guild: Guild | null;
     public guildID: Snowflake | null;
     public id: Snowflake;
-    public member: GuildMember | RawInteractionGuildMember | null;
+    public member: GuildMember | APIInteractionGuildMember | null;
     public readonly token: string;
     public type: InteractionType;
     public user: User;
@@ -1341,7 +1341,7 @@ declare module 'discord.js' {
     constructor(data?: MessageButton | MessageButtonOptions);
     public customID: string | null;
     public disabled: boolean;
-    public emoji: RawEmoji | null;
+    public emoji: APIPartialEmoji | null;
     public label: string | null;
     public style: MessageButtonStyle | null;
     public type: 'BUTTON';
@@ -1377,15 +1377,15 @@ declare module 'discord.js' {
     public customID: string;
     public deferred: boolean;
     public ephemeral: boolean | null;
-    public message: Message | RawMessage;
+    public message: Message | APIMessage;
     public replied: boolean;
     public webhook: InteractionWebhook;
     public defer(options?: InteractionDeferOptions): Promise<void>;
     public deferUpdate(): Promise<void>;
     public deleteReply(): Promise<void>;
-    public editReply(options: string | MessagePayload | WebhookEditMessageOptions): Promise<Message | RawMessage>;
-    public fetchReply(): Promise<Message | RawMessage>;
-    public followUp(options: string | MessagePayload | InteractionReplyOptions): Promise<Message | RawMessage>;
+    public editReply(options: string | MessagePayload | WebhookEditMessageOptions): Promise<Message | APIMessage>;
+    public fetchReply(): Promise<Message | APIMessage>;
+    public followUp(options: string | MessagePayload | InteractionReplyOptions): Promise<Message | APIMessage>;
     public reply(options: string | MessagePayload | InteractionReplyOptions): Promise<void>;
     public update(content: string | MessagePayload | WebhookEditMessageOptions): Promise<void>;
     public static resolveType(type: MessageComponentTypeResolvable): MessageComponentType;
@@ -1587,7 +1587,7 @@ declare module 'discord.js' {
       options: PermissionOverwriteOptions,
       initialPermissions: { allow?: PermissionResolvable; deny?: PermissionResolvable },
     ): ResolvedOverwriteOptions;
-    public static resolve(overwrite: OverwriteResolvable, guild: Guild): RawOverwrite;
+    public static resolve(overwrite: OverwriteResolvable, guild: Guild): APIOverwrite;
   }
 
   export class Permissions extends BitField<PermissionString, bigint> {
@@ -2026,7 +2026,7 @@ declare module 'discord.js' {
     public static moveElementInArray(array: any[], element: any, newIndex: number, offset?: boolean): number;
     public static parseEmoji(text: string): { animated: boolean; name: string; id: Snowflake | null } | null;
     public static resolveColor(color: ColorResolvable): number;
-    public static resolvePartialEmoji(emoji: EmojiIdentifierResolvable): Partial<RawEmoji> | null;
+    public static resolvePartialEmoji(emoji: EmojiIdentifierResolvable): Partial<APIPartialEmoji> | null;
     public static verifyString(data: string, error?: typeof Error, errorMessage?: string, allowEmpty?: boolean): string;
     public static setPosition<T extends Channel | Role>(
       item: T,
@@ -3095,9 +3095,9 @@ declare module 'discord.js' {
     value?: string | number | boolean;
     options?: Collection<string, CommandInteractionOption>;
     user?: User;
-    member?: GuildMember | RawInteractionDataResolvedGuildMember;
-    channel?: GuildChannel | RawInteractionDataResolvedChannel;
-    role?: Role | RawRole;
+    member?: GuildMember | APIInteractionDataResolvedGuildMember;
+    channel?: GuildChannel | APIInteractionDataResolvedChannel;
+    role?: Role | APIRole;
   }
 
   interface CreateRoleOptions extends RoleData {
@@ -3776,7 +3776,7 @@ declare module 'discord.js' {
   interface MessageSelectOption {
     default: boolean;
     description: string | null;
-    emoji: RawEmoji | null;
+    emoji: APIPartialEmoji | null;
     label: string;
     value: string;
   }
