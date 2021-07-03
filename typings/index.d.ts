0dc5dd5808af213ac4ede9f1084b522225a5c661
DTrombett <73136330+DTrombett@users.noreply.github.com>
2021-07-03 09:58:18 +0200

    types(*): create mention types (#6003)

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -439,7 +439,7 @@ declare module 'discord.js' {
     public fetch(force?: boolean): Promise<Channel>;
     public isText(): this is TextChannel | DMChannel | NewsChannel | ThreadChannel;
     public isThread(): this is ThreadChannel;
-    public toString(): string;
+    public toString(): ChannelMention;
   }
 
   export class Client extends BaseClient {
@@ -1103,7 +1103,7 @@ declare module 'discord.js' {
     public permissionsIn(channel: GuildChannelResolvable): Readonly<Permissions>;
     public setNickname(nickname: string | null, reason?: string): Promise<GuildMember>;
     public toJSON(): unknown;
-    public toString(): string;
+    public toString(): MemberMention;
     public valueOf(): string;
   }
 
@@ -1757,7 +1757,7 @@ declare module 'discord.js' {
     public setPermissions(permissions: PermissionResolvable, reason?: string): Promise<Role>;
     public setPosition(position: number, options?: SetRolePositionOptions): Promise<Role>;
     public toJSON(): unknown;
-    public toString(): string;
+    public toString(): RoleMention;
 
     public static comparePositions(role1: Role, role2: Role): number;
   }
@@ -2052,7 +2052,7 @@ declare module 'discord.js' {
     public equals(user: User): boolean;
     public fetch(force?: boolean): Promise<User>;
     public fetchFlags(force?: boolean): Promise<UserFlags>;
-    public toString(): string;
+    public toString(): UserMention;
     public typingDurationIn(channel: ChannelResolvable): number;
     public typingIn(channel: ChannelResolvable): boolean;
     public typingSinceIn(channel: ChannelResolvable): Date;
@@ -3034,6 +3034,8 @@ declare module 'discord.js' {
     around?: Snowflake;
   }
 
+  type ChannelMention = `<#${Snowflake}>`;
+
   interface ChannelPosition {
     channel: ChannelResolvable;
     lockPermissions?: boolean;
@@ -3715,6 +3717,8 @@ declare module 'discord.js' {
     stack: string;
   }
 
+  type MemberMention = UserMention | `<@!${Snowflake}>`;
+
   type MembershipState = keyof typeof MembershipStates;
 
   type MessageActionRowComponent = MessageButton | MessageSelectMenu;
@@ -4214,6 +4218,8 @@ declare module 'discord.js' {
     mentionable?: boolean;
   }
 
+  type RoleMention = '@everyone' | `<@&${Snowflake}>`;
+
   interface RolePosition {
     role: RoleResolvable;
     position: number;
@@ -4342,6 +4348,8 @@ declare module 'discord.js' {
     | 'VERIFIED_BOT'
     | 'EARLY_VERIFIED_BOT_DEVELOPER'
     | 'DISCORD_CERTIFIED_MODERATOR';
+
+  type UserMention = `<@${Snowflake}>`;
 
   type UserResolvable = User | Snowflake | Message | GuildMember | ThreadMember;
 
