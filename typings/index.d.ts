d7a117a83b0bf9c4a0243b8522007a61b8418412
PanSzelescik <panszelescik@gmail.com>
2021-07-05 14:46:05 +0200

    fix: typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1372,7 +1372,7 @@ export class Role extends Base {
 
 export class SelectMenuInteraction extends MessageComponentInteraction {
   public componentType: 'SELECT_MENU';
-  public values: string[] | null;
+  public values: string[];
 }
 
 export class Shard extends EventEmitter {
