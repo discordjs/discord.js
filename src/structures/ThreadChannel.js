88b894b619463806e2d52d9a0b22b95b9ee20815
Rodry <38259440+ImRodry@users.noreply.github.com>
2021-07-29 19:43:11 +0100

    fix(ThreadChannel): correctly point to the manager

--- src/structures/ThreadChannel.js
+++ src/structures/ThreadChannel.js
@@ -241,7 +241,7 @@ class ThreadChannel extends Channel {
    */
   async fetchOwner({ cache = true, force = false } = {}) {
     if (!force) {
-      const existing = this.cache.get(this.ownerId);
+      const existing = this.members.cache.get(this.ownerId);
       if (existing) return existing;
     }
 
