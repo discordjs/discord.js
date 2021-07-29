f86cbb42536b40e20c92405818d5ac8974499b9d
A1ex <31402774+a9ex@users.noreply.github.com>
2021-07-29 11:25:52 +0200

    docs(MessageMentions): mentions works only in Guilds

--- src/structures/MessageMentions.js
+++ src/structures/MessageMentions.js
@@ -134,7 +134,7 @@ class MessageMentions {
   }
 
   /**
-   * Any members that were mentioned (only in {@link TextChannel}s and {@link ThreadChannel}s)
+   * Any members that were mentioned (only in {@link Guild}s)
    * <info>Order as received from the API, not as they appear in the message content</info>
    * @type {?Collection<Snowflake, GuildMember>}
    * @readonly
