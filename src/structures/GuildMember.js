288a0a7de7438f6e7d0ce0dc29a16fc87bca3689
Jiralite <Jiralite@Live.co.uk>
2021-07-31 08:56:36 +0100

    docs(GuildMember): Move line up and favour "disconnect"
    Favouring "disconnect" here instead of "kick" as it is consistent with the documentation in VoiceState#setChannel().

--- src/structures/GuildMember.js
+++ src/structures/GuildMember.js
@@ -237,8 +237,7 @@ class GuildMember extends Base {
    * @property {boolean} [mute] Whether or not the member should be muted
    * @property {boolean} [deaf] Whether or not the member should be deafened
    * @property {GuildVoiceChannelResolvable|null} [channel] Channel to move member to (if they are connected to voice),
-   * or `null`
-   * if you want to kick them from voice
+   * or `null` if you want to disconnect them from voice
    */
 
   /**
