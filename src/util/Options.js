a6d4035176ca784e75bd1cbdf30e039658c62fd4
ckohen <chaikohen@gmail.com>
2021-08-04 14:04:40 -0700

    fix: remove support for overriding caches that break functionality (#6282)
    Co-authored-by: Noel <buechler.noel@outlook.com>

--- src/util/Options.js
+++ src/util/Options.js
@@ -35,6 +35,8 @@
  * (e.g. recommended shard count, shard count of the ShardingManager)
  * @property {CacheFactory} [makeCache] Function to create a cache.
  * You can use your own function, or the {@link Options} class to customize the Collection used for the cache.
+ * <warn>Overriding the cache used in `GuildManager`, `ChannelManager`, 'GuildChannelManager', `RoleManager`,
+ * and `PermissionOverwriteManager` is unsupported and **will** break functionality</warn>
  * @property {number} [messageCacheLifetime=0] DEPRECATED: Use `makeCache` with a `LimitedCollection` instead.
  * How long a message should stay in the cache until it is considered sweepable (in seconds, 0 for forever)
  * @property {number} [messageSweepInterval=0] DEPRECATED: Use `makeCache` with a `LimitedCollection` instead.
