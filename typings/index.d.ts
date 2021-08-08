a67bf87fd08de936a0169af37e77e18421fb336d
JeroenoBoy <Jeroen@vandegeest.eu>
2021-08-08 15:23:29 +0200

    fixed ApplicationCommandOption

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -2875,6 +2875,7 @@ export interface ApplicationCommandOptionData {
 
 export interface ApplicationCommandOption extends ApplicationCommandOptionData {
   type: ApplicationCommandOptionType;
+  options?: ApplicationCommandOption[]
 }
 
 export interface ApplicationCommandOptionChoice {
