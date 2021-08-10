5b1ac45171ce180a5cf8a521505007b792de0a4e
Jiralite <33201955+Jiralite@users.noreply.github.com>
2021-08-10 21:33:08 +0100

    refactor: fix to actions

--- packages/discord.js/src/client/actions/VoiceStateUpdate.js
+++ packages/discord.js/src/client/actions/VoiceStateUpdate.js
@@ -11,7 +11,8 @@ class VoiceStateUpdate extends Action {
     if (guild) {
       // Update the state
       const oldState =
-        guild.voiceStates.cache.get(data.user_id)?._clone() ?? new VoiceState(guild, { user_id: data.user_id });
+        guild.voiceStates.cache.get(data.user_id)?._clone() ??
+        new VoiceState(guild, { user_id: data.user_id, suppress: data.suppress });
 
       const newState = guild.voiceStates._add(data);
 
