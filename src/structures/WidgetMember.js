44bbfa5c46a4bb35777aec8681f18590dec9aea4
Jiralite <33201955+Jiralite@users.noreply.github.com>
2021-08-12 15:06:11 +0100

    fix(WidgetMember): Default to `null` and not `undefined` (#6399)

--- src/structures/WidgetMember.js
+++ src/structures/WidgetMember.js
@@ -53,37 +53,37 @@ class WidgetMember extends Base {
      * IIf the member is server deafened
      * @type {?boolean}
      */
-    this.deaf = data.deaf;
+    this.deaf = data.deaf ?? null;
 
     /**
      * If the member is server muted
      * @type {?boolean}
      */
-    this.mute = data.mute;
+    this.mute = data.mute ?? null;
 
     /**
      * If the member is self deafened
      * @type {?boolean}
      */
-    this.selfDeaf = data.self_deaf;
+    this.selfDeaf = data.self_deaf ?? null;
 
     /**
      * If the member is self muted
      * @type {?boolean}
      */
-    this.selfMute = data.self_mute;
+    this.selfMute = data.self_mute ?? null;
 
     /**
      * If the member is suppressed
      * @type {?boolean}
      */
-    this.suppress = data.suppress;
+    this.suppress = data.suppress ?? null;
 
     /**
      * The id of the voice channel the member is in, if any
      * @type {?Snowflake}
      */
-    this.channelId = data.channel_id;
+    this.channelId = data.channel_id ?? null;
 
     /**
      * The avatar URL of the member.
@@ -95,7 +95,7 @@ class WidgetMember extends Base {
      * The activity of the member.
      * @type {?WidgetActivity}
      */
-    this.activity = data.activity;
+    this.activity = data.activity ?? null;
   }
 }
 
