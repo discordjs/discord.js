c9898ed028709c6cb1857c528e2d7af9e24b9b9d
Advaith <advaithj1@gmail.com>
2021-08-17 14:13:42 -0700

    feat: add new names

--- src/util/Permissions.js
+++ src/util/Permissions.js
@@ -91,8 +91,10 @@ class Permissions extends BitField {
  * * `USE_APPLICATION_COMMANDS`
  * * `REQUEST_TO_SPEAK`
  * * `MANAGE_THREADS`
- * * `USE_PUBLIC_THREADS` (create threads)
- * * `USE_PRIVATE_THREADS` (create private threads)
+ * * `USE_PUBLIC_THREADS` (deprecated)
+ * * `CREATE_PUBLIC_THREADS`
+ * * `USE_PRIVATE_THREADS` (deprecated)
+ * * `CREATE_PRIVATE_THREADS`
  * * `USE_EXTERNAL_STICKERS` (use stickers from different guilds)
  * * `SEND_MESSAGES_IN_THREADS`
  * @type {Object<string, bigint>}
@@ -133,9 +135,11 @@ Permissions.FLAGS = {
   USE_APPLICATION_COMMANDS: 1n << 31n,
   REQUEST_TO_SPEAK: 1n << 32n,
   MANAGE_THREADS: 1n << 34n,
-  // TODO: Rename USE_*_THREADS to CREATE_*_THREADS in v14
+  // TODO: Remove deprecated USE_*_THREADS flags in v14
   USE_PUBLIC_THREADS: 1n << 35n,
+  CREATE_PUBLIC_THREADS: 1n << 35n,
   USE_PRIVATE_THREADS: 1n << 36n,
+  CREATE_PRIVATE_THREADS: 1n << 36n,
   USE_EXTERNAL_STICKERS: 1n << 37n,
   SEND_MESSAGES_IN_THREADS: 1n << 38n,
 };
