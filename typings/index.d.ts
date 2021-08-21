d7baceefa01a0d845d75774c9e53e55875a43faa
Jiralite <33201955+Jiralite@users.noreply.github.com>
2021-08-21 02:14:48 +0100

    feat(Message): resolveComponent method

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1168,6 +1168,7 @@ export class Message extends Base {
   public react(emoji: EmojiIdentifierResolvable): Promise<MessageReaction>;
   public removeAttachments(): Promise<Message>;
   public reply(options: string | MessagePayload | ReplyMessageOptions): Promise<Message>;
+  public resolveComponent(customId: string): MessageActionRowComponent | null;
   public startThread(options: StartThreadOptions): Promise<ThreadChannel>;
   public suppressEmbeds(suppress?: boolean): Promise<Message>;
   public toJSON(): unknown;
