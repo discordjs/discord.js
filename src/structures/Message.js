1f4b93cd5783acc16816fd039b050f7e8f8d47be
Rodry <38259440+ImRodry@users.noreply.github.com>
2021-09-03 14:14:06 +0100

    fix(Message): pinnable returning false in non-DEFAULT messages

--- src/structures/Message.js
+++ src/structures/Message.js
@@ -571,7 +571,7 @@ class Message extends Base {
    */
   get pinnable() {
     return (
-      this.type === 'DEFAULT' &&
+      ['DEFAULT', 'REPLY', 'APPLICATION_COMMAND', 'THREAD_STARTER_MESSAGE'].includes(this.type) &&
       (!this.guild || this.channel.permissionsFor(this.client.user).has(Permissions.FLAGS.MANAGE_MESSAGES, false))
     );
   }
