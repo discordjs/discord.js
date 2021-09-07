95c0575e9bffb98e1e1b8a284b8d736f633b07ba
Rodry <38259440+ImRodry@users.noreply.github.com>
2021-09-07 14:04:11 +0100

    fix(Message): system messages are not pinnable

--- src/structures/Message.js
+++ src/structures/Message.js
@@ -573,6 +573,7 @@ class Message extends Base {
     return (
       ['DEFAULT', 'REPLY', 'APPLICATION_COMMAND', 'THREAD_STARTER_MESSAGE'].includes(this.type) &&
       (!this.guild || this.channel.permissionsFor(this.client.user).has(Permissions.FLAGS.MANAGE_MESSAGES, false))
+      !this.system
     );
   }
 
