082dab23a6c021cbdeee7da08a792f4f77975009
cycloptux <ale.impe@gmail.com>
2021-10-08 09:42:26 +0200

    fix(Guild): Make Guild#available assignment consistent with other checks, and avoid an unnecessary assignment.

--- src/structures/Guild.js
+++ src/structures/Guild.js
@@ -137,7 +137,11 @@ class Guild extends AnonymousGuild {
     this.id = data.id;
     if ('name' in data) this.name = data.name;
     if ('icon' in data) this.icon = data.icon;
-    this.available = 'unavailable' in data ? !data.unavailable : this.available ?? true;
+    if ('unavailable' in data) {
+      this.available = !data.unavailable;
+    } else {
+      this.available ??= true;
+    }
 
     if ('discovery_splash' in data) {
       /**
