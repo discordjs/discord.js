c2003630f07c21cfed7d9ce3048524c910eaa34d
IlluminatiFish <45714340+IlluminatiFish@users.noreply.github.com>
2021-10-11 20:38:21 +0100

    Allowed a broader range of webhook IDs to be matched
    From 18 digits strictly => between the ranges of 17 and 19 digits

--- packages/discord.js/src/client/WebhookClient.js
+++ packages/discord.js/src/client/WebhookClient.js
@@ -30,7 +30,7 @@ class WebhookClient extends BaseClient {
     if ('url' in data) {
       const url = data.url.match(
         // eslint-disable-next-line no-useless-escape
-        /https?:\/\/(?:ptb\.|canary\.)?discord\.com\/api(?:\/v\d{1,2})?\/webhooks\/(\d{18})\/([\w\-]{68})/i,
+        /https?:\/\/(?:ptb\.|canary\.)?discord\.com\/api(?:\/v\d{1,2})?\/webhooks\/(\d{17,19})\/([\w\-]{68})/i,
       );
 
       if (!url || url.length <= 1) throw new Error('WEBHOOK_URL_INVALID');
