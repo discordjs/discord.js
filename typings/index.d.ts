7c42673679b6739ec0d4b9b8a9317c6a688ad23a
Jake Ward <geek@gaminggeek.dev>
2021-10-20 23:39:46 +0100

    feat(MessageAttachment): description (alt text) support

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1375,6 +1375,7 @@ export class MessageAttachment {
 
   public attachment: BufferResolvable | Stream;
   public contentType: string | null;
+  public description: string | null;
   public ephemeral: boolean;
   public height: number | null;
   public id: Snowflake;
@@ -1384,6 +1385,7 @@ export class MessageAttachment {
   public readonly spoiler: boolean;
   public url: string;
   public width: number | null;
+  public setDescription(description: string): this;
   public setFile(attachment: BufferResolvable | Stream, name?: string): this;
   public setName(name: string): this;
   public setSpoiler(spoiler?: boolean): this;
