652fcfa407933f1f4a5cdb4ccd011bdc4a40151e
Suneet Tipirneni <77477100+suneettipirneni@users.noreply.github.com>
2021-10-26 12:34:03 -0400

    Update src/managers/ThreadMemberManager.js
    Co-authored-by: Vlad Frangu <kingdgrizzle@gmail.com>

--- src/managers/ThreadMemberManager.js
+++ src/managers/ThreadMemberManager.js
@@ -109,9 +109,9 @@ class ThreadMemberManager extends CachedManager {
 
   /**
    * Fetches member(s) for the thread from Discord, requires access to the `GUILD_MEMBERS` gateway intent.
-   * @param {?UserResolvable} member The member to fetch, if `undefined` `options.cached` is
-   * `true` and all members in the thread are fetched. If boolean, this serves the purpose
-   * of `options.cache`.
+   * @param {UserResolvable|boolean} [member] The member to fetch. If `undefined`, all members 
+   * in the thread are fetched, and will be cached based on `options.cache`. If boolean, this serves 
+   * the purpose of `options.cache`.
    * @param {BaseFetchOptions} [options] Additional options for this fetch
    * @returns {Promise<ThreadMember|Collection<Snowflake, ThreadMember>>}
    */
