9dbbbed1f9a4e9ef6d2b89c9bd847c9620bd806c
Jiralite <33201955+Jiralite@users.noreply.github.com>
2021-11-02 01:27:17 +0000

    refactor: initial deprecation

--- src/client/websocket/handlers/APPLICATION_COMMAND_CREATE.js
+++ src/client/websocket/handlers/APPLICATION_COMMAND_CREATE.js
@@ -2,6 +2,8 @@
 
 const { Events } = require('../../../util/Constants');
 
+let deprecationEmitted = false;
+
 module.exports = (client, { d: data }) => {
   const commandManager = data.guild_id ? client.guilds.cache.get(data.guild_id)?.commands : client.application.commands;
   if (!commandManager) return;
@@ -12,6 +14,16 @@ module.exports = (client, { d: data }) => {
    * Emitted when a guild application command is created.
    * @event Client#applicationCommandCreate
    * @param {ApplicationCommand} command The command which was created
+   * @deprecated See [this issue](https://github.com/discord/discord-api-docs/issues/3690) for more information.
    */
-  client.emit(Events.APPLICATION_COMMAND_CREATE, command);
+
+  if (client.emit(Events.APPLICATION_COMMAND_CREATE, command) && !deprecationEmitted) {
+    deprecationEmitted = true;
+
+    process.emitWarning(
+      /* eslint-disable-next-line max-len */
+      'The applicationCommandCreate event is deprecated. See https://github.com/discord/discord-api-docs/issues/3690 for more information.',
+      'DeprecationWarning',
+    );
+  }
 };
