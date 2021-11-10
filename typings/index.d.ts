098addd470e0f3b642d723f0d48bd39f7aab4a7d
Jiralite <33201955+Jiralite@users.noreply.github.com>
2021-11-10 02:37:00 +0000

    types: more concise message

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1571,10 +1571,7 @@ export class MessageEmbed {
   public addFields(...fields: EmbedFieldData[] | EmbedFieldData[][]): this;
   public setFields(...fields: EmbedFieldData[] | EmbedFieldData[][]): this;
   public setAuthor(options: string | EmbedAuthorData): this;
-  /**
-   * @deprecated Supply a lone object instead of more parameters.
-   * See [the documentation](https://discord.js.org/#/docs/main/stable/class/MessageEmbed?scrollTo=setAuthor) on how to use this method moving forward.
-   */
+  /** @deprecated Supply a lone object of interface {@link EmbedAuthorData} instead of more parameters. */
   public setAuthor(name: string, iconURL?: string, url?: string): this;
   public setColor(color: ColorResolvable): this;
   public setDescription(description: string): this;
