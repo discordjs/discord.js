9883b80fdd62bbab6ac9c2c9145e7046700ac3cb
That_Guy977 <72870724+That-Guy977@users.noreply.github.com>
2021-11-13 19:30:45 +0700

    Fix discordSort in Util
    The Discord client sorts channels with the same position by ID, ascending. Previously `discordSort` would sort descending

--- src/util/Util.js
+++ src/util/Util.js
@@ -484,8 +484,8 @@ class Util extends null {
     return collection.sorted(
       (a, b) =>
         a.rawPosition - b.rawPosition ||
-        parseInt(b.id.slice(0, -10)) - parseInt(a.id.slice(0, -10)) ||
-        parseInt(b.id.slice(10)) - parseInt(a.id.slice(10)),
+        parseInt(a.id.slice(0, -10)) - parseInt(b.id.slice(0, -10)) ||
+        parseInt(a.id.slice(10)) - parseInt(b.id.slice(10)),
     );
   }
 
