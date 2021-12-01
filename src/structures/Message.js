85e6812ce2077a8c82a600a6709ec6bc904f4285
Jiralite <33201955+Jiralite@users.noreply.github.com>
2021-12-01 11:27:24 +0000

    docs(MessageReference): Fix static link (#7041)

--- src/structures/Message.js
+++ src/structures/Message.js
@@ -293,7 +293,7 @@ class Message extends Base {
     /**
      * Reference data sent in a message that contains ids identifying the referenced message.
      * This can be present in the following types of message:
-     * * Crossposted messages (IS_CROSSPOST {@link MessageFlags#FLAGS message flag})
+     * * Crossposted messages (IS_CROSSPOST {@link MessageFlags.FLAGS message flag})
      * * CHANNEL_FOLLOW_ADD
      * * CHANNEL_PINNED_MESSAGE
      * * REPLY
