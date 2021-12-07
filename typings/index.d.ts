e2b7753c6e52aa0e618b9949d75495d9cc440bcc
Suneet Tipirneni <suneettipirneni@icloud.com>
2021-12-07 14:27:24 -0500

    types: guild id non-null when in guild

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1379,7 +1379,7 @@ export class Message<Cached extends boolean = boolean> extends Base {
   public editedTimestamp: number | null;
   public embeds: MessageEmbed[];
   public groupActivityApplication: ClientApplication | null;
-  public guildId: Snowflake | null;
+  public guildId: If<Cached, Snowflake>;
   public readonly guild: If<Cached, Guild>;
   public readonly hasThread: boolean;
   public id: Snowflake;
