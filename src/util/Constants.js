fdba146f9bd9f2743041f6302ca5e38e26c8dc44
Rodry <38259440+ImRodry@users.noreply.github.com>
2021-12-08 17:51:07 +0000

    docs(StoreChannel): add deprecation warnings (#7080)
    Co-authored-by: Antonio Román <kyradiscord@gmail.com>

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -465,6 +465,9 @@ exports.ActivityTypes = createEnum(['PLAYING', 'STREAMING', 'LISTENING', 'WATCHI
  * * `GROUP_DM` - a group DM channel
  * * `GUILD_CATEGORY` - a guild category channel
  * * `GUILD_NEWS` - a guild news channel
+ * <warn>Store channels are deprecated and will be removed from Discord in March 2022. See
+ * [Self-serve Game Selling Deprecation](https://support-dev.discord.com/hc/en-us/articles/4414590563479)
+ * for more information.</warn>
  * * `GUILD_STORE` - a guild store channel
  * * `GUILD_NEWS_THREAD` - a guild news channel's public thread channel
  * * `GUILD_PUBLIC_THREAD` - a guild text channel's public thread channel
