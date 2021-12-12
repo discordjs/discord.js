f5ce4177d20c388e8944cd75d0c26f1bbb7a1382
Jiralite <33201955+Jiralite@users.noreply.github.com>
2021-12-12 16:24:31 +0000

    feat: add PATCH

--- src/managers/GuildMemberManager.js
+++ src/managers/GuildMemberManager.js
@@ -232,6 +232,8 @@ class GuildMemberManager extends CachedManager {
    * @property {boolean} [deaf] Whether or not the member should be deafened
    * @property {GuildVoiceChannelResolvable|null} [channel] Channel to move the member to
    * (if they are connected to voice), or `null` if you want to disconnect them from voice
+   * @property {number|null} [communicationDisabledUntil] The timestamp
+   * for the member's communication to be disabled until. Provide `null` to remove the timeout.
    */
 
   /**
@@ -260,6 +262,7 @@ class GuildMemberManager extends CachedManager {
       _data.channel = undefined;
     }
     _data.roles &&= _data.roles.map(role => (role instanceof Role ? role.id : role));
+    _data.communication_disabled_until = _data.communicationDisabledUntil;
     let endpoint = this.client.api.guilds(this.guild.id);
     if (id === this.client.user.id) {
       const keys = Object.keys(_data);
