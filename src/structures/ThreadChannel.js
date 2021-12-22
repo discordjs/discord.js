0c64dde307a36f3791a9a492c0d10a9160ce9570
Jiralite <33201955+Jiralite@users.noreply.github.com>
2021-12-22 10:24:10 +0000

    feat(ThreadChannel): add permission checks

--- src/structures/ThreadChannel.js
+++ src/structures/ThreadChannel.js
@@ -439,7 +439,12 @@ class ThreadChannel extends Channel {
    * @readonly
    */
   get manageable() {
-    return this.permissionsFor(this.client.user)?.has(Permissions.FLAGS.MANAGE_THREADS, false);
+    const permissions = this.permissionsFor(this.client.user);
+    if (!permissions) return false;
+    // This flag allows managing even if timed out
+    if (permissions.has(Permissions.FLAGS.ADMINISTRATOR, false)) return true;
+    if (!permissions.has(Permissions.FLAGS.MANAGE_THREADS, false)) return false;
+    return this.guild.me.communicationDisabledUntilTimestamp < Date.now();
   }
 
   /**
@@ -460,11 +465,16 @@ class ThreadChannel extends Channel {
    * @readonly
    */
   get sendable() {
+    const permissions = this.permissionsFor(this.client.user);
+    if (!permissions) return false;
+    // This flag allows sending even if timed out
+    if (permissions.has(Permissions.FLAGS.ADMINISTRATOR, false)) return true;
+
     return (
-      (!(this.archived && this.locked && !this.manageable) &&
-        (this.type !== 'GUILD_PRIVATE_THREAD' || this.joined || this.manageable) &&
-        this.permissionsFor(this.client.user)?.has(Permissions.FLAGS.SEND_MESSAGES_IN_THREADS, false)) ??
-      false
+      !(this.archived && this.locked && !this.manageable) &&
+      (this.type !== 'GUILD_PRIVATE_THREAD' || this.joined || this.manageable) &&
+      permissions.has(Permissions.FLAGS.SEND_MESSAGES_IN_THREADS, false) &&
+      this.guild.me.communicationDisabledUntilTimestamp < Date.now()
     );
   }
 
