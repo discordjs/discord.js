4a2dbd62382f904d596b34da0116d50e724b81c4
iCrawl <buechler.noel@outlook.com>
2022-01-08 23:16:38 +0100

    test: fix voice secretbox tests

--- packages/voice/src/util/__tests__/Secretbox.test.ts
+++ packages/voice/src/util/__tests__/Secretbox.test.ts
@@ -1,14 +1,6 @@
 import { methods } from '../Secretbox';
-jest.mock(
-	'tweetnacl',
-	() => ({
-		secretbox: {
-			// eslint-disable-next-line @typescript-eslint/no-empty-function
-			open() {},
-		},
-	}),
-	{ virtual: true },
-);
+
+jest.mock('tweetnacl');
 
 test('Does not throw error with a package installed', () => {
 	// @ts-expect-error
