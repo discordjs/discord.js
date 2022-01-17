e65da44d9c564d1ffcb0f4df2bcdaf0ce0636f35
Almeida <almeidx@pm.me>
2022-01-17 12:15:46 +0000

    docs(shardingmanager): fix type of `execArgv` option (#7284)

--- packages/discord.js/src/sharding/ShardingManager.js
+++ packages/discord.js/src/sharding/ShardingManager.js
@@ -36,7 +36,7 @@ class ShardingManager extends EventEmitter {
    * @property {boolean} [respawn=true] Whether shards should automatically respawn upon exiting
    * @property {string[]} [shardArgs=[]] Arguments to pass to the shard script when spawning
    * (only available when mode is set to 'process')
-   * @property {string} [execArgv=[]] Arguments to pass to the shard script executable when spawning
+   * @property {string[]} [execArgv=[]] Arguments to pass to the shard script executable when spawning
    * (only available when mode is set to 'process')
    * @property {string} [token] Token to use for automatic shard count and passing to shards
    */
