8c10eafa5372ef906c03deaec84fce9a8a582b31
Suneet Tipirneni <77477100+suneettipirneni@users.noreply.github.com>
2022-01-19 19:49:38 -0500

    Update packages/discord.js/src/structures/ThreadChannel.js
    Co-authored-by: Almeida <almeidx@pm.me>

--- packages/discord.js/src/structures/ThreadChannel.js
+++ packages/discord.js/src/structures/ThreadChannel.js
@@ -214,7 +214,7 @@ class ThreadChannel extends Channel {
    * @readonly
    */
   get createdAt() {
-    return this.createdTimestamp === null ? null : new Date(this.createdTimestamp);
+    return this.createdTimestamp && new Date(this.createdTimestamp);
   }
 
   /**
