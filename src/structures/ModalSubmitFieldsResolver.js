01cb2d2f33e716a8ed94076d6298b73096b4d659
monbrey <monbrey@gmail.com>
2022-02-10 10:31:30 +1100

    fix(resolver): some v13ish things

--- src/structures/ModalSubmitFieldsResolver.js
+++ src/structures/ModalSubmitFieldsResolver.js
@@ -18,7 +18,7 @@ class ModalSubmitFieldsResolver {
    * @private
    */
   get _fields() {
-    return this.components.reduce((previous, next) => previous.concat(next), []);
+    return this.components.reduce((previous, next) => previous.concat(next.components), []);
   }
 
   /**
@@ -39,7 +39,7 @@ class ModalSubmitFieldsResolver {
    */
   getTextInputValue(customId) {
     const field = this.getField(customId);
-    const expectedType = MessageComponentTypes.TEXT_INPUT;
+    const expectedType = MessageComponentTypes[MessageComponentTypes.TEXT_INPUT];
     if (field.type !== expectedType) {
       throw new TypeError('MODAL_SUBMIT_INTERACTION_FIELD_TYPE', customId, field.type, expectedType);
     }
