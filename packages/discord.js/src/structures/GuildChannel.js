c070073621aa301b380f8bc26ceb884aef2f076f
TheBigWolfy <51050518+thebigwolfy@users.noreply.github.com>
2022-02-15 14:28:41 +0100

    Fix

--- packages/discord.js/src/structures/GuildChannel.js
+++ packages/discord.js/src/structures/GuildChannel.js
@@ -355,7 +355,8 @@ class GuildChannel extends Channel {
    * @returns {Promise<GuildChannel>}
    */
   clone(options = {}) {
-    return this.guild.channels.create(options.name ?? this.name, {
+    return this.guild.channels.create({
+      name: options.name ?? this.name,
       permissionOverwrites: this.permissionOverwrites.cache,
       topic: this.topic,
       type: this.type,
