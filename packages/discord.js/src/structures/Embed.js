000a693cd20fda330d62d029729b5101f23e6eff
suneettipirneni <suneettipirneni@icloud.com>
2022-03-03 19:08:57 -0500

    docs: document djs setColor

--- packages/discord.js/src/structures/Embed.js
+++ packages/discord.js/src/structures/Embed.js
@@ -4,11 +4,19 @@ const { Embed: BuildersEmbed } = require('@discordjs/builders');
 const Transformers = require('../util/Transformers');
 const { Util } = require('../util/Util');
 
+/**
+ * Represents an embed object
+ */
 class Embed extends BuildersEmbed {
   constructor(data) {
     super(Transformers.toSnakeCase(data));
   }
 
+  /**
+   * Sets the color of this embed
+   * @param {ColorResolvable} color The color of the embed
+   * @returns {Embed}
+   */
   setColor(color) {
     if (color === null) {
       return super.setColor(null);
