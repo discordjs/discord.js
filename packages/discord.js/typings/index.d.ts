2297c2b9479ace16f5f7155479605a4ac2718e3d
Rodry <38259440+ImRodry@users.noreply.github.com>
2022-03-14 11:04:07 +0000

    types(ColorResolvable): simplify string types (#7643)

--- packages/discord.js/typings/index.d.ts
+++ packages/discord.js/typings/index.d.ts
@@ -3837,36 +3837,7 @@ export interface CollectorResetTimerOptions {
 }
 
 export type ColorResolvable =
-  | 'Default'
-  | 'White'
-  | 'Aqua'
-  | 'Green'
-  | 'Blue'
-  | 'Yellow'
-  | 'Purple'
-  | 'LuminousVividPink'
-  | 'Fuchsia'
-  | 'Gold'
-  | 'Orange'
-  | 'Red'
-  | 'Grey'
-  | 'Navy'
-  | 'DarkAqua'
-  | 'DarkGreen'
-  | 'DarkBlue'
-  | 'DarkPurple'
-  | 'DarkVividPink'
-  | 'DarkGold'
-  | 'DarkOrange'
-  | 'DarkRed'
-  | 'DarkGrey'
-  | 'DarkerGrey'
-  | 'LightGrey'
-  | 'DarkNavy'
-  | 'Blurple'
-  | 'Greyple'
-  | 'DarkButNotBlack'
-  | 'NotQuiteBlack'
+  | keyof typeof Colors
   | 'Random'
   | readonly [red: number, green: number, blue: number]
   | number
