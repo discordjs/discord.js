1192a30a09ad7f15e090b8ce52a5e890e34af6c6
suneettipirneni <suneettipirneni@icloud.com>
2022-03-23 16:40:27 -0400

    chore: remove memberof docs

--- packages/discord.js/src/util/Formatters.js
+++ packages/discord.js/src/util/Formatters.js
@@ -28,7 +28,6 @@ class Formatters extends null {
   /**
    * Formats the content into a block quote. This needs to be at the start of the line for Discord to format it.
    * @method blockQuote
-   * @memberof Formatters
    * @param {string} content The content to wrap.
    * @returns {string}
    */
@@ -37,7 +36,6 @@ class Formatters extends null {
   /**
    * Formats the content into bold text.
    * @method bold
-   * @memberof Formatters
    * @param {string} content The content to wrap.
    * @returns {string}
    */
@@ -46,7 +44,6 @@ class Formatters extends null {
   /**
    * Formats a channel id into a channel mention.
    * @method channelMention
-   * @memberof Formatters
    * @param {string} channelId The channel id to format.
    * @returns {string}
    */
@@ -55,7 +52,6 @@ class Formatters extends null {
   /**
    * Wraps the content inside a code block with an optional language.
    * @method codeBlock
-   * @memberof Formatters
    * @param {string} contentOrLanguage The language to use, content if a second parameter isn't provided.
    * @param {string} [content] The content to wrap.
    * @returns {string}
@@ -65,7 +61,6 @@ class Formatters extends null {
   /**
    * Formats an emoji id into a fully qualified emoji identifier
    * @method formatEmoji
-   * @memberof Formatters
    * @param {string} emojiId The emoji id to format.
    * @param {boolean} [animated] Whether the emoji is animated or not. Defaults to `false`
    * @returns {string}
@@ -75,7 +70,6 @@ class Formatters extends null {
   /**
    * Wraps the URL into `<>`, which stops it from embedding.
    * @method hideLinkEmbed
-   * @memberof Formatters
    * @param {string} content The content to wrap.
    * @returns {string}
    */
@@ -84,7 +78,6 @@ class Formatters extends null {
   /**
    * Formats the content and the URL into a masked URL with an optional title.
    * @method hyperlink
-   * @memberof Formatters
    * @param {string} content The content to display.
    * @param {string} url The URL the content links to.
    * @param {string} [title] The title shown when hovering on the masked link.
@@ -95,7 +88,6 @@ class Formatters extends null {
   /**
    * Wraps the content inside \`backticks\`, which formats it as inline code.
    * @method inlineCode
-   * @memberof Formatters
    * @param {string} content The content to wrap.
    * @returns {string}
    */
@@ -104,7 +96,6 @@ class Formatters extends null {
   /**
    * Formats the content into italic text.
    * @method italic
-   * @memberof Formatters
    * @param {string} content The content to wrap.
    * @returns {string}
    */
@@ -113,7 +104,6 @@ class Formatters extends null {
   /**
    * Formats a user id into a member-nickname mention.
    * @method memberNicknameMention
-   * @memberof Formatters
    * @param {string} memberId The user id to format.
    * @returns {string}
    */
@@ -122,7 +112,6 @@ class Formatters extends null {
   /**
    * Formats the content into a quote. This needs to be at the start of the line for Discord to format it.
    * @method quote
-   * @memberof Formatters
    * @param {string} content The content to wrap.
    * @returns {string}
    */
@@ -131,7 +120,6 @@ class Formatters extends null {
   /**
    * Formats a role id into a role mention.
    * @method roleMention
-   * @memberof Formatters
    * @param {string} roleId The role id to format.
    * @returns {string}
    */
@@ -140,7 +128,6 @@ class Formatters extends null {
   /**
    * Formats the content into spoiler text.
    * @method spoiler
-   * @memberof Formatters
    * @param {string} content The content to spoiler.
    * @returns {string}
    */
@@ -149,7 +136,6 @@ class Formatters extends null {
   /**
    * Formats the content into strike-through text.
    * @method strikethrough
-   * @memberof Formatters
    * @param {string} content The content to wrap.
    * @returns {string}
    */
@@ -158,7 +144,6 @@ class Formatters extends null {
   /**
    * Formats a date into a short date-time string.
    * @method time
-   * @memberof Formatters
    * @param {number|Date} [date] The date to format.
    * @param {TimestampStylesString} [style] The style to use.
    * @returns {string}
@@ -181,7 +166,6 @@ class Formatters extends null {
   /**
    * The message formatting timestamp
    * [styles](https://discord.com/developers/docs/reference#message-formatting-timestamp-styles) supported by Discord.
-   * @memberof Formatters
    * @type {Object<string, TimestampStylesString>}
    */
   static TimestampStyles = TimestampStyles;
@@ -189,7 +173,6 @@ class Formatters extends null {
   /**
    * Formats the content into underscored text.
    * @method underscore
-   * @memberof Formatters
    * @param {string} content The content to wrap.
    * @returns {string}
    */
@@ -198,7 +181,6 @@ class Formatters extends null {
   /**
    * Formats a user id into a user mention.
    * @method userMention
-   * @memberof Formatters
    * @param {string} userId The user id to format.
    * @returns {string}
    */
