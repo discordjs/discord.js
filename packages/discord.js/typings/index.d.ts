daf2829cb58d1a44cb1f1ece21e428d1a23e99c9
muchnameless <12682826+muchnameless@users.noreply.github.com>
2022-03-24 20:58:39 +0100

    types(InteractionResponseFields): add webhook (#7597)

--- packages/discord.js/typings/index.d.ts
+++ packages/discord.js/typings/index.d.ts
@@ -363,6 +363,7 @@ export interface InteractionResponseFields<Cached extends CacheType = CacheType>
   deferred: boolean;
   ephemeral: boolean | null;
   replied: boolean;
+  webhook: InteractionWebhook;
   reply(options: InteractionReplyOptions & { fetchReply: true }): Promise<GuildCacheMessage<Cached>>;
   reply(options: string | MessagePayload | InteractionReplyOptions): Promise<void>;
   deleteReply(): Promise<void>;
