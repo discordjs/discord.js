066339e51b50fdde448613803ba4763b835efbbe
Jiralite <33201955+Jiralite@users.noreply.github.com>
2022-03-25 16:37:01 +0000

    feat(StageInstance): add support for associated guild event

--- src/structures/StageInstance.js
+++ src/structures/StageInstance.js
@@ -72,6 +72,16 @@ class StageInstance extends Base {
     } else {
       this.discoverableDisabled ??= null;
     }
+
+    if ('guild_scheduled_event_id' in data) {
+      /**
+       * The associated guild scheduled event id of this stage instance
+       * @type {?Snowflake}
+       */
+      this.guildScheduledEventId = data.guild_scheduled_event_id;
+    } else {
+      this.guildScheduledEventId ??= null;
+    }
   }
 
   /**
@@ -81,6 +91,15 @@ class StageInstance extends Base {
    */
   get channel() {
     return this.client.channels.resolve(this.channelId);
+  }
+
+  /**
+   * The associated guild scheduled event of this stage instance
+   * @type {?GuildScheduledEvent}
+   * @readonly
+   */
+  get guildScheduledEvent() {
+    return this.guild?.scheduledEvents.resolve(this.guildScheduledEventId) ?? null;
   }
 
   /**
