0216814cb7f62ee30f48a10106aaea9b243f5d31
Jiralite <33201955+Jiralite@users.noreply.github.com>
2022-03-25 11:33:18 +0000

    fix: handle components

--- packages/discord.js/src/structures/ModalBuilder.js
+++ packages/discord.js/src/structures/ModalBuilder.js
@@ -7,10 +7,12 @@ const Transformers = require('../util/Transformers');
  * Represents a modal builder.
  */
 class ModalBuilder extends BuildersModal {
-  constructor(data) {
-    super(Transformers.toSnakeCase(data));
+  constructor({ components, ...data }) {
+    super({
+      components: components?.map(c => (isJSONEncodable(c) ? c.toJSON() : Transformers.toSnakeCase(c))),
+      ...Transformers.toSnakeCase(data),
+    });
   }
-
   /**
    * Creates a new modal builder from JSON data
    * @param {JSONEncodable<APIModalComponent> | APIModalComponent} other The other data
