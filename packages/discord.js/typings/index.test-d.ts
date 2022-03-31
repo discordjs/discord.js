28a4f4700b605cc0edd54bfe9114880a780af3e9
Jiralite <33201955+Jiralite@users.noreply.github.com>
2022-03-31 23:37:31 +0100

    chore: formatting

--- packages/discord.js/typings/index.test-d.ts
+++ packages/discord.js/typings/index.test-d.ts
@@ -1034,16 +1034,16 @@ expectType<Promise<GuildEmoji>>(guildEmojiManager.fetch('0'));
 
 declare const guildBanManager: GuildBanManager;
 {
-  expectType<Promise<GuildBan>>(guildBanManager.fetch("1234567890"));
-  expectType<Promise<GuildBan>>(guildBanManager.fetch({ user: "1234567890" }));
-  expectType<Promise<GuildBan>>(guildBanManager.fetch({ user: "1234567890", cache: true, force: false }));
+  expectType<Promise<GuildBan>>(guildBanManager.fetch('1234567890'));
+  expectType<Promise<GuildBan>>(guildBanManager.fetch({ user: '1234567890' }));
+  expectType<Promise<GuildBan>>(guildBanManager.fetch({ user: '1234567890', cache: true, force: false }));
   expectType<Promise<Collection<Snowflake, GuildBan>>>(guildBanManager.fetch());
   expectType<Promise<Collection<Snowflake, GuildBan>>>(guildBanManager.fetch({}));
-  expectType<Promise<Collection<Snowflake, GuildBan>>>(guildBanManager.fetch({ limit: 100, before: "1234567890" }));
+  expectType<Promise<Collection<Snowflake, GuildBan>>>(guildBanManager.fetch({ limit: 100, before: '1234567890' }));
   // @ts-expect-error
   guildBanManager.fetch({ cache: true, force: false });
   // @ts-expect-error
-  guildBanManager.fetch({ user: "1234567890", after: "1234567890", cache: true, force: false });
+  guildBanManager.fetch({ user: '1234567890', after: '1234567890', cache: true, force: false });
 }
 
 declare const typing: Typing;
