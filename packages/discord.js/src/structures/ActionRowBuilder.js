ef9713542d4badfc9c5efb818c02fea2d7e2aa69
Jiralite <33201955+Jiralite@users.noreply.github.com>
2022-04-04 12:02:14 +0100

    refactor: apply suggestion from review

--- packages/discord.js/src/structures/ActionRowBuilder.js
+++ packages/discord.js/src/structures/ActionRowBuilder.js
@@ -9,8 +9,8 @@ const Transformers = require('../util/Transformers');
 class ActionRowBuilder extends BuildersActionRow {
   constructor({ components, ...data } = {}) {
     super({
-      components: components?.map(c => (c instanceof ComponentBuilder ? c : Transformers.toSnakeCase(c))),
       ...Transformers.toSnakeCase(data),
+      components: components?.map(c => (c instanceof ComponentBuilder ? c : Transformers.toSnakeCase(c))),
     });
   }
 }
