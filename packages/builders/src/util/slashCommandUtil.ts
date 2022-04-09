2bc18eae713cbbb9fbbd5880adc2b236f8751561
suneettipirneni <suneettipirneni@icloud.com>
2022-04-09 14:49:24 -0400

    fix: prevent unnecessary spread

--- packages/builders/src/util/slashCommandUtil.ts
+++ packages/builders/src/util/slashCommandUtil.ts
@@ -1,7 +1,5 @@
 import type { LocaleString, LocalizationMap } from 'discord-api-types/v10';
 
 export function flattenLocaleMap(localeMap: Map<LocaleString, string> | LocalizationMap): [LocaleString, string][] {
-	return localeMap instanceof Map
-		? [...localeMap.entries()]
-		: ([...Object.entries(localeMap)] as [LocaleString, string][]);
+	return localeMap instanceof Map ? [...localeMap.entries()] : (Object.entries(localeMap) as [LocaleString, string][]);
 }
