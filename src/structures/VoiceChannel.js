39bfcf70b85a4b0cad50169897dc324c7f6f6077
Jiralite <33201955+Jiralite@users.noreply.github.com>
2022-04-13 01:06:24 +0100

    feat: backport

--- src/structures/VoiceChannel.js
+++ src/structures/VoiceChannel.js
@@ -11,6 +11,20 @@ let deprecationEmittedForEditable = false;
  * @extends {BaseGuildVoiceChannel}
  */
 class VoiceChannel extends BaseGuildVoiceChannel {
+  _patch(data) {
+    super._patch(data);
+
+    if ('video_quality_mode' in data) {
+      /**
+       * The camera video quality mode of the channel.
+       * @type {?number}
+       */
+      this.videoQualityMode = data.video_quality_mode;
+    } else {
+      this.videoQualityMode ??= null;
+    }
+  }
+
   /**
    * Whether the channel is editable by the client user
    * @type {boolean}
@@ -85,6 +99,16 @@ class VoiceChannel extends BaseGuildVoiceChannel {
    */
   setUserLimit(userLimit, reason) {
     return this.edit({ userLimit }, reason);
+  }
+
+  /**
+   * Sets the camera video quality mode of the channel.
+   * @param {VideoQualityMode|number} videoQualityMode The new camera video quality mode.
+   * @param {string} [reason] Reason for changing the camera video quality mode.
+   * @returns {Promise<VoiceChannel>}
+   */
+  setVideoQualityMode(videoQualityMode, reason) {
+    return this.edit({ videoQualityMode }, reason);
   }
 
   /**
