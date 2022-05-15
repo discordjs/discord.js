57ae6b629989469a40148b201c54682d9c78c0d0
didinele <didinele.dev@gmail.com>
2022-05-15 21:50:34 +0300

    refactor(proxyRequests): use REST#raw

--- packages/proxy/src/index.ts
+++ packages/proxy/src/index.ts
@@ -1,5 +1,5 @@
 import type { IncomingMessage, ServerResponse } from 'http';
-import { DiscordAPIError, RequestMethod, REST, RouteLike } from '@discordjs/rest';
+import { DiscordAPIError, parseResponse, RequestMethod, REST, RouteLike } from '@discordjs/rest';
 
 type Awaitable<T> = T | PromiseLike<T>;
 type RequestHandler = (req: IncomingMessage, res: ServerResponse) => Awaitable<void>;
@@ -22,26 +22,26 @@ export function proxyRequests(rest: REST): RequestHandler {
 		}
 
 		try {
-			const discordResponse = await rest.request({
+			const discordResponse = await rest.raw({
 				body: req,
-				// Unsure how safe this cast is
 				fullRoute: url as RouteLike,
 				method: method as RequestMethod,
 				passThroughBody: true,
 			});
 
-			// This is a bit of an issue. As per the parseResponse method in REST, `res` will be an ArrayBuffer if the Content-Type
-			// Discord provided is for some reason NOT application/json
-			// Realistically though folks could be using their own request handler and handle this different, not to mention ideally we'd relay
-			// The Content-Type Discord actually returned - unsure what to use here otherwise
-			const contentType = ArrayBuffer.isView(discordResponse) ? 'text/plain' : 'application/json';
+			res.statusCode = discordResponse.statusCode;
 
-			// This is also problematic. Some routes return 204, ideally we'd relay that back
-			// Could potentially check if `res` is an empty object?
-			// Ideally I wouldn't have to re-compute headers and the status code, but REST provides no method to get a raw Response back atm.
-			res.statusCode = 200;
-			res.setHeader('Content-Type', contentType);
-			res.end(JSON.stringify(discordResponse));
+			for (const header of Object.keys(discordResponse.headers)) {
+				// Strip ratelimit headers
+				if (header.startsWith('x-ratelimit')) {
+					continue;
+				}
+
+				res.setHeader(header, discordResponse.headers[header]!);
+			}
+
+			const data = await parseResponse(discordResponse);
+			res.end(discordResponse.headers['content-type']?.startsWith('application/json') ? data : JSON.stringify(data));
 		} catch (error) {
 			if (!(error instanceof DiscordAPIError)) {
 				// Unclear if there's better course of action here. Any web framework allow to pass in an error handler for something like this
