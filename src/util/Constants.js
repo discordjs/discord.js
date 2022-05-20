681c4e2fa1083b62c653fdd211f9b7fc9c4cde5e
GrapeColor <grapecolor@users.noreply.github.com>
2022-05-21 01:48:26 +0900

    fix(ApplicationCommandOptionType): Add attachment to jsdoc

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -1036,6 +1036,7 @@ exports.ApplicationCommandTypes = createEnum([null, 'CHAT_INPUT', 'USER', 'MESSA
  * * ROLE
  * * MENTIONABLE
  * * NUMBER
+ * * ATTACHMENT
  * @typedef {string} ApplicationCommandOptionType
  * @see {@link https://discord.com/developers/docs/interactions/application-commands#application-command-object-application-command-option-type}
  */
