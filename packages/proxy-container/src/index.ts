03559ed23f3d0d9dc51fb9a9fa4fe35031579027
didinele <didinele.dev@gmail.com>
2022-06-05 16:31:58 +0300

    feat: docker build

--- packages/proxy-container/src/index.ts
+++ packages/proxy-container/src/index.ts
@@ -2,8 +2,14 @@ import { createServer } from 'node:http';
 import { proxyRequests } from '@discordjs/proxy';
 import { REST } from '@discordjs/rest';
 
+if (!process.env.DISCORD_TOKEN) {
+	throw new Error('A DISCORD_TOKEN env var is required');
+}
+
 // We want to let upstream handle retrying
-const api = new REST({ rejectOnRateLimit: () => true, retries: 0 });
+const api = new REST({ rejectOnRateLimit: () => true, retries: 0 }).setToken(process.env.DISCORD_TOKEN);
 // eslint-disable-next-line @typescript-eslint/no-misused-promises
 const server = createServer(proxyRequests(api));
-server.listen(parseInt(process.env.PORT ?? '8080', 10));
+
+const port = parseInt(process.env.PORT ?? '8080', 10);
+server.listen(port, () => console.log(`Listening on port ${port}`));
