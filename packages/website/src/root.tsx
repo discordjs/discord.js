3ae2633c3f3f009592453a1fbae994ef6159ede2
iCrawl <buechler.noel@outlook.com>
2022-06-08 23:40:03 +0200

    feat(website): add some styling

--- packages/website/src/root.tsx
+++ packages/website/src/root.tsx
@@ -1,10 +1,12 @@
 import type { MetaFunction, LinksFunction } from '@remix-run/node';
 import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
 import unocssReset from '@unocss/reset/normalize.css';
+import maincss from './styles/main.css';
 import unocss from './styles/unocss.css';
 
 export const links: LinksFunction = () => [
 	{ rel: 'stylesheet', href: unocssReset },
+	{ rel: 'stylesheet', href: maincss },
 	{ rel: 'stylesheet', href: unocss },
 ];
 
--- packages/website/src/routes/index.tsx
+++ packages/website/src/routes/index.tsx
@@ -1,3 +1,9 @@
 export default function IndexRoute() {
-	return <div className="text-2xl">Hello World</div>;
+	return (
+		<main className="w-full h-screen">
+			<div className="h-screen grid place-content-center">
+				<h1 className="">Coming soon...</h1>
+			</div>
+		</main>
+	);
 }
