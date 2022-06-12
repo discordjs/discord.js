87b03be03adab6837bf87a27a5b42fe00d308668
almeidx <almeidx@pm.me>
2022-06-12 18:20:53 +0100

    docs: update outdated examples
    closes #8080

--- packages/discord.js/src/managers/GuildStickerManager.js
+++ packages/discord.js/src/managers/GuildStickerManager.js
@@ -48,12 +48,12 @@ class GuildStickerManager extends CachedManager {
    * @returns {Promise<Sticker>} The created sticker
    * @example
    * // Create a new sticker from a URL
-   * guild.stickers.create('https://i.imgur.com/w3duR07.png', 'rip', 'headstone')
+   * guild.stickers.create({ file: 'https://i.imgur.com/w3duR07.png', name: 'rip', tags: 'headstone' })
    *   .then(sticker => console.log(`Created new sticker with name ${sticker.name}!`))
    *   .catch(console.error);
    * @example
    * // Create a new sticker from a file on your computer
-   * guild.stickers.create('./memes/banana.png', 'banana', 'banana')
+   * guild.stickers.create({ file: './memes/banana.png', name: 'banana', tags: 'banana' })
    *   .then(sticker => console.log(`Created new sticker with name ${sticker.name}!`))
    *   .catch(console.error);
    */
