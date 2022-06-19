268a9b4be5a6e5121c4292b745a6ecfce8b2cdae
DD <didinele.dev@gmail.com>
2022-06-19 14:18:36 +0300

    fix: vitest recursion (#8121)

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	test: {
-		exclude: ['node_modules', 'dist', '.idea', '.git', '.cache', 'packages/discord.js', 'packages/voice'],
+		exclude: ['**/node_modules', '**/dist', '.idea', '.git', '.cache', 'packages/discord.js', 'packages/voice'],
 		passWithNoTests: true,
 		coverage: {
 			enabled: true,
