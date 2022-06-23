65444f510d33b856af2af549676bee406c1fef29
Jiralite <33201955+Jiralite@users.noreply.github.com>
2022-06-23 11:37:28 +0100

    docs: `TextBasedChannel`-> `TextBasedChannels` typos (v13) (#8155)

--- src/structures/Message.js
+++ src/structures/Message.js
@@ -383,7 +383,7 @@ class Message extends Base {
 
   /**
    * The channel that the message was sent in
-   * @type {TextBasedChannel}
+   * @type {TextBasedChannels}
    * @readonly
    */
   get channel() {
