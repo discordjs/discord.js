d7095f038c8fdcf3bbebd6d70f4b6f1a16b0a5ba
Tyler Resch <developer@midspike.com>
2022-07-01 19:36:16 -0400

    docs(MessageInteraction): commandName description
    # Documents breaking change for `Message#interaction`.
    
    # Source Docs
    
    > Starting **July 18, 2022**, the `name` field for message interaction objects will now include subcommands and subcommand groups in the value (along with the existing top-level command). In the future, we recommend not relying on this message interaction field.
    
    https://github.com/discord/discord-api-docs/blob/main/docs/Change_Log.md#add-subcommand-groups-and-subcommands-to-message-interaction-objects
    
    # Inspiration for new description
    
    https://github.com/discord/discord-api-docs/pull/5140/files#diff-b44c2bafd9f1fb4cd3383e25770ef07d972796aa56dedd73ba1a7853df2d62d0R117

--- packages/discord.js/src/structures/Message.js
+++ packages/discord.js/src/structures/Message.js
@@ -329,7 +329,7 @@ class Message extends Base {
      * @typedef {Object} MessageInteraction
      * @property {Snowflake} id The interaction's id
      * @property {InteractionType} type The type of the interaction
-     * @property {string} commandName The name of the interaction's application command
+     * @property {string} commandName The name of the interaction's application command, including subcommands and subcommand groups
      * @property {User} user The user that invoked the interaction
      */
 
