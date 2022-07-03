631abee69310d59eb4625501cde1c44f6c44a7af
Jiralite <33201955+Jiralite@users.noreply.github.com>
2022-07-03 17:04:35 +0100

    types(GuildMemberManager): Non-void return of `edit()` (v13) (#8187)

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -3278,7 +3278,7 @@ export class GuildMemberManager extends CachedManager<Snowflake, GuildMember, Gu
   ): Promise<GuildMember | null>;
   public add(user: UserResolvable, options: AddGuildMemberOptions): Promise<GuildMember>;
   public ban(user: UserResolvable, options?: BanOptions): Promise<GuildMember | User | Snowflake>;
-  public edit(user: UserResolvable, data: GuildMemberEditData, reason?: string): Promise<void>;
+  public edit(user: UserResolvable, data: GuildMemberEditData, reason?: string): Promise<GuildMember>;
   public fetch(
     options: UserResolvable | FetchMemberOptions | (FetchMembersOptions & { user: UserResolvable }),
   ): Promise<GuildMember>;
