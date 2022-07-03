f10f4cdcd88ca6be7ec735ed3a415ba13da83db0
ckohen <chaikohen@gmail.com>
2022-07-03 06:33:18 -0700

    feat: codecov (#8219)

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,12 +2,15 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	test: {
-		exclude: ['**/node_modules', '**/dist', '.idea', '.git', '.cache', 'packages/discord.js', 'packages/voice'],
+		exclude: ['**/node_modules', '**/dist', '.idea', '.git', '.cache'],
 		passWithNoTests: true,
 		coverage: {
 			enabled: true,
+			all: true,
 			reporter: ['text', 'lcov', 'clover'],
-			exclude: ['**/dist', '**/__tests__'],
+			include: ['src'],
+			// All ts files that only contain types, due to ALL
+			exclude: ['**/*.{interface,type,d}.ts'],
 		},
 	},
 });
