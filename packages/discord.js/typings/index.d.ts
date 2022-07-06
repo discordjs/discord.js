2bfce892cb26d261c15f945e7b284a661ec66003
Tyler Resch <developer@midspike.com>
2022-07-06 19:56:51 -0400

    types: convert `Colors`, `Events` to enum

--- packages/discord.js/typings/index.d.ts
+++ packages/discord.js/typings/index.d.ts
@@ -4216,110 +4216,110 @@ export type AutocompleteFocusedOption = Pick<CommandInteractionOption, 'name'> &
   value: string;
 };
 
-export declare const Colors: {
-  Default: 0x000000;
-  White: 0xffffff;
-  Aqua: 0x1abc9c;
-  Green: 0x57f287;
-  Blue: 0x3498db;
-  Yellow: 0xfee75c;
-  Purple: 0x9b59b6;
-  LuminousVividPink: 0xe91e63;
-  Fuchsia: 0xeb459e;
-  Gold: 0xf1c40f;
-  Orange: 0xe67e22;
-  Red: 0xed4245;
-  Grey: 0x95a5a6;
-  Navy: 0x34495e;
-  DarkAqua: 0x11806a;
-  DarkGreen: 0x1f8b4c;
-  DarkBlue: 0x206694;
-  DarkPurple: 0x71368a;
-  DarkVividPink: 0xad1457;
-  DarkGold: 0xc27c0e;
-  DarkOrange: 0xa84300;
-  DarkRed: 0x992d22;
-  DarkGrey: 0x979c9f;
-  DarkerGrey: 0x7f8c8d;
-  LightGrey: 0xbcc0c0;
-  DarkNavy: 0x2c3e50;
-  Blurple: 0x5865f2;
-  Greyple: 0x99aab5;
-  DarkButNotBlack: 0x2c2f33;
-  NotQuiteBlack: 0x23272a;
-};
+export enum Colors {
+  Default = 0x000000,
+  White = 0xffffff,
+  Aqua = 0x1abc9c,
+  Green = 0x57f287,
+  Blue = 0x3498db,
+  Yellow = 0xfee75c,
+  Purple = 0x9b59b6,
+  LuminousVividPink = 0xe91e63,
+  Fuchsia = 0xeb459e,
+  Gold = 0xf1c40f,
+  Orange = 0xe67e22,
+  Red = 0xed4245,
+  Grey = 0x95a5a6,
+  Navy = 0x34495e,
+  DarkAqua = 0x11806a,
+  DarkGreen = 0x1f8b4c,
+  DarkBlue = 0x206694,
+  DarkPurple = 0x71368a,
+  DarkVividPink = 0xad1457,
+  DarkGold = 0xc27c0e,
+  DarkOrange = 0xa84300,
+  DarkRed = 0x992d22,
+  DarkGrey = 0x979c9f,
+  DarkerGrey = 0x7f8c8d,
+  LightGrey = 0xbcc0c0,
+  DarkNavy = 0x2c3e50,
+  Blurple = 0x5865f2,
+  Greyple = 0x99aab5,
+  DarkButNotBlack = 0x2c2f33,
+  NotQuiteBlack = 0x23272a,
+}
 
-export declare const Events: {
-  ApplicationCommandPermissionsUpdate: 'applicationCommandPermissionsUpdate';
-  ClientReady: 'ready';
-  GuildCreate: 'guildCreate';
-  GuildDelete: 'guildDelete';
-  GuildUpdate: 'guildUpdate';
-  GuildUnavailable: 'guildUnavailable';
-  GuildMemberAdd: 'guildMemberAdd';
-  GuildMemberRemove: 'guildMemberRemove';
-  GuildMemberUpdate: 'guildMemberUpdate';
-  GuildMemberAvailable: 'guildMemberAvailable';
-  GuildMembersChunk: 'guildMembersChunk';
-  GuildIntegrationsUpdate: 'guildIntegrationsUpdate';
-  GuildRoleCreate: 'roleCreate';
-  GuildRoleDelete: 'roleDelete';
-  InviteCreate: 'inviteCreate';
-  InviteDelete: 'inviteDelete';
-  GuildRoleUpdate: 'roleUpdate';
-  GuildEmojiCreate: 'emojiCreate';
-  GuildEmojiDelete: 'emojiDelete';
-  GuildEmojiUpdate: 'emojiUpdate';
-  GuildBanAdd: 'guildBanAdd';
-  GuildBanRemove: 'guildBanRemove';
-  ChannelCreate: 'channelCreate';
-  ChannelDelete: 'channelDelete';
-  ChannelUpdate: 'channelUpdate';
-  ChannelPinsUpdate: 'channelPinsUpdate';
-  MessageCreate: 'messageCreate';
-  MessageDelete: 'messageDelete';
-  MessageUpdate: 'messageUpdate';
-  MessageBulkDelete: 'messageDeleteBulk';
-  MessageReactionAdd: 'messageReactionAdd';
-  MessageReactionRemove: 'messageReactionRemove';
-  MessageReactionRemoveAll: 'messageReactionRemoveAll';
-  MessageReactionRemoveEmoji: 'messageReactionRemoveEmoji';
-  ThreadCreate: 'threadCreate';
-  ThreadDelete: 'threadDelete';
-  ThreadUpdate: 'threadUpdate';
-  ThreadListSync: 'threadListSync';
-  ThreadMemberUpdate: 'threadMemberUpdate';
-  ThreadMembersUpdate: 'threadMembersUpdate';
-  UserUpdate: 'userUpdate';
-  PresenceUpdate: 'presenceUpdate';
-  VoiceServerUpdate: 'voiceServerUpdate';
-  VoiceStateUpdate: 'voiceStateUpdate';
-  TypingStart: 'typingStart';
-  WebhooksUpdate: 'webhookUpdate';
-  InteractionCreate: 'interactionCreate';
-  Error: 'error';
-  Warn: 'warn';
-  Debug: 'debug';
-  CacheSweep: 'cacheSweep';
-  ShardDisconnect: 'shardDisconnect';
-  ShardError: 'shardError';
-  ShardReconnecting: 'shardReconnecting';
-  ShardReady: 'shardReady';
-  ShardResume: 'shardResume';
-  Invalidated: 'invalidated';
-  Raw: 'raw';
-  StageInstanceCreate: 'stageInstanceCreate';
-  StageInstanceUpdate: 'stageInstanceUpdate';
-  StageInstanceDelete: 'stageInstanceDelete';
-  GuildStickerCreate: 'stickerCreate';
-  GuildStickerDelete: 'stickerDelete';
-  GuildStickerUpdate: 'stickerUpdate';
-  GuildScheduledEventCreate: 'guildScheduledEventCreate';
-  GuildScheduledEventUpdate: 'guildScheduledEventUpdate';
-  GuildScheduledEventDelete: 'guildScheduledEventDelete';
-  GuildScheduledEventUserAdd: 'guildScheduledEventUserAdd';
-  GuildScheduledEventUserRemove: 'guildScheduledEventUserRemove';
-};
+export enum Events {
+  ApplicationCommandPermissionsUpdate = 'applicationCommandPermissionsUpdate',
+  ClientReady = 'ready',
+  GuildCreate = 'guildCreate',
+  GuildDelete = 'guildDelete',
+  GuildUpdate = 'guildUpdate',
+  GuildUnavailable = 'guildUnavailable',
+  GuildMemberAdd = 'guildMemberAdd',
+  GuildMemberRemove = 'guildMemberRemove',
+  GuildMemberUpdate = 'guildMemberUpdate',
+  GuildMemberAvailable = 'guildMemberAvailable',
+  GuildMembersChunk = 'guildMembersChunk',
+  GuildIntegrationsUpdate = 'guildIntegrationsUpdate',
+  GuildRoleCreate = 'roleCreate',
+  GuildRoleDelete = 'roleDelete',
+  InviteCreate = 'inviteCreate',
+  InviteDelete = 'inviteDelete',
+  GuildRoleUpdate = 'roleUpdate',
+  GuildEmojiCreate = 'emojiCreate',
+  GuildEmojiDelete = 'emojiDelete',
+  GuildEmojiUpdate = 'emojiUpdate',
+  GuildBanAdd = 'guildBanAdd',
+  GuildBanRemove = 'guildBanRemove',
+  ChannelCreate = 'channelCreate',
+  ChannelDelete = 'channelDelete',
+  ChannelUpdate = 'channelUpdate',
+  ChannelPinsUpdate = 'channelPinsUpdate',
+  MessageCreate = 'messageCreate',
+  MessageDelete = 'messageDelete',
+  MessageUpdate = 'messageUpdate',
+  MessageBulkDelete = 'messageDeleteBulk',
+  MessageReactionAdd = 'messageReactionAdd',
+  MessageReactionRemove = 'messageReactionRemove',
+  MessageReactionRemoveAll = 'messageReactionRemoveAll',
+  MessageReactionRemoveEmoji = 'messageReactionRemoveEmoji',
+  ThreadCreate = 'threadCreate',
+  ThreadDelete = 'threadDelete',
+  ThreadUpdate = 'threadUpdate',
+  ThreadListSync = 'threadListSync',
+  ThreadMemberUpdate = 'threadMemberUpdate',
+  ThreadMembersUpdate = 'threadMembersUpdate',
+  UserUpdate = 'userUpdate',
+  PresenceUpdate = 'presenceUpdate',
+  VoiceServerUpdate = 'voiceServerUpdate',
+  VoiceStateUpdate = 'voiceStateUpdate',
+  TypingStart = 'typingStart',
+  WebhooksUpdate = 'webhookUpdate',
+  InteractionCreate = 'interactionCreate',
+  Error = 'error',
+  Warn = 'warn',
+  Debug = 'debug',
+  CacheSweep = 'cacheSweep',
+  ShardDisconnect = 'shardDisconnect',
+  ShardError = 'shardError',
+  ShardReconnecting = 'shardReconnecting',
+  ShardReady = 'shardReady',
+  ShardResume = 'shardResume',
+  Invalidated = 'invalidated',
+  Raw = 'raw',
+  StageInstanceCreate = 'stageInstanceCreate',
+  StageInstanceUpdate = 'stageInstanceUpdate',
+  StageInstanceDelete = 'stageInstanceDelete',
+  GuildStickerCreate = 'stickerCreate',
+  GuildStickerDelete = 'stickerDelete',
+  GuildStickerUpdate = 'stickerUpdate',
+  GuildScheduledEventCreate = 'guildScheduledEventCreate',
+  GuildScheduledEventUpdate = 'guildScheduledEventUpdate',
+  GuildScheduledEventDelete = 'guildScheduledEventDelete',
+  GuildScheduledEventUserAdd = 'guildScheduledEventUserAdd',
+  GuildScheduledEventUserRemove = 'guildScheduledEventUserRemove',
+}
 
 export enum ShardEvents {
   Close = 'close',
