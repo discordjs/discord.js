849afa7a910f732918b269dfbbefb72b3212a80b
Tyler Resch <developer@midspike.com>
2022-07-07 08:31:23 -0400

    revert(Colors): fixes incorrect indexation

--- packages/discord.js/typings/index.d.ts
+++ packages/discord.js/typings/index.d.ts
@@ -4216,38 +4216,38 @@ export type AutocompleteFocusedOption = Pick<CommandInteractionOption, 'name'> &
   value: string;
 };
 
-export enum Colors {
-  Default = 0x000000,
-  White = 0xffffff,
-  Aqua = 0x1abc9c,
-  Green = 0x57f287,
-  Blue = 0x3498db,
-  Yellow = 0xfee75c,
-  Purple = 0x9b59b6,
-  LuminousVividPink = 0xe91e63,
-  Fuchsia = 0xeb459e,
-  Gold = 0xf1c40f,
-  Orange = 0xe67e22,
-  Red = 0xed4245,
-  Grey = 0x95a5a6,
-  Navy = 0x34495e,
-  DarkAqua = 0x11806a,
-  DarkGreen = 0x1f8b4c,
-  DarkBlue = 0x206694,
-  DarkPurple = 0x71368a,
-  DarkVividPink = 0xad1457,
-  DarkGold = 0xc27c0e,
-  DarkOrange = 0xa84300,
-  DarkRed = 0x992d22,
-  DarkGrey = 0x979c9f,
-  DarkerGrey = 0x7f8c8d,
-  LightGrey = 0xbcc0c0,
-  DarkNavy = 0x2c3e50,
-  Blurple = 0x5865f2,
-  Greyple = 0x99aab5,
-  DarkButNotBlack = 0x2c2f33,
-  NotQuiteBlack = 0x23272a,
-}
+export declare const Colors: {
+  Default: 0x000000;
+  White: 0xffffff;
+  Aqua: 0x1abc9c;
+  Green: 0x57f287;
+  Blue: 0x3498db;
+  Yellow: 0xfee75c;
+  Purple: 0x9b59b6;
+  LuminousVividPink: 0xe91e63;
+  Fuchsia: 0xeb459e;
+  Gold: 0xf1c40f;
+  Orange: 0xe67e22;
+  Red: 0xed4245;
+  Grey: 0x95a5a6;
+  Navy: 0x34495e;
+  DarkAqua: 0x11806a;
+  DarkGreen: 0x1f8b4c;
+  DarkBlue: 0x206694;
+  DarkPurple: 0x71368a;
+  DarkVividPink: 0xad1457;
+  DarkGold: 0xc27c0e;
+  DarkOrange: 0xa84300;
+  DarkRed: 0x992d22;
+  DarkGrey: 0x979c9f;
+  DarkerGrey: 0x7f8c8d;
+  LightGrey: 0xbcc0c0;
+  DarkNavy: 0x2c3e50;
+  Blurple: 0x5865f2;
+  Greyple: 0x99aab5;
+  DarkButNotBlack: 0x2c2f33;
+  NotQuiteBlack: 0x23272a;
+};
 
 export enum Events {
   ApplicationCommandPermissionsUpdate = 'applicationCommandPermissionsUpdate',
