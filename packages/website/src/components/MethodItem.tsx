29496464851ee0265b50f58b74f0ea11ff202652
iCrawl <buechler.noel@outlook.com>
2022-07-08 21:49:08 +0200

    feat: anchor links

--- packages/website/src/components/MethodItem.tsx
+++ packages/website/src/components/MethodItem.tsx
@@ -1,3 +1,4 @@
+import { FiLink } from 'react-icons/fi';
 import { ParameterTable } from './ParameterTable';
 import type { DocMethod } from '~/DocModel/DocMethod';
 import type { DocMethodSignature } from '~/DocModel/DocMethodSignature';
@@ -19,16 +20,32 @@ function getShorthandName(data: MethodResolvable) {
 	}, '')})`;
 }
 
+function onAnchorClick() {
+	console.log('anchor clicked');
+	// Todo implement jump-to links
+}
+
 export function MethodItem({ data }: MethodItemProps) {
 	return (
-		<div className="flex flex-col ml-3">
-			<div className="w-full flex flex-row">
-				<h4 className="font-mono my-0 break-all">{`${getShorthandName(data)}`}</h4>
-				<h4 className="mx-3 my-0">:</h4>
-				<h4 className="font-mono color-blue-800 my-0 break-all ">{constructHyperlinkedText(data.returnTypeTokens)}</h4>
+		<div className="flex flex-col">
+			<div className="flex content-center">
+				<button className="bg-transparent border-none cursor-pointer" onClick={onAnchorClick}>
+					<FiLink size={16} />
+				</button>
+				<div className="flex flex-col ml-3">
+					<div className="w-full flex flex-row">
+						<h4 className="font-mono my-0 break-all">{`${getShorthandName(data)}`}</h4>
+						<h4 className="mx-3 my-0">:</h4>
+						<h4 className="font-mono color-blue-800 my-0 break-all ">
+							{constructHyperlinkedText(data.returnTypeTokens)}
+						</h4>
+					</div>
+				</div>
 			</div>
-			{data.summary && <p className="color-slate-500 mt-2">{data.summary}</p>}
-			{data.parameters.length ? <ParameterTable className="m-b-10 m-x-10" data={data.parameters} /> : null}
+			<div className="mx-10">
+				{data.summary && <p className="color-slate-500 mt-2">{data.summary}</p>}
+				{data.parameters.length ? <ParameterTable className="mb-10 mx-10" data={data.parameters} /> : null}
+			</div>
 		</div>
 	);
 }
