21c3a51dbd35d55e3f8dc67ea9cd019747fbd648
Jiralite <33201955+Jiralite@users.noreply.github.com>
2022-07-16 14:39:15 +0100

    fix(GuildChannelManager): access `resolveId` correctly

--- src/managers/GuildChannelManager.js
+++ src/managers/GuildChannelManager.js
@@ -403,7 +403,7 @@ class GuildChannelManager extends CachedManager {
       id: this.client.channels.resolveId(r.channel),
       position: r.position,
       lock_permissions: r.lockPermissions,
-      parent_id: typeof r.parent !== 'undefined' ? this.channels.resolveId(r.parent) : undefined,
+      parent_id: typeof r.parent !== 'undefined' ? this.resolveId(r.parent) : undefined,
     }));
 
     await this.client.api.guilds(this.guild.id).channels.patch({ data: channelPositions });
