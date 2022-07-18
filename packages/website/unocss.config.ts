b5d4b2d78bfbcb2b5d5f48fc28af7e0e4bd1f1a5
Suneet Tipirneni <77477100+suneettipirneni@users.noreply.github.com>
2022-07-18 13:11:18 -0400

    feat(website): add basic landing page (#8313)
    Co-authored-by: Noel <buechler.noel@outlook.com>
    Co-authored-by: Jiralite <33201955+Jiralite@users.noreply.github.com>

--- packages/website/unocss.config.ts
+++ packages/website/unocss.config.ts
@@ -2,8 +2,15 @@ import presetWebFonts from '@unocss/preset-web-fonts';
 import { defineConfig, presetUno } from 'unocss';
 
 export default defineConfig({
+	theme: {
+		colors: {
+			blurple: '#5865F2',
+		},
+	},
 	presets: [
-		presetUno({ dark: 'media' }),
+		presetUno({
+			dark: 'media',
+		}),
 		presetWebFonts({
 			provider: 'google',
 			fonts: {
