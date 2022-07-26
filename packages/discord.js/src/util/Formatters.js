46ca77a3be64053855215d6ed15578be8898bc2a
Jiralite <33201955+Jiralite@users.noreply.github.com>
2022-07-26 23:05:28 +0100

    docs: deprecate `Formatter` class

--- packages/discord.js/src/util/Formatters.js
+++ packages/discord.js/src/util/Formatters.js
@@ -21,15 +21,150 @@ const {
 } = require('@discordjs/builders');
 
 /**
+ * Wraps the content inside a code block with an optional language.
+ * @method codeBlock
+ * @param {string} contentOrLanguage The language to use or content if a second parameter isn't provided
+ * @param {string} [content] The content to wrap
+ * @returns {string}
+ */
+
+/**
+ * Wraps the content inside \`backticks\`, which formats it as inline code.
+ * @method inlineCode
+ * @param {string} content The content to wrap
+ * @returns {string}
+ */
+
+/**
+ * Formats the content into italic text.
+ * @method italic
+ * @param {string} content The content to wrap
+ * @returns {string}
+ */
+
+/**
+ * Formats the content into bold text.
+ * @method bold
+ * @param {string} content The content to wrap
+ * @returns {string}
+ */
+
+/**
+ * Formats the content into underscored text.
+ * @method underscore
+ * @param {string} content The content to wrap
+ * @returns {string}
+ */
+
+/**
+ * Formats the content into strike-through text.
+ * @method strikethrough
+ * @param {string} content The content to wrap
+ * @returns {string}
+ */
+
+/**
+ * Formats the content into a quote.
+ * <info>This needs to be at the start of the line for Discord to format it.</info>
+ * @method quote
+ * @param {string} content The content to wrap
+ * @returns {string}
+ */
+
+/**
+ * Formats the content into a block quote.
+ * <info>This needs to be at the start of the line for Discord to format it.</info>
+ * @method blockQuote
+ * @param {string} content The content to wrap
+ * @returns {string}
+ */
+
+/**
+ * Wraps the URL into `<>`, which stops it from embedding.
+ * @method hideLinkEmbed
+ * @param {string} content The content to wrap
+ * @returns {string}
+ */
+
+/**
+ * Formats the content and the URL into a masked URL with an optional title.
+ * @method hyperlink
+ * @param {string} content The content to display
+ * @param {string} url The URL the content links to
+ * @param {string} [title] The title shown when hovering on the masked link
+ * @returns {string}
+ */
+
+/**
+ * Formats the content into spoiler text.
+ * @method spoiler
+ * @param {string} content The content to spoiler
+ * @returns {string}
+ */
+
+/**
+ * Formats a user id into a user mention.
+ * @method userMention
+ * @param {Snowflake} userId The user id to format
+ * @returns {string}
+ */
+
+/**
+ * Formats a channel id into a channel mention.
+ * @method channelMention
+ * @param {Snowflake} channelId The channel id to format
+ * @returns {string}
+ */
+
+/**
+ * Formats a role id into a role mention.
+ * @method roleMention
+ * @param {Snowflake} roleId The role id to format
+ * @returns {string}
+ */
+
+/**
+ * Formats an emoji id into a fully qualified emoji identifier.
+ * @method formatEmoji
+ * @param {Snowflake} emojiId The emoji id to format
+ * @param {boolean} [animated=false] Whether the emoji is animated
+ * @returns {string}
+ */
+
+/**
+ * A message formatting timestamp style, as defined in
+ * [here](https://discord.com/developers/docs/reference#message-formatting-timestamp-styles).
+ * * `t` Short time format, consisting of hours and minutes, e.g. 16:20.
+ * * `T` Long time format, consisting of hours, minutes, and seconds, e.g. 16:20:30.
+ * * `d` Short date format, consisting of day, month, and year, e.g. 20/04/2021.
+ * * `D` Long date format, consisting of day, month, and year, e.g. 20 April 2021.
+ * * `f` Short date-time format, consisting of short date and short time formats, e.g. 20 April 2021 16:20.
+ * * `F` Long date-time format, consisting of long date and short time formats, e.g. Tuesday, 20 April 2021 16:20.
+ * * `R` Relative time format, consisting of a relative duration format, e.g. 2 months ago.
+ * @typedef {string} TimestampStylesString
+ */
+
+/**
+ * Formats a date into a short date-time string.
+ * @method time
+ * @param {number|Date} [date] The date to format
+ * @param {TimestampStylesString} [style] The style to use
+ * @returns {string}
+ */
+
+/**
  * Contains various Discord-specific functions for formatting messages.
+ * @deprecated This class is redundant as all methods of the class are top-level exports.
  */
 class Formatters extends null {
   /**
-   * Formats the content into a block quote. This needs to be at the start of the line for Discord to format it.
+   * Formats the content into a block quote.
+   * <info>This needs to be at the start of the line for Discord to format it.</info>
    * @method blockQuote
    * @memberof Formatters
-   * @param {string} content The content to wrap.
+   * @param {string} content The content to wrap
    * @returns {string}
+   * @deprecated Use this method as a top-level function instead.
    */
   static blockQuote = blockQuote;
 
@@ -37,8 +172,9 @@ class Formatters extends null {
    * Formats the content into bold text.
    * @method bold
    * @memberof Formatters
-   * @param {string} content The content to wrap.
+   * @param {string} content The content to wrap
    * @returns {string}
+   * @deprecated Use this method as a top-level function instead.
    */
   static bold = bold;
 
@@ -46,8 +182,9 @@ class Formatters extends null {
    * Formats a channel id into a channel mention.
    * @method channelMention
    * @memberof Formatters
-   * @param {string} channelId The channel id to format.
+   * @param {Snowflake} channelId The channel id to format
    * @returns {string}
+   * @deprecated Use this method as a top-level function instead.
    */
   static channelMention = channelMention;
 
@@ -55,20 +192,21 @@ class Formatters extends null {
    * Wraps the content inside a code block with an optional language.
    * @method codeBlock
    * @memberof Formatters
-   * @param {string} contentOrLanguage The language to use, content if a second parameter isn't provided.
-   * @param {string} [content] The content to wrap.
+   * @param {string} contentOrLanguage The language to use or content if a second parameter isn't provided
+   * @param {string} [content] The content to wrap
    * @returns {string}
+   * @deprecated Use this method as a top-level function instead.
    */
   static codeBlock = codeBlock;
 
   /**
-   * Formats an emoji id into a fully qualified emoji identifier
+   * Formats an emoji id into a fully qualified emoji identifier.
    * @method formatEmoji
    * @memberof Formatters
-   * @memberof Formatters
-   * @param {string} emojiId The emoji id to format.
-   * @param {boolean} [animated] Whether the emoji is animated or not. Defaults to `false`
+   * @param {string} emojiId The emoji id to format
+   * @param {boolean} [animated=false] Whether the emoji is animated
    * @returns {string}
+   * @deprecated Use this method as a top-level function instead.
    */
   static formatEmoji = formatEmoji;
 
@@ -76,8 +214,9 @@ class Formatters extends null {
    * Wraps the URL into `<>`, which stops it from embedding.
    * @method hideLinkEmbed
    * @memberof Formatters
-   * @param {string} content The content to wrap.
+   * @param {string} content The content to wrap
    * @returns {string}
+   * @deprecated Use this method as a top-level function instead.
    */
   static hideLinkEmbed = hideLinkEmbed;
 
@@ -85,10 +224,11 @@ class Formatters extends null {
    * Formats the content and the URL into a masked URL with an optional title.
    * @method hyperlink
    * @memberof Formatters
-   * @param {string} content The content to display.
-   * @param {string} url The URL the content links to.
-   * @param {string} [title] The title shown when hovering on the masked link.
+   * @param {string} content The content to display
+   * @param {string} url The URL the content links to
+   * @param {string} [title] The title shown when hovering on the masked link
    * @returns {string}
+   * @deprecated Use this method as a top-level function instead.
    */
   static hyperlink = hyperlink;
 
@@ -96,8 +236,9 @@ class Formatters extends null {
    * Wraps the content inside \`backticks\`, which formats it as inline code.
    * @method inlineCode
    * @memberof Formatters
-   * @param {string} content The content to wrap.
+   * @param {string} content The content to wrap
    * @returns {string}
+   * @deprecated Use this method as a top-level function instead.
    */
   static inlineCode = inlineCode;
 
@@ -105,8 +246,9 @@ class Formatters extends null {
    * Formats the content into italic text.
    * @method italic
    * @memberof Formatters
-   * @param {string} content The content to wrap.
+   * @param {string} content The content to wrap
    * @returns {string}
+   * @deprecated Use this method as a top-level function instead.
    */
   static italic = italic;
 
@@ -114,8 +256,9 @@ class Formatters extends null {
    * Formats the content into a quote. This needs to be at the start of the line for Discord to format it.
    * @method quote
    * @memberof Formatters
-   * @param {string} content The content to wrap.
+   * @param {string} content The content to wrap
    * @returns {string}
+   * @deprecated Use this method as a top-level function instead.
    */
   static quote = quote;
 
@@ -123,8 +266,9 @@ class Formatters extends null {
    * Formats a role id into a role mention.
    * @method roleMention
    * @memberof Formatters
-   * @param {string} roleId The role id to format.
+   * @param {Snowflake} roleId The role id to format
    * @returns {string}
+   * @deprecated Use this method as a top-level function instead.
    */
   static roleMention = roleMention;
 
@@ -132,8 +276,9 @@ class Formatters extends null {
    * Formats the content into spoiler text.
    * @method spoiler
    * @memberof Formatters
-   * @param {string} content The content to spoiler.
+   * @param {string} content The content to spoiler
    * @returns {string}
+   * @deprecated Use this method as a top-level function instead.
    */
   static spoiler = spoiler;
 
@@ -141,8 +286,9 @@ class Formatters extends null {
    * Formats the content into strike-through text.
    * @method strikethrough
    * @memberof Formatters
-   * @param {string} content The content to wrap.
+   * @param {string} content The content to wrap
    * @returns {string}
+   * @deprecated Use this method as a top-level function instead.
    */
   static strikethrough = strikethrough;
 
@@ -150,30 +296,19 @@ class Formatters extends null {
    * Formats a date into a short date-time string.
    * @method time
    * @memberof Formatters
-   * @param {number|Date} [date] The date to format.
-   * @param {TimestampStylesString} [style] The style to use.
+   * @param {number|Date} [date] The date to format
+   * @param {TimestampStylesString} [style] The style to use
    * @returns {string}
+   * @deprecated Use this method as a top-level function instead.
    */
   static time = time;
-
-  /**
-   * A message formatting timestamp style, as defined in
-   * [here](https://discord.com/developers/docs/reference#message-formatting-timestamp-styles).
-   * * `t` Short time format, consisting of hours and minutes, e.g. 16:20.
-   * * `T` Long time format, consisting of hours, minutes, and seconds, e.g. 16:20:30.
-   * * `d` Short date format, consisting of day, month, and year, e.g. 20/04/2021.
-   * * `D` Long date format, consisting of day, month, and year, e.g. 20 April 2021.
-   * * `f` Short date-time format, consisting of short date and short time formats, e.g. 20 April 2021 16:20.
-   * * `F` Long date-time format, consisting of long date and short time formats, e.g. Tuesday, 20 April 2021 16:20.
-   * * `R` Relative time format, consisting of a relative duration format, e.g. 2 months ago.
-   * @typedef {string} TimestampStylesString
-   */
 
   /**
    * The message formatting timestamp
    * [styles](https://discord.com/developers/docs/reference#message-formatting-timestamp-styles) supported by Discord.
    * @type {Object<string, TimestampStylesString>}
    * @memberof Formatters
+   * @deprecated Use this property as a top-level property instead.
    */
   static TimestampStyles = TimestampStyles;
 
@@ -181,8 +316,9 @@ class Formatters extends null {
    * Formats the content into underscored text.
    * @method underscore
    * @memberof Formatters
-   * @param {string} content The content to wrap.
+   * @param {string} content The content to wrap
    * @returns {string}
+   * @deprecated Use this method as a top-level function instead.
    */
   static underscore = underscore;
 
@@ -190,8 +326,9 @@ class Formatters extends null {
    * Formats a user id into a user mention.
    * @method userMention
    * @memberof Formatters
-   * @param {string} userId The user id to format.
+   * @param {Snowflake} userId The user id to format
    * @returns {string}
+   * @deprecated Use this method as a top-level function instead.
    */
   static userMention = userMention;
 }
