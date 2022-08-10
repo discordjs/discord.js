68ce0880580ba003be33e10eaeae88987faab132
RedGuy12 <paul@reid-family.org>
2022-08-10 13:35:51 -0500

    Update index.d.ts

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -3537,7 +3537,7 @@ export interface TextBasedChannelFields extends PartialTextBasedChannelFields {
   bulkDelete(
     messages: Collection<Snowflake, Message> | readonly MessageResolvable[] | number,
     filterOld?: boolean,
-  ): Promise<Collection<Snowflake, Message|undefined>>;
+  ): Promise<Collection<Snowflake, Message | PartialMessage | undefined>>;
   createMessageComponentCollector<T extends MessageComponentTypeResolvable = 'ACTION_ROW'>(
     options?: MessageChannelCollectorOptionsParams<T, true>,
   ): InteractionCollector<MappedInteractionTypes[T]>;
