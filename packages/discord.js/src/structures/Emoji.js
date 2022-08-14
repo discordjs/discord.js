2388a857703290a91d0de02bdf785161d14a3333
MohamedAshraf <55748253+dager-mohamed@users.noreply.github.com>
2022-08-15 00:58:44 +0200

    feat(AddEmoji): support more extentions in `url()`

--- packages/discord.js/src/structures/Emoji.js
+++ packages/discord.js/src/structures/Emoji.js
@@ -47,13 +47,32 @@ class Emoji extends Base {
     return encodeURIComponent(this.name);
   }
 
-  /**
+/**
    * The URL to the emoji file if it's a custom emoji
    * @type {?string}
-   * @readonly
+   * @param {string} extension the extension of emoji
+   * @returns {string}
    */
-  get url() {
-    return this.id && this.client.rest.cdn.emoji(this.id, this.animated ? 'gif' : 'png');
+  url(extension) {
+    let final_result;
+    if (extension === 'png' && !this.animated) {
+      final_result = this.id && this.client.rest.cdn.emoji(this.id, 'png');
+    } else if (this.animated && extension === 'gif') {
+      final_result = this.id && this.client.rest.cdn.emoji(this.id, 'gif');
+    } else if (extension === 'webp' && !this.animated) {
+      final_result = this.id && this.client.rest.cdn.emoji(this.id, 'webp');
+    } else if (extension === 'jpeg' && !this.animated) {
+      final_result = this.id && this.client.rest.cdn.emoji(this.id, 'jpeg');
+    } else if (extension === 'jpg' && !this.animated) {
+      final_result = this.id && this.client.rest.cdn.emoji(this.id, 'jpg');
+    } else if (!extension && !this.animated) {
+      final_result = this.id && this.client.rest.cdn.emoji(this.id, 'png');
+    } else if (!extension && this.animated) {
+      final_result = this.id && this.client.rest.cdn.emoji(this.id, 'gif');
+    } else {
+      final_result = undefined;
+    }
+    return final_result;
   }
 
   /**
