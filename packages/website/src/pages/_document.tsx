5dc7946df2ed78fc9473d35d4eeb4ae6ee51c3dc
iCrawl <buechler.noel@outlook.com>
2022-08-15 18:23:10 +0200

    fix: properly render critical styles on the server

--- packages/website/src/pages/_document.tsx
+++ packages/website/src/pages/_document.tsx
@@ -1,9 +1,17 @@
-import { createGetInitialProps } from '@mantine/next';
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { createStylesServer, ServerStyles } from '@mantine/next';
+import Document, { Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps } from 'next/document';
 
-const getInitialProps = createGetInitialProps();
+const stylesServer = createStylesServer();
+
 export default class _Document extends Document {
-	public static override getInitialProps = getInitialProps;
+	public static override async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+		const initialProps = await Document.getInitialProps(ctx);
+
+		return {
+			...initialProps,
+			styles: [initialProps.styles, <ServerStyles html={initialProps.html} server={stylesServer} key="styles" />],
+		};
+	}
 
 	public override render() {
 		return (
