88e92cde37ac64a0ff01b0b703cf69caff52e1bc
MohamedAshraf <55748253+dager-mohamed@users.noreply.github.com>
2022-08-15 04:25:18 +0200

    Create emoji.js

---
+++ packages/discord.js/test/emoji.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const { GatewayIntentBits } = require('discord-api-types/v10');
+const { token } = require('./auth.js');
+const { Client } = require('../src');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.on('ready', () => {
+  console.log('ready');
+});
+
+client.on('messageCreate', message => {
+  if (message.content === 'emoji-test') {
+    message.guild.emojis.fetch('1008538563236872193').then(m => console.log(m.url()));
+  }
+});
+
+client.login(token);
