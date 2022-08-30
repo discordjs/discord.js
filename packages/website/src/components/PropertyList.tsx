30a270b0edaa5d3ccd64a85abe6b559dd6b16ae8
suneettipirneni <suneettipirneni@icloud.com>
2022-08-30 12:48:51 -0400

    chore(website): put seperators between properties

--- packages/website/src/components/PropertyList.tsx
+++ packages/website/src/components/PropertyList.tsx
@@ -1,22 +1,25 @@
 import type { ApiPropertyItemJSON } from '@discordjs/api-extractor-utils';
-import { Stack } from '@mantine/core';
+import { Divider, Stack } from '@mantine/core';
+import { Fragment } from 'react';
 import { CodeListing } from './CodeListing';
 
 export function PropertyList({ data }: { data: ApiPropertyItemJSON[] }) {
 	return (
 		<Stack>
 			{data.map((prop) => (
-				<CodeListing
-					key={prop.name}
-					name={prop.name}
-					typeTokens={prop.propertyTypeTokens}
-					readonly={prop.readonly}
-					optional={prop.optional}
-					summary={prop.summary}
-					comment={prop.comment}
-					deprecation={prop.deprecated}
-					inheritanceData={prop.inheritanceData}
-				/>
+				<Fragment key={prop.name}>
+					<CodeListing
+						name={prop.name}
+						typeTokens={prop.propertyTypeTokens}
+						readonly={prop.readonly}
+						optional={prop.optional}
+						summary={prop.summary}
+						comment={prop.comment}
+						deprecation={prop.deprecated}
+						inheritanceData={prop.inheritanceData}
+					/>
+					<Divider size="md" />
+				</Fragment>
 			))}
 		</Stack>
 	);
