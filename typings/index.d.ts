56177998c5ad67e277244880040e1e31735997dd
JsCoder2022 <108687185+JsCoder2022@users.noreply.github.com>
2022-09-02 20:22:59 +0500

    add MESSAGE_CONTENT intent (v13) (#8580)

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -5218,6 +5218,7 @@ export type IntentsString =
   | 'DIRECT_MESSAGES'
   | 'DIRECT_MESSAGE_REACTIONS'
   | 'DIRECT_MESSAGE_TYPING'
+  | 'MESSAGE_CONTENT'
   | 'GUILD_SCHEDULED_EVENTS';
 
 export interface InviteGenerationOptions {
