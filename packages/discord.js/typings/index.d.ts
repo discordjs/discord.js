9cf89a4b1fdad9da754096e6c4633871c67fde92
Idris <idris.gamer.2006@gmail.com>
2022-09-11 19:40:13 +0200

    fix(types): Client#options

--- packages/discord.js/typings/index.d.ts
+++ packages/discord.js/typings/index.d.ts
@@ -754,7 +754,7 @@ export class Client<Ready extends boolean = boolean> extends BaseClient {
   public channels: ChannelManager;
   public get emojis(): BaseGuildEmojiManager;
   public guilds: GuildManager;
-  public options: ClientOptions;
+  public options: Omit<ClientOptions, "intents"> & { intents: IntentsBitField };
   public get readyAt(): If<Ready, Date>;
   public readyTimestamp: If<Ready, number>;
   public sweepers: Sweepers;
