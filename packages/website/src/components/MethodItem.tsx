ac854649a59d9540f7b0c5dcd75869340b3edd43
iCrawl <buechler.noel@outlook.com>
2022-09-17 13:54:25 +0200

    fix: design adjustments

--- packages/website/src/components/MethodItem.tsx
+++ packages/website/src/components/MethodItem.tsx
@@ -2,7 +2,7 @@ import type { ApiMethodJSON, ApiMethodSignatureJSON } from '@discordjs/api-extra
 import { Menu, MenuButton, MenuItem, useMenuState } from 'ariakit';
 import { useCallback, useMemo, useState } from 'react';
 import { FiLink } from 'react-icons/fi';
-import { VscChevronDown } from 'react-icons/vsc';
+import { VscChevronDown, VscVersions } from 'react-icons/vsc';
 import { HyperlinkedText } from './HyperlinkedText';
 import { InheritanceText } from './InheritanceText';
 import { ParameterTable } from './ParameterTable';
@@ -10,10 +10,9 @@ import { TSDoc } from './tsdoc/TSDoc';
 
 export function MethodItem({ data }: { data: ApiMethodJSON | ApiMethodSignatureJSON }) {
 	const method = data as ApiMethodJSON;
-
 	const [overloadIndex, setOverloadIndex] = useState(1);
 	const overloadedData = method.mergedSiblings[overloadIndex - 1]!;
-	const menuState = useMenuState({ gutter: 8 });
+	const menu = useMenuState({ gutter: 8, sameWidth: true, fitViewport: true });
 
 	const key = useMemo(
 		() => `${data.name}${data.overloadIndex && data.overloadIndex > 1 ? `:${data.overloadIndex}` : ''}`,
@@ -70,21 +69,31 @@ export function MethodItem({ data }: { data: ApiMethodJSON | ApiMethodSignatureJ
 				</div>
 			</div>
 			{data.mergedSiblings.length > 1 ? (
-				<div className="flex flex-shrink">
+				<div className="flex flex-row place-items-center gap-2">
 					<MenuButton
-						state={menuState}
-						className="dark:bg-dark-600 border-light-800 dark:border-dark-100 z-20 flex flex-col rounded border bg-white p-1"
+						state={menu}
+						className="bg-light-600 hover:bg-light-700 active:bg-light-800 dark:bg-dark-600 dark:hover:bg-dark-500 dark:active:bg-dark-400 rounded p-3"
 					>
-						<div className="space-x-sm flex items-center">
-							{`Overload ${overloadIndex} of ${data.mergedSiblings.length}`}
-							<VscChevronDown />
+						<div className="flex flex-row place-content-between place-items-center gap-2">
+							<VscVersions size={20} />
+							<div>
+								<span className="font-semibold">{`Overload ${overloadIndex}`}</span>
+								{` of ${data.mergedSiblings.length}`}
+							</div>
+							<VscChevronDown
+								className={`transform transition duration-150 ease-in-out ${menu.open ? 'rotate-180' : 'rotate-0'}`}
+								size={20}
+							/>
 						</div>
 					</MenuButton>
-					<Menu state={menuState} className="dark:border-dark-100 rounded border">
+					<Menu
+						state={menu}
+						className="dark:bg-dark-600 border-light-800 dark:border-dark-100 z-20 flex flex-col rounded border bg-white p-1"
+					>
 						{data.mergedSiblings.map((_, idx) => (
 							<MenuItem
-								className="hover:bg-light-700 active:bg-light-800 dark:bg-dark-600 dark:hover:bg-dark-500 dark:active:bg-dark-400 cursor-pointer rounded bg-white p-3 text-sm"
 								key={idx}
+								className="hover:bg-light-700 active:bg-light-800 dark:bg-dark-600 dark:hover:bg-dark-500 dark:active:bg-dark-400 cursor-pointer rounded bg-white p-3 text-sm"
 								onClick={() => setOverloadIndex(idx + 1)}
 							>{`Overload ${idx + 1}`}</MenuItem>
 						))}
