e1a8e750e163155c0be8d69e1ced795f938c71ab
MrMythical <91077061+MrMythicalYT@users.noreply.github.com>
2022-09-25 21:28:14 -0400

    fix: lint

--- packages/discord.js/typings/index.d.ts
+++ packages/discord.js/typings/index.d.ts
@@ -193,7 +193,10 @@ import {
 declare module 'node:events' {
   class EventEmitter {
     // Add type overloads for client events.
-    public static once<E extends EventEmitter, K extends keyof ClientEvents>(eventEmitter: E, eventName: E extends Client ? K : string): Promise<E extends Client ? ClientEvents[K] : any[]>;
+    public static once<E extends EventEmitter, K extends keyof ClientEvents>(
+      eventEmitter: E,
+      eventName: E extends Client ? K : string,
+    ): Promise<E extends Client ? ClientEvents[K] : any[]>;
     public static on<E extends EventEmitter, K extends keyof ClientEvents>(
       eventEmitter: E,
       eventName: E extends Client ? K : string,
