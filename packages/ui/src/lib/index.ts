57308660733043b079a432cbc6826b59b9e8071f
iCrawl <buechler.noel@outlook.com>
2022-10-07 17:49:03 +0200

    feat: alert & discord components

--- packages/ui/src/lib/index.ts
+++ packages/ui/src/lib/index.ts
@@ -1,1 +1,14 @@
+export * from './components/Alert.jsx';
 export * from './components/Section.jsx';
+
+export * from './components/discord/Message.jsx';
+export * from './components/discord/MessageAuthor.jsx';
+export * from './components/discord/MessageAuthorReply.jsx';
+export * from './components/discord/MessageBaseReply.jsx';
+export * from './components/discord/MessageEmbed.jsx';
+export * from './components/discord/MessageEmbedAuthor.jsx';
+export * from './components/discord/MessageEmbedFooter.jsx';
+export * from './components/discord/MessageEmbedTitle.jsx';
+export * from './components/discord/MessageInteraction.jsx';
+export * from './components/discord/MessageReply.jsx';
+export * from './components/discord/Messages.jsx';
--- packages/website/src/components/tsdoc/BlockComment.tsx
+++ packages/website/src/components/tsdoc/BlockComment.tsx
@@ -1,6 +1,6 @@
+import { Alert } from '@discordjs/ui';
 import { StandardTags } from '@microsoft/tsdoc';
 import type { PropsWithChildren } from 'react';
-import { VscWarning } from 'react-icons/vsc';
 
 export function Block({ children, title }: PropsWithChildren<{ title: string }>) {
 	return (
@@ -16,26 +16,6 @@ export function ExampleBlock({
 	exampleIndex,
 }: PropsWithChildren<{ exampleIndex?: number | undefined }>): JSX.Element {
 	return <Block title={`Example ${exampleIndex ? exampleIndex : ''}`}>{children}</Block>;
-}
-
-export function DeprecatedBlock({ children }: PropsWithChildren): JSX.Element {
-	return (
-		<div className="my-4">
-			<div className="relative flex">
-				<div className="p-4">{children}</div>
-				<div className="absolute flex h-full w-full">
-					<div className="rounded-tl-1.5 rounded-bl-1.5 w-4 shrink-0 border-t-2 border-b-2 border-l-2 border-red-500" />
-					<div className="relative border-b-2 border-red-500">
-						<div className="-translate-y-50% flex place-items-center gap-2 px-2 text-red-500">
-							<VscWarning size={20} />
-							<span className="font-semibold text-red-500">Deprecated</span>
-						</div>
-					</div>
-					<div className="rounded-tr-1.5 rounded-br-1.5 flex-1 border-t-2 border-b-2 border-r-2 border-red-500" />
-				</div>
-			</div>
-		</div>
-	);
 }
 
 export function DefaultValueBlock({ children }: PropsWithChildren): JSX.Element {
@@ -58,7 +38,11 @@ export function BlockComment({
 		case StandardTags.example.tagNameWithUpperCase:
 			return <ExampleBlock exampleIndex={index}>{children}</ExampleBlock>;
 		case StandardTags.deprecated.tagNameWithUpperCase:
-			return <DeprecatedBlock>{children}</DeprecatedBlock>;
+			return (
+				<Alert title="Deprecated" type="danger">
+					{children}
+				</Alert>
+			);
 		case StandardTags.remarks.tagNameWithUpperCase:
 			return <RemarksBlock>{children}</RemarksBlock>;
 		case StandardTags.defaultValue.tagNameWithUpperCase:
