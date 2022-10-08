ed936ecc1281f34daa58f1a666938ed07a7c67bd
iCrawl <buechler.noel@outlook.com>
2022-10-08 03:18:59 +0200

    feat: style outline

--- packages/guide/src/components/Outline.tsx
+++ packages/guide/src/components/Outline.tsx
@@ -1,15 +1,28 @@
 import type { MarkdownHeading } from 'astro';
+import { useMemo } from 'react';
 
-export function Outline({ headings, className }: { className?: string; headings: MarkdownHeading[] }) {
+export function Outline({ headings }: { headings: MarkdownHeading[] }) {
+	const headingItems = useMemo(
+		() =>
+			headings.map((heading) => (
+				<a
+					className="dark:border-dark-100 border-light-800 dark:hover:bg-dark-200 dark:active:bg-dark-100 hover:bg-light-700 active:bg-light-800 pl-6.5 focus:ring-width-2 focus:ring-blurple ml-[10px] border-l p-[5px] text-sm outline-0 focus:rounded focus:border-0 focus:ring"
+					href={`#${heading.slug}`}
+					key={heading.slug}
+					style={{ marginLeft: `${heading.depth * 14}px` }}
+					title={heading.text}
+				>
+					<span className="line-clamp-1">{heading.text}</span>
+				</a>
+			)),
+		[headings],
+	);
+
 	return (
-		<div className={`flex flex-col space-y-2 ${className}`}>
-			{headings.map((heading) => (
-				<div key={heading.slug} style={{ marginLeft: `${heading.depth * 20}px` }}>
-					<a className="text-black" href={`#${heading.slug}`}>
-						{heading.text}
-					</a>
-				</div>
-			))}
+		<div className="flex flex-col break-all pb-8">
+			<div className="mt-5.5 flex flex-col gap-2">
+				<div className="flex flex-col">{headingItems}</div>
+			</div>
 		</div>
 	);
 }
