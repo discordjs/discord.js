58847f991d8ce7bbc6c120d534efb81098c1f5ac
iCrawl <buechler.noel@outlook.com>
2022-10-08 03:35:17 +0200

    fix: styling

--- packages/guide/src/components/Outline.tsx
+++ packages/guide/src/components/Outline.tsx
@@ -1,5 +1,6 @@
 import type { MarkdownHeading } from 'astro';
 import { useMemo } from 'react';
+import { VscListSelection } from 'react-icons/vsc';
 
 export function Outline({ headings }: { headings: MarkdownHeading[] }) {
 	const headingItems = useMemo(
@@ -9,7 +10,7 @@ export function Outline({ headings }: { headings: MarkdownHeading[] }) {
 					className="dark:border-dark-100 border-light-800 dark:hover:bg-dark-200 dark:active:bg-dark-100 hover:bg-light-700 active:bg-light-800 pl-6.5 focus:ring-width-2 focus:ring-blurple ml-[10px] border-l p-[5px] text-sm outline-0 focus:rounded focus:border-0 focus:ring"
 					href={`#${heading.slug}`}
 					key={heading.slug}
-					style={{ marginLeft: `${heading.depth * 14}px` }}
+					style={{ paddingLeft: `${heading.depth * 14}px` }}
 					title={heading.text}
 				>
 					<span className="line-clamp-1">{heading.text}</span>
@@ -19,8 +20,12 @@ export function Outline({ headings }: { headings: MarkdownHeading[] }) {
 	);
 
 	return (
-		<div className="flex flex-col break-all pb-8">
-			<div className="mt-5.5 flex flex-col gap-2">
+		<div className="flex flex-col break-all p-3 pb-8">
+			<div className="mt-4 ml-2 flex flex-row gap-2">
+				<VscListSelection size={25} />
+				<span className="font-semibold">Contents</span>
+			</div>
+			<div className="mt-4 ml-2 flex flex-col gap-2">
 				<div className="flex flex-col">{headingItems}</div>
 			</div>
 		</div>
