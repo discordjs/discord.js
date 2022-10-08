466da8e2582750931147f7357a203d980fb6172e
suneettipirneni <suneettipirneni@icloud.com>
2022-10-08 17:51:26 -0400

    chore: make text tinier

--- packages/guide/src/components/ExternalLink.tsx
+++ packages/guide/src/components/ExternalLink.tsx
@@ -3,11 +3,11 @@ import { FiExternalLink } from 'react-icons/fi';
 export function ExternalLink({ href, title }: { href: string; title: string }) {
 	return (
 		<a
-			className="color-blurple flex flex flex content-center items-center justify-center space-x-2 font-semibold"
+			className="text-blurple flex flex flex content-center items-center justify-center gap-2 text-sm font-semibold"
 			href={href}
 		>
 			<p>{title}</p>
-			<FiExternalLink size={22} />
+			<FiExternalLink size={18} />
 		</a>
 	);
 }
