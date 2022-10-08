ee5b375c38b10a1f52f7a55c0bc70ae99a55ce4a
suneettipirneni <suneettipirneni@icloud.com>
2022-10-08 15:16:18 -0400

    feat(guide): add github links for pages

--- packages/guide/src/components/DocsLink.tsx
+++ packages/guide/src/components/DocsLink.tsx
@@ -1,3 +1,12 @@
-export function DocsLink() {
-	return null;
+import { FiExternalLink } from 'react-icons/fi';
+
+export function DocsLink({ href, title }: { href: string; title: string }) {
+	return (
+		<a className="color-blurple flex content-center items-center justify-center  font-semibold" href={href}>
+			<div className="flex space-x-2">
+				<p>{title}</p>
+				<FiExternalLink size={22} />
+			</div>
+		</a>
+	);
 }
--- packages/guide/src/util/constants.ts
+++ packages/guide/src/util/constants.ts
@@ -1,1 +1,2 @@
 export const DESCRIPTION = 'Imagine a guide... that explores the many possibilities for your discord.js bot.';
+export const GITHUB_BASE_PAGES_PATH = 'https://github.com/discordjs/discord.js/tree/main/packages/guide/src/pages';
