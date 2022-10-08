de65df44058b38ff28f9ab6ff3867eca61d4314b
suneettipirneni <suneettipirneni@icloud.com>
2022-10-08 18:32:05 -0400

    chore: make requested changes

--- packages/guide/src/components/ExternalLink.tsx
+++ packages/guide/src/components/ExternalLink.tsx
@@ -2,10 +2,7 @@ import { FiExternalLink } from 'react-icons/fi';
 
 export function ExternalLink({ href, title }: { href: string; title: string }) {
 	return (
-		<a
-			className="text-blurple flex flex flex content-center items-center justify-center gap-2 text-sm font-semibold"
-			href={href}
-		>
+		<a className="text-blurple inline-flex place-items-center gap-2 text-sm font-semibold" href={href}>
 			<p>{title}</p>
 			<FiExternalLink size={18} />
 		</a>
