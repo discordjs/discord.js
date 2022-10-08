5d671a098d682599a96ac5af3cd01c526aad0c0d
suneettipirneni <suneettipirneni@icloud.com>
2022-10-08 15:53:27 -0400

    chore: move to another component

--- packages/guide/src/components/DocsLink.tsx
+++ packages/guide/src/components/DocsLink.tsx
@@ -1,12 +1,3 @@
-import { FiExternalLink } from 'react-icons/fi';
-
-export function DocsLink({ href, title }: { href: string; title: string }) {
-	return (
-		<a className="color-blurple flex content-center items-center justify-center  font-semibold" href={href}>
-			<div className="flex space-x-2">
-				<p>{title}</p>
-				<FiExternalLink size={22} />
-			</div>
-		</a>
-	);
+export function DocsLink() {
+	return null;
 }
