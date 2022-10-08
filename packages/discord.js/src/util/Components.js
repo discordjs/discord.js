3483c3f1a4b13a63c06b26c5ed86a0c27c2a02b0
monbrey <monbrey@gmail.com>
2022-10-09 08:56:56 +1100

    fix: correctly construct a builder

--- packages/discord.js/src/util/Components.js
+++ packages/discord.js/src/util/Components.js
@@ -109,7 +109,7 @@ function createComponentBuilder(data) {
     case ComponentType.SelectMenu:
       return new SelectMenuBuilder(data);
     case ComponentType.TextInput:
-      return new TextInputComponent(data);
+      return new TextInputBuilder(data);
     default:
       throw new Error(`Found unknown component type: ${data.type}`);
   }
@@ -124,6 +124,7 @@ const ButtonComponent = require('../structures/ButtonComponent');
 const Component = require('../structures/Component');
 const SelectMenuBuilder = require('../structures/SelectMenuBuilder');
 const SelectMenuComponent = require('../structures/SelectMenuComponent');
+const TextInputBuilder = require('../structures/TextInputBuilder');
 const TextInputComponent = require('../structures/TextInputComponent');
 
 /**
