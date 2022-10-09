5fa916e4aaa83f2141f845c310958d782751b9a3
Noel <buechler.noel@outlook.com>
2022-10-09 22:26:56 +0200

    Apply suggestions from code review
    Co-authored-by: A. Román <kyradiscord@gmail.com>

--- src/structures/UserContextMenuInteraction.js
+++ src/structures/UserContextMenuInteraction.js
@@ -8,7 +8,7 @@ const ContextMenuInteraction = require('./ContextMenuInteraction');
  */
 class UserContextMenuInteraction extends ContextMenuInteraction {
   /**
-   * The user of the target for this interaction
+   * The target user from this interaction
    * @type {User}
    * @readonly
    */
@@ -17,7 +17,7 @@ class UserContextMenuInteraction extends ContextMenuInteraction {
   }
 
   /**
-   * The member of the target for this interaction
+   * The target member from this interaction
    * @type {?(GuildMember|APIGuildMember)}
    * @readonly
    */
