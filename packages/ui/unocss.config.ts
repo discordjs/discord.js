5f72d8b645db095605ded58865760d62164e52b8
iCrawl <buechler.noel@outlook.com>
2022-10-09 02:10:35 +0200

    fix: website fonts

--- packages/ui/unocss.config.ts
+++ packages/ui/unocss.config.ts
@@ -22,7 +22,23 @@ export default defineConfig({
 		presetWebFonts({
 			provider: 'bunny',
 			fonts: {
-				mono: ['JetBrains Mono', 'JetBrains Mono:400,600,700'],
+				mono: ['JetBrains Mono:400,600,700'],
+				'source-sans-pro': [
+					'Source Sans Pro:300,400,500,600',
+					{ name: 'ui-sans-serif', provider: 'none' },
+					{ name: 'system-ui', provider: 'none' },
+					{ name: '-apple-system', provider: 'none' },
+					{ name: 'BlinkMacSystemFont', provider: 'none' },
+					{ name: 'Segoe UI', provider: 'none' },
+					{ name: 'Roboto', provider: 'none' },
+					{ name: 'Helvetica Neue', provider: 'none' },
+					{ name: 'Arial', provider: 'none' },
+					{ name: 'sans-serif', provider: 'none' },
+					{ name: 'Apple Color Emoji', provider: 'none' },
+					{ name: 'Segoe UI Emoji', provider: 'none' },
+					{ name: 'Segoe UI Symbol', provider: 'none' },
+					{ name: 'Noto Color Emoji', provider: 'none' },
+				],
 			},
 		}),
 		presetTypography({
