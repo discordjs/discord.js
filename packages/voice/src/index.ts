02ffce8173e2cd885e593023d0dd4969c96758e8
Almeida <almeidx@pm.me>
2022-10-10 15:42:03 +0100

    chore: make 'Note to developers' on version constant a regular comment (#8730)
    chore: make 'Note to developers' a regular comment
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- packages/voice/src/index.ts
+++ packages/voice/src/index.ts
@@ -23,8 +23,7 @@ export { type JoinConfig, getVoiceConnection, getVoiceConnections, getGroups } f
 /**
  * The [\@discordjs/voice](https://github.com/discordjs/discord.js/blob/main/packages/voice/#readme) version
  * that you are currently using.
- *
- * Note to developers: This needs to explicitly be `string` so it is not typed as a "const string" that gets injected by esbuild
  */
+// This needs to explicitly be `string` so it is not typed as a "const string" that gets injected by esbuild
 // eslint-disable-next-line @typescript-eslint/no-inferrable-types
 export const version: string = '[VI]{{inject}}[/VI]';
