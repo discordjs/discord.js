4b6a43305b2c8dcd6d7aa566c53fdc51663c370e
Gamer Omega <76910135+gamer0mega@users.noreply.github.com>
2022-11-20 14:49:22 +0300

    fix(WebSocketShard): Remove .unref() of timeouts

--- packages/discord.js/src/client/websocket/WebSocketShard.js
+++ packages/discord.js/src/client/websocket/WebSocketShard.js
@@ -592,7 +592,7 @@ class WebSocketShard extends EventEmitter {
     this.helloTimeout = setTimeout(() => {
       this.debug('Did not receive HELLO in time. Destroying and connecting again.');
       this.destroy({ reset: true, closeCode: 4009 });
-    }, 20_000).unref();
+    }, 20_000);
   }
 
   /**
@@ -617,7 +617,7 @@ class WebSocketShard extends EventEmitter {
       }
       this.debug('Did not receive RESUMED in time. Destroying and connecting again.');
       this.destroy({ reset: false, closeCode: 4009 });
-    }, 20_000).unref();
+    }, 20_000);
   }
 
   /**
@@ -638,7 +638,7 @@ class WebSocketShard extends EventEmitter {
     this.readyDispatchTimeout = setTimeout(() => {
       this.debug('Did not receive READY in time. Destroying and connecting again.');
       this.destroy({ reset: true, closeCode: 4009 });
-    }, 20_000).unref();
+    }, 20_000);
   }
 
   /**
