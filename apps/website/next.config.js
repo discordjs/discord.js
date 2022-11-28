2f4bdf7394b2734ca7a6675f123e9ae4b3fcf53e
iCrawl <buechler.noel@outlook.com>
2022-11-29 00:20:23 +0100

    fix: remove output file tracing option

--- apps/website/next.config.js
+++ apps/website/next.config.js
@@ -15,7 +15,6 @@ export default withBundleAnalyzer({
 		ignoreDuringBuilds: true,
 	},
 	cleanDistDir: true,
-	outputFileTracing: true,
 	experimental: {
 		appDir: true,
 		serverComponentsExternalPackages: ['@microsoft/api-extractor-model', 'jju', 'shiki'],
