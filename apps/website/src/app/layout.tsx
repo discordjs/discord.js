72c30d15786033cca2cc6bf49690f6b07e6f5e67
iCrawl <buechler.noel@outlook.com>
2022-11-30 04:41:07 +0100

    revert: theme fix

--- apps/website/src/app/layout.tsx
+++ apps/website/src/app/layout.tsx
@@ -1,6 +1,5 @@
-// import { ServerThemeProvider } from 'next-themes';
+import { ServerThemeProvider } from 'next-themes';
 import type { PropsWithChildren } from 'react';
-import { Providers } from './providers';
 
 import '@unocss/reset/tailwind.css';
 import '../styles/inter.css';
@@ -10,21 +9,19 @@ import '../styles/main.css';
 
 export default function RootLayout({ children }: PropsWithChildren) {
 	return (
-		// <ServerThemeProvider
-		// 	attribute="class"
-		// 	defaultTheme="system"
-		// 	disableTransitionOnChange
-		// 	value={{
-		// 		light: 'light',
-		// 		dark: 'dark',
-		// 	}}
-		// >
-		<html lang="en">
-			<head />
-			<body className="dark:bg-dark-800 bg-white">
-				<Providers>{children}</Providers>
-			</body>
-		</html>
-		// </ServerThemeProvider>
+		<ServerThemeProvider
+			attribute="class"
+			defaultTheme="system"
+			disableTransitionOnChange
+			value={{
+				light: 'light',
+				dark: 'dark',
+			}}
+		>
+			<html lang="en">
+				<head />
+				<body className="dark:bg-dark-800 bg-white">{children}</body>
+			</html>
+		</ServerThemeProvider>
 	);
 }
