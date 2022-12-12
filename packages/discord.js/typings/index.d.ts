b14604abdecca575b1fca693c1593e3585bcca8c
muchnameless <12682826+muchnameless@users.noreply.github.com>
2022-12-12 09:49:24 +0100

    fix: re-export formatters (#8909)
    * fix: re-export formatters
    
    * future-proof typings
    
    Co-authored-by: SpaceEEC <spaceeec@yahoo.com>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- packages/discord.js/typings/index.d.ts
+++ packages/discord.js/typings/index.d.ts
@@ -6266,5 +6266,6 @@ export type InternalDiscordGatewayAdapterCreator = (
 // External
 export * from 'discord-api-types/v10';
 export * from '@discordjs/builders';
+export * from '@discordjs/formatters';
 export * from '@discordjs/rest';
 export * from '@discordjs/util';
