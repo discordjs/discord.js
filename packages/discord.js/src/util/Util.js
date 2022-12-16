c76e17078602914c3e1d227a3acc98eaa99c18d4
Almeida <almeidx@pm.me>
2022-12-16 13:28:18 +0000

    fix(resolveColor): invalid colors (#8933)
    Fixes https://github.com/discordjs/discord.js/issues/8932

--- packages/discord.js/src/util/Util.js
+++ packages/discord.js/src/util/Util.js
@@ -477,13 +477,14 @@ function resolveColor(color) {
   if (typeof color === 'string') {
     if (color === 'Random') return Math.floor(Math.random() * (0xffffff + 1));
     if (color === 'Default') return 0;
-    color = Colors[color] ?? parseInt(color.replace('#', ''), 16);
+    if (/^#?[\da-f]{6}$/i.test(color)) return parseInt(color.replace('#', ''), 16);
+    color = Colors[color];
   } else if (Array.isArray(color)) {
     color = (color[0] << 16) + (color[1] << 8) + color[2];
   }
 
   if (color < 0 || color > 0xffffff) throw new DiscordjsRangeError(ErrorCodes.ColorRange);
-  else if (Number.isNaN(color)) throw new DiscordjsTypeError(ErrorCodes.ColorConvert);
+  if (typeof color !== 'number' || Number.isNaN(color)) throw new DiscordjsTypeError(ErrorCodes.ColorConvert);
 
   return color;
 }
