649058055a3159277a19e3173c115a67226f0ee0
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-01-01 17:28:56 +0000

    types: Swap message reaction and emoji identifier types (#8970)
    Co-authored-by: Aura Román <kyradiscord@gmail.com>

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -4638,7 +4638,11 @@ export interface EmbedFooterData {
   iconURL?: string;
 }
 
-export type EmojiIdentifierResolvable = string | EmojiResolvable;
+export type EmojiIdentifierResolvable =
+  | EmojiResolvable
+  | `${'' | 'a:'}${string}:${Snowflake}`
+  | `<${'' | 'a'}:${string}:${Snowflake}>`
+  | string;
 
 export type EmojiResolvable = Snowflake | GuildEmoji | ReactionEmoji;
 
@@ -5490,13 +5494,7 @@ export interface MessageOptions {
   flags?: BitFieldResolvable<'SUPPRESS_EMBEDS', number>;
 }
 
-export type MessageReactionResolvable =
-  | MessageReaction
-  | Snowflake
-  | `${string}:${Snowflake}`
-  | `<:${string}:${Snowflake}>`
-  | `<a:${string}:${Snowflake}>`
-  | string;
+export type MessageReactionResolvable = MessageReaction | Snowflake | string;
 
 export interface MessageReference {
   channelId: Snowflake;
