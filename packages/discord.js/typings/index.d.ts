2798ba1eb3d734f0cf2eeccd2e16cfba6804873b
Almeida <almeidx@pm.me>
2023-01-12 08:23:02 +0000

    style: run prettier (#9041)

--- packages/discord.js/typings/index.d.ts
+++ packages/discord.js/typings/index.d.ts
@@ -4620,8 +4620,8 @@ export type CacheConstructors = {
 // Narrowing the type of `manager.name` doesn't propagate type information to `holds` and the return type.
 export type CacheFactory = (
   manager: CacheConstructors[keyof Caches],
-  holds: Caches[typeof manager['name']][1],
-) => typeof manager['prototype'] extends DataManager<infer K, infer V, any> ? Collection<K, V> : never;
+  holds: Caches[(typeof manager)['name']][1],
+) => (typeof manager)['prototype'] extends DataManager<infer K, infer V, any> ? Collection<K, V> : never;
 
 export type CacheWithLimitsOptions = {
   [K in keyof Caches]?: Caches[K][0]['prototype'] extends DataManager<infer K, infer V, any>
--- packages/ws/__tests__/strategy/WorkerContextFetchingStrategy.test.ts
+++ packages/ws/__tests__/strategy/WorkerContextFetchingStrategy.test.ts
@@ -46,6 +46,7 @@ vi.mock('node:worker_threads', async () => {
 	return {
 		parentPort: new MockParentPort(),
 		isMainThread: false,
+		workerData: {},
 	};
 });
 
