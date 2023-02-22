87ce3b955386e9ebcb8ddabae1d386156bc2642f
Elysia <71698422+aiko-chan-ai@users.noreply.github.com>
2023-02-22 23:30:36 +0700

    update

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -96,7 +96,8 @@ exports.Endpoints = {
         makeImageUrl(`${root}/discovery-splashes/${guildId}/${hash}`, { size, format }),
       TeamIcon: (teamId, hash, options) => makeImageUrl(`${root}/team-icons/${teamId}/${hash}`, options),
       Sticker: (stickerId, stickerFormat) =>
-        `${root}/stickers/${stickerId}.${stickerFormat === 'LOTTIE' ? 'json' : 'png'}`,
+        `${root}/stickers/${stickerId}.${stickerFormat === 'LOTTIE' ? 'json' : stickerFormat === 'GIF' ? 'gif' : 'png'
+        }`,
       RoleIcon: (roleId, hash, format = 'webp', size) =>
         makeImageUrl(`${root}/role-icons/${roleId}/${hash}`, { size, format }),
       guildScheduledEventCover: (scheduledEventId, coverHash, format, size) =>
