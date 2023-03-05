f069872b02ba67b621c1c1a38160a98fe1fbccbe
Vlad Frangu <kingdgrizzle@gmail.com>
2023-03-06 00:27:30 +0200

    fix(Actions): inject built data by using a symbol

--- src/client/actions/ActionsManager.js
+++ src/client/actions/ActionsManager.js
@@ -4,6 +4,13 @@ class ActionsManager {
   constructor(client) {
     this.client = client;
 
+    // These symbols represent fully built data that we inject at times when calling actions manually. Action#getUser,
+    // for example, will return the injected data (which is assumed to be a built structure) instead of trying to make
+    // it from provided data
+    this.injectedUser = Symbol('djs.actions.injectedUser');
+    this.injectedChannel = Symbol('djs.actions.injectedChannel');
+    this.injectedMessage = Symbol('djs.actions.injectedMessage');
+
     this.register(require('./ApplicationCommandPermissionsUpdate'));
     this.register(require('./AutoModerationActionExecution'));
     this.register(require('./AutoModerationRuleCreate'));
