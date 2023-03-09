e3801be01b677b283e4c2dd5cad031f7a6401a8c
Erwan <erwan977@gmail.com>
2023-03-09 22:18:09 +0100

    fix(Message#editable): add check in archived threads

--- packages/discord.js/src/structures/Message.js
+++ packages/discord.js/src/structures/Message.js
@@ -604,8 +604,10 @@ class Message extends Base {
     const precheck = Boolean(this.author.id === this.client.user.id && (!this.guild || this.channel?.viewable));
 
     // Regardless of permissions thread messages cannot be edited if
-    // the thread is locked and the bot if the thread is locked and the bot does not have permission to manage threads.
+    // the thread is archived or the thread is locked and the bot does not have permission to manage threads
     if (this.channel?.isThread()) {
+      if (this.channel.archived) return false;
+
       const permissions = this.permissionsFor(this.client.user);
       if (!permissions?.has(PermissionFlagsBits.ManageThreads, true)) return false;
     }
