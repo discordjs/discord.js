3b1666162860d08dea6e323c0d2cc9b5de29c074
suneettipirneni <suneettipirneni@icloud.com>
2023-03-23 10:06:54 -0400

    refactor: add headers for all other items

--- apps/website/src/components/model/enum/Enum.tsx
+++ apps/website/src/components/model/enum/Enum.tsx
@@ -3,14 +3,14 @@ import { VscSymbolEnum } from '@react-icons/all-files/vsc/VscSymbolEnum';
 import { Documentation } from '../../documentation/Documentation';
 import { EnumMember } from './EnumMember';
 import { Panel } from '~/components/Panel';
-import { SyntaxHighlighter } from '~/components/SyntaxHighlighter';
+import { ObjectHeader } from '~/components/documentation/ObjectHeader';
 import { ResponsiveSection } from '~/components/documentation/section/ResponsiveSection';
 import { SummarySection } from '~/components/documentation/section/SummarySection';
 
 export function Enum({ item }: { item: ApiEnum }) {
 	return (
-		<Documentation item={item}>
-			<SyntaxHighlighter code={item.excerpt.text} />
+		<Documentation>
+			<ObjectHeader item={item} />
 			<SummarySection item={item} />
 			<ResponsiveSection icon={<VscSymbolEnum size={20} />} padded title="Members">
 				<div className="flex flex-col gap-4">
