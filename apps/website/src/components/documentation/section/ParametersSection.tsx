c8c02f957dc4bc49e4b44d61fdf09c89d81e148e
Suneet Tipirneni <77477100+suneettipirneni@users.noreply.github.com>
2023-03-23 15:44:04 -0400

    fix(website): remove layout shift (#9062)
    Co-authored-by: Noel <buechler.noel@outlook.com>

--- apps/website/src/components/documentation/section/ParametersSection.tsx
+++ apps/website/src/components/documentation/section/ParametersSection.tsx
@@ -1,12 +1,12 @@
 import type { ApiParameterListMixin } from '@microsoft/api-extractor-model';
 import { VscSymbolParameter } from '@react-icons/all-files/vsc/VscSymbolParameter';
-import { ResponsiveSection } from './ResponsiveSection';
+import { DocumentationSection } from './DocumentationSection';
 import { ParameterTable } from '~/components/ParameterTable';
 
 export function ParameterSection({ item }: { item: ApiParameterListMixin }) {
 	return (
-		<ResponsiveSection icon={<VscSymbolParameter size={20} />} padded title="Parameters">
+		<DocumentationSection icon={<VscSymbolParameter size={20} />} padded title="Parameters">
 			<ParameterTable item={item} />
-		</ResponsiveSection>
+		</DocumentationSection>
 	);
 }
