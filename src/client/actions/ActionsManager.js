4bb876e5f659495d4c17ececeee78af81be3c67f
Jaw0r3k <jaworekwiadomosci@gmail.com>
2023-03-30 17:41:04 +0200

    fix: keep symbols in actions manager (#9293)
    * fix: keep symbols in actions manager
    
    Co-authored-by: Jiralite <33201955+Jiralite@users.noreply.github.com>

--- src/client/actions/ActionsManager.js
+++ src/client/actions/ActionsManager.js
@@ -4,9 +4,9 @@ class ActionsManager {
   constructor(client) {
     this.client = client;
 
-    // These symbols represent fully built data that we inject at times when calling actions manually. Action#getUser,
-    // for example, will return the injected data (which is assumed to be a built structure) instead of trying to make
-    // it from provided data
+    // These symbols represent fully built data that we inject at times when calling actions manually.
+    // Action#getUser, for example, will return the injected data (which is assumed to be a built structure)
+    // instead of trying to make it from provided data
     this.injectedUser = Symbol('djs.actions.injectedUser');
     this.injectedChannel = Symbol('djs.actions.injectedChannel');
     this.injectedMessage = Symbol('djs.actions.injectedMessage');
