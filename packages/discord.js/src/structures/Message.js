2a105fd1a7c5f53f8758f4da33804b69c41d003a
Erwan <erwan977@gmail.com>
2023-04-02 22:11:54 +0200

    fix(Message#editable): fix permissions check if channel is thread & locked

--- packages/discord.js/src/structures/Message.js
+++ packages/discord.js/src/structures/Message.js
@@ -608,7 +608,7 @@ class Message extends Base {
     if (this.channel?.isThread()) {
       if (this.channel.archived) return false;
       if (this.channel.locked) {
-        const permissions = this.permissionsFor(this.client.user);
+        const permissions = this.channel.permissionsFor(this.client.user);
         if (!permissions?.has(PermissionFlagsBits.ManageThreads, true)) return false;
       }
     }
