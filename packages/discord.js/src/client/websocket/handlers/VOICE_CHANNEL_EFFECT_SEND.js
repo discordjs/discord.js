b27c1d0819ce408899fd0ced404c0a3dd6b19be9
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-04-02 09:14:05 +0100

    docs: document event

--- packages/discord.js/src/client/websocket/handlers/VOICE_CHANNEL_EFFECT_SEND.js
+++ packages/discord.js/src/client/websocket/handlers/VOICE_CHANNEL_EFFECT_SEND.js
@@ -6,5 +6,11 @@ const Events = require('../../../util/Events');
 module.exports = (client, { d: data }) => {
   const guild = client.guilds.cache.get(data.guild_id);
   if (!guild) return;
+
+  /**
+   * Emmited when someone sends an effect, such as an emoji reaction, in a voice channel the client is connected to.
+   * @event Client#voiceChannelEffectSend
+   * @param {VoiceChannelEffect} voiceChannelEffect The sent voice channel effect
+   */
   client.emit(Events.VoiceChannelEffectSend, new VoiceChannelEffect(data, guild));
 };
