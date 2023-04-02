f1d98d6543667255c4de9fe9bce94229870d1e9e
TetieWasTaken <tjvssr@gmail.com>
2023-04-02 12:46:54 +0200

    chore: fix select menu link

--- packages/discord.js/src/structures/StringSelectMenuBuilder.js
+++ packages/discord.js/src/structures/StringSelectMenuBuilder.js
@@ -74,5 +74,5 @@ module.exports = StringSelectMenuBuilder;
 
 /**
  * @external BuildersSelectMenu
- * @see {@link https://discord.js.org/docs/packages/builders/main/SelectMenuBuilder:Class}
+ * @see {@link https://discord.js.org/docs/packages/builders/main/StringSelectMenuBuilder:Class}
  */
