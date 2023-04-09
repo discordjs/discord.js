ab80e2642ba2eba10a3e8509a42445681dad927d
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-04-10 00:25:09 +0100

    docs(builders): add some basic documentation

--- packages/builders/src/util/validation.ts
+++ packages/builders/src/util/validation.ts
@@ -1,5 +1,16 @@
 let validate = true;
 
+/**
+ * Enables validators.
+ */
 export const enableValidators = () => (validate = true);
+
+/**
+ * Disables validators.
+ */
 export const disableValidators = () => (validate = false);
+
+/**
+ * Checks whether validation is occurring.
+ */
 export const isValidationEnabled = () => validate;
