1a420bb69112fc8f89b94dc411170c31db6125ca
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-04-10 03:42:41 +0100

    feat: add support for default values

--- apps/website/src/components/documentation/tsdoc/TSDoc.tsx
+++ apps/website/src/components/documentation/tsdoc/TSDoc.tsx
@@ -6,7 +6,7 @@ import { Fragment, useCallback, type ReactNode } from 'react';
 import { ItemLink } from '../../ItemLink';
 import { SyntaxHighlighter } from '../../SyntaxHighlighter';
 import { resolveItemURI } from '../util';
-import { DeprecatedBlock, ExampleBlock, RemarksBlock, SeeBlock } from './BlockComment';
+import { DefaultValueBlock, DeprecatedBlock, ExampleBlock, RemarksBlock, SeeBlock } from './BlockComment';
 
 export function TSDoc({ item, tsdoc }: { item: ApiItem; tsdoc: DocNode }): JSX.Element {
 	const createNode = useCallback(
@@ -84,12 +84,19 @@ export function TSDoc({ item, tsdoc }: { item: ApiItem; tsdoc: DocNode }): JSX.E
 						(block) => block.blockTag.tagName.toUpperCase() === StandardTags.example.tagNameWithUpperCase,
 					);
 
+					const defaultValueBlock = comment.customBlocks.find(
+						(block) => block.blockTag.tagName.toUpperCase() === StandardTags.defaultValue.tagNameWithUpperCase,
+					);
+
 					return (
 						<div className="flex flex-col space-y-2">
 							{comment.deprecatedBlock ? (
 								<DeprecatedBlock>{createNode(comment.deprecatedBlock.content)}</DeprecatedBlock>
 							) : null}
 							{comment.summarySection ? createNode(comment.summarySection) : null}
+							{defaultValueBlock ? (
+								<DefaultValueBlock>{createNode(defaultValueBlock.content)}</DefaultValueBlock>
+							) : null}
 							{comment.remarksBlock ? <RemarksBlock>{createNode(comment.remarksBlock.content)}</RemarksBlock> : null}
 							{exampleBlocks.length
 								? exampleBlocks.map((block, idx) => <ExampleBlock key={idx}>{createNode(block.content)}</ExampleBlock>)
