00197008693e0eaca963a6e5f40b3f2cf85accfa
iCrawl <buechler.noel@outlook.com>
2023-04-10 12:51:13 +0200

    chore(website): switch to revalidate on fetch level

--- packages/actions/src/uploadDocumentation/index.ts
+++ packages/actions/src/uploadDocumentation/index.ts
@@ -6,7 +6,7 @@ import { connect } from '@planetscale/database';
 import { fetch } from 'undici';
 
 if (!process.env.DATABASE_URL) {
-	process.exit(0);
+	setFailed('DATABASE_URL is not set');
 }
 
 const pkg = getInput('package', { required: true });
@@ -14,13 +14,14 @@ const version = getInput('version') || 'main';
 
 const sql = connect({
 	fetch,
-	url: process.env.DATABASE_URL,
+	url: process.env.DATABASE_URL!,
 });
 
 const globber = await create(`packages/${pkg}/docs/docs.api.json`);
 for await (const file of globber.globGenerator()) {
 	const data = await readFile(file, 'utf8');
 	try {
+		console.log(`Uploading ${file} with ${version}...`);
 		await sql.execute('replace into documentation (version, data) values (?, ?)', [version, data]);
 	} catch (error) {
 		const err = error as Error;
