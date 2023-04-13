990be809aaabcae779ef58b4ae2027f2875b0425
didinele <didinele.dev@gmail.com>
2023-04-13 13:58:33 +0300

    fix: properly block for same key

--- packages/ws/src/utils/IdentifyThrottler.ts
+++ packages/ws/src/utils/IdentifyThrottler.ts
@@ -2,10 +2,10 @@ import { setTimeout as sleep } from 'node:timers/promises';
 import { Collection } from '@discordjs/collection';
 import { AsyncQueue } from '@sapphire/async-queue';
 import type { WebSocketManager } from '../ws/WebSocketManager.js';
+import { WebSocketShardEvents } from '../ws/WebSocketShard.js';
 
 interface SessionState {
 	queue: AsyncQueue;
-	remaining: number;
 	resetsAt: number;
 }
 
@@ -20,26 +20,24 @@ export class IdentifyThrottler {
 
 		const state = this.states.ensure(key, () => ({
 			queue: new AsyncQueue(),
-			remaining: info.session_start_limit.max_concurrency,
-			resetsAt: Date.now() + 5_000,
+			resetsAt: Number.POSITIVE_INFINITY,
 		}));
 
 		await state.queue.wait();
 
 		try {
-			if (state.remaining <= 0) {
-				const diff = state.resetsAt - Date.now();
-				if (diff <= 5_000) {
-					// To account for the latency the IDENTIFY payload goes through, we add a bit more wait time
-					const time = diff + Math.random() * 1_500;
-					await sleep(time);
-				}
-
-				state.remaining = info.session_start_limit.max_concurrency;
-				state.resetsAt = Date.now() + 5_000;
+			const diff = state.resetsAt - Date.now();
+			if (diff <= 5_000) {
+				// To account for the latency the IDENTIFY payload goes through, we add a bit more wait time
+				const time = diff + Math.random() * 1_500;
+				this.manager.emit(WebSocketShardEvents.Debug, {
+					shardId,
+					message: `Waiting ${time}ms before IDENTIFYing`,
+				});
+				await sleep(time);
 			}
 
-			state.remaining--;
+			state.resetsAt = Date.now() + 5_000;
 		} finally {
 			state.queue.shift();
 		}
