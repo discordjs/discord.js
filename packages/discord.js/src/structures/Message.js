300059cb266e6cca42c30ee7f997c48ab2cc565e
BoogeyMan <76151726+BoogeyMan24@users.noreply.github.com>
2023-04-14 23:46:15 +0200

    docs: cleanup MessageCreateOptions and MessageReplyOptions (#9283)
    * Fixed MessageReplyOptions
    
    * updated MessageReplyOptions
    
    * Deduplication - created BaseMessageCreateOptions
    
    * Update packages/discord.js/src/structures/interfaces/TextBasedChannel.js
    
    Co-authored-by: Jaw0r3k <jaworekwiadomosci@gmail.com>
    
    * Fixed spacing
    
    ---------
    
    Co-authored-by: Jaw0r3k <jaworekwiadomosci@gmail.com>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- packages/discord.js/src/structures/Message.js
+++ packages/discord.js/src/structures/Message.js
@@ -822,7 +822,6 @@ class Message extends Base {
    * @typedef {BaseMessageCreateOptions} MessageReplyOptions
    * @property {boolean} [failIfNotExists=this.client.options.failIfNotExists] Whether to error if the referenced
    * message does not exist (creates a standard message in this case when false)
-   * @property {StickerResolvable[]} [stickers=[]] Stickers to send in the message
    */
 
   /**
