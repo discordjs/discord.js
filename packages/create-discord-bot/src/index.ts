eae784a54a365cf5e789dc0eb0d8e951fe065d8c
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-04-19 11:28:34 +0100

    feat: basic initialisation

---
+++ packages/create-discord-bot/src/index.ts
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+// eslint-disable-next-line n/shebang
+import { cpSync, existsSync, mkdirSync } from 'node:fs';
+import path from 'node:path';
+import process from 'node:process';
+import { URL } from 'node:url';
+import chalk from 'chalk';
+import { program } from 'commander';
+
+// A directory must be specified.
+program
+	.description('Create a template bot.')
+	.argument('<directory>', 'The directory where this will be created.')
+	.parse();
+
+const [directory] = program.args;
+
+if (!directory) {
+	console.error(chalk.red('Please specify the project directory!'));
+	process.exit(1);
+}
+
+// Resolve the root of the project.
+const root = path.resolve(directory);
+const name = path.basename(root);
+
+// Ensure the directory exists.
+if (!existsSync(root)) mkdirSync(root, { recursive: true });
+console.log(`Creating ${name} in ${chalk.green(root)}.`);
+
+// Copy template!
+cpSync(new URL('../template', import.meta.url), root, { recursive: true });
+chalk.green('All done! Be sure to check out the discord.js guide too!');
