f36152b3d610b793830bf50697f898bea06a0a0d
GreggJEduardoPH <greggjeduardoph@gmail.com>
2023-04-22 15:59:56 -0600

    discord.js
    @millette #9385 
    @cking #9439 
    @Gawdl3y #3403 
    @Drahcirius #3012 
    @PhaxeNor #3015

---
+++ discord.js
@@ -0,0 +1,39 @@
+
+discordia.js
+
+
+servidor de discordia versión npm descargas npm Estado de las pruebas Cobertura de código
+
+Vercel
+
+Acerca de
+Este repositorio contiene múltiples paquetes con versiones separadas . Puede encontrar el contenedor API de Discord ensamblado en discord.js. Es un poderoso módulo de Node.js que te permite interactuar fácilmente con la API de Discord .
+
+Paquetes
+discord.js( fuente ) - Un poderoso módulo Node.js para interactuar con la API de Discord
+@discordjs/brokers( fuente ) - Una colección de corredores para usar con discord.js
+@discordjs/builders( fuente ) - Un paquete de utilidades para crear fácilmente cargas útiles de la API de Discord
+@discordjs/collection( fuente ) - Una poderosa estructura de datos de servicios públicos
+@discordjs/core( fuente ) - Una envoltura finamente resumida alrededor de los componentes centrales de la API de Discord
+@discordjs/formatters( fuente ) - Una colección de funciones para formatear cadenas
+@discordjs/proxy( fuente ) - Un contenedor @discordjs/restpara ejecutar un proxy HTTP
+@discordjs/rest( fuente ) - Un módulo para interactuar con la API REST de Discord
+@discordjs/voice( fuente ) - Un módulo para interactuar con Discord Voice API
+@discordjs/util( fuente ) - Una colección de funciones de utilidad
+@discordjs/ws( fuente ) - Un envoltorio alrededor de la puerta de enlace de Discord
+Enlaces
+Sitio web ( fuente )
+Documentación
+Guía ( fuente ) Consulte también la Guía de actualización v13 a v14 , que incluye elementos actualizados y eliminados de la biblioteca.
+discord.js Servidor de discordia
+Discord API Servidor de discordia
+GitHub
+npm
+Bibliotecas relacionadas
+Extensiones
+RPC ( fuente )
+contribuyendo
+Lea nuestras pautas de contribución antes de iniciar una solicitud de extracción. Damos la bienvenida a contribuciones de todo tipo, ¡no solo de código! Si no tiene ideas, busque la etiqueta buena del primer problema en los problemas del repositorio. Si tiene alguna pregunta sobre el proyecto, no dude en hacerla en Discord . Antes de crear su propia publicación o solicitud de extracción, ¡siempre verifique si ya existe una! No apresure las contribuciones, tómese su tiempo y asegúrese de hacerlo correctamente.
+
+Ayuda
+Si no entiende algo en la documentación, tiene problemas o simplemente necesita un pequeño empujón en la dirección correcta, únase a nuestro servidor de Discord .
