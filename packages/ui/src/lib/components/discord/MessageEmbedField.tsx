35d8fa20b693dbde82fc489fff3ff2735c02f770
almeidx <almeidx@pm.me>
2023-04-24 18:59:16 +0100

    fix: no inline on mobile

--- packages/ui/src/lib/components/discord/MessageEmbedField.tsx
+++ packages/ui/src/lib/components/discord/MessageEmbedField.tsx
@@ -6,7 +6,7 @@ export interface IDiscordMessageEmbedField {
 
 export function DiscordMessageEmbedField({ name, value, inline }: IDiscordMessageEmbedField) {
 	return (
-		<div className={`${inline ? 'col-span-4' : 'col-span-12'} flex flex-col`}>
+		<div className={`${inline ? 'sm:col-span-4' : 'sm:col-span-12'} flex flex-col`}>
 			<span className="font-medium">{name}</span>
 			<span className="text-gray-300">{value}</span>
 		</div>
--- packages/ui/src/lib/components/discord/MessageEmbedFields.tsx
+++ packages/ui/src/lib/components/discord/MessageEmbedFields.tsx
@@ -6,7 +6,7 @@ export interface IDiscordMessageEmbedFields {
 
 export function DiscordMessageEmbedFields({ fields }: IDiscordMessageEmbedFields) {
 	return (
-		<div className="grid grid-cols-12 mt-2 gap-2">
+		<div className="grid grid-cols-1 mt-2 gap-2 sm:grid-cols-12">
 			{fields.map((field, idx) => (
 				<DiscordMessageEmbedField key={idx} {...field} />
 			))}
