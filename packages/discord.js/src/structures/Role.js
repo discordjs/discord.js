1ff8c977922f5bf47b8ce0989a7934702d6b8a67
JMTK <jake.morrison24@gmail.com>
2023-05-01 16:07:31 -0400

    Revert editable change

--- packages/discord.js/src/structures/Role.js
+++ packages/discord.js/src/structures/Role.js
@@ -177,7 +177,7 @@ class Role extends Base {
    */
   get editable() {
     if (this.managed) return false;
-    const clientMember = this.guild.members.me;
+    const clientMember = this.guild.members.resolve(this.client.user);
     if (!clientMember.permissions.has(PermissionFlagsBits.ManageRoles)) return false;
     return clientMember.roles.highest.comparePositionTo(this) > 0;
   }
