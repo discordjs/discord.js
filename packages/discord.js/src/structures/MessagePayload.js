201b002ad405b845ace5f708077c1f157bb4126d
Almeida <almeidx@pm.me>
2023-05-01 23:25:51 +0100

    fix: correct `isJSONEncodable()` import (#9495)
    Co-authored-by: Jiralite <33201955+Jiralite@users.noreply.github.com>

--- packages/discord.js/src/structures/MessagePayload.js
+++ packages/discord.js/src/structures/MessagePayload.js
@@ -1,8 +1,7 @@
 'use strict';
 
 const { Buffer } = require('node:buffer');
-const { isJSONEncodable } = require('@discordjs/builders');
-const { lazy } = require('@discordjs/util');
+const { lazy, isJSONEncodable } = require('@discordjs/util');
 const { MessageFlags } = require('discord-api-types/v10');
 const ActionRowBuilder = require('./ActionRowBuilder');
 const { DiscordjsRangeError, ErrorCodes } = require('../errors');
