6b2c3fb9d0571de808e990cf064f73240ab93451
Almeida <almeidx@pm.me>
2023-05-05 19:39:58 +0100

    feat(Guild): safety alerts channel and mention raid protection (#8959)
    * feat(Guild): add safety alerts channel and mention raid protection
    
    * docs: add missing tag
    
    * fix: keep other properties in triggerMetadata
    
    * docs(Guild): update example usage
    
    ---------
    
    Co-authored-by: Jiralite <33201955+Jiralite@users.noreply.github.com>

--- packages/discord.js/src/managers/AutoModerationRuleManager.js
+++ packages/discord.js/src/managers/AutoModerationRuleManager.js
@@ -59,6 +59,7 @@ class AutoModerationRuleManager extends CachedManager {
    * @property {string[]} [allowList] The substrings that will be exempt from triggering
    * {@link AutoModerationRuleTriggerType.Keyword} and {@link AutoModerationRuleTriggerType.KeywordPreset}
    * @property {?number} [mentionTotalLimit] The total number of role & user mentions allowed per message
+   * @property {boolean} [mentionRaidProtectionEnabled] Whether to automatically detect mention raids
    */
 
   /**
@@ -125,6 +126,7 @@ class AutoModerationRuleManager extends CachedManager {
           presets: triggerMetadata.presets,
           allow_list: triggerMetadata.allowList,
           mention_total_limit: triggerMetadata.mentionTotalLimit,
+          mention_raid_protection_enabled: triggerMetadata.mentionRaidProtectionEnabled,
         },
         actions: actions.map(action => ({
           type: action.type,
@@ -182,6 +184,7 @@ class AutoModerationRuleManager extends CachedManager {
           presets: triggerMetadata.presets,
           allow_list: triggerMetadata.allowList,
           mention_total_limit: triggerMetadata.mentionTotalLimit,
+          mention_raid_protection_enabled: triggerMetadata.mentionRaidProtectionEnabled,
         },
         actions: actions?.map(action => ({
           type: action.type,
