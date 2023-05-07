174e5971e33c7f5bd51cc68f0788f7a7e90604d8
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-05-07 16:12:13 +0100

    refactor: prefer "the"

--- packages/create-discord-bot/src/create-discord-bot.ts
+++ packages/create-discord-bot/src/create-discord-bot.ts
@@ -14,7 +14,7 @@ import { GUIDE_URL } from './util/constants.js';
 // A directory must be specified.
 program
 	.description('Create a basic discord.js bot.')
-	.option('--typescript', 'Whether to use a TypeScript template.')
+	.option('--typescript', 'Whether to use the TypeScript template.')
 	.argument('<directory>', 'The directory where this will be created.')
 	.parse();
 
