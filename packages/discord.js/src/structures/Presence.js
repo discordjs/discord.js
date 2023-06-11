discordjs/discord.js

commit 9a6e691eaa6c3d133098b2734414590cb838de2e

refactor: remove undocumented checks (#7637)

packages/discord.js/src/structures/Presence.js
@@ -376,12 +376,6 @@ class RichPresenceAssets {
       switch (platform) {
         case 'mp':
           return `https://media.discordapp.net/${id}`;
-        case 'spotify':
-          return `https://i.scdn.co/image/${id}`;
-        case 'youtube':
-          return `https://i.ytimg.com/vi/${id}/hqdefault_live.jpg`;
-        case 'twitch':
-          return `https://static-cdn.jtvnw.net/previews-ttv/live_user_${id}.png`;
         default:
           return null;
       }

commit 05e5fada2770a2a971447b31467cbf181cb3c4ce

feat(presence): re-introduce image resolving for other platforms
* spotify, youtube, twitch
* regression born from misunderstanding in 9a6e691

packages/discord.js/src/structures/Presence.js
@@ -358,6 +358,12 @@ class RichPresenceAssets {
       switch (platform) {
         case 'mp':
           return `https://media.discordapp.net/${id}`;
+        case 'spotify':
+          return `https://i.scdn.co/image/${id}`;
+        case 'youtube':
+          return `https://i.ytimg.com/vi/${id}/hqdefault_live.jpg`;
+        case 'twitch':
+          return `https://static-cdn.jtvnw.net/previews-ttv/live_user_${id}.png`;
         default:
           return null;
       }