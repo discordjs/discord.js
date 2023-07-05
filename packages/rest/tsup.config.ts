d4da3a1662ecf24df4dd41c5e5685a38efb1c897
suneettipirneni <suneettipirneni@icloud.com>
2023-07-05 16:49:27 -0400

    fix: entrypoints

--- packages/rest/tsup.config.ts
+++ packages/rest/tsup.config.ts
@@ -2,12 +2,6 @@ import { esbuildPluginVersionInjector } from 'esbuild-plugin-version-injector';
 import { createTsupConfig } from '../../tsup.config.js';
 
 export default createTsupConfig({
-	entry: [
-		'src/agnostic.ts',
-		'src/web.ts',
-		'src/strategies/undiciRequest.ts',
-		'src/strategies/fetchRequest.ts',
-		'src/strategies/*.ts',
-	],
+	entry: ['src/agnostic.ts', 'src/web.ts', 'src/strategies/*.ts'],
 	esbuildPlugins: [esbuildPluginVersionInjector()],
 });
