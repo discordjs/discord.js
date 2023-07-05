16eb0a21549e61d8ebc50c981a2ee14f18c0c448
suneettipirneni <suneettipirneni@icloud.com>
2023-07-05 16:38:06 -0400

    chore: requested changes

--- packages/rest/tsup.config.ts
+++ packages/rest/tsup.config.ts
@@ -2,6 +2,12 @@ import { esbuildPluginVersionInjector } from 'esbuild-plugin-version-injector';
 import { createTsupConfig } from '../../tsup.config.js';
 
 export default createTsupConfig({
-	entry: ['src/agnostic.ts', 'src/web.ts'],
+	entry: [
+		'src/agnostic.ts',
+		'src/web.ts',
+		'src/strategies/undiciRequest.ts',
+		'src/strategies/fetchRequest.ts',
+		'src/strategies/*.ts',
+	],
 	esbuildPlugins: [esbuildPluginVersionInjector()],
 });
--- packages/util/src/functions/userAgent.ts
+++ packages/util/src/functions/userAgent.ts
@@ -6,13 +6,13 @@
  */
 export function getUserAgent(): string | null {
 	// Most (if not all) edge environments will have `process` defined. Withing a web browser we'll extract it using `navigator.userAgent`.
-	return typeof process === 'object'
-		? process.release?.name === 'node'
-			? `Node.js/${process.version}`
+	return typeof globalThis.process === 'object'
+		? globalThis.process.release?.name === 'node'
+			? `Node.js/${globalThis.process.version}`
 			: ''
 		: // @ts-expect-error web env
-		typeof window === 'object'
+		typeof globalThis.window === 'object'
 		? // @ts-expect-error web env
-		  window.navigator.userAgent
+		  globalThis.window.navigator.userAgent
 		: null;
 }
