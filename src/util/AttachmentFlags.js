2ac8be09a163d89bd546954739aa1a98a5e333e3
Jaw0r3k <jaw0r3k.g@gmail.com>
2023-08-12 14:29:40 +0200

    feat(Attachment): add flags (#9686)
    Co-authored-by: Almeida <almeidx@pm.me>

---
+++ src/util/AttachmentFlags.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const BitField = require('./BitField');
+
+/**
+ * Data structure that makes it easy to interact with an {@link GuildMember#flags} bitfield.
+ * @extends {BitField}
+ */
+class AttachmentFlags extends BitField {}
+
+/**
+ * @name AttachmentFlags
+ * @kind constructor
+ * @memberof AttachmentFlags
+ * @param {BitFieldResolvable} [bits=0] Bit(s) to read from
+ */
+
+/**
+ * Numeric guild member flags. All available properties:
+ * * `IS_REMIX`
+ * @type {Object}
+ * @see {@link https://discord.com/developers/docs/resources/channel#attachment-object-attachment-structure-attachment-flags}
+ */
+AttachmentFlags.FLAGS = {
+  IS_REMIX: 1 << 2,
+};
+
+/**
+ * Data that can be resolved to give a guild attachment bitfield. This can be:
+ * * A string (see {@link AttachmentFlags.FLAGS})
+ * * A attachment flag
+ * * An instance of AttachmentFlags
+ * * An Array of AttachmentFlagsResolvable
+ * @typedef {string|number|AttachmentFlags|AttachmentFlagsResolvable[]} AttachmentFlagsResolvable
+ */
+
+module.exports = AttachmentFlags;
