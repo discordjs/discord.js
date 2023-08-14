24fbb11ba2f7e8f8f604752159d2053f2cee16ec
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-08-14 18:24:00 +0100

    fix(Action): Do not add the client user as a recipient (#9774)
    fix(Action): do not add the client user as a recipient
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- packages/discord.js/src/client/actions/Action.js
+++ packages/discord.js/src/client/actions/Action.js
@@ -28,8 +28,17 @@ class GenericAction {
   }
 
   getChannel(data) {
-    const payloadData = { recipients: data.recipients ?? [data.author ?? data.user ?? { id: data.user_id }] };
+    const payloadData = {};
     const id = data.channel_id ?? data.id;
+
+    if ('recipients' in data) {
+      payloadData.recipients = data.recipients;
+    } else {
+      // Try to resolve the recipient, but do not add the client user.
+      const recipient = data.author ?? data.user ?? { id: data.user_id };
+      if (recipient.id !== this.client.user.id) payloadData.recipients = [recipient];
+    }
+
     if (id !== undefined) payloadData.id = id;
     if ('guild_id' in data) payloadData.guild_id = data.guild_id;
     if ('last_message_id' in data) payloadData.last_message_id = data.last_message_id;
