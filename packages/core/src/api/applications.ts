7dd3f6d4c55945b36d7ed66a7764170294629ebf
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-08-31 19:14:25 +0100

    docs(ApplicationsAPI): fix `getCurrent()` options description

--- packages/core/src/api/applications.ts
+++ packages/core/src/api/applications.ts
@@ -10,7 +10,7 @@ export class ApplicationsAPI {
 	 * Fetches the application associated with the requesting bot user.
 	 *
 	 * @see {@link https://discord.com/developers/docs/resources/application#get-current-application}
-	 * @param options - The options for editing the application
+	 * @param options - The options for fetching the application
 	 */
 	public async getCurrent({ signal }: Pick<RequestData, 'signal'> = {}) {
 		return this.rest.get(Routes.currentApplication(), { signal }) as Promise<RESTGetCurrentApplicationResult>;
