a1d7ecc7c6104efa1b534c0af0b8fcc3dcfb645f
Qjuh <76154676+Qjuh@users.noreply.github.com>
2023-11-07 19:39:40 +0100

    fix: icon size again

--- apps/website/src/components/documentation/SourceLink.tsx
+++ apps/website/src/components/documentation/SourceLink.tsx
@@ -1,15 +1,17 @@
 import { VscFileCode } from '@react-icons/all-files/vsc/VscFileCode';
 
 export function SourceLink({
+	className,
 	sourceURL,
 	sourceLine,
 }: {
+	readonly className?: string | undefined;
 	readonly sourceLine?: number | undefined;
 	readonly sourceURL?: string | undefined;
 }) {
 	return (
 		<a
-			className="text-2xl text-blurple"
+			className={` text-blurple ${className}`}
 			href={sourceLine ? `${sourceURL}#L${sourceLine}` : sourceURL}
 			rel="external noopener noreferrer"
 			target="_blank"
