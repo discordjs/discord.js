f9177be61b5538ffc0bf9c256088db6a098a4f90
iCrawl <buechler.noel@outlook.com>
2023-11-14 02:04:02 +0100

    fix: don't hardcode discordjs special case

--- apps/website/src/middleware.ts
+++ apps/website/src/middleware.ts
@@ -10,7 +10,7 @@ async function fetchLatestVersion(packageName: string): Promise<string> {
 
 	const { rows } = await sql`select version from documentation where name = ${packageName} order by version desc`;
 
-	return rows.map((row) => row.version).at(packageName === 'discord.js' ? 0 : 1) ?? 'main';
+	return rows.map((row) => row.version).at(1) ?? 'main';
 }
 
 export default async function middleware(request: NextRequest) {
