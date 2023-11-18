5a715706dfe895ad6cf13d3b5e3877c4e13d56c6
Qjuh <76154676+Qjuh@users.noreply.github.com>
2023-11-18 11:44:06 +0100

    fix(website): search index generation (#9980)
    * types: fix links in @deprecated tags
    
    * Merge branch 'main' into fix/deprecated-links-d.ts
    
    * fix: searchIndices
    
    * refactor: apply review suggestions

--- packages/scripts/src/generateIndex.ts
+++ packages/scripts/src/generateIndex.ts
@@ -1,12 +1,17 @@
 import { stat, mkdir, writeFile } from 'node:fs/promises';
 import { join } from 'node:path';
 import { cwd } from 'node:process';
-import type { ApiPackage } from '@discordjs/api-extractor-model';
-import { ApiItem, ApiModel, ApiDeclaredItem, ApiItemContainerMixin, ApiItemKind } from '@discordjs/api-extractor-model';
+import {
+	type ApiItem,
+	ApiPackage,
+	ApiModel,
+	ApiDeclaredItem,
+	ApiItemContainerMixin,
+	ApiItemKind,
+} from '@discordjs/api-extractor-model';
 import { generatePath } from '@discordjs/api-extractor-utils';
-import { DocNodeKind, TSDocConfiguration } from '@microsoft/tsdoc';
+import { DocNodeKind } from '@microsoft/tsdoc';
 import type { DocLinkTag, DocCodeSpan, DocNode, DocParagraph, DocPlainText } from '@microsoft/tsdoc';
-import { TSDocConfigFile } from '@microsoft/tsdoc-config';
 import { request } from 'undici';
 
 export interface MemberJSON {
@@ -33,24 +38,7 @@ export const PACKAGES = [
 let idx = 0;
 
 export function addPackageToModel(model: ApiModel, data: any) {
-	let apiPackage: ApiPackage;
-	if (data.metadata) {
-		const tsdocConfiguration = new TSDocConfiguration();
-		const tsdocConfigFile = TSDocConfigFile.loadFromObject(data.metadata.tsdocConfig);
-		tsdocConfigFile.configureParser(tsdocConfiguration);
-
-		apiPackage = ApiItem.deserialize(data, {
-			apiJsonFilename: '',
-			toolPackage: data.metadata.toolPackage,
-			toolVersion: data.metadata.toolVersion,
-			versionToDeserialize: data.metadata.schemaVersion,
-			tsdocConfiguration,
-		}) as ApiPackage;
-	} else {
-		apiPackage = ApiItem.deserializeDocgen(data, 'discord.js') as ApiPackage;
-	}
-
-	model.addMember(apiPackage);
+	model.addMember(ApiPackage.loadFromJson(data));
 	return model;
 }
 
