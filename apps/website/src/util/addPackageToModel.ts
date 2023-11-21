3d1c884926587d2003a27d4eeab37ca21776f16e
Qjuh <76154676+Qjuh@users.noreply.github.com>
2023-11-21 10:06:13 +0100

    refactor(website): remove unneccessary addPackageToModel function (#9983)
    * types: fix links in @deprecated tags
    
    * Merge branch 'main' into fix/deprecated-links-d.ts
    
    * fix: searchIndices
    
    * refactor: apply review suggestions
    
    * refactor: remove addPackageToModel function
    
    * fix: event links in search index
    
    * fix: wrong overload condition

--- apps/website/src/util/addPackageToModel.ts
+++
@@ -1,6 +0,0 @@
-import { type ApiModel, ApiPackage } from '@discordjs/api-extractor-model';
-
-export const addPackageToModel = (model: ApiModel, data: any) => {
-	model.addMember(ApiPackage.loadFromJson(data));
-	return model;
-};
--- packages/api-extractor-utils/src/parse.ts
+++ packages/api-extractor-utils/src/parse.ts
@@ -11,6 +11,8 @@ import {
 	type Parameter,
 	type ApiFunction,
 	ApiDeclaredItem,
+	type ApiMethod,
+	type ApiMethodSignature,
 } from '@discordjs/api-extractor-model';
 import type { DocNode, DocParagraph, DocPlainText } from '@microsoft/tsdoc';
 import { type Meaning, ModuleSource } from '@microsoft/tsdoc/lib-commonjs/beta/DeclarationReference.js';
@@ -21,6 +23,10 @@ export function findPackage(model: ApiModel, name: string): ApiPackage | undefin
 	return (model.findMembersByName(name)[0] ?? model.findMembersByName(`@discordjs/${name}`)[0]) as
 		| ApiPackage
 		| undefined;
+}
+
+function hasOverloadIndex(item: ApiItem): item is ApiFunction | ApiMethod | ApiMethodSignature {
+	return 'overloadIndex' in item;
 }
 
 export function generatePath(items: readonly ApiItem[], version: string) {
@@ -36,17 +42,19 @@ export function generatePath(items: readonly ApiItem[], version: string) {
 				path += `/${item.displayName}`;
 				break;
 			case ApiItemKind.Function:
-				// eslint-disable-next-line no-case-declarations
-				const functionItem = item as ApiFunction;
-				path += `/${functionItem.displayName}${
-					functionItem.overloadIndex && functionItem.overloadIndex > 1 ? `:${functionItem.overloadIndex}` : ''
+				path += `/${item.displayName}${
+					hasOverloadIndex(item) && item.overloadIndex > 1 ? `:${item.overloadIndex}` : ''
 				}:${item.kind}`;
 				break;
-			case ApiItemKind.Property:
 			case ApiItemKind.Method:
 			case ApiItemKind.MethodSignature:
+				path += `#${item.displayName}${
+					hasOverloadIndex(item) && item.overloadIndex > 1 ? `:${item.overloadIndex}` : ''
+				}`;
+				break;
+			case ApiItemKind.Property:
 			case ApiItemKind.PropertySignature:
-				// TODO: Take overloads into account
+			case ApiItemKind.Event:
 				path += `#${item.displayName}`;
 				break;
 			default:
@@ -55,8 +63,8 @@ export function generatePath(items: readonly ApiItem[], version: string) {
 	}
 
 	return path.includes('@discordjs/')
-		? path.replace(/@discordjs\/(.*)\/(.*)?/, `$1/${version}/$2`)
-		: path.replace(/(.*)\/(.*)?/, `$1/${version}/$2`);
+		? path.replace(/@discordjs\/(?<package>.*)\/(?<member>.*)?/, `$<package>/${version}/$<member>`)
+		: path.replace(/(?<oackage>.*)\/(?<member>.*)?/, `$<package>/${version}/$<member>`);
 }
 
 export function resolveDocComment(item: ApiDeclaredItem) {
