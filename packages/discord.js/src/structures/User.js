e1b80543dcaee3079543be517dbc43cfcdde60c1
Almeida <almeidx@pm.me>
2023-12-03 01:20:28 +0000

    fix(User): check avatar decoration in equals() methods

--- packages/discord.js/src/structures/User.js
+++ packages/discord.js/src/structures/User.js
@@ -310,7 +310,10 @@ class User extends Base {
       this.avatar === user.avatar &&
       this.flags?.bitfield === user.flags?.bitfield &&
       this.banner === user.banner &&
-      this.accentColor === user.accentColor
+      this.accentColor === user.accentColor &&
+      this.avatarDecoration === user.avatarDecoration &&
+      this.avatarDecorationData?.asset === user.avatarDecorationData?.asset &&
+      this.avatarDecorationData?.skuId === user.avatarDecorationData?.skuId
     );
   }
 
@@ -330,7 +333,12 @@ class User extends Base {
       this.avatar === user.avatar &&
       this.flags?.bitfield === user.public_flags &&
       ('banner' in user ? this.banner === user.banner : true) &&
-      ('accent_color' in user ? this.accentColor === user.accent_color : true)
+      ('accent_color' in user ? this.accentColor === user.accent_color : true) &&
+      ('avatar_decoration' in user ? this.avatarDecoration === user.avatar_decoration : true) &&
+      ('avatar_decoration_data' in user
+        ? this.avatarDecorationData?.asset === user.avatar_decoration_data?.asset &&
+          this.avatarDecorationData?.skuId === user.avatar_decoration_data?.sku_id
+        : true)
     );
   }
 
