3dcc2e60748d666658f51ead564a1846b7d36197
Danial Raza <danialrazafb@gmail.com>
2023-12-29 17:10:53 +0100

    docs(Gateway): update link to `WebSocketManager`

--- packages/core/src/Gateway.ts
+++ packages/core/src/Gateway.ts
@@ -5,7 +5,7 @@ import type { GatewaySendPayload } from 'discord-api-types/v10';
 /**
  * Gateway-like structure that can be used to interact with an actual WebSocket connection.
  * You can provide a custom implementation, useful for running a message broker between your app and your gateway,
- * or you can simply use the {@link https://discord.js.org/docs/packages/ws/stable/WebSocketManager:Class | WebSocketManager}
+ * or you can simply use the {@link @discordjs/ws#(WebSocketManager:class)}.
  */
 export interface Gateway {
 	getShardCount(): Awaitable<number>;
