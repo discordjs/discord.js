c928a7a84d9a69793d6ec1d9b5180ed2bf75d25b
Jeroen Claassens <support@favware.tech>
2024-05-20 17:58:24 +0200

    feat: update to @sapphire/shapeshift v4

--- packages/builders/src/messages/embed/Assertions.ts
+++ packages/builders/src/messages/embed/Assertions.ts
@@ -2,17 +2,19 @@ import { s } from '@sapphire/shapeshift';
 import type { APIEmbedField } from 'discord-api-types/v10';
 import { isValidationEnabled } from '../../util/validation.js';
 
-export const fieldNamePredicate = s.string
+export const fieldNamePredicate = s
+	.string()
 	.lengthGreaterThanOrEqual(1)
 	.lengthLessThanOrEqual(256)
 	.setValidationEnabled(isValidationEnabled);
 
-export const fieldValuePredicate = s.string
+export const fieldValuePredicate = s
+	.string()
 	.lengthGreaterThanOrEqual(1)
 	.lengthLessThanOrEqual(1_024)
 	.setValidationEnabled(isValidationEnabled);
 
-export const fieldInlinePredicate = s.boolean.optional;
+export const fieldInlinePredicate = s.boolean().optional();
 
 export const embedFieldPredicate = s
 	.object({
@@ -22,27 +24,31 @@ export const embedFieldPredicate = s
 	})
 	.setValidationEnabled(isValidationEnabled);
 
-export const embedFieldsArrayPredicate = embedFieldPredicate.array.setValidationEnabled(isValidationEnabled);
+export const embedFieldsArrayPredicate = embedFieldPredicate.array().setValidationEnabled(isValidationEnabled);
 
-export const fieldLengthPredicate = s.number.lessThanOrEqual(25).setValidationEnabled(isValidationEnabled);
+export const fieldLengthPredicate = s.number().lessThanOrEqual(25).setValidationEnabled(isValidationEnabled);
 
 export function validateFieldLength(amountAdding: number, fields?: APIEmbedField[]): void {
 	fieldLengthPredicate.parse((fields?.length ?? 0) + amountAdding);
 }
 
-export const authorNamePredicate = fieldNamePredicate.nullable.setValidationEnabled(isValidationEnabled);
+export const authorNamePredicate = fieldNamePredicate.nullable().setValidationEnabled(isValidationEnabled);
 
-export const imageURLPredicate = s.string
+export const imageURLPredicate = s
+	.string()
 	.url({
 		allowedProtocols: ['http:', 'https:', 'attachment:'],
 	})
-	.nullish.setValidationEnabled(isValidationEnabled);
+	.nullish()
+	.setValidationEnabled(isValidationEnabled);
 
-export const urlPredicate = s.string
+export const urlPredicate = s
+	.string()
 	.url({
 		allowedProtocols: ['http:', 'https:'],
 	})
-	.nullish.setValidationEnabled(isValidationEnabled);
+	.nullish()
+	.setValidationEnabled(isValidationEnabled);
 
 export const embedAuthorPredicate = s
 	.object({
@@ -52,25 +58,34 @@ export const embedAuthorPredicate = s
 	})
 	.setValidationEnabled(isValidationEnabled);
 
-export const RGBPredicate = s.number.int
+export const RGBPredicate = s
+	.number()
+	.int()
 	.greaterThanOrEqual(0)
 	.lessThanOrEqual(255)
 	.setValidationEnabled(isValidationEnabled);
-export const colorPredicate = s.number.int
+export const colorPredicate = s
+	.number()
+	.int()
 	.greaterThanOrEqual(0)
 	.lessThanOrEqual(0xffffff)
 	.or(s.tuple([RGBPredicate, RGBPredicate, RGBPredicate]))
-	.nullable.setValidationEnabled(isValidationEnabled);
+	.nullable()
+	.setValidationEnabled(isValidationEnabled);
 
-export const descriptionPredicate = s.string
+export const descriptionPredicate = s
+	.string()
 	.lengthGreaterThanOrEqual(1)
 	.lengthLessThanOrEqual(4_096)
-	.nullable.setValidationEnabled(isValidationEnabled);
+	.nullable()
+	.setValidationEnabled(isValidationEnabled);
 
-export const footerTextPredicate = s.string
+export const footerTextPredicate = s
+	.string()
 	.lengthGreaterThanOrEqual(1)
 	.lengthLessThanOrEqual(2_048)
-	.nullable.setValidationEnabled(isValidationEnabled);
+	.nullable()
+	.setValidationEnabled(isValidationEnabled);
 
 export const embedFooterPredicate = s
 	.object({
@@ -79,6 +94,6 @@ export const embedFooterPredicate = s
 	})
 	.setValidationEnabled(isValidationEnabled);
 
-export const timestampPredicate = s.union(s.number, s.date).nullable.setValidationEnabled(isValidationEnabled);
+export const timestampPredicate = s.union([s.number(), s.date()]).nullable().setValidationEnabled(isValidationEnabled);
 
-export const titlePredicate = fieldNamePredicate.nullable.setValidationEnabled(isValidationEnabled);
+export const titlePredicate = fieldNamePredicate.nullable().setValidationEnabled(isValidationEnabled);
