1d5795b2810e7f5bbe7a96f8fa2d4d1cc0eb76a2
LEODAS07 <tigerhell82@gmail.com>
2024-06-26 09:49:29 +0530

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,10 @@
+```javascript
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', (!) => {
+    console.log('Bot is online!');
+});
+
+client.login('MTI1NTE3NDk1MzY3NTAwMTkzNw.GGhZNz.vJL7-UvRqstw4BsImdjyPoV3iAoSOmOnlJijeo);
+```
