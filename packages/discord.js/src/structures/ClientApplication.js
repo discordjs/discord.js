4c4bd4b59209e9c0c4b246f6ac1cea4658a4c26f
Danial Raza <danialrazafb@gmail.com>
2024-07-20 07:21:44 +0000

    feat: application emojis

--- packages/discord.js/src/structures/ClientApplication.js
+++ packages/discord.js/src/structures/ClientApplication.js
@@ -7,6 +7,7 @@ const { SKU } = require('./SKU');
 const Team = require('./Team');
 const Application = require('./interfaces/Application');
 const ApplicationCommandManager = require('../managers/ApplicationCommandManager');
+const ApplicationEmojiManager = require('../managers/ApplicationEmojiManager');
 const { EntitlementManager } = require('../managers/EntitlementManager');
 const ApplicationFlagsBitField = require('../util/ApplicationFlagsBitField');
 const { resolveImage } = require('../util/DataResolver');
@@ -31,6 +32,12 @@ class ClientApplication extends Application {
      * @type {ApplicationCommandManager}
      */
     this.commands = new ApplicationCommandManager(this.client);
+
+    /**
+     * The application emoji manager for this application
+     * @type {ApplicationEmojiManager}
+     */
+    this.emojis = new ApplicationEmojiManager(this);
 
     /**
      * The entitlement manager for this application
