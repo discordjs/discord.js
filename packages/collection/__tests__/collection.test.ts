02ec7698b79f74bd8e88ac15ddc6846e6bb4a66b
Renegade334 <contact.9a5d6388@renegade334.me.uk>
2024-07-22 15:32:03 +0100

    test: explicit expect() types

--- packages/collection/__tests__/collection.test.ts
+++ packages/collection/__tests__/collection.test.ts
@@ -715,7 +715,7 @@ describe('reduce() tests', () => {
 
 	test('reduce empty collection with initial value', () => {
 		const coll = createCollection();
-		expect(coll.reduce((a, x) => a + x, 0)).toStrictEqual(0);
+		expect<number>(coll.reduce((a, x) => a + x, 0)).toStrictEqual(0);
 	});
 
 	test('reduce empty collection without initial value', () => {
@@ -738,17 +738,17 @@ describe('reduceRight() tests', () => {
 
 	test('reduce collection into a single value with initial value', () => {
 		const sum = coll.reduceRight((a, x) => a + x, 0);
-		expect(sum).toStrictEqual(6);
+		expect<number>(sum).toStrictEqual(6);
 	});
 
 	test('reduce collection into a single value without initial value', () => {
 		const sum = coll.reduceRight((a, x) => a + x);
-		expect(sum).toStrictEqual(6);
+		expect<number>(sum).toStrictEqual(6);
 	});
 
 	test('reduce empty collection with initial value', () => {
 		const coll = createCollection();
-		expect(coll.reduceRight((a, x) => a + x, 0)).toStrictEqual(0);
+		expect<number>(coll.reduceRight((a, x) => a + x, 0)).toStrictEqual(0);
 	});
 
 	test('reduce empty collection without initial value', () => {
