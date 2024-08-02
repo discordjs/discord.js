e308b8e6d2d6bd591cb18b5ef460c210317771e3
Qjuh <76154676+Qjuh@users.noreply.github.com>
2024-08-02 09:11:39 +0200

    fix: tests

--- packages/discord.js/typings/index.test-d.ts
+++ packages/discord.js/typings/index.test-d.ts
@@ -665,7 +665,7 @@ client.on('presenceUpdate', (oldPresence, { client }) => {
 declare const slashCommandBuilder: SlashCommandBuilder;
 declare const contextMenuCommandBuilder: ContextMenuCommandBuilder;
 
-client.on('ready', async client => {
+client.on('clientReady', async client => {
   expectType<Client<true>>(client);
   console.log(`Client is logged in as ${client.user.tag} and ready!`);
 
