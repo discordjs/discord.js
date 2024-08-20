9907ff915e7c72e7e980d68bf005763a3aacad1c
Naiyar <137700126+imnaiyar@users.noreply.github.com>
2024-08-20 15:32:17 +0530

    feat(VoiceState): add methods for fetching voice state (#10442)
    * feat(VoiceState): add methods for fetching voice state
    
    * fix: links to new methods
    
    * chore: remove unused import
    
    * chore: use member id
    
    * chore: requested changes
    
    * chore: '@me' as fetch param
    
    * chore: add ediUserVoiceState return type
    
    * refactor: redirect function calls to VoiceAPI
    
    ---------
    
    Co-authored-by: Almeida <almeidx@pm.me>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- packages/discord.js/typings/index.d.ts
+++ packages/discord.js/typings/index.d.ts
@@ -3625,6 +3625,7 @@ export class VoiceState extends Base {
   public setRequestToSpeak(request?: boolean): Promise<this>;
   public setSuppressed(suppressed?: boolean): Promise<this>;
   public edit(options: VoiceStateEditOptions): Promise<this>;
+  public fetch(force?: boolean): Promise<VoiceState>;
 }
 
 // tslint:disable-next-line no-empty-interface
@@ -4627,6 +4628,7 @@ export class UserManager extends CachedManager<Snowflake, User, UserResolvable> 
 export class VoiceStateManager extends CachedManager<Snowflake, VoiceState, typeof VoiceState> {
   private constructor(guild: Guild, iterable?: Iterable<RawVoiceStateData>);
   public guild: Guild;
+  public fetch(member: GuildMemberResolvable | '@me', options?: BaseFetchOptions): Promise<VoiceState>;
 }
 
 //#endregion
