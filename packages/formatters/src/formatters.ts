3dc3b51cc45d943d746f68a4e84217cae288b3d7
Almeida <github@almeidx.dev>
2024-09-04 18:39:02 +0100

    docs: remove locale

--- packages/formatters/src/formatters.ts
+++ packages/formatters/src/formatters.ts
@@ -765,7 +765,7 @@ export enum GuildNavigationMentions {
 	 */
 	Guide = '<id:guide>',
 	/**
-	 * {@link https://support.discord.com/hc/en-us/articles/10388356626711 | Linked Roles} tab.
+	 * {@link https://support.discord.com/hc/articles/10388356626711 | Linked Roles} tab.
 	 */
 	LinkedRoles = '<id:linked-roles>',
 }
