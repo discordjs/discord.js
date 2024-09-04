f331b8fa5909f77dfc0eb99014ae381e9c5bcf74
Danial Raza <danialrazafb@gmail.com>
2024-09-04 21:29:42 +0000

    docs: update discord documentation links

--- packages/discord.js/src/structures/Message.js
+++ packages/discord.js/src/structures/Message.js
@@ -359,7 +359,7 @@ class Message extends Base {
      * * {@link MessageType.ChannelFollowAdd}
      * * {@link MessageType.Reply}
      * * {@link MessageType.ThreadStarterMessage}
-     * @see {@link https://discord.com/developers/docs/resources/channel#message-types}
+     * @see {@link https://discord.com/developers/docs/resources/message#message-object-message-types}
      * @typedef {Object} MessageReference
      * @property {Snowflake} channelId The channel id that was referenced
      * @property {Snowflake|undefined} guildId The guild id that was referenced
