c3bd0099d9ffa9fe809207828c1b07a4689940e2
nyapat <73502164+nyapat@users.noreply.github.com>
2024-10-07 20:47:14 +1100

    fix(test): fix decryption tests

--- packages/voice/__tests__/Secretbox.test.ts
+++ packages/voice/__tests__/Secretbox.test.ts
@@ -1,9 +1,16 @@
 import { test, expect, vitest } from 'vitest';
 import { methods } from '../src/util/Secretbox';
 
+async function wait(ms: number) {
+	/* eslint-disable no-promise-executor-return */
+	return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 vitest.mock('@stablelib/xchacha20poly1305');
 
-// #todo: fix test: use vitest
-test('Does not throw error with a package installed', () => {
-	//expect(() => methods.crypto_aead_xchacha20poly1305_ietf_encrypt()).not.toThrowError();
+test('Does not throw error with a package installed', async () => {
+	// The async loop in Secretbox will not have finished importing unless we wait
+	await wait(100);
+
+	expect(() => methods.crypto_aead_xchacha20poly1305_ietf_decrypt()).not.toThrowError();
 });
