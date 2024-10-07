846b594b204f206e39caf7b9b6dac1294786788f
nyapat <73502164+nyapat@users.noreply.github.com>
2024-10-07 20:49:26 +1100

    refactor: remove unused methods, fix variable names

--- packages/voice/src/util/Secretbox.ts
+++ packages/voice/src/util/Secretbox.ts
@@ -1,18 +1,14 @@
 import { Buffer } from 'node:buffer';
 
 interface Methods {
-	close(opusPacket: Buffer, nonce: Buffer, secretKey: Uint8Array): Buffer;
-	open(buffer: Buffer, nonce: Buffer, secretKey: Uint8Array): Buffer | null;
-	random(bytes: number, nonce: Buffer): Buffer;
-
-	crypto_aead_xchacha20poly1305_ietf_encrypt(
-		plaintext: Buffer,
+	crypto_aead_xchacha20poly1305_ietf_decrypt(
+		cipherText: Buffer,
 		additionalData: Buffer,
 		nonce: Buffer,
 		key: ArrayBufferLike,
 	): Buffer;
-	crypto_aead_xchacha20poly1305_ietf_decrypt(
-		cipherText: Buffer,
+	crypto_aead_xchacha20poly1305_ietf_encrypt(
+		plaintext: Buffer,
 		additionalData: Buffer,
 		nonce: Buffer,
 		key: ArrayBufferLike,
@@ -21,34 +17,6 @@ interface Methods {
 
 const libs = {
 	'sodium-native': (sodium: any): Methods => ({
-		open: (buffer: Buffer, nonce: Buffer, secretKey: Uint8Array) => {
-			if (buffer) {
-				const output = Buffer.allocUnsafe(buffer.length - sodium.crypto_box_MACBYTES);
-				if (sodium.crypto_secretbox_open_easy(output, buffer, nonce, secretKey)) return output;
-			}
-
-			return null;
-		},
-		close: (opusPacket: Buffer, nonce: Buffer, secretKey: Uint8Array) => {
-			const output = Buffer.allocUnsafe(opusPacket.length + sodium.crypto_box_MACBYTES);
-			sodium.crypto_secretbox_easy(output, opusPacket, nonce, secretKey);
-			return output;
-		},
-		random: (num: number, buffer: Buffer = Buffer.allocUnsafe(num)) => {
-			sodium.randombytes_buf(buffer);
-			return buffer;
-		},
-
-		crypto_aead_xchacha20poly1305_ietf_encrypt: (
-			plaintext: Buffer,
-			additionalData: Buffer,
-			nonce: Buffer,
-			key: ArrayBufferLike,
-		) => {
-			const cipherText = Buffer.alloc(plaintext.length + sodium.crypto_aead_xchacha20poly1305_ietf_ABYTES);
-			sodium.crypto_aead_xchacha20poly1305_ietf_encrypt(cipherText, plaintext, additionalData, null, nonce, key);
-			return cipherText;
-		},
 		crypto_aead_xchacha20poly1305_ietf_decrypt: (
 			cipherText: Buffer,
 			additionalData: Buffer,
@@ -59,23 +27,18 @@ const libs = {
 			sodium.crypto_aead_xchacha20poly1305_ietf_decrypt(message, null, cipherText, additionalData, nonce, key);
 			return message;
 		},
-	}),
-	sodium: (sodium: any): Methods => ({
-		open: sodium.api.crypto_secretbox_open_easy,
-		close: sodium.api.crypto_secretbox_easy,
-		random: (num: number, buffer: Buffer = Buffer.allocUnsafe(num)) => {
-			sodium.api.randombytes_buf(buffer);
-			return buffer;
-		},
-
 		crypto_aead_xchacha20poly1305_ietf_encrypt: (
 			plaintext: Buffer,
 			additionalData: Buffer,
 			nonce: Buffer,
 			key: ArrayBufferLike,
 		) => {
-			return sodium.api.crypto_aead_xchacha20poly1305_ietf_encrypt(plaintext, additionalData, null, nonce, key);
+			const cipherText = Buffer.alloc(plaintext.length + sodium.crypto_aead_xchacha20poly1305_ietf_ABYTES);
+			sodium.crypto_aead_xchacha20poly1305_ietf_encrypt(cipherText, plaintext, additionalData, null, nonce, key);
+			return cipherText;
 		},
+	}),
+	sodium: (sodium: any): Methods => ({
 		crypto_aead_xchacha20poly1305_ietf_decrypt: (
 			cipherText: Buffer,
 			additionalData: Buffer,
@@ -84,20 +47,16 @@ const libs = {
 		) => {
 			return sodium.api.crypto_aead_xchacha20poly1305_ietf_decrypt(cipherText, additionalData, null, nonce, key);
 		},
-	}),
-	'libsodium-wrappers': (sodium: any): Methods => ({
-		open: sodium.crypto_secretbox_open_easy,
-		close: sodium.crypto_secretbox_easy,
-		random: sodium.randombytes_buf,
-
 		crypto_aead_xchacha20poly1305_ietf_encrypt: (
 			plaintext: Buffer,
 			additionalData: Buffer,
 			nonce: Buffer,
 			key: ArrayBufferLike,
 		) => {
-			return sodium.crypto_aead_xchacha20poly1305_ietf_encrypt(plaintext, additionalData, null, nonce, key);
+			return sodium.api.crypto_aead_xchacha20poly1305_ietf_encrypt(plaintext, additionalData, null, nonce, key);
 		},
+	}),
+	'libsodium-wrappers': (sodium: any): Methods => ({
 		crypto_aead_xchacha20poly1305_ietf_decrypt: (
 			cipherText: Buffer,
 			additionalData: Buffer,
@@ -106,20 +65,23 @@ const libs = {
 		) => {
 			return sodium.crypto_aead_xchacha20poly1305_ietf_decrypt(null, cipherText, additionalData, nonce, key);
 		},
+		crypto_aead_xchacha20poly1305_ietf_encrypt: (
+			plaintext: Buffer,
+			additionalData: Buffer,
+			nonce: Buffer,
+			key: ArrayBufferLike,
+		) => {
+			return sodium.crypto_aead_xchacha20poly1305_ietf_encrypt(plaintext, additionalData, null, nonce, key);
+		},
 	}),
 	'@stablelib/xchacha20poly1305': (stablelib: any): Methods => ({
-		// functions below don't actually exist i just need them here until i remove open/close/random since they arent needed anymore
-		open: stablelib.open,
-		close: stablelib.secretbox,
-		random: stablelib.randomBytes,
-
-		crypto_aead_xchacha20poly1305_ietf_encrypt(cipherText, additionalData, nonce, key) {
-			const crypto = new stablelib.XChaCha20Poly1305(key);
-			return crypto.seal(nonce, cipherText, additionalData);
-		},
 		crypto_aead_xchacha20poly1305_ietf_decrypt(plaintext, additionalData, nonce, key) {
 			const crypto = new stablelib.XChaCha20Poly1305(key);
 			return crypto.open(nonce, plaintext, additionalData);
+		},
+		crypto_aead_xchacha20poly1305_ietf_encrypt(cipherText, additionalData, nonce, key) {
+			const crypto = new stablelib.XChaCha20Poly1305(key);
+			return crypto.seal(nonce, cipherText, additionalData);
 		},
 	}),
 } as const;
@@ -133,10 +95,6 @@ const fallbackError = () => {
 };
 
 const methods: Methods = {
-	open: fallbackError,
-	close: fallbackError,
-	random: fallbackError,
-
 	crypto_aead_xchacha20poly1305_ietf_encrypt: fallbackError,
 	crypto_aead_xchacha20poly1305_ietf_decrypt: fallbackError,
 };
@@ -144,7 +102,6 @@ const methods: Methods = {
 void (async () => {
 	for (const libName of Object.keys(libs) as (keyof typeof libs)[]) {
 		try {
-			// eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires
 			const lib = await import(libName);
 			if (libName === 'libsodium-wrappers' && lib.ready) await lib.ready;
 			Object.assign(methods, libs[libName](lib));
