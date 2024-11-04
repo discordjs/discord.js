1fd662629d24521426c581cd89025e17820ec3e9
Danial Raza <danialrazafb@gmail.com>
2024-11-04 11:48:41 +0100

    feat: add subscriptions (#10486)
    * feat: add subscriptions
    
    * docs: requested changes
    
    Co-authored-by: Almeida <github@almeidx.dev>
    
    ---------
    
    Co-authored-by: Almeida <github@almeidx.dev>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- packages/core/src/client.ts
+++ packages/core/src/client.ts
@@ -66,6 +66,9 @@ import {
 	type GatewayStageInstanceCreateDispatchData,
 	type GatewayStageInstanceDeleteDispatchData,
 	type GatewayStageInstanceUpdateDispatchData,
+	type GatewaySubscriptionCreateDispatchData,
+	type GatewaySubscriptionDeleteDispatchData,
+	type GatewaySubscriptionUpdateDispatchData,
 	type GatewayThreadCreateDispatchData,
 	type GatewayThreadDeleteDispatchData,
 	type GatewayThreadListSyncDispatchData,
@@ -163,6 +166,9 @@ export interface MappedEvents {
 	[GatewayDispatchEvents.StageInstanceCreate]: [ToEventProps<GatewayStageInstanceCreateDispatchData>];
 	[GatewayDispatchEvents.StageInstanceDelete]: [ToEventProps<GatewayStageInstanceDeleteDispatchData>];
 	[GatewayDispatchEvents.StageInstanceUpdate]: [ToEventProps<GatewayStageInstanceUpdateDispatchData>];
+	[GatewayDispatchEvents.SubscriptionCreate]: [ToEventProps<GatewaySubscriptionCreateDispatchData>];
+	[GatewayDispatchEvents.SubscriptionDelete]: [ToEventProps<GatewaySubscriptionDeleteDispatchData>];
+	[GatewayDispatchEvents.SubscriptionUpdate]: [ToEventProps<GatewaySubscriptionUpdateDispatchData>];
 	[GatewayDispatchEvents.ThreadCreate]: [ToEventProps<GatewayThreadCreateDispatchData>];
 	[GatewayDispatchEvents.ThreadDelete]: [ToEventProps<GatewayThreadDeleteDispatchData>];
 	[GatewayDispatchEvents.ThreadListSync]: [ToEventProps<GatewayThreadListSyncDispatchData>];
