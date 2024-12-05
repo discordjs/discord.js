7b2a2e3a154afd69ff892da615ea75c46730f226
Naiyar <137700126+imnaiyar@users.noreply.github.com>
2024-12-06 00:22:02 +0530

    feat(ClientApplication): add webhook events (#10588)
    * feat(ClientApplication): add webhook events
    
    * refactor: update enum names and add external types
    
    * docs(APITypes): reorder
    
    * chore: requested changes
    
    * chore: requested changes
    
    * docs: remove redundancy
    
    * Update ClientApplication.js
    
    ---------
    
    Co-authored-by: Jiralite <33201955+Jiralite@users.noreply.github.com>
    Co-authored-by: Almeida <github@almeidx.dev>

--- packages/discord.js/typings/index.d.ts
+++ packages/discord.js/typings/index.d.ts
@@ -194,6 +194,8 @@ import {
   ApplicationWebhookEventType,
   GatewaySendPayload,
   GatewayDispatchPayload,
+  ApplicationWebhookEventStatus,
+  ApplicationWebhookEventType,
   RESTPostAPIInteractionCallbackWithResponseResult,
   RESTAPIInteractionCallbackObject,
   RESTAPIInteractionCallbackResourceObject,
