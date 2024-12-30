7f1206b2aaa007c6b78bec8f6f2ceb1a043b90ff
Apokalypt <58733915+Apokalypt@users.noreply.github.com>
2024-12-31 00:12:28 +0100

    fix(ThreadChannel): do not rely on current cached value while editing

--- packages/discord.js/src/structures/ThreadChannel.js
+++ packages/discord.js/src/structures/ThreadChannel.js
@@ -352,7 +352,7 @@ class ThreadChannel extends BaseChannel {
   async edit(options) {
     const newData = await this.client.rest.patch(Routes.channel(this.id), {
       body: {
-        name: (options.name ?? this.name).trim(),
+        name: options.name?.trim(),
         archived: options.archived,
         auto_archive_duration: options.autoArchiveDuration,
         rate_limit_per_user: options.rateLimitPerUser,
