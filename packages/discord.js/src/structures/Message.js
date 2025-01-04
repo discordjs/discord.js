46bf8f0146b67d7c480a3512ade1edbfb16e7a26
Jiralite <33201955+Jiralite@users.noreply.github.com>
2025-01-04 17:19:56 +0000

    fix(Message): Ensure channel is defined for clean content (#10681)
    fix(Message): ensure channel is defined for clean content
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- packages/discord.js/src/structures/Message.js
+++ packages/discord.js/src/structures/Message.js
@@ -590,7 +590,7 @@ class Message extends Base {
    */
   get cleanContent() {
     // eslint-disable-next-line eqeqeq
-    return this.content != null ? cleanContent(this.content, this.channel) : null;
+    return this.content != null && this.channel ? cleanContent(this.content, this.channel) : null;
   }
 
   /**
