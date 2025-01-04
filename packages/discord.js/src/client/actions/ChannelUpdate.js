dc440f226a1ac5ac36cbfa28b3dc58d2549b431d
Apokalypt <58733915+Apokalypt@users.noreply.github.com>
2025-01-05 00:32:59 +0100

    fix(ChannelUpdateAction): emit client event in action to not miss change

--- packages/discord.js/src/client/actions/ChannelUpdate.js
+++ packages/discord.js/src/client/actions/ChannelUpdate.js
@@ -2,6 +2,7 @@
 
 const Action = require('./Action');
 const { createChannel } = require('../../util/Channels');
+const Events = require('../../util/Events');
 
 class ChannelUpdateAction extends Action {
   handle(data) {
@@ -25,6 +26,24 @@ class ChannelUpdateAction extends Action {
 
         channel = newChannel;
         this.client.channels.cache.set(channel.id, channel);
+      }
+
+      if (channel.isThread()) {
+        /**
+         * Emitted whenever a thread is updated - e.g. name change, archive state change, locked state change.
+         * @event Client#threadUpdate
+         * @param {ThreadChannel} oldThread The thread before the update
+         * @param {ThreadChannel} newThread The thread after the update
+         */
+        client.emit(Events.ThreadUpdate, old, channel);
+      } else {
+        /**
+         * Emitted whenever a channel is updated - e.g. name change, topic change, channel type change.
+         * @event Client#channelUpdate
+         * @param {DMChannel|GuildChannel} oldChannel The channel before the update
+         * @param {DMChannel|GuildChannel} newChannel The channel after the update
+         */
+        client.emit(Events.ChannelUpdate, old, channel);
       }
 
       return {
