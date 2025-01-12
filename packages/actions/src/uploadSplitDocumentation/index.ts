3c699cd5feb8b9b1219768bd30cc1dd61eb6d0a0
Chai Kohen <chaikohen@gmail.com>
2025-01-11 17:24:22 -0800

    fix: set failed on missing uploads

--- packages/actions/src/uploadSplitDocumentation/index.ts
+++ packages/actions/src/uploadSplitDocumentation/index.ts
@@ -2,7 +2,7 @@ import { readFile } from 'node:fs/promises';
 import { basename, dirname, relative, sep } from 'node:path';
 import { cwd } from 'node:process';
 import { setTimeout as sleep } from 'node:timers/promises';
-import { getInput } from '@actions/core';
+import { setFailed, getInput } from '@actions/core';
 import { create } from '@actions/glob';
 import { put } from '@vercel/blob';
 import PQueue from 'p-queue';
@@ -12,6 +12,7 @@ const version = getInput('version') || 'main';
 
 const queue = new PQueue({ concurrency: 10, interval: 60_000, intervalCap: 1_000 });
 const promises = [];
+const failedUploads: string[] = [];
 
 const globber = await create(`packages/${pkg}/docs/${pkg}/split/*.api.json`);
 console.log('Glob: ', await globber.glob());
@@ -33,6 +34,7 @@ for await (const file of globber.globGenerator()) {
 					} catch (error) {
 						if (retries > 3) {
 							console.error(`Could not upload ${file} after 3 retries`, error);
+							failedUploads.push(name);
 							return;
 						}
 
@@ -41,6 +43,7 @@ for await (const file of globber.globGenerator()) {
 							return upload(retries + 1);
 						} else {
 							console.error(`Could not upload ${file}`, error);
+							failedUploads.push(name);
 						}
 					}
 				}
@@ -55,6 +58,9 @@ for await (const file of globber.globGenerator()) {
 
 try {
 	await Promise.all(promises);
+	if (failedUploads.length) {
+		setFailed(`Failed to upload ${failedUploads.length} files: ${failedUploads.join(', ')}`);
+	}
 } catch (error) {
 	console.log(error);
 }
