18ab0cf62a3ed86b5b6256b882e04d18eeb5e956
Almeida <github@almeidx.dev>
2025-01-13 11:29:57 +0000

    refactor(actions): removed unnecessary actions (#10666)
    Removed actions that were only being used in their
    respective websocket handlers. The remaining
    actions either were either being used elsewhere or
    were using methods from the `GenericAction` class.
    
    Co-authored-by: Vlad Frangu <me@vladfrangu.dev>

--- packages/discord.js/src/client/websocket/handlers/ENTITLEMENT_CREATE.js
+++ packages/discord.js/src/client/websocket/handlers/ENTITLEMENT_CREATE.js
@@ -1,5 +1,14 @@
 'use strict';
 
-module.exports = (client, packet) => {
-  client.actions.EntitlementCreate.handle(packet.d);
+const Events = require('../../../util/Events.js');
+
+module.exports = (client, { d: data }) => {
+  const entitlement = client.application.entitlements._add(data);
+
+  /**
+   * Emitted whenever an entitlement is created.
+   * @event Client#entitlementCreate
+   * @param {Entitlement} entitlement The entitlement that was created
+   */
+  client.emit(Events.EntitlementCreate, entitlement);
 };
