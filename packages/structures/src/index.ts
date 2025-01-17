3fa6880304053c8cede81f95d99591b7300eb132
ckohen <chaikohen@gmail.com>
2025-01-17 04:12:07 -0800

    fix(structures): data optimization call behavior from perf testing

--- packages/structures/src/index.ts
+++ packages/structures/src/index.ts
@@ -1,4 +1,6 @@
+export * from './channels/index.js';
 export * from './invites/index.js';
 export * from './users/index.js';
 export * from './Structure.js';
 export * from './Mixin.js';
+export * from './utils/optimization.js';
