f1a3ebfbeee2eee7e9a98a1c8db537341afaba98
Vlad Frangu <me@vladfrangu.dev>
2025-01-19 00:56:00 +0200

    types: remove fields that cannot be set by the client

--- packages/discord.js/typings/index.d.ts
+++ packages/discord.js/typings/index.d.ts
@@ -6398,8 +6398,6 @@ type BaseGuildScheduledEventRecurrenceRuleOptions<
   Extra extends {},
 > = {
   startAt: DateResolvable;
-  endAt?: DateResolvable;
-  count?: number;
   interval: number;
   frequency: Frequency;
 } & Extra;
