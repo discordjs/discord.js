c6e16c36758a789f3167179bedeac759fddf7943
Danial Raza <danialrazafb@gmail.com>
2025-01-18 08:38:00 +0100

    types(ThreadOnlyChannel): remove incorrect `messages` property (#10708)
    * types(ThreadOnlyChannel): remove incorrect `messages` property
    
    Co-authored-by: TÆMBØ <TAEMBO@users.noreply.github.com>
    
    * test: t e s t s
    
    * test: revamp tests
    
    ---------
    
    Co-authored-by: TÆMBØ <TAEMBO@users.noreply.github.com>
    Co-authored-by: Jiralite <33201955+Jiralite@users.noreply.github.com>

--- packages/discord.js/typings/index.d.ts
+++ packages/discord.js/typings/index.d.ts
@@ -2653,6 +2653,7 @@ export interface ThreadOnlyChannel
     | 'awaitMessages'
     | 'createMessageComponentCollector'
     | 'awaitMessageComponent'
+    | 'messages'
   > {}
 export abstract class ThreadOnlyChannel extends GuildChannel {
   public type: ChannelType.GuildForum | ChannelType.GuildMedia;
