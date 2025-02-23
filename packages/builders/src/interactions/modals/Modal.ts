0bf9f296f27409c0933d0549d0f5f21acfb111e5
Vlad Frangu <me@vladfrangu.dev>
2025-02-23 16:52:46 +0200

    chore: bump alpha version of dtypes

--- packages/builders/src/interactions/modals/Modal.ts
+++ packages/builders/src/interactions/modals/Modal.ts
@@ -3,7 +3,7 @@
 import type { JSONEncodable } from '@discordjs/util';
 import type {
 	APIActionRowComponent,
-	APIModalActionRowComponent,
+	APIComponentInModalActionRow,
 	APIModalInteractionResponseCallbackData,
 } from 'discord-api-types/v10';
 import { ActionRowBuilder } from '../../components/ActionRow.js';
@@ -73,7 +73,7 @@ export class ModalBuilder implements JSONEncodable<APIModalInteractionResponseCa
 	public addActionRows(
 		...components: RestOrArray<
 			| ActionRowBuilder
-			| APIActionRowComponent<APIModalActionRowComponent>
+			| APIActionRowComponent<APIComponentInModalActionRow>
 			| ((builder: ActionRowBuilder) => ActionRowBuilder)
 		>
 	) {
@@ -93,7 +93,7 @@ export class ModalBuilder implements JSONEncodable<APIModalInteractionResponseCa
 	public setActionRows(
 		...components: RestOrArray<
 			| ActionRowBuilder
-			| APIActionRowComponent<APIModalActionRowComponent>
+			| APIActionRowComponent<APIComponentInModalActionRow>
 			| ((builder: ActionRowBuilder) => ActionRowBuilder)
 		>
 	) {
@@ -137,7 +137,7 @@ export class ModalBuilder implements JSONEncodable<APIModalInteractionResponseCa
 		deleteCount: number,
 		...rows: (
 			| ActionRowBuilder
-			| APIActionRowComponent<APIModalActionRowComponent>
+			| APIActionRowComponent<APIComponentInModalActionRow>
 			| ((builder: ActionRowBuilder) => ActionRowBuilder)
 		)[]
 	): this {
