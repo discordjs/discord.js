780f915f2738fb63d62bb47e62b8554ef96c4450
Qjuh <76154676+Qjuh@users.noreply.github.com>
2025-02-23 17:03:40 +0100

    fix(website): add type to variables

--- apps/website/src/components/UnionMember.tsx
+++ apps/website/src/components/UnionMember.tsx
@@ -3,9 +3,9 @@ import { ExcerptNode } from './ExcerptNode';
 export async function UnionMember({ node, version }: { readonly node: any; readonly version: string }) {
 	return (
 		<div className="flex flex-col gap-4">
-			<h2 className="flex place-items-center gap-2 p-2 text-xl font-bold">Union Members</h2>
+			{node.length > 1 ? <h2 className="flex place-items-center gap-2 p-2 text-xl font-bold">Union Members</h2> : ''}
 
-			<span className="flex flex-col gap-4 px-2 font-mono text-sm break-words">
+			<span className="flex flex-col gap-4 break-words px-2 font-mono text-sm">
 				<ExcerptNode node={node} version={version} />
 			</span>
 		</div>
