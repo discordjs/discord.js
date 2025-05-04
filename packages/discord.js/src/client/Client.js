c8687cdd45089e6d06f8656609bf0a7d9dfa08c0
Qjuh <76154676+Qjuh@users.noreply.github.com>
2025-05-04 11:17:32 +0200

    fix: snowflake docs

--- packages/discord.js/src/client/Client.js
+++ packages/discord.js/src/client/Client.js
@@ -818,8 +818,8 @@ exports.Client = Client;
  * If we have a snowflake '266241948824764416' we can represent it as binary:
  * ```
  * 64                                          22     17     12          0
- * 000000111011000111100001101001000101000000  00001  00000  000000000000
- * number of milliseconds since Discord epoch  worker  pid    increment
+ *  000000111011000111100001101001000101000000  00001  00000  000000000000
+ *  number of milliseconds since Discord epoch  worker  pid    increment
  * ```
  *
  * @typedef {string} Snowflake
