1585eb65770e262be71f989a62b8c7e8ed34f072
iCrawl <buechler.noel@outlook.com>
2025-05-12 21:34:46 +0200

    feat: website

---
+++ apps/website/src/components/EntrypointSelect.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import { useParams, useRouter } from 'next/navigation';
+import { use } from 'react';
+import { parseDocsPathParams } from '@/util/parseDocsPathParams';
+import { Select, SelectList, SelectOption, SelectTrigger } from './ui/Select';
+
+export function EntryPointSelect({
+	entryPointsPromise,
+}: {
+	readonly entryPointsPromise: Promise<{ readonly entryPoint: string }[]>;
+}) {
+	const router = useRouter();
+	const params = useParams();
+	const entryPoints = use(entryPointsPromise);
+
+	const { entryPoints: parsedEntrypoints } = parseDocsPathParams(params.item as string[] | undefined);
+
+	return (
+		<Select
+			aria-label="Select an entrypoint"
+			defaultSelectedKey={parsedEntrypoints.length ? parsedEntrypoints.join('/') : 'global'}
+		>
+			<SelectTrigger className="bg-[#f3f3f4] dark:bg-[#121214]" />
+			<SelectList classNames={{ popover: 'bg-[#f3f3f4] dark:bg-[#28282d]' }} items={entryPoints}>
+				{(item) => (
+					<SelectOption
+						className="dark:pressed:bg-[#313135] bg-[#f3f3f4] dark:bg-[#28282d] dark:hover:bg-[#313135]"
+						href={`/docs/packages/${params.packageName}/${params.version}/${item.entryPoint}`}
+						id={item.entryPoint || 'global'}
+						key={item.entryPoint || 'global'}
+						onHoverStart={() =>
+							router.prefetch(`/docs/packages/${params.packageName}/${params.version}/${item.entryPoint}`)
+						}
+						textValue={item.entryPoint || 'global'}
+					>
+						{item.entryPoint || 'global'}
+					</SelectOption>
+				)}
+			</SelectList>
+		</Select>
+	);
+}
