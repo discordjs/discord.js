cf87cbb04103feccb7bd515d89a820adddbe7d8f
LJNeon <23249107+LJNeon@users.noreply.github.com>
2025-05-27 23:57:44 -0700

    chore(WebSocketShard): move zlib-sync error numbers to constants file

--- packages/ws/src/ws/WebSocketShard.ts
+++ packages/ws/src/ws/WebSocketShard.ts
@@ -25,6 +25,7 @@ import type { IContextFetchingStrategy } from '../strategies/context/IContextFet
 import {
 	CompressionMethod,
 	CompressionParameterMap,
+	ZlibErrorCodes,
 	ImportantGatewayOpcodes,
 	getInitialSendRateLimitState,
 } from '../utils/constants.js';
@@ -57,17 +58,6 @@ export enum WebSocketShardStatus {
 export enum WebSocketShardDestroyRecovery {
 	Reconnect,
 	Resume,
-}
-
-enum ZlibErrorCodes {
-	Z_NEED_DICT = 2,
-	Z_STREAM_END = 1,
-	Z_ERRNO = -1,
-	Z_STREAM_ERROR = -2,
-	Z_DATA_ERROR = -3,
-	Z_MEM_ERROR = -4,
-	Z_BUF_ERROR = -5,
-	Z_VERSION_ERROR = -6,
 }
 
 export interface WebSocketShardEventsMap {
