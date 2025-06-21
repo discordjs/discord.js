e6a5a8d3b8a5606f71e30da4bd5edad329163967
almostSouji <timoqueezle@gmail.com>
2025-06-21 22:47:34 +0200

    feat: subguide-based accent styles

--- apps/guide/src/app/guide/layout.tsx
+++ apps/guide/src/app/guide/layout.tsx
@@ -5,7 +5,38 @@ import { source } from '@/lib/source';
 
 export default function Layout({ children }: { readonly children: ReactNode }) {
 	return (
-		<DocsLayout tree={source.pageTree} {...baseOptions}>
+		<DocsLayout
+			sidebar={{
+				tabs: {
+					transform(option, node) {
+						const meta = source.getNodeMeta(node);
+						if (!meta || !node.icon) return option;
+
+						// category selection color based on path src/styles/base.css
+						const color = `var(--${meta.file.path.split('/')[0]}-color, var(--color-fd-foreground))`;
+
+						return {
+							...option,
+							icon: (
+								<div
+									className="border rounded-lg p-1.5 shadow-lg md:mb-auto [&_svg]:size-6 md:rounded-md md:p-1 md:[&_svg]:size-5"
+									style={
+										{
+											color,
+											backgroundColor: `color-mix(in oklab, ${color} 10%, transparent)`,
+										} as object
+									}
+								>
+									{node.icon}
+								</div>
+							),
+						};
+					},
+				},
+			}}
+			tree={source.pageTree}
+			{...baseOptions}
+		>
 			{children}
 		</DocsLayout>
 	);
