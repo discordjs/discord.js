cae2a6831aaf38da597bb9979081585546295b38
Danial Raza <danialrazafb@gmail.com>
2025-07-05 03:27:50 +0200

    refactor: move to user primary guild to its own class

--- packages/discord.js/typings/index.d.ts
+++ packages/discord.js/typings/index.d.ts
@@ -31,10 +31,7 @@ import {
   APIComponentInModalActionRow,
   APIContainerComponent,
   APIEmbed,
-  APIEmbedAuthor,
   APIEmbedField,
-  APIEmbedFooter,
-  APIEmbedImage,
   APIEmbedProvider,
   APIEmoji,
   APIEntitlement,
@@ -3497,11 +3494,15 @@ export interface AvatarDecorationData {
   skuId: Snowflake;
 }
 
-export interface UserPrimaryGuild {
-  badge: string | null;
-  identityEnabled: boolean | null;
-  identityGuildId: Snowflake | null;
-  tag: string | null;
+export class UserPrimaryGuild {
+  // TODO: Change to APIUserPrimaryGuild
+  private constructor(data: unknown, client: Client<true>);
+  public identityGuildId: Snowflake | null;
+  public identityEnabled: boolean | null;
+  public tag: string | null;
+  public badge: string | null;
+  public get identityGuild(): Guild | null;
+  public guildTagBadgeURL(options?: ImageURLOptions): string | null;
 }
 
 export interface UnfurledMediaItemData {
@@ -3542,7 +3543,6 @@ export class User extends Base {
   public avatarURL(options?: ImageURLOptions): string | null;
   public avatarDecorationURL(): string | null;
   public bannerURL(options?: ImageURLOptions): string | null | undefined;
-  public guildTagBadgeURL(options?: ImageURLOptions): string | null;
   public createDM(force?: boolean): Promise<DMChannel>;
   public deleteDM(): Promise<DMChannel>;
   public displayAvatarURL(options?: ImageURLOptions): string;
