edca6c9ddd731b6feea9f1b737bb657a8217f72b
Qjuh <76154676+Qjuh@users.noreply.github.com>
2025-07-12 23:09:07 +0200

    feat: implement structures in mainlib

---
+++ packages/next/src/managers/ChannelManager.ts
@@ -0,0 +1,14 @@
+import type { APIChannel } from 'discord-api-types/v10';
+import type { Client } from '../Client.js';
+import { Channel } from '../structures/Channel.js';
+import { CachedlManager } from './CachedManager.js';
+
+export class ChannelManager extends CachedlManager<Channel> {
+	public constructor(data: APIChannel[], client: Client) {
+		super(client, Channel);
+
+		for (const channel of data) {
+			this.cache.set(channel.id, new Channel(channel, client));
+		}
+	}
+}
