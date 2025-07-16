0eaf6d52e837d62b5f5d9e08cfa39cf10c83e341
Jiralite <33201955+Jiralite@users.noreply.github.com>
2025-07-16 11:45:57 +0100

    refactor: use `BaseInvite` for pattern

--- packages/discord.js/src/util/DataResolver.js
+++ packages/discord.js/src/util/DataResolver.js
@@ -5,7 +5,7 @@ const fs = require('node:fs/promises');
 const path = require('node:path');
 const { fetch } = require('undici');
 const { DiscordjsError, DiscordjsTypeError, ErrorCodes } = require('../errors/index.js');
-const { GuildInvite } = require('../structures/GuildInvite.js');
+const { BaseInvite } = require('../structures/BaseInvite.js');
 
 /**
  * Data that can be resolved to give an invite code. This can be:
@@ -43,7 +43,7 @@ function resolveCode(data, regex) {
  * @private
  */
 function resolveInviteCode(data) {
-  return resolveCode(data, GuildInvite.InvitesPattern);
+  return resolveCode(data, BaseInvite.InvitesPattern);
 }
 
 /**
