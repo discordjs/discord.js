e90deb7e9390cf977b43deccd520401205221995
Qjuh <76154676+Qjuh@users.noreply.github.com>
2025-07-20 13:17:37 +0200

    refactor: createStructure in managers

--- packages/next/src/managers/ChannelManager.ts
+++ packages/next/src/managers/ChannelManager.ts
@@ -1,14 +1,72 @@
-import type { APIChannel } from 'discord-api-types/v10';
+import { ChannelType, type APIChannel } from 'discord-api-types/v10';
 import type { Client } from '../Client.js';
-import { Channel } from '../structures/Channel.js';
+import { AnnouncementChannel } from '../structures/AnnouncementChannel.js';
+import { AnnouncementThreadChannel } from '../structures/AnnouncementThreadChannel.js';
+import { BaseChannel } from '../structures/BaseChannel.js';
+import { CategoryChannel } from '../structures/CategoryChannel.js';
+import { DMChannel } from '../structures/DMChannel.js';
+import { ForumChannel } from '../structures/ForumChannel.js';
+import { GroupDMChannel } from '../structures/GroupDMChannel.js';
+import { MediaChannel } from '../structures/MediaChannel.js';
+import { PrivateThreadChannel } from '../structures/PrivateThreadChannel.js';
+import { PublicThreadChannel } from '../structures/PublicThreadChannel.js';
+import { StageChannel } from '../structures/StageChannel.js';
+import { TextChannel } from '../structures/TextChannel.js';
+import { VoiceChannel } from '../structures/VoiceChannel.js';
 import { CachedManager } from './CachedManager.js';
+
+export type Channel =
+	| AnnouncementChannel
+	| AnnouncementThreadChannel
+	| BaseChannel
+	| CategoryChannel
+	| DMChannel
+	| ForumChannel
+	| GroupDMChannel
+	| MediaChannel
+	| PrivateThreadChannel
+	| PublicThreadChannel
+	| StageChannel
+	| TextChannel
+	| VoiceChannel;
 
 export class ChannelManager extends CachedManager<Channel> {
 	public constructor(data: APIChannel[], client: Client) {
-		super(client, Channel);
+		super(client);
 
 		for (const channel of data) {
-			this.cache.set(channel.id, new Channel(channel, client));
+			this.cache.set(channel.id, this.createStructure(channel));
+		}
+	}
+
+	protected override createStructure(data: APIChannel): Channel {
+		switch (data.type) {
+			case ChannelType.AnnouncementThread:
+				return new AnnouncementThreadChannel(data);
+			case ChannelType.DM:
+				return new DMChannel(data);
+			case ChannelType.GroupDM:
+				return new GroupDMChannel(data);
+			case ChannelType.GuildAnnouncement:
+				return new AnnouncementChannel(data);
+			case ChannelType.GuildCategory:
+				return new CategoryChannel(data);
+			case ChannelType.GuildForum:
+				return new ForumChannel(data);
+			case ChannelType.GuildMedia:
+				return new MediaChannel(data);
+			case ChannelType.GuildStageVoice:
+				return new StageChannel(data);
+			case ChannelType.GuildText:
+				return new TextChannel(data);
+			case ChannelType.GuildVoice:
+				return new VoiceChannel(data);
+			case ChannelType.PrivateThread:
+				return new PrivateThreadChannel(data);
+			case ChannelType.PublicThread:
+				return new PublicThreadChannel(data);
+			default:
+				return new BaseChannel(data);
 		}
 	}
 }
--- packages/next/src/structures/mixins/BaseChannelMixin.ts
+++ packages/next/src/structures/mixins/BaseChannelMixin.ts
@@ -3,6 +3,7 @@ import { equal } from 'node:assert';
 import type { ChannelType } from '@discordjs/core';
 import { kPatch, type Channel, type ChannelDataType } from '@discordjs/structures';
 import type { Client } from '../../Client.js';
+import { container } from '../../util/container.js';
 
 export interface BaseChannelMixin<Type extends ChannelType = ChannelType> extends Channel<Type> {}
 
@@ -10,12 +11,12 @@ export class BaseChannelMixin<Type extends ChannelType = ChannelType> {
 	declare public readonly client: Client;
 
 	public async delete() {
-		await this.client['core'].api.channels.delete(this.id);
+		await container.get('client')['core'].api.channels.delete(this.id);
 		return this;
 	}
 
 	public async fetch() {
-		const data = await this.client['core'].api.channels.get(this.id);
+		const data = await container.get('client')['core'].api.channels.get(this.id);
 
 		equal(data.type, this.type);
 		return this[kPatch](data as ChannelDataType<Type>);
