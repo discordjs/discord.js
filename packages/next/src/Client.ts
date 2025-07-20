4948de4d16e508f874e482127a81f7b53f1cfd91
Qjuh <76154676+Qjuh@users.noreply.github.com>
2025-07-20 13:17:37 +0200

    refactor: createStructure in managers

--- packages/next/src/Client.ts
+++ packages/next/src/Client.ts
@@ -1,11 +1,11 @@
 import process from 'node:process';
-import { Collection } from '@discordjs/collection';
 import type { RESTGetAPIGatewayBotResult, Snowflake } from '@discordjs/core';
 import { Client as CoreClient, Routes, GatewayDispatchEvents } from '@discordjs/core';
 import { REST } from '@discordjs/rest';
 import type { Structure } from '@discordjs/structures';
 import { WebSocketManager } from '@discordjs/ws';
-import type { CacheConstructor, Cache } from './util/types';
+import { CollectionCache, type CacheConstructor, type StructureCache } from './util/cache.js';
+import { container } from './util/container.js';
 
 export class Client {
 	private core!: CoreClient;
@@ -14,16 +14,23 @@ export class Client {
 
 	#token: string;
 
-	public CacheConstructor<Value extends Structure<object>>(_value: new (...args: any[]) => Value) {
-		return new this.cacheConstructor() as Cache<Snowflake, Value>;
+	public CacheConstructor<
+		Value extends Structure<{ id: Snowflake }>,
+		Raw extends { id: Snowflake } = Value extends Structure<infer Type> ? Type : never,
+	>() {
+		return new this.cacheConstructor() as StructureCache<Raw, Value>;
 	}
 
 	protected cacheConstructor: CacheConstructor;
 
-	public constructor({ token = process.env.DISCORD_TOKEN }: { token?: string }, cache: CacheConstructor = Collection) {
+	public constructor(
+		{ token = process.env.DISCORD_TOKEN }: { token?: string },
+		cache: CacheConstructor = CollectionCache,
+	) {
 		this.options = { token };
 		this.#token = token!;
 		this.cacheConstructor = cache;
+		container.set('client', this);
 	}
 
 	public async login(token?: string) {
