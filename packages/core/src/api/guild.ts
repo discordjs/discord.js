ac6ff15b7dc4a88753b7cfdf1bca1b4bcc1cc260
Jiralite <33201955+Jiralite@users.noreply.github.com>
2025-08-04 19:46:26 +0100

    fix(guild): Creating a template actually creates a template (#11030)
    feat(guild): add creating a template

--- packages/core/src/api/guild.ts
+++ packages/core/src/api/guild.ts
@@ -1287,16 +1287,16 @@ export class GuildsAPI {
 	 * Creates a new template
 	 *
 	 * @see {@link https://discord.com/developers/docs/resources/guild-template#create-guild-template}
-	 * @param templateCode - The code of the template
+	 * @param guildId - The id of the guild
 	 * @param body - The data for creating the template
 	 * @param options - The options for creating the template
 	 */
 	public async createTemplate(
-		templateCode: string,
+		guildId: Snowflake,
 		body: RESTPostAPIGuildTemplatesJSONBody,
 		{ signal }: Pick<RequestData, 'signal'> = {},
 	) {
-		return this.rest.post(Routes.template(templateCode), { body, signal }) as Promise<RESTPostAPIGuildTemplatesResult>;
+		return this.rest.post(Routes.guildTemplates(guildId), { body, signal }) as Promise<RESTPostAPIGuildTemplatesResult>;
 	}
 
 	/**
