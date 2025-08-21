f6ef099e17d90b0ad1ce2996e604f0bb455759ec
almeidx <github@almeidx.dev>
2025-08-21 14:55:06 +0100

    chore: bump dependencies

--- packages/create-discord-bot/template/Deno/src/commands/index.ts
+++ packages/create-discord-bot/template/Deno/src/commands/index.ts
@@ -1,5 +1,5 @@
 import type { RESTPostAPIApplicationCommandsJSONBody, CommandInteraction } from 'npm:discord.js@^14.22.0';
-import { z } from 'npm:zod@^3.24.1';
+import { z } from 'npm:zod@^3.25.76';
 import type { StructurePredicate } from '../util/loaders.ts';
 
 /**
