d0b4b00e6edc6316f164701d6127a5710ce2a7ba
ckohen <chaikohen@gmail.com>
2025-08-27 23:32:59 -0700

    fix: explicit globalThis

--- packages/rest/src/lib/handlers/Shared.ts
+++ packages/rest/src/lib/handlers/Shared.ts
@@ -148,8 +148,8 @@ export async function handleErrors(
 				if (isDiscordError && data.code !== 0 && !authFalseWarningEmitted) {
 					const errorText = `Encountered HTTP 401 with error ${data.code}: ${data.message}. Your token will be removed from this REST instance. If you are using @discordjs/rest directly, consider adding 'auth: false' to the request. Open an issue with your library if not.`;
 					// Use emitWarning if possible, probably not available in edge / web
-					if (typeof process !== 'undefined' && typeof process.emitWarning === 'function') {
-						process.emitWarning(errorText);
+					if (typeof globalThis.process !== 'undefined' && typeof globalThis.process.emitWarning === 'function') {
+						globalThis.process.emitWarning(errorText);
 					} else {
 						console.warn(errorText);
 					}
