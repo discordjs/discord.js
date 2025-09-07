f442a8b32ecce73a9c0bde1aeaddf31eb455a919
sathishxmirror7 <sathish11377@gmail.com>
2025-09-07 11:37:45 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+<> index.js
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+});
+
+const TOKEN = 'MTM5Mzg0NDEzMDYyMjQ3MjM4NQ.G8uzMI.F7wpqVE1OUABnrBYmwV93HshAu7tUdzIyjygdw';
+
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', (message) => {
+    if (message.author.bot) return;
+
+    if (message.content === '!chat') {
+        message.channel.send('content!');
+    }
+});
+
+client.login(TOKEN);
