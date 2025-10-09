cae3454dbe4a1023fda80a9b9e55af267f5b69f7
didinele <didinele.dev@gmail.com>
2025-10-10 00:03:52 +0300

    fix(ApplicationCommandPermissionsManager): truly optional arg

--- packages/discord.js/typings/index.d.ts
+++ packages/discord.js/typings/index.d.ts
@@ -4227,7 +4227,9 @@ export class ApplicationCommandPermissionsManager<
     },
   ): Promise<boolean>;
   public fetch(options: FetchSingleOptions): Promise<ApplicationCommandPermissions[]>;
-  public fetch(options: BaseOptions): Promise<Collection<Snowflake, ApplicationCommandPermissions[]>>;
+  public fetch(
+    ...args: {} extends BaseOptions ? [] | [BaseOptions] : [BaseOptions]
+  ): Promise<Collection<Snowflake, ApplicationCommandPermissions[]>>;
   public remove(
     options:
       | (FetchSingleOptions & {
