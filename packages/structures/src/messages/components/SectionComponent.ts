79082d40e231b514a0ac7b6b1ed667de9bf9cc9e
Qjuh <76154676+Qjuh@users.noreply.github.com>
2025-10-20 15:08:39 +0200

    chore: add LabelComponent

--- packages/structures/src/messages/components/SectionComponent.ts
+++ packages/structures/src/messages/components/SectionComponent.ts
@@ -6,7 +6,7 @@ import { Component } from './Component.js';
  * Represents a section component on a message.
  *
  * @typeParam Omitted - Specify the properties that will not be stored in the raw data field as a union, implement via `DataTemplate`
- * @remarks has substructures for the components contained in the section (e.g. `TextDisplayComponent`, `ButtonComponent`, ...) which need to be instantiated and stored by an extending class using it
+ * @remarks has `Component`s as substructures which need to be instantiated and stored by an extending class using it
  */
 export class SectionComponent<Omitted extends keyof APISectionComponent | '' = ''> extends Component<
 	APISectionComponent,
