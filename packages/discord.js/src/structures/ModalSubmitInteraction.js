f6eba5f747cd48a4ea8c19ea48c2f491dfea37c5
Jiralite <33201955+Jiralite@users.noreply.github.com>
2025-10-25 23:51:32 +0100

    docs: update more fields

--- packages/discord.js/src/structures/ModalSubmitInteraction.js
+++ packages/discord.js/src/structures/ModalSubmitInteraction.js
@@ -21,10 +21,10 @@ const getAttachment = lazy(() => require('./Attachment.js').Attachment);
  * @typedef {BaseModalData} SelectMenuModalData
  * @property {string} customId The custom id of the component
  * @property {string[]} values The values of the component
- * @property {Collection<string, GuildMember|APIGuildMember>} [members] The resolved members
- * @property {Collection<string, User|APIUser>} [users] The resolved users
- * @property {Collection<string, Role|APIRole>} [roles] The resolved roles
- * @property {Collection<string, BaseChannel|APIChannel>} [channels] The resolved channels
+ * @property {Collection<Snowflake, GuildMember|APIGuildMember>} [members] The resolved members
+ * @property {Collection<Snowflake, User|APIUser>} [users] The resolved users
+ * @property {Collection<Snowflake, Role|APIRole>} [roles] The resolved roles
+ * @property {Collection<Snowflake, BaseChannel|APIChannel>} [channels] The resolved channels
  */
 
 /**
