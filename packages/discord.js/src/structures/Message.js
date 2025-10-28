cd9870a96ff6284eb64d9d538118dcd768bdcf60
Jiralite <33201955+Jiralite@users.noreply.github.com>
2025-10-28 12:32:15 +0000

    Update packages/discord.js/src/structures/Message.js

--- packages/discord.js/src/structures/Message.js
+++ packages/discord.js/src/structures/Message.js
@@ -809,7 +809,7 @@ class Message extends Base {
     if (!this.guild) return true;
     if (VoiceBasedChannelTypes.includes(channel?.type) || !channel?.viewable) return false;
 
-    const permissions = channel?.permissionsFor(this.client.user);
+    const permissions = channel.permissionsFor(this.client.user);
     if (!permissions) return false;
 
     return permissions.has(PermissionFlagsBits.ReadMessageHistory | PermissionFlagsBits.PinMessages);
