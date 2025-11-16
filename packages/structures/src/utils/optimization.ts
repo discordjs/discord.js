43f0071a551c429dbc16885aa508c0387ff1c3d6
Qjuh <76154676+Qjuh@users.noreply.github.com>
2025-11-16 14:12:12 +0100

    chore: add comment explaining timestamp conversion

--- packages/structures/src/utils/optimization.ts
+++ packages/structures/src/utils/optimization.ts
@@ -9,6 +9,14 @@ export function extendTemplate<SuperTemplate extends Record<string, unknown>>(
 		SuperTemplate;
 }
 
+/**
+ * Turns a JavaScript Date object into the timestamp format used by Discord in payloads.
+ * E.g. `2025-11-16T14:09:25.239000+00:00`
+ *
+ * @private
+ * @param date a Date instance
+ * @returns an ISO8601 timestamp with microseconds precision and explicit +00:00 timezone
+ */
 export function dateToDiscordISOTimestamp(date: Date) {
 	return `${date.getUTCFullYear()}-${(date.getUTCMonth() + 1).toString().padStart(2, '0')}-${date.getUTCDate().toString().padStart(2, '0')}T${date.getUTCHours().toString().padStart(2, '0')}:${date.getUTCMinutes().toString().padStart(2, '0')}:${date.getUTCSeconds().toString().padStart(2, '0')}.${date.getUTCMilliseconds().toString().padEnd(6, '0')}+00:00`;
 }
