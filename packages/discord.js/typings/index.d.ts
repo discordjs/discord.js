427636ee7555a92fa9780cd78f28cc0037b9241c
Pavel-Boyazov <65123753+Pavel-Boyazov@users.noreply.github.com>
2025-12-17 22:04:17 +0300

    types(Message): specify `rawData` arg type (#11123)
    Co-authored-by: Almeida <github@almeidx.dev>

--- packages/discord.js/typings/index.d.ts
+++ packages/discord.js/typings/index.d.ts
@@ -2242,7 +2242,7 @@ export class Message<InGuild extends boolean = boolean> extends Base {
       | MessagePayload
       | string,
   ): Promise<OmitPartialGroupDMChannel<Message<InGuild>>>;
-  public equals(message: Message, rawData: unknown): boolean;
+  public equals(message: Message, rawData?: APIMessage): boolean;
   public fetchReference(): Promise<OmitPartialGroupDMChannel<Message<InGuild>>>;
   public fetchWebhook(): Promise<Webhook>;
   public crosspost(): Promise<OmitPartialGroupDMChannel<Message<InGuild>>>;
