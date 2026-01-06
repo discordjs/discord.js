b67d8ec4ff51576e6e3d16122428edc9cffc861f
Asad <105254706+AsadHumayun@users.noreply.github.com>
2026-01-06 00:28:29 +0000

    feat(structures): create and update barrel exports

---
+++ packages/structures/src/entitlements/index.ts
@@ -0,0 +1,1 @@
+export * from './Entitlement.js';
--- packages/structures/src/index.ts
+++ packages/structures/src/index.ts
@@ -1,5 +1,6 @@
 export * from './bitfields/index.js';
 export * from './channels/index.js';
+export * from './entitlements/index.js';
 export * from './interactions/index.js';
 export * from './invites/index.js';
 export * from './messages/index.js';
