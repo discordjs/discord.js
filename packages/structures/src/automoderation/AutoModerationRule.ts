1f52e9805f86a978ee58382260e108837b4ee9c0
Asad <105254706+AsadHumayun@users.noreply.github.com>
2026-01-11 19:28:35 +0000

    feat(structures): add AutoModerationRule structure

---
+++ packages/structures/src/automoderation/AutoModerationRule.ts
@@ -0,0 +1,90 @@
+import type { APIAutoModerationRule } from 'discord-api-types/v10';
+import { Structure } from '../Structure.js';
+import { kData } from '../utils/symbols.js';
+import type { Partialize } from '../utils/types.js';
+
+/**
+ * Represents an auto moderation rule on Discord.
+ *
+ * @typeParam Omitted - Specify the properties that will not be stored in the raw data field as a union, implement via `DataTemplate`
+ * @remarks has substructure `TriggerMetadata` which needs to be instantiated and stored by an extending class using it
+ */
+export class AutoModerationRule<Omitted extends keyof APIAutoModerationRule | '' = ''> extends Structure<
+	APIAutoModerationRule,
+	Omitted
+> {
+	/**
+	 * The template used for removing data from the raw data stored for each auto moderation rule
+	 */
+	public static override DataTemplate: Partial<APIAutoModerationRule> = {};
+
+	/**
+	 * @param data - The raw data received from the API for the auto moderation rule
+	 */
+	public constructor(data: Partialize<APIAutoModerationRule, Omitted>) {
+		super(data);
+	}
+
+	/**
+	 * The id of this rule
+	 */
+	public get id() {
+		return this[kData].id;
+	}
+
+	/**
+	 * The id of the guild which this rule belongs to
+	 */
+	public get guildId() {
+		return this[kData].guild_id;
+	}
+
+	/**
+	 * The rule name
+	 */
+	public get name() {
+		return this[kData].name;
+	}
+
+	/**
+	 * The user who first created this rule
+	 */
+	public get creatorId() {
+		return this[kData].creator_id;
+	}
+
+	/**
+	 * The rule {@link https://discord.com/developers/docs/resources/auto-moderation#auto-moderation-rule-object-event-types | event type}
+	 */
+	public get eventType() {
+		return this[kData].event_type;
+	}
+
+	/**
+	 * The rule {@link https://discord.com/developers/docs/resources/auto-moderation#auto-moderation-rule-object-trigger-types | trigger type}
+	 */
+	public get triggerType() {
+		return this[kData].trigger_type;
+	}
+
+	/**
+	 * Wether the rule is enabled
+	 */
+	public get enabled() {
+		return this[kData].enabled;
+	}
+
+	/**
+	 * The role ids that should not be affected by this rule (Maximum of 20)
+	 */
+	public get exemptRoles() {
+		return this[kData].exempt_roles;
+	}
+
+	/**
+	 * The channel ids that should not be affected by this rule (Maximum of 50)
+	 */
+	public get exemptChannels() {
+		return this[kData].exempt_channels;
+	}
+}
