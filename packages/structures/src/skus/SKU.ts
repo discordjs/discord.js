62a35113d90f3fa9cd8a0b1a8f95e9af24f070fc
Asad <105254706+AsadHumayun@users.noreply.github.com>
2026-01-20 00:53:14 +0000

    feat(structures): add SKU structure

---
+++ packages/structures/src/skus/SKU.ts
@@ -0,0 +1,68 @@
+import type { SKUFlags, APISKU } from 'discord-api-types/v10';
+import { Structure } from '../Structure.js';
+import { SKUFlagsBitField } from '../bitfields/SKUFlagsBitField.js';
+import { kData } from '../utils/symbols.js';
+import type { Partialize } from '../utils/types.js';
+
+/**
+ * Represents any SKU (stock-keeping units) on Discord.
+ *
+ * @typeParam Omitted - Specify the properties that will not be stored in the raw data field as a union, implement via `DataTemplate`
+ */
+export class SKU<Omitted extends keyof APISKU | '' = ''> extends Structure<APISKU, Omitted> {
+	/**
+	 * The template used for removing data from the raw data stored for each SKU
+	 */
+	public static override readonly DataTemplate: Partial<APISKU> = {};
+
+	/**
+	 * @param data - The raw data received from the API for the SKU
+	 */
+	public constructor(data: Partialize<APISKU, Omitted>) {
+		super(data);
+	}
+
+	/**
+	 * Id of the SKU
+	 */
+	public get id() {
+		return this[kData].id;
+	}
+
+	/**
+	 * {@link https://discord.com/developers/docs/resources/sku#sku-object-sku-types | Type of SKU}
+	 */
+	public get type() {
+		return this[kData].type;
+	}
+
+	/**
+	 * Id of the parent application
+	 */
+	public get applicationId() {
+		return this[kData].application_id;
+	}
+
+	/**
+	 * Customer-facing name of your premium offering
+	 */
+	public get name() {
+		return this[kData].name;
+	}
+
+	/**
+	 * System-generated URL slug based on the SKU's name
+	 */
+	public get slug() {
+		return this[kData].slug;
+	}
+
+	/**
+	 * {@link SKUFlags | SKU flags} combined as a {@link https://en.wikipedia.org/wiki/Bit_field | bitfield}
+	 */
+	public get flags() {
+		const flags = this[kData].flags;
+
+		return flags ? new SKUFlagsBitField(this[kData].flags as SKUFlags) : null;
+	}
+}
