763bd9e5052df16ffb3dcd0cf0feef09dff7c0c4
Asad <105254706+AsadHumayun@users.noreply.github.com>
2026-01-20 00:52:46 +0000

    feat(structures): add SKUFlagsBitField to /bitfields

---
+++ packages/structures/src/bitfields/SKUFlagsBitField.ts
@@ -0,0 +1,16 @@
+import { SKUFlags } from 'discord-api-types/v10';
+import { BitField } from './BitField.js';
+
+/**
+ * Data structure that makes it easy to interact with an {@link SKUFlags} bitfield.
+ */
+export class SKUFlagsBitField extends BitField<keyof SKUFlags> {
+	/**
+	 * Numeric SKU flags.
+	 */
+	public static override readonly Flags = SKUFlags;
+
+	public override toJSON() {
+		return super.toJSON(true);
+	}
+}
