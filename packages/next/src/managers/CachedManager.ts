4f3a575367222c642c6d0c6663c9e850f2e34d85
Qjuh <76154676+Qjuh@users.noreply.github.com>
2026-01-22 20:41:20 +0100

    chore: improve cache constructor

--- packages/next/src/managers/CachedManager.ts
+++ packages/next/src/managers/CachedManager.ts
@@ -2,21 +2,21 @@ import type { Structure } from '@discordjs/structures';
 import { kClone, kPatch } from '@discordjs/structures';
 import type { Snowflake } from 'discord-api-types/globals';
 import type { Client } from '../Client.js';
-import type { StructureCache } from '../util/cache.js';
+import type { RawAPIType, StructureCache } from '../util/cache.js';
 
 export abstract class CachedManager<
 	Value extends Structure<{ id: Snowflake }> & { get id(): Snowflake },
-	Raw extends { id: Snowflake } = Value extends Structure<infer Type> ? Type : never,
+	Raw extends RawAPIType<Value> = RawAPIType<Value>,
 > {
 	public client: Client;
 
-	public cache: StructureCache<Raw, Value>;
+	public cache: StructureCache<Value>;
 
-	protected abstract createStructure(data: Raw, ...extras: unknown[]): Value;
+	protected abstract createStructure(data: Partial<RawAPIType<Value>>, ...extras: unknown[]): Value;
 
 	public constructor(client: Client) {
 		this.client = client;
-		this.cache = client.CacheConstructor<Value, Raw>();
+		this.cache = client.CacheConstructor<Value>(this.createStructure.bind(this));
 	}
 
 	protected async _add(data: Raw, cache = true, { id, extras = [] }: { extras?: unknown[]; id?: Snowflake } = {}) {
--- packages/next/src/util/cache.ts
+++ packages/next/src/util/cache.ts
@@ -3,20 +3,48 @@ import { kPatch, type Structure } from '@discordjs/structures';
 import type { Awaitable } from '@discordjs/util';
 import type { Snowflake } from 'discord-api-types/globals';
 
-export interface StructureCache<Raw, Value extends Structure<object>> {
+export type RawAPIType<Value extends Structure<{}>> = Value extends Structure<infer Type> ? Type : never;
+
+export interface StructureCache<
+	Value extends Structure<{ id: Snowflake }>,
+	Raw extends RawAPIType<Value> = RawAPIType<Value>,
+> {
 	add(key: Snowflake, data: Partial<Raw>): Value;
 	get(key: Snowflake): Awaitable<Value | undefined>;
 	set(key: Snowflake, value: Value): Awaitable<this>;
 }
 
-export type CacheConstructor = new (...args: any[]) => StructureCache<unknown, Structure<object>>;
+export type CacheConstructor = new <Value extends Structure<{ id: Snowflake }>>(
+	creator: StructureCreator<Value>,
+	...args: any[]
+) => StructureCache<Value>;
 
-export class CollectionCache<Raw, Value extends Structure<object>>
+export type StructureCreator<
+	Value extends Structure<{ id: Snowflake }>,
+	Raw extends RawAPIType<Value> = RawAPIType<Value>,
+> = (data: Partial<Raw>) => Value;
+
+export class CollectionCache<
+	Value extends Structure<{ id: Snowflake }>,
+	Raw extends RawAPIType<Value> = RawAPIType<Value>,
+>
 	extends Collection<Snowflake, Value>
-	implements StructureCache<Raw, Value>
+	implements StructureCache<Value, Raw>
 {
+	private readonly structureCreator: StructureCreator<Value>;
+
+	public constructor(
+		creator: StructureCreator<Value>,
+		...args: ConstructorParameters<typeof Collection<Snowflake, Value>>
+	) {
+		super(...args);
+		this.structureCreator = creator;
+	}
+
 	public add(_key: Snowflake, _data: Partial<Raw>): Value {
-		throw new Error('Method not implemented.');
+		const value = this.structureCreator(_data);
+		this.set(_key, value);
+		return value;
 	}
 
 	public patch(key: Snowflake, data: Partial<Raw>): Value {
