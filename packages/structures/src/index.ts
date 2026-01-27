693fcbc32b64568671b5163a1acda33a65e843f0
Asad <105254706+AsadHumayun@users.noreply.github.com>
2026-01-27 23:03:09 +0000

    feat(structures): add Stage Instance structure (#11397)
    * feat(structures): update barrel exports for stage instance structure
    
    * feat(structures): add stage instance structure
    
    * docs(structure): cleanup and use see/link correctly
    
    ---------
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- packages/structures/src/index.ts
+++ packages/structures/src/index.ts
@@ -10,6 +10,7 @@ export * from './polls/index.js';
 export * from './stickers/index.js';
 export * from './teams/index.js';
 export * from './users/index.js';
+export * from './stageInstances/index.js';
 export * from './Structure.js';
 export * from './subscriptions/index.js';
 export * from './Mixin.js';
