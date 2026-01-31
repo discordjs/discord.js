85b96b30555a038f7cb5a9e2d9d4ee5257e081b2
Asad <105254706+AsadHumayun@users.noreply.github.com>
2026-01-31 21:56:25 +0000

    feat(structures): add barrel exports for new structs

---
+++ packages/structures/src/gatewayPresences/GatewayActivity/index.ts
@@ -0,0 +1,6 @@
+export * from './GatewayActivityAssets';
+export * from './GatewayActivityButton';
+export * from './GatewayActivityEmoji';
+export * from './GatewayActivityParty';
+export * from './GatewayActivitySecrets';
+export * from './GatewayActivityTimestamps';
