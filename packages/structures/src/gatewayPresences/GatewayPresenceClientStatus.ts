5d0c9ef87cd876c769eb235b51a0bb40d0fc5d8c
Asad <105254706+AsadHumayun@users.noreply.github.com>
2026-02-01 15:57:34 +0000

    feat(structures): add GatewayPresenceClientStatus

--- packages/structures/src/gatewayPresences/GatewayPresenceClientStatus.ts
+++ packages/structures/src/gatewayPresences/GatewayPresenceClientStatus.ts
@@ -1,12 +1,8 @@
-// import { DiscordSnowflake } from '@sapphire/snowflake';
 import type { GatewayPresenceClientStatus as GatewayPresenceClientStatusTypedef } from 'discord-api-types/v10';
 import { Structure } from '../Structure.js';
-// import { kData } from '../utils/symbols.js';
-// import { isIdSet } from '../utils/type-guards.js';
+import { kData } from '../utils/symbols.js';
 import type { Partialize } from '../utils/types.js';
-/**
- * @todo
- */
+
 /**
  * Represents any client status on Discord.
  *
@@ -16,14 +12,35 @@ export class GatewayPresenceClientStatus<
 	Omitted extends keyof GatewayPresenceClientStatusTypedef | '' = '',
 > extends Structure<GatewayPresenceClientStatusTypedef, Omitted> {
 	/**
-	 * The template used for removing data from the raw data stored for each client status.
+	 * The template used for removing data from the raw data stored for each client status
 	 */
 	public static override readonly DataTemplate: Partial<GatewayPresenceClientStatusTypedef> = {};
 
 	/**
-	 * @param data - The raw data received from the API for the client status.
+	 * @param data - The raw data received from the API for the client status
 	 */
 	public constructor(data: Partialize<GatewayPresenceClientStatusTypedef, Omitted>) {
 		super(data);
+	}
+
+	/**
+	 * User's status set for an active desktop (Windows, Linux, Mac) application session
+	 */
+	public get desktop() {
+		return this[kData].desktop;
+	}
+
+	/**
+	 * User's status set for an active mobile (iOS, Android) application session
+	 */
+	public get mobile() {
+		return this[kData].mobile;
+	}
+
+	/**
+	 * User's status set for an active web (browser, bot user) application session
+	 */
+	public get web() {
+		return this[kData].web;
 	}
 }
