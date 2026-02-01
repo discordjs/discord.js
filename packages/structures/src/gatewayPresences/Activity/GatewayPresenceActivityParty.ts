67f33c1cacd1ab300be6dd1c9b872a6ee1ec08fc
Asad <105254706+AsadHumayun@users.noreply.github.com>
2026-02-01 01:31:39 +0000

    feat(structures): add GatewayPresenceActivityParty

--- packages/structures/src/gatewayPresences/Activity/GatewayPresenceActivityParty.ts
+++ packages/structures/src/gatewayPresences/Activity/GatewayPresenceActivityParty.ts
@@ -1,12 +1,10 @@
-// import { DiscordSnowflake } from '@sapphire/snowflake';
+import { DiscordSnowflake } from '@sapphire/snowflake';
 import type { GatewayActivityParty } from 'discord-api-types/v10';
 import { Structure } from '../../Structure.js';
-// import { kData } from '../../utils/symbols.js';
-// import { isIdSet } from '../../utils/type-guards.js';
+import { kData } from '../../utils/symbols.js';
+import { isIdSet } from '../../utils/type-guards.js';
 import type { Partialize } from '../../utils/types.js';
-/**
- * @todo
- */
+
 /**
  * Represents any activity party on Discord.
  *
@@ -26,5 +24,36 @@ export class GatewayPresenceActivityParty<Omitted extends keyof GatewayActivityP
 	 */
 	public constructor(data: Partialize<GatewayActivityParty, Omitted>) {
 		super(data);
+	}
+
+	/**
+	 * The id of the party.
+	 */
+	public get id() {
+		return this[kData].id;
+	}
+
+	/**
+	 * Used to show the party's current and maximum size.
+	 *
+	 * @remarks array of two integers (`current_size`, `max_size`)
+	 */
+	public get size() {
+		return this[kData].size;
+	}
+
+	/**
+	 * The timestamp the activity party was created at
+	 */
+	public get createdTimestamp() {
+		return isIdSet(this.id) ? DiscordSnowflake.timestampFrom(this.id) : null;
+	}
+
+	/**
+	 * The time the activity party was created at
+	 */
+	public get createdAt() {
+		const createdTimestamp = this.createdTimestamp;
+		return createdTimestamp ? new Date(createdTimestamp) : null;
 	}
 }
