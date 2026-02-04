e85436e6ebe4cfb10ad39b0392120ca5b0c257d3
almeidx <github@almeidx.dev>
2026-02-04 11:32:55 +0000

    feat: emit voiceServerUpdate event

--- packages/discord.js/src/client/websocket/handlers/VOICE_SERVER_UPDATE.js
+++ packages/discord.js/src/client/websocket/handlers/VOICE_SERVER_UPDATE.js
@@ -1,6 +1,28 @@
 'use strict';
 
-module.exports = (client, packet) => {
-  client.emit('debug', `[VOICE] received voice server: ${JSON.stringify(packet)}`);
-  client.voice.onVoiceServer(packet.d);
+const { Events } = require('../../../util/Events.js');
+
+module.exports = (client, { d: data }) => {
+  /**
+   * Represents the properties of a voice server update
+   *
+   * @typedef {Object} VoiceServerUpdateData
+   * @property {Snowflake} guildId The id of the guild this voice server update is for
+   * @property {?string} endpoint The voice server host
+   * @property {string} token The voice connection token
+   */
+
+  /**
+   * Emitted whenever a voice server is updated.
+   *
+   * @event Client#voiceServerUpdate
+   * @param {VoiceServerUpdateData} data The voice server update data
+   */
+  client.emit(Events.VoiceServerUpdate, {
+    guildId: data.guild_id,
+    endpoint: data.endpoint,
+    token: data.token,
+  });
+
+  client.voice.onVoiceServer(data);
 };
