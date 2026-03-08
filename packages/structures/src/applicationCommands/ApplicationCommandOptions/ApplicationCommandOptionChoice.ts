486240373cff8ac02fcbf9889d2e67bec320260d
Asad Humayun <asad.humayun@asadh.io>
2026-03-08 14:47:24 +0000

    feat: ApplicationCommandOptionChoice

--- packages/structures/src/applicationCommands/ApplicationCommandOptions/ApplicationCommandOptionChoice.ts
+++ packages/structures/src/applicationCommands/ApplicationCommandOptions/ApplicationCommandOptionChoice.ts
@@ -1,35 +1,40 @@
-// import { DiscordSnowflake } from '@sapphire/snowflake';
-// import type { APIApplicationCommandOptionChoice } from 'discord-api-types/v10';
-// import { Structure } from '../../Structure.js';
-// import { kData } from '../../utils/symbols.js';
-// import { isIdSet } from '../../utils/type-guards.js';
-// import type { Partialize } from '../../utils/types.js';
+import type { APIApplicationCommandOptionChoice } from 'discord-api-types/v10';
+import { Structure } from '../../Structure.js';
+import { kData } from '../../utils/symbols.js';
+import type { Partialize } from '../../utils/types.js';
 
-// /**
-//  * Represents any application command option choice on Discord.
-//  *
-//  * @typeParam Omitted - Specify the properties that will not be stored in the raw data field as a union, implement via `DataTemplate`
-//  * @remarks has substructure `User` which needs to be instantiated and stored by an extending class using it
-//  * @remarks intentionally does not export `roles` so that extending classes can resolve `Snowflake[]` to `Role[]`
-//  */
-// export class ApplicationCommandOptionChoice<Omitted extends keyof ApplicationCommandOptionChoice | '' = ''> extends Structure<ApplicationCommandOptionChoice, Omitted> {
-// 	/**
-// 	 * The template used for removing data from the raw data stored for each application command option choice
-// 	 */
-// 	public static override readonly DataTemplate: Partial<ApplicationCommandOptionChoice> = {};
+/**
+ * Represents any application command option choice on Discord.
+ *
+ * @typeParam Omitted - Specify the properties that will not be stored in the raw data field as a union, implement via `DataTemplate`
+ * @remarks has substructure `User` which needs to be instantiated and stored by an extending class using it
+ */
+export class ApplicationCommandOptionChoice<
+	Omitted extends keyof APIApplicationCommandOptionChoice | '' = '',
+> extends Structure<APIApplicationCommandOptionChoice, Omitted> {
+	/**
+	 * The template used for removing data from the raw data stored for each application command option choice
+	 */
+	public static override readonly DataTemplate: Partial<APIApplicationCommandOptionChoice> = {};
 
-// 	/**
-// 	 * @param data - The raw data received from the API for the application command option choice
-// 	 */
-// 	public constructor(data: Partialize<ApplicationCommandOptionChoice, Omitted>) {
-// 		super(data);
-// 	}
+	/**
+	 * @param data - The raw data received from the API for the application command option choice
+	 */
+	public constructor(data: Partialize<APIApplicationCommandOptionChoice, Omitted>) {
+		super(data);
+	}
 
-// 	/**
-// 	 * 1-100 character choice name
-// 	 */
-// 	public get name() {
-// 		return this[kData].name
-// 	}
-// }
-console.log();
+	/**
+	 * 1-100 character choice name
+	 */
+	public get name() {
+		return this[kData].name;
+	}
+
+	/**
+	 * Type of value depends on the option type that the choice belongs to
+	 */
+	public get value() {
+		return this[kData].value;
+	}
+}
