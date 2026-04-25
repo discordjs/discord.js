dad3dfb92a6ce9aa246edf18a77f922c70fce36a
Asad Humayun <105254706+AsadHumayun@users.noreply.github.com>
2026-04-25 23:36:43 +0100

    fix(structures): use correct type on Presence

--- packages/structures/src/presences/Presence.ts
+++ packages/structures/src/presences/Presence.ts
@@ -1,4 +1,4 @@
-import type { GatewayPresenceUpdateData as PresenceData } from 'discord-api-types/v10';
+import type { GatewayGuildMembersChunkPresence } from 'discord-api-types/v10';
 import { Structure } from '../Structure.js';
 import { kData } from '../utils/symbols.js';
 import type { Partialize } from '../utils/types.js';
@@ -7,34 +7,22 @@ import type { Partialize } from '../utils/types.js';
  * Represents any presence update on Discord.
  *
  * @typeParam Omitted - Specify the properties that will not be stored in the raw data field as a union, implement via `DataTemplate`
+ * @remarks has substructures `Activity`, `ClientStatus` which need to be instantiated and stored by an extending class using it
  */
-export class Presence<Omitted extends keyof PresenceData | '' = ''> extends Structure<PresenceData, Omitted> {
+export class Presence<Omitted extends keyof GatewayGuildMembersChunkPresence | '' = ''> extends Structure<
+	GatewayGuildMembersChunkPresence,
+	Omitted
+> {
 	/**
 	 * The template used for removing data from the raw data stored for each presence update
 	 */
-	public static override readonly DataTemplate: Partial<PresenceData> = {};
+	public static override readonly DataTemplate: Partial<GatewayGuildMembersChunkPresence> = {};
 
 	/**
 	 * @param data - The raw data received from the API for the presence update
 	 */
-	public constructor(data: Partialize<PresenceData, Omitted>) {
+	public constructor(data: Partialize<GatewayGuildMembersChunkPresence, Omitted>) {
 		super(data);
-	}
-
-	/**
-	 * Unix timestamp (in milliseconds) of when the client went idle, or null if the client is not idle
-	 */
-	public get sinceTimestamp() {
-		return this[kData].since;
-	}
-
-	/**
-	 * Time of when the client went idle, or null if the client is not idle
-	 */
-	public get since() {
-		const timestamp = this.sinceTimestamp;
-
-		return timestamp ? new Date(timestamp as number) : null;
 	}
 
 	/**
@@ -44,12 +32,5 @@ export class Presence<Omitted extends keyof PresenceData | '' = ''> extends Stru
 	 */
 	public get status() {
 		return this[kData].status;
-	}
-
-	/**
-	 * Whether the user is AFK
-	 */
-	public get afk() {
-		return this[kData].afk;
 	}
 }
