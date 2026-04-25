f6ceddb90f5291392def01e2de68c86cc7dd731e
Asad Humayun <105254706+AsadHumayun@users.noreply.github.com>
2026-04-25 23:36:32 +0100

    feat(structures): presence structures

---
+++ packages/structures/src/gatewayPresences/Activity/GatewayPresenceActivityParty.ts
@@ -0,0 +1,30 @@
+// import { DiscordSnowflake } from '@sapphire/snowflake';
+import type { GatewayActivityParty } from 'discord-api-types/v10';
+import { Structure } from '../../Structure.js';
+// import { kData } from '../../utils/symbols.js';
+// import { isIdSet } from '../../utils/type-guards.js';
+import type { Partialize } from '../../utils/types.js';
+/**
+ * @todo
+ */
+/**
+ * Represents any activity party on Discord.
+ *
+ * @typeParam Omitted - Specify the properties that will not be stored in the raw data field as a union, implement via `DataTemplate`
+ */
+export class GatewayPresenceActivityParty<Omitted extends keyof GatewayActivityParty | '' = ''> extends Structure<
+	GatewayActivityParty,
+	Omitted
+> {
+	/**
+	 * The template used for removing data from the raw data stored for each activity party.
+	 */
+	public static override readonly DataTemplate: Partial<GatewayActivityParty> = {};
+
+	/**
+	 * @param data - The raw data received from the API for the activity party.
+	 */
+	public constructor(data: Partialize<GatewayActivityParty, Omitted>) {
+		super(data);
+	}
+}
