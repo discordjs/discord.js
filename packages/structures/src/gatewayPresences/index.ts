ddcea5e2e57ad646c2e7bf434008ea3cb815af24
Asad Humayun <105254706+AsadHumayun@users.noreply.github.com>
2026-04-25 23:36:40 +0100

    docs: rm todo comment

--- packages/structures/src/gatewayPresences/index.ts
+++ packages/structures/src/gatewayPresences/index.ts
@@ -2,11 +2,3 @@ export * from './Activity/index.js';
 
 export * from './GatewayPresenceClientStatus.js';
 export * from './GatewayPresenceUpdate.js';
-
-/**
- * @todo ://
- *
- * https://github.com/discordjs/discord.js/blob/c71228aab3b8859610be3b16042988cbacf1860e/packages/structures/src/voice/VoiceState.ts#L102-L107
- *
- * I think I missed requestToSpeak[At] getter for the timestamp value for that.
- */
