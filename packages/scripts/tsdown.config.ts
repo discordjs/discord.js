b44f8d78cfb309c33f8bb58d7c4263d5e11ca65f
Qjuh <76154676+Qjuh@users.noreply.github.com>
2026-04-30 17:29:12 +0200

    chore: remove legacy CJS builds

--- packages/scripts/tsdown.config.ts
+++ packages/scripts/tsdown.config.ts
@@ -1,11 +1,5 @@
 import { createTsdownConfig } from '../../tsdown.config.js';
 
-export default [
-	createTsdownConfig({
-		entry: ['src/index.ts', 'bin/generateSplitDocumentation.ts'],
-	}),
-	createTsdownConfig({
-		entry: ['src/populateDevDatabaseBranch.ts', 'bin/sortLabels.ts'],
-		format: 'esm',
-	}),
-];
+export default createTsdownConfig({
+	entry: ['src/index.ts', 'src/populateDevDatabaseBranch.ts', 'bin/generateSplitDocumentation.ts', 'bin/sortLabels.ts'],
+});
