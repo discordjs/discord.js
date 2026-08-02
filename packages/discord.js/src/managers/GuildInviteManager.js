1bcdb387857f0333d0cba3ed42903613ba361285
Naiyar <137700126+imnaiyar@users.noreply.github.com>
2026-08-03 01:00:41 +0600

    docs: return type

--- packages/discord.js/src/managers/GuildInviteManager.js
+++ packages/discord.js/src/managers/GuildInviteManager.js
@@ -298,7 +298,7 @@ class GuildInviteManager extends CachedManager {
    * Get status of the job processing target users of an invite.
    *
    * @param {InviteResolvable} invite The invite to get the target users for
-   * @returns {Promise<TargetUsersJobStatusForInvite[]>} The target users
+   * @returns {Promise<TargetUsersJobStatusForInvite>} The target users
    */
   async fetchTargetUsersJobStatus(invite) {
     const code = resolveInviteCode(invite);
